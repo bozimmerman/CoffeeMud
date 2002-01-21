ca05536df88ad15bc52e92ca52b17ca0f1140656
bozimmerman <bo@zimmers.net>
2002-01-21 07:13:16 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@80 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Abilities/Properties/Prop_SafePet.java
+++ com/planet_ink/coffee_mud/Abilities/Properties/Prop_SafePet.java
@@ -27,6 +27,8 @@ public class Prop_SafePet extends Property
 		if((Util.bset(affect.targetCode(),Affect.MASK_MALICIOUS)&&(affect.amITarget(affected))&&(affected!=null)))
 		{
 			affect.source().tell("Ah, leave "+affected.name()+" alone.");
+			if(affected instanceof MOB)
+				((MOB)affected).makePeace();
 			return false;
 		}
 		return super.okAffect(affect);
