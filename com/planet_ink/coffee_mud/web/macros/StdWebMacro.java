b896e3e06fcfac70db7f6111d8f365c76329afdb
bozimmerman <bo@zimmers.net>
2002-03-11 19:39:12 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@346 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/web/macros/StdWebMacro.java
@@ -0,0 +1,20 @@
+package com.planet_ink.coffee_mud.web;
+import com.planet_ink.coffee_mud.interfaces.*;
+
+abstract public class StdWebMacro implements WebMacro
+{
+	public String ID()		{return name();}
+	public String name()	{return "UNKNOWN";}
+
+	public String macroID()	{return "@" + name().toUpperCase() + "@";}
+
+	// not yet implemented!
+	public boolean isAdminMacro()	{return false;}
+
+	
+	public String runMacro(ProcessHTTPrequest httpReq)
+	{
+		return "[Unimplemented macro!]";
+	}
+
+}
