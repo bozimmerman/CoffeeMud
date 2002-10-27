12db90ac63722e7616658332158bbcd9b15472a7
bozimmerman <bo@zimmers.net>
2002-10-27 04:20:30 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@1006 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Abilities/Thief/Skill_Appraise.java
@@ -0,0 +1,90 @@
+package com.planet_ink.coffee_mud.Abilities.Skills;
+import com.planet_ink.coffee_mud.Abilities.StdAbility;
+import com.planet_ink.coffee_mud.interfaces.*;
+import com.planet_ink.coffee_mud.common.*;
+import com.planet_ink.coffee_mud.utils.*;
+import java.util.*;
+
+public class Skill_Appraise extends StdAbility
+{
+
+	public Skill_Appraise()
+	{
+		super();
+		myID=this.getClass().getName().substring(this.getClass().getName().lastIndexOf('.')+1);
+		name="Appraise";
+		displayText="";
+		miscText="";
+
+		triggerStrings.addElement("APPRAISE");
+
+		canBeUninvoked=true;
+		isAutoinvoked=false;
+
+		baseEnvStats().setLevel(1);
+
+		recoverEnvStats();
+	}
+
+	public Environmental newInstance()
+	{
+		return new Skill_Appraise();
+	}
+
+	public int classificationCode()
+	{
+		return Ability.SKILL;
+	}
+
+	public boolean invoke(MOB mob, Vector commands, Environmental givenTarget, boolean auto)
+	{
+		if(commands.size()<1)
+		{
+			mob.tell("What would you like to appraise?");
+			return false;
+		}
+		Item target=mob.fetchCarried(null,(String)commands.elementAt(0));
+		if((target==null)||((target!=null)&&(!Sense.canBeSeenBy(target,mob))))
+		{
+			mob.tell("You don't see '"+((String)commands.elementAt(0))+"' here.");
+			return false;
+		}
+		if(!super.invoke(mob,commands,givenTarget,auto))
+			return false;
+
+		int levelDiff=target.envStats().level()-mob.envStats().level();
+		if(levelDiff>0) levelDiff=0;
+		boolean success=profficiencyCheck(levelDiff,auto);
+
+		FullMsg msg=new FullMsg(mob,target,null,Affect.MSG_DELICATE_HANDS_ACT,"<S-NAME> appraise(s) <T-NAMESELF>.");
+		if(mob.location().okAffect(msg))
+		{
+			mob.location().send(mob,msg);
+			int realValue=target.value();
+			int materialCode=target.material();
+			int weight=target.baseEnvStats().weight();
+			int height=target.baseEnvStats().height();
+			int allWeight=target.envStats().weight();
+			if(!success)
+			{
+				double deviance=Util.div(Dice.roll(1,100,0)+50,100);
+				realValue=(int)Math.round(Util.mul(realValue,deviance));
+				materialCode=Dice.roll(1,EnvResource.RESOURCE_DESCS.length,-1);
+				weight=(int)Math.round(Util.mul(weight,deviance));
+				height=(int)Math.round(Util.mul(height,deviance));
+				allWeight=(int)Math.round(Util.mul(allWeight,deviance));
+			}
+			StringBuffer str=new StringBuffer("");
+			str.append(target.name()+" is made of "+EnvResource.RESOURCE_DESCS[materialCode&EnvResource.RESOURCE_MASK]+" is worth about "+realValue+" gold.");
+			if(target instanceof Armor)
+				str.append("\n\r"+target.name()+" is a size "+height+".");
+			if(weight!=allWeight)
+				str.append("\n\rIt weighs "+weight+" pounds empty and "+allWeight+" pounds right now.");
+			else
+				str.append("\n\rIt weighs "+weight+" pounds.");
+			mob.tell(str.toString());
+		}
+		return success;
+	}
+
+}
