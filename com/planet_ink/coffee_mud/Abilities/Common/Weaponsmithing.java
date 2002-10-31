9411283445ee69d0f0d9ff57501daf825fef0bb9
bozimmerman <bo@zimmers.net>
2002-10-31 18:32:06 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@1067 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Abilities/Common/Weaponsmithing.java
+++ com/planet_ink/coffee_mud/Abilities/Common/Weaponsmithing.java
@@ -1,5 +1,83 @@
 package com.planet_ink.coffee_mud.Abilities.Common;
 
-public class Weaponsmithing
+import com.planet_ink.coffee_mud.interfaces.*;
+import com.planet_ink.coffee_mud.common.*;
+import com.planet_ink.coffee_mud.utils.*;
+import java.util.*;
+
+public class Weaponsmithing extends CommonSkill
 {
+	private Item building=null;
+	public Weaponsmithing()
+	{
+		super();
+		myID=this.getClass().getName().substring(this.getClass().getName().lastIndexOf('.')+1);
+		name="Weaponsmithing";
+
+		miscText="";
+		triggerStrings.addElement("WEAPONSMITH");
+		triggerStrings.addElement("WEAPONSMITHING");
+		quality=Ability.INDIFFERENT;
+
+		recoverEnvStats();
+		//CMAble.addCharAbilityMapping("All",1,ID(),false);
+	}
+	
+	public Environmental newInstance()
+	{
+		return new Weaponsmithing();
+	}
+	
+	public boolean tick(int tickID)
+	{
+		if((affected!=null)&&(affected instanceof MOB)&&(tickID==Host.MOB_TICK))
+		{
+			MOB mob=(MOB)affected;
+			if(building==null)
+				unInvoke();
+			else
+			if(tickUp==0)
+			{
+				mob.tell("You start smithing "+building.name()+".");
+				displayText="You are smithing "+building.name();
+				verb="smithing "+building.name();
+			}
+		}
+		return super.tick(tickID);
+	}
+	
+	public void unInvoke()
+	{
+		if((affected!=null)&&(affected instanceof MOB))
+		{
+			MOB mob=(MOB)affected;
+			if(building!=null)
+				mob.location().addItem(building);
+			building=null;
+		}
+		super.unInvoke();
+	}
+	
+	public boolean invoke(MOB mob, Vector commands, Environmental givenTarget, boolean auto)
+	{
+		if(commands.size()==0)
+		{
+			mob.tell("Smith what? Enter \"weaponsmith list\" for a list, or \"weaponsmith mend <item>\".");
+			return false;
+		}
+		if(!super.invoke(mob,commands,givenTarget,auto))
+			return false;
+		
+		String name=Util.combine(commands,0);
+		int profficiencyAdjustment=0;
+		int completion=6;
+		if(completion<4) completion=4;
+		FullMsg msg=new FullMsg(mob,null,Affect.MSG_NOISYMOVEMENT,null);
+		if(mob.location().okAffect(msg))
+		{
+			mob.location().send(mob,msg);
+			beneficialAffect(mob,mob,completion);
+		}
+		return true;
+	}
 }
