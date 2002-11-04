710b0a5bb44dd1894af46019b7f199e36c7888ac
bozimmerman <bo@zimmers.net>
2002-11-04 04:27:34 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@1095 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Abilities/Spells/Spell_StoneFlesh.java
+++ com/planet_ink/coffee_mud/Abilities/Spells/Spell_StoneFlesh.java
@@ -52,7 +52,10 @@ public class Spell_StoneFlesh extends Spell
 
 		if(revokeThis==null)
 		{
-			mob.tell(mob,target,"<T-NAME> can not be affected by this spell.");
+			if(auto)
+				mob.tell(mob,target,"Nothing happens.");
+			else
+				mob.tell(mob,target,"<T-NAME> can not be affected by this spell.");
 			return false;
 		}
 
@@ -63,7 +66,7 @@ public class Spell_StoneFlesh extends Spell
 		boolean success=profficiencyCheck(0,auto);
 		if(success)
 		{
-			FullMsg msg=new FullMsg(mob,target,this,affectType,"<S-NAME> dispel(s) "+revokeThis.name()+" from <T-NAMESELF>.");
+			FullMsg msg=new FullMsg(mob,target,this,affectType,auto?"":"<S-NAME> dispel(s) "+revokeThis.name()+" from <T-NAMESELF>.");
 			if(mob.location().okAffect(msg))
 			{
 				mob.location().send(mob,msg);
