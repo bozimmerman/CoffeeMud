0c0c00872dd153823c6bc092ba011fec5f37af60
bozimmerman <bo@zimmers.net>
2002-12-03 19:46:38 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@1232 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Races/Chicken.java
+++ com/planet_ink/coffee_mud/Races/Chicken.java
@@ -1,5 +1,109 @@
 package com.planet_ink.coffee_mud.Races;
 
-public class Chicken
+import com.planet_ink.coffee_mud.interfaces.*;
+import com.planet_ink.coffee_mud.common.*;
+import com.planet_ink.coffee_mud.utils.*;
+import java.util.*;
+
+public class Chicken extends StdRace
 {
+	protected static Vector resources=new Vector();
+	public Chicken()
+	{
+		super();
+		myID=this.getClass().getName().substring(this.getClass().getName().lastIndexOf('.')+1);
+		name=myID;
+		shortestMale=13;
+		shortestFemale=13;
+		heightVariance=6;
+		lightestWeight=20;
+		weightVariance=5;
+		forbiddenWornBits=Integer.MAX_VALUE-Item.ON_HEAD-Item.ON_EYES;
+	}
+	public boolean playerSelectable(){return false;}
+	public void affectCharStats(MOB affectedMOB, CharStats affectableStats)
+		
+	{
+		super.affectCharStats(affectedMOB, affectableStats);
+		affectableStats.setStat(CharStats.STRENGTH,3);
+		affectableStats.setStat(CharStats.DEXTERITY,4);
+		affectableStats.setStat(CharStats.INTELLIGENCE,1);
+	}
+	public String arriveStr()
+	{
+		return "walks in";
+	}
+	public String leaveStr()
+	{
+		return "walks";
+	}
+	public Weapon myNaturalWeapon()
+	{
+		if(naturalWeapon==null)
+		{
+			naturalWeapon=CMClass.getWeapon("StdWeapon");
+			naturalWeapon.setName("a menacing beak");
+			naturalWeapon.setWeaponType(Weapon.TYPE_NATURAL);
+		}
+		return naturalWeapon;
+	}
+	
+	public String healthText(MOB mob)
+	{
+		double pct=(Util.div(mob.curState().getHitPoints(),mob.maxState().getHitPoints()));
+
+		if(pct<.10)
+			return "^r" + mob.name() + "^r is hovering on deaths door!^N";
+		else
+		if(pct<.20)
+			return "^r" + mob.name() + "^r is covered in blood and matted feathers.^N";
+		else
+		if(pct<.30)
+			return "^r" + mob.name() + "^r is bleeding badly from lots of wounds.^N";
+		else
+		if(pct<.40)
+			return "^y" + mob.name() + "^y has numerous bloody matted feathers.^N";
+		else
+		if(pct<.50)
+			return "^y" + mob.name() + "^y has some bloody matted feathers.^N";
+		else
+		if(pct<.60)
+			return "^p" + mob.name() + "^p has a lot of missing feathers.^N";
+		else
+		if(pct<.70)
+			return "^p" + mob.name() + "^p has a few missing feathers.^N";
+		else
+		if(pct<.80)
+			return "^g" + mob.name() + "^g has a missing feather.^N";
+		else
+		if(pct<.90)
+			return "^g" + mob.name() + "^g has a few feathers out of place.^N";
+		else
+		if(pct<.99)
+			return "^g" + mob.name() + "^g has a some ruffled features.^N";
+		else
+			return "^c" + mob.name() + "^c is in perfect health^N";
+	}
+	public Vector myResources()
+	{
+		synchronized(resources)
+		{
+			if(resources.size()==0)
+			{
+				resources.addElement(makeResource
+				("some "+name.toLowerCase()+" lips",EnvResource.RESOURCE_MEAT));
+				resources.addElement(makeResource
+				("a "+name.toLowerCase()+" egg",EnvResource.RESOURCE_EGGS));
+				resources.addElement(makeResource
+				("some "+name.toLowerCase()+" feathers",EnvResource.RESOURCE_FEATHERS));
+				resources.addElement(makeResource
+				("some "+name.toLowerCase()+" meat",EnvResource.RESOURCE_POULTRY));
+				resources.addElement(makeResource
+				("some "+name.toLowerCase()+" blood",EnvResource.RESOURCE_BLOOD));
+				resources.addElement(makeResource
+				("a pile of "+name.toLowerCase()+" bones",EnvResource.RESOURCE_BONE));
+			}
+		}
+		return resources;
+	}
 }
--- com/planet_ink/coffee_mud/Races/Smurf.java
+++ com/planet_ink/coffee_mud/Races/Smurf.java
@@ -1,5 +1,131 @@
 package com.planet_ink.coffee_mud.Races;
 
-public class Smurf
+import com.planet_ink.coffee_mud.interfaces.*;
+import com.planet_ink.coffee_mud.common.*;
+import com.planet_ink.coffee_mud.utils.*;
+import java.util.*;
+
+public class Smurf extends StdRace
 {
+	protected static Vector resources=new Vector();
+	public Smurf()
+	{
+		super();
+		myID=this.getClass().getName().substring(this.getClass().getName().lastIndexOf('.')+1);
+		name=myID;
+		// inches
+		shortestMale=7;
+		shortestFemale=7;
+		heightVariance=1;
+		// pounds
+		lightestWeight=5;
+		weightVariance=2;
+		forbiddenWornBits=0;
+	}
+	public boolean playerSelectable(){return false;}
+
+	public Weapon funHumanoidWeapon()
+	{
+		if(naturalWeaponChoices==null)
+		{
+			naturalWeaponChoices=new Vector();
+			for(int i=1;i<11;i++)
+			{
+				naturalWeapon=CMClass.getWeapon("StdWeapon");
+				switch(i)
+				{
+					case 1:
+					case 2:
+					case 3:
+					naturalWeapon.setName("a smurfy punch");
+					naturalWeapon.setWeaponType(Weapon.TYPE_BASHING);
+					break;
+					case 4:
+					naturalWeapon.setName("some smurfy teeth");
+					naturalWeapon.setWeaponType(Weapon.TYPE_PIERCING);
+					break;
+					case 5:
+					naturalWeapon.setName("a smurfy elbow");
+					naturalWeapon.setWeaponType(Weapon.TYPE_NATURAL);
+					break;
+					case 6:
+					naturalWeapon.setName("a smurfy backhand");
+					naturalWeapon.setWeaponType(Weapon.TYPE_BASHING);
+					break;
+					case 7:
+					naturalWeapon.setName("a smurfy jab");
+					naturalWeapon.setWeaponType(Weapon.TYPE_BASHING);
+					break;
+					case 8:
+					naturalWeapon.setName("a smurfy poke");
+					naturalWeapon.setWeaponType(Weapon.TYPE_BASHING);
+					break;
+					case 9:
+					naturalWeapon.setName("a smurfy knee");
+					naturalWeapon.setWeaponType(Weapon.TYPE_BASHING);
+					break;
+					case 10:
+					naturalWeapon.setName("a smurfy head butt");
+					naturalWeapon.setWeaponType(Weapon.TYPE_BASHING);
+					break;
+				}
+				naturalWeaponChoices.addElement(naturalWeapon);
+			}
+		}
+		return (Weapon)naturalWeaponChoices.elementAt(Dice.roll(1,naturalWeaponChoices.size(),0)-1);
+	}
+	public Weapon myNaturalWeapon()
+	{ return funHumanoidWeapon();	}
+	public String healthText(MOB mob)
+	{
+		double pct=(Util.div(mob.curState().getHitPoints(),mob.maxState().getHitPoints()));
+
+		if(pct<.10)
+			return "^r" + mob.name() + "^r is mortally wounded and will soon die.^N";
+		else
+		if(pct<.20)
+			return "^r" + mob.name() + "^r is covered in blood.^N";
+		else
+		if(pct<.30)
+			return "^r" + mob.name() + "^r is bleeding badly from lots of wounds.^N";
+		else
+		if(pct<.40)
+			return "^y" + mob.name() + "^y has numerous bloody wounds and gashes.^N";
+		else
+		if(pct<.50)
+			return "^y" + mob.name() + "^y has some bloody wounds and gashes.^N";
+		else
+		if(pct<.60)
+			return "^p" + mob.name() + "^p has a few bloody wounds.^N";
+		else
+		if(pct<.70)
+			return "^p" + mob.name() + "^p is cut and bruised.^N";
+		else
+		if(pct<.80)
+			return "^g" + mob.name() + "^g has some minor cuts and bruises.^N";
+		else
+		if(pct<.90)
+			return "^g" + mob.name() + "^g has a few bruises and scratches.^N";
+		else
+		if(pct<.99)
+			return "^g" + mob.name() + "^g has a few small bruises.^N";
+		else
+			return "^c" + mob.name() + "^c is in perfect health^N";
+	}
+	public Vector myResources()
+	{
+		synchronized(resources)
+		{
+			if(resources.size()==0)
+			{
+				resources.addElement(makeResource
+				("a "+name.toLowerCase()+" head",EnvResource.RESOURCE_MEAT));
+				resources.addElement(makeResource
+				("some "+name.toLowerCase()+" blood",EnvResource.RESOURCE_BLOOD));
+				resources.addElement(makeResource
+				("a pile of "+name.toLowerCase()+" bones",EnvResource.RESOURCE_BONE));
+			}
+		}
+		return resources;
+	}
 }
