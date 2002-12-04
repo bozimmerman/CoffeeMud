9cdf58c2065b6b360750516cbcdaa3aa142d276c
bozimmerman <bo@zimmers.net>
2002-12-04 09:19:00 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@1240 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Commands/base/sysop/Reset.java
+++ com/planet_ink/coffee_mud/Commands/base/sysop/Reset.java
@@ -42,6 +42,132 @@ public class Reset
 			area(mob.location().getArea());
 			mob.tell("Done.");
 		}
+		else
+		if(s.equalsIgnoreCase("areaoramamana"))
+		{
+			// this is just utility code and will change frequently
+			Area A=mob.location().getArea();
+			area(A);
+			A.toggleMobility(false);
+			Vector rooms=A.getMyMap();
+			try{
+			for(int r=0;r<rooms.size();r++)
+			{
+				Room room=(Room)rooms.elementAt(r);
+				room(room);
+				boolean somethingDone=false;
+				mob.tell(room.ID()+"/"+room.name()+"/"+room.displayText());
+				for(int i=0;i<room.numItems();i++)
+				{
+					Item I=room.fetchItem(i);
+					String str=mob.session().prompt(I.name()+"/"+EnvResource.RESOURCE_DESCS[I.material()&EnvResource.RESOURCE_MASK],"");
+					if(str.length()>0)
+					for(int ii=0;ii<EnvResource.RESOURCE_DESCS.length;ii++)
+					{
+						if(str.equalsIgnoreCase(EnvResource.RESOURCE_DESCS[ii]))
+						{
+							I.setMaterial(EnvResource.RESOURCE_DATA[ii][0]);
+							somethingDone=true;
+							mob.tell("Changed to "+EnvResource.RESOURCE_DESCS[I.material()&EnvResource.RESOURCE_MASK]);
+							break;
+						}
+					}
+				}
+				if(somethingDone)
+					ExternalPlay.DBUpdateItems(room);
+				somethingDone=false;
+				for(int m=0;m<room.numInhabitants();m++)
+				{
+					MOB M=room.fetchInhabitant(m);
+					String str=mob.session().prompt(M.name()+"/"+M.charStats().getMyRace().ID(),"");
+					if(str.length()>0)
+					{
+						Race R=CMClass.getRace(str);
+						if(R==null)
+							str=mob.session().prompt(M.name()+"/"+M.charStats().getMyRace().ID(),"");
+						else
+						if(R!=null)
+						{
+							mob.tell("Changed to "+R.ID());
+							M.baseCharStats().setMyRace(R);
+							R.setHeightWeight(M.baseEnvStats(),(char)M.baseCharStats().getStat(CharStats.GENDER));
+							M.recoverCharStats();
+							M.recoverEnvStats();
+							somethingDone=true;
+						}
+					}
+					for(int i=M.inventorySize()-1;i>=0;i--)
+					{
+						Item I=M.fetchInventory(i);
+						str=mob.session().prompt(I.name()+"/"+EnvResource.RESOURCE_DESCS[I.material()&EnvResource.RESOURCE_MASK],"");
+						if(str.equalsIgnoreCase("delete"))
+							M.delInventory(I);
+						else
+						if(str.length()>0)
+						for(int ii=0;ii<EnvResource.RESOURCE_DESCS.length;ii++)
+						{
+							if(str.equalsIgnoreCase(EnvResource.RESOURCE_DESCS[ii]))
+							{
+								I.setMaterial(EnvResource.RESOURCE_DATA[ii][0]);
+								somethingDone=true;
+								mob.tell("Changed to "+EnvResource.RESOURCE_DESCS[I.material()&EnvResource.RESOURCE_MASK]);
+								break;
+							}
+						}
+					}
+					if(M instanceof ShopKeeper)
+					{
+						Vector V=((ShopKeeper)M).getBaseInventory();
+						for(int i=V.size()-1;i>=0;i--)
+						{
+							Environmental E=(Environmental)V.elementAt(i);
+							if(E instanceof Item)
+							{
+								Item I=(Item)E;
+								str=mob.session().prompt(I.name()+"/"+EnvResource.RESOURCE_DESCS[I.material()&EnvResource.RESOURCE_MASK],"");
+								if(str.equalsIgnoreCase("delete"))
+									((ShopKeeper)M).delStoreInventory(I);
+								else
+								if(str.length()>0)
+								for(int ii=0;ii<EnvResource.RESOURCE_DESCS.length;ii++)
+								{
+									if(str.equalsIgnoreCase(EnvResource.RESOURCE_DESCS[ii]))
+									{
+										int numInStock=((ShopKeeper)M).numberInStock(I);
+										I.setMaterial(EnvResource.RESOURCE_DATA[ii][0]);
+										somethingDone=true;
+										mob.tell("Changed to "+EnvResource.RESOURCE_DESCS[I.material()&EnvResource.RESOURCE_MASK]);
+										((ShopKeeper)M).delStoreInventory(I);
+										((ShopKeeper)M).addStoreInventory(I,numInStock);
+										break;
+									}
+								}
+							}
+						}
+					}
+					if(M.fetchAbility("Chopping")!=null)
+					{
+						somethingDone=true;
+						M.delAbility(M.fetchAbility("Chopping"));
+					}
+					for(int i=0;i<M.numBehaviors();i++)
+					{
+						Behavior B=M.fetchBehavior(i);
+						if((B.ID().equalsIgnoreCase("Mobile"))&&(B.getParms().trim().length()>0))
+						{
+							somethingDone=true;
+							B.setParms("");
+						}
+					}
+				}
+				if(somethingDone)
+					ExternalPlay.DBUpdateMOBs(room);
+			}
+			}
+			catch(java.io.IOException e){}
+			A.toggleMobility(true);
+			mob.tell("Done.");
+		}
 	}
 	public void room(Room room)
 	{
--- com/planet_ink/coffee_mud/Races/WoodGolem.java
+++ com/planet_ink/coffee_mud/Races/WoodGolem.java
@@ -1,5 +1,86 @@
 package com.planet_ink.coffee_mud.Races;
 
-public class WoodGolem
+import com.planet_ink.coffee_mud.interfaces.*;
+import com.planet_ink.coffee_mud.common.*;
+import com.planet_ink.coffee_mud.utils.*;
+import java.util.*;
+
+public class WoodGolem extends StdRace
 {
+	protected static Vector resources=new Vector();
+	public WoodGolem()
+	{
+		super();
+		myID=this.getClass().getName().substring(this.getClass().getName().lastIndexOf('.')+1);
+		name=myID;
+		// inches
+		shortestMale=64;
+		shortestFemale=60;
+		heightVariance=12;
+		// pounds
+		lightestWeight=400;
+		weightVariance=100;
+		forbiddenWornBits=0;
+	}
+	public boolean playerSelectable(){return false;}
+
+	public void affectCharStats(MOB affectedMOB, CharStats affectableStats)
+	{
+		super.affectCharStats(affectedMOB, affectableStats);
+		affectableStats.setStat(CharStats.SAVE_POISON,affectableStats.getStat(CharStats.SAVE_POISON)+100);
+		affectableStats.setStat(CharStats.SAVE_MIND,affectableStats.getStat(CharStats.SAVE_MIND)+100);
+		affectableStats.setStat(CharStats.SAVE_GAS,affectableStats.getStat(CharStats.SAVE_GAS)+100);
+		affectableStats.setStat(CharStats.SAVE_PARALYSIS,affectableStats.getStat(CharStats.SAVE_PARALYSIS)+100);
+		affectableStats.setStat(CharStats.SAVE_UNDEAD,affectableStats.getStat(CharStats.SAVE_UNDEAD)+100);
+		affectableStats.setStat(CharStats.SAVE_DISEASE,affectableStats.getStat(CharStats.SAVE_DISEASE)+100);
+	}
+	public String healthText(MOB mob)
+	{
+		double pct=(Util.div(mob.curState().getHitPoints(),mob.maxState().getHitPoints()));
+
+		if(pct<.10)
+			return "^r" + mob.name() + "^r is near destruction!^N";
+		else
+		if(pct<.20)
+			return "^r" + mob.name() + "^r is massively splintered and damaged.^N";
+		else
+		if(pct<.30)
+			return "^r" + mob.name() + "^r is extremeley splintered and damaged.^N";
+		else
+		if(pct<.40)
+			return "^y" + mob.name() + "^y is very splintered and damaged.^N";
+		else
+		if(pct<.50)
+			return "^y" + mob.name() + "^y is splintered and damaged.^N";
+		else
+		if(pct<.60)
+			return "^p" + mob.name() + "^p is splintered and slightly damaged.^N";
+		else
+		if(pct<.70)
+			return "^p" + mob.name() + "^p is showing large splinters.^N";
+		else
+		if(pct<.80)
+			return "^g" + mob.name() + "^g is showing some splinters.^N";
+		else
+		if(pct<.90)
+			return "^g" + mob.name() + "^g is showing small splinters.^N";
+		else
+		if(pct<.99)
+			return "^g" + mob.name() + "^g is no longer in perfect condition.^N";
+		else
+			return "^c" + mob.name() + "^c is in perfect condition.^N";
+	}
+	public Vector myResources()
+	{
+		synchronized(resources)
+		{
+			if(resources.size()==0)
+			{
+				resources.addElement(makeResource
+					("a pound of oak",EnvResource.RESOURCE_OAK));
+			}
+		}
+		return resources;
+	}
 }
+
