e3a163dbb52b159fdc545142a1e6db2647129402
bozimmerman <bo@zimmers.net>
2003-03-07 12:39:24 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@1563 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Locales/OverTheLedge.java
+++ com/planet_ink/coffee_mud/Locales/OverTheLedge.java
@@ -1,5 +1,28 @@
 package com.planet_ink.coffee_mud.Locales;
 
-public class OverTheLedge
+import com.planet_ink.coffee_mud.interfaces.*;
+import com.planet_ink.coffee_mud.common.*;
+import com.planet_ink.coffee_mud.utils.Directions;
+import com.planet_ink.coffee_mud.utils.Sense;
+
+import java.util.*;
+
+
+public class OverTheLedge extends InTheAir
 {
+	public boolean okAffect(Affect affect)
+	{
+		if(!super.okAffect(affect)) return false;
+		
+		if(Sense.isSleeping(this)) return true;
+		if((affect.targetMinor()==affect.TYP_ENTER)
+		&&(affect.amITarget(this))
+		&&(getRoomInDir(Directions.DOWN)!=affect.source().location()))
+			return true;
+		return isOkAffect(this,affect);
+	}
+	public Environmental newInstance()
+	{
+		return new OverTheLedge();
+	}
 }
