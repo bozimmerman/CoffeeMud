c447754e66f372ee3c3fa4f47dd4c0c91b5b438e
bozimmerman <bo@zimmers.net>
2003-03-19 04:46:32 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@1714 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Abilities/Fighter/Fighter_CriticalShot.java
@@ -0,0 +1,50 @@
+package com.planet_ink.coffee_mud.Abilities.Fighter;
+import com.planet_ink.coffee_mud.Abilities.StdAbility;
+import com.planet_ink.coffee_mud.interfaces.*;
+import com.planet_ink.coffee_mud.common.*;
+import com.planet_ink.coffee_mud.utils.*;
+import java.util.*;
+
+public class Fighter_CriticalShot extends StdAbility
+{
+	private int oldDamage=0;
+	public String ID() { return "Fighter_CriticalShot"; }
+	public String name(){ return "Critical Shot";}
+	public String displayText(){ return "";}
+	public int quality(){return Ability.BENEFICIAL_SELF;}
+	protected int canAffectCode(){return Ability.CAN_MOBS;}
+	protected int canTargetCode(){return 0;}
+	public boolean isAutoInvoked(){return true;}
+	public boolean canBeUninvoked(){return false;}
+	public Environmental newInstance(){	return new Fighter_CriticalShot();}
+	public int classificationCode(){return Ability.SKILL;}
+
+	public boolean okAffect(Affect affect)
+	{
+		if(!super.okAffect(affect))
+			return false;
+
+		if((affected==null)||(!(affected instanceof MOB)))
+			return true;
+
+		MOB mob=(MOB)affected;
+		if(affect.amISource(mob)
+		&&(Sense.aliveAwakeMobile(mob,true))
+		&&(Util.bset(affect.targetCode(),Affect.MASK_HURT))
+		&&(affect.target()!=null)
+		&&(mob.getVictim()==affect.target())
+		&&(mob.rangeToTarget()==0)
+		&&(affect.tool()!=null)
+		&&(affect.tool() instanceof Weapon)
+		&&((((Weapon)affect.tool()).weaponClassification()==Weapon.CLASS_RANGED)
+			||(((Weapon)affect.tool()).weaponClassification()==Weapon.CLASS_THROWN))
+		&&(profficiencyCheck(-50+mob.charStats().getStat(CharStats.STRENGTH),false)))
+		{
+			double pctRecovery=(Util.div(profficiency(),100.0)*Math.random());
+			int bonus=(int)Math.round(Util.mul((affect.targetCode()-Affect.MASK_HURT),pctRecovery));
+			affect.modify(affect.source(),affect.target(),affect.tool(),affect.sourceCode(),affect.sourceMessage(),affect.targetCode()+bonus,affect.targetMessage(),affect.othersCode(),affect.othersMessage());
+			helpProfficiency(mob);
+		}
+		return true;
+	}
+}
