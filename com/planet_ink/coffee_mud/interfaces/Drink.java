3bddbebd1b2bc872df4094a7c4c6e62ca312316a
bozimmerman <bo@zimmers.net>
2003-04-29 00:39:12 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@2099 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/interfaces/Drink.java
+++ com/planet_ink/coffee_mud/interfaces/Drink.java
@@ -13,45 +13,4 @@ public interface Drink extends Environmental
 	public void setLiquidRemaining(int amount);
 	
 	public boolean containsDrink();
-	
-	// poison types are stored in the ability code of poisons
-	public final static int POISON_NOT=0;
-	public final static int POISON_BEESTING=1;
-	public final static int POISON_BLOODBOIL=2;
-	public final static int POISON_PEPPERSAUCE=3;
-	public final static int POISON_VENOM=4;
-	public final static int POISON_MINDSAP=5;
-	public final static int POISON_HEARTSTOPPER=6;
-	public final static int POISON_GOBLINDRIP=7;
-	public final static int POISON_GHOULTOUCH=8;
-	public final static int POISON_DECREPTIFIER=9;
-	public final static int POISON_XXX=10;
-	public final static int POISON_TRANQUILIZER=11;
-	public final static int POISON_DRAINING=12;
-	public final static String[] Poison_Descs={
-		"HARMLESS",
-		"BEESTING",
-		"BLOODBOIL",
-		"PEPPERSAUCE",
-		"VENOM",
-		"MINDSAP",
-		"HEARTSTOPPER",
-		"GOBLINDRIP",
-		"GHOULTOUCH",
-		"DECRETIFIER",
-		"XXX",
-		"TRANQUILIZER",
-		"DRAINING"
-	};
-	
-	public final static int LIQUOR_BEER=0;
-	public final static int LIQUOR_LIQUOR=1;
-	public final static int LIQUOR_FIREBREATHER=2;
-	public final static int LIQUOR_TEQUILA=3;
-	public final static String[] Liquor_Descs={
-		"BEER/ALE",
-		"HARD LIQUOR",
-		"FIREBREATHER",
-		"TEQUILA"
-	};
 }
