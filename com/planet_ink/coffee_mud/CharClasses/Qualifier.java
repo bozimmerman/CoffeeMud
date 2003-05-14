ee66fe98ee9c3ab23aadc9bc9313cffe986caff6
bozimmerman <bo@zimmers.net>
2003-05-14 20:41:14 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@2241 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/CharClasses/Qualifier.java
+++ com/planet_ink/coffee_mud/CharClasses/Qualifier.java
@@ -1,5 +1,52 @@
 package com.planet_ink.coffee_mud.CharClasses;
 
-public class Qualifier
+import java.util.*;
+import com.planet_ink.coffee_mud.utils.*;
+import com.planet_ink.coffee_mud.interfaces.*;
+import com.planet_ink.coffee_mud.common.*;
+
+public class Qualifier extends StdCharClass
 {
+	public String ID(){return "Qualifier";}
+	public String name(){return "Qualifier";}
+	public String baseClass(){return ID();}
+	private static boolean abilitiesLoaded=false;
+	public boolean loaded(){return abilitiesLoaded;}
+	public void setLoaded(boolean truefalse){abilitiesLoaded=truefalse;};
+	
+	public Qualifier()
+	{
+		super();
+		for(int i=0;i<=5;i++)
+			maxStat[i]=25;
+	}
+
+	public boolean playerSelectable()
+	{
+		return false;
+	}
+
+	public String statQualifications(){return "Must be granted by an Archon.";}
+	public boolean qualifiesForThisClass(MOB mob, boolean quiet)
+	{
+		if(!quiet)
+			mob.tell("This class cannot be learned.");
+		return false;
+	}
+	
+	public void startCharacter(MOB mob, boolean isBorrowedClass, boolean verifyOnly)
+	{
+		if((verifyOnly)&&(!loaded()))
+		{
+			setLoaded(true);
+			for(Enumeration a=CMClass.abilities();a.hasMoreElements();)
+			{
+				Ability A=(Ability)a.nextElement();
+				int lvl=CMAble.lowestQualifyingLevel(A.ID());
+				if(lvl>0)
+					CMAble.addCharAbilityMapping(ID(),lvl,A.ID(),false);
+			}
+		}
+		super.startCharacter(mob, false, verifyOnly);
+	}
 }
