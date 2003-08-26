b5b3ed70da802bc47958fadd6cb1583c012dc6f6
bozimmerman <bo@zimmers.net>
2003-08-26 06:59:34 +0000

    svnimporter generated comment: Deleted . Changed path: [/Version 4.0/com/planet_ink/coffee_mud/system/I3/packets/LocateReplyPacket.java (deleted), /Version 4.0/com/planet_ink/coffee_mud/system/I3/packets/ChannelDelete.java (deleted), /Version 4.0/com/planet_ink/coffee_mud/system/I3/packets/Packet.java (deleted), /Version 4.0/com/planet_ink/coffee_mud/system/I3/packets/ChannelListen.java (deleted), /Version 4.0/com/planet_ink/coffee_mud/system/I3/packets/ChannelPacket.java (deleted), /Version 4.0/com/planet_ink/coffee_mud/system/I3/packets/ImudServices.java (deleted), /Version 4.0/com/planet_ink/coffee_mud/system/I3/packets/ChannelAdd.java (deleted), /Version 4.0/com/planet_ink/coffee_mud/system/I3/packets/NameServer.java (deleted), /Version 4.0/com/planet_ink/coffee_mud/system/I3/packets/ChannelWhoReply.java (deleted), /Version 4.0/com/planet_ink/coffee_mud/system/I3/packets/ChannelList.java (deleted), /Version 4.0/com/planet_ink/coffee_mud/system/I3/packets/Channel.java (deleted), /Version 4.0/com/planet_ink/coffee_mud/system/I3/packets/LPCData.java (deleted), /Version 4.0/com/planet_ink/coffee_mud/system/I3/packets/InvalidPacketException.java (deleted), /Version 4.0/com/planet_ink/coffee_mud/system/I3/packets/MudList.java (deleted), /Version 4.0/com/planet_ink/coffee_mud/system/I3/packets/I3Exception.java (deleted), /Version 4.0/com/planet_ink/coffee_mud/system/I3/packets/ChannelWhoRequest.java (deleted), /Version 4.0/com/planet_ink/coffee_mud/system/I3/packets/WhoPacket.java (deleted), /Version 4.0/com/planet_ink/coffee_mud/system/I3/packets/Intermud.java (deleted), /Version 4.0/com/planet_ink/coffee_mud/system/I3/packets/Mud.java (deleted), /Version 4.0/com/planet_ink/coffee_mud/system/I3/packets/TellPacket.java (deleted), /Version 4.0/com/planet_ink/coffee_mud/system/I3/packets/LocateQueryPacket.java (deleted)]
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@2928 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/system/I3/packets/InvalidPacketException.java
+++
@@ -1,7 +0,0 @@
-package com.planet_ink.coffee_mud.system.I3.packets;
-
-public class InvalidPacketException extends I3Exception {
-    public InvalidPacketException() {
-        super("Invalid packet.");
-    }
-}
