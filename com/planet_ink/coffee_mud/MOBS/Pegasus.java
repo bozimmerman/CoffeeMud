f0620e4c085ea9f9acda853296e497c7c2fcf0ec
bozimmerman <bo@zimmers.net>
2003-08-29 04:14:52 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@2949 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/MOBS/Pegasus.java
@@ -0,0 +1,57 @@
+package com.planet_ink.coffee_mud.MOBS;
+
+import java.util.*;
+import com.planet_ink.coffee_mud.utils.*;
+import com.planet_ink.coffee_mud.interfaces.*;
+import com.planet_ink.coffee_mud.common.*;
+public class Pegasus extends StdRideable
+{
+    Random randomizer = null;
+   
+    public Pegasus()
+    {
+        super();
+        randomizer = new Random(System.currentTimeMillis());
+
+        Username="a Pegasus";
+        setDescription("a beautiful, white stallion with wings.");
+        setDisplayText("A Pegasus flaps its wings.");
+        setAlignment(500);
+        setMoney(0);
+        setWimpHitPoint(0);
+        rideBasis=Rideable.RIDEABLE_AIR;
+
+        baseEnvStats.setWeight(1500 + Math.abs(randomizer.nextInt() % 200));
+
+
+        baseCharStats().setStat(CharStats.INTELLIGENCE,8 + Math.abs(randomizer.nextInt() % 3));
+        baseCharStats().setStat(CharStats.STRENGTH,11);
+        baseCharStats().setStat(CharStats.DEXTERITY,17);
+        baseCharStats().setMyRace(CMClass.getRace("Horse"));
+        baseCharStats().getMyRace().startRacing(this,false);
+
+        baseEnvStats().setDamage(8);
+        baseEnvStats().setSpeed(3.0);
+        baseEnvStats().setAbility(0);
+        baseEnvStats().setLevel(4);
+        baseEnvStats().setArmor(60);
+        baseEnvStats().setDisposition(baseEnvStats().disposition()|EnvStats.IS_FLYING);
+
+        baseState.setHitPoints(Dice.roll(baseEnvStats().level(),20,baseEnvStats().level()));
+
+        recoverMaxState();
+        resetToMaxState();
+        recoverEnvStats();
+        recoverCharStats();
+    }    
+    
+    
+    public Environmental newInstance()
+    {
+        int percentage = Math.abs(randomizer.nextInt() % 100);
+        if(percentage < 5)
+            return new PegasusGreater();
+        else
+            return new Pegasus();
+    }
+}
---
+++ com/planet_ink/coffee_mud/MOBS/PegasusGreater.java
@@ -0,0 +1,50 @@
+package com.planet_ink.coffee_mud.MOBS;
+
+import java.util.*;
+import com.planet_ink.coffee_mud.utils.*;
+import com.planet_ink.coffee_mud.interfaces.*;
+import com.planet_ink.coffee_mud.common.*;
+public class PegasusGreater extends StdRideable
+{
+    protected int rideBasis=Rideable.RIDEABLE_AIR;
+
+    public PegasusGreater()
+    {
+        super();
+        Random randomizer = new Random(System.currentTimeMillis());
+
+        Username="a Greater Pegasus";
+        setDescription("a beautiful, white stallion with wings.");
+        setDisplayText("A regal Pegasus flaps its wings.");
+        setAlignment(500);
+        setMoney(0);
+        setWimpHitPoint(0);
+
+        baseEnvStats.setWeight(1500 + Math.abs(randomizer.nextInt() % 200));
+
+
+        baseCharStats().setStat(CharStats.INTELLIGENCE,8 + Math.abs(randomizer.nextInt() % 3));
+        baseCharStats().setStat(CharStats.STRENGTH,11);
+        baseCharStats().setStat(CharStats.DEXTERITY,17);
+        baseCharStats().setMyRace(CMClass.getRace("Horse"));
+        baseCharStats().getMyRace().startRacing(this,false);
+
+        baseEnvStats().setDamage(8);
+        baseEnvStats().setSpeed(3.0);
+        baseEnvStats().setAbility(0);
+        baseEnvStats().setLevel(6);
+        baseEnvStats().setArmor(60);
+        baseEnvStats().setDisposition(baseEnvStats().disposition()|EnvStats.IS_FLYING);
+
+        baseState.setHitPoints(Dice.roll(baseEnvStats().level(),20,baseEnvStats().level()));
+
+        recoverMaxState();
+        resetToMaxState();
+        recoverEnvStats();
+        recoverCharStats();
+    }
+    public Environmental newInstance()
+    {
+      return new PegasusGreater();
+    }
+}
