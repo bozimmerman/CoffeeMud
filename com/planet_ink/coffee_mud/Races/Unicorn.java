7c4da596cc0f5538cd3321dccfa6077127509b63
bozimmerman <bo@zimmers.net>
2003-09-25 19:45:26 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@3047 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Races/Unicorn.java
+++ com/planet_ink/coffee_mud/Races/Unicorn.java
@@ -1,5 +1,97 @@
 package com.planet_ink.coffee_mud.Races;
 
-public class Unicorn
+import com.planet_ink.coffee_mud.interfaces.*;
+import com.planet_ink.coffee_mud.common.*;
+import com.planet_ink.coffee_mud.utils.*;
+import java.util.*;
+
+public class Unicorn extends StdRace
 {
+	public String ID(){	return "Unicorn"; }
+	public String name(){ return "Unicorn"; }
+	protected int shortestMale(){return 60;}
+	protected int shortestFemale(){return 60;}
+	protected int heightVariance(){return 12;}
+	protected int lightestWeight(){return 350;}
+	protected int weightVariance(){return 100;}
+	protected long forbiddenWornBits(){return Integer.MAX_VALUE-Item.ON_HEAD-Item.ON_EARS-Item.ON_EYES;}
+	public String racialCategory(){return "Equine";}
+
+	//                                an ey ea he ne ar ha to le fo no gi mo wa ta wi
+	private static final int[] parts={0 ,2 ,2 ,1 ,1 ,0 ,0 ,1 ,4 ,4 ,1 ,0 ,1 ,1 ,1 ,0 };
+	public int[] bodyMask(){return parts;}
+
+	protected static Vector resources=new Vector();
+	public boolean playerSelectable(){return false;}
+
+	public void affectCharStats(MOB affectedMOB, CharStats affectableStats)
+	{
+		super.affectCharStats(affectedMOB, affectableStats);
+		affectableStats.setStat(CharStats.STRENGTH,18);
+		affectableStats.setStat(CharStats.DEXTERITY,6);
+		affectableStats.setStat(CharStats.INTELLIGENCE,1);
+	}
+	public Weapon myNaturalWeapon()
+	{
+		if(naturalWeapon==null)
+		{
+			naturalWeapon=CMClass.getWeapon("StdWeapon");
+			naturalWeapon.setName("a Unicorn Horn");
+			naturalWeapon.setWeaponType(Weapon.TYPE_PIERCING);
+		}
+		return naturalWeapon;
+	}
+
+	public String healthText(MOB mob)
+	{
+		double pct=(Util.div(mob.curState().getHitPoints(),mob.maxState().getHitPoints()));
+
+		if(pct<.10)
+			return "^r" + mob.name() + "^r is hovering on deaths door!^N";
+		else
+		if(pct<.20)
+			return "^r" + mob.name() + "^r is covered in blood and matted hair.^N";
+		else
+		if(pct<.30)
+			return "^r" + mob.name() + "^r is bleeding badly from lots of wounds.^N";
+		else
+		if(pct<.40)
+			return "^y" + mob.name() + "^y has large patches of bloody matted fur.^N";
+		else
+		if(pct<.50)
+			return "^y" + mob.name() + "^y has some bloody matted fur.^N";
+		else
+		if(pct<.60)
+			return "^p" + mob.name() + "^p has a lot of cuts and gashes.^N";
+		else
+		if(pct<.70)
+			return "^p" + mob.name() + "^p has a few cut patches.^N";
+		else
+		if(pct<.80)
+			return "^g" + mob.name() + "^g has a cut patch of fur.^N";
+		else
+		if(pct<.90)
+			return "^g" + mob.name() + "^g has some disheveled fur.^N";
+		else
+		if(pct<.99)
+			return "^g" + mob.name() + "^g has some misplaced hairs.^N";
+		else
+			return "^c" + mob.name() + "^c is in perfect health^N";
+	}
+	public Vector myResources()
+	{
+		synchronized(resources)
+		{
+			if(resources.size()==0)
+			{
+				resources.addElement(makeResource("a "+name().toLowerCase()+" mane",EnvResource.RESOURCE_FUR));
+				for(int i=0;i<2;i++)
+					resources.addElement(makeResource("a piece of "+name().toLowerCase()+" leather",EnvResource.RESOURCE_LEATHER));
+				resources.addElement(makeResource("a pound of "+name().toLowerCase()+" meat",EnvResource.RESOURCE_BEEF));
+				resources.addElement(makeResource("a pint of "+name().toLowerCase()+" blood",EnvResource.RESOURCE_BLOOD));
+				resources.addElement(makeResource("a "+name().toLowerCase()+" horn",EnvResource.RESOURCE_BONE));
+			}
+		}
+		return resources;
+	}
 }
