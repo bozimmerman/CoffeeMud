1f7df2373720d46f4724f2853e2211d0141e028c
bozimmerman <bo@zimmers.net>
2003-11-04 03:18:28 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@3139 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Abilities/Spells/Spell_WaterCannon.java
@@ -0,0 +1,85 @@
+package com.planet_ink.coffee_mud.Abilities.Spells;
+
+import com.planet_ink.coffee_mud.interfaces.*;
+import com.planet_ink.coffee_mud.common.*;
+import com.planet_ink.coffee_mud.utils.*;
+import java.util.*;
+
+public class Spell_WaterCannon extends Spell
+{
+	public String ID() { return "Spell_WaterCannon"; }
+	public String name(){return "Water Cannon";}
+	public int minRange(){return 2;}
+	public int maxRange(){return 3;}
+	public int quality(){return MALICIOUS;};
+	public Environmental newInstance(){ return new Spell_WaterCannon();}
+	public int classificationCode(){ return Ability.SPELL|Ability.DOMAIN_CONJURATION;}
+
+   public void affectEnvStats(Environmental affected, EnvStats affectableStats)
+	{
+		super.affectEnvStats(affected,affectableStats);
+		// when this spell is on a MOBs Affected list,
+		// it should consistantly put the mob into
+		// a sleeping state, so that nothing they do
+		// can get them out of it.
+		affectableStats.setDisposition(affectableStats.disposition()|EnvStats.IS_SITTING);
+	}
+
+	public boolean invoke(MOB mob, Vector commands, Environmental givenTarget, boolean auto)
+	{
+		MOB target=this.getTarget(mob,commands,givenTarget);
+		if(target==null) return false;
+
+		// the invoke method for spells receives as
+		// parameters the invoker, and the REMAINING
+		// command line parameters, divided into words,
+		// and added as String objects to a vector.
+		if(!super.invoke(mob,commands,givenTarget,auto))
+			return false;
+
+		// now see if it worked
+		boolean success=profficiencyCheck(0,auto);
+
+		if(success)
+		{
+			// it worked, so build a copy of this ability,
+			// and add it to the affects list of the
+			// affected MOB.  Then tell everyone else
+			// what happened.
+			FullMsg msg=new FullMsg(mob,target,this,affectType(auto),"<S-NAME> incant(s) at <T-NAMESELF> and geyser of water towards <S-HIM-HER>>");
+			FullMsg msg2=new FullMsg(mob,target,this,Affect.MSK_CAST_MALICIOUS_VERBAL|Affect.TYP_WATER|(auto?Affect.MASK_GENERAL:0),null);
+			if((mob.location().okAffect(mob,msg))&&(mob.location().okAffect(mob,msg2)))
+			{
+				mob.location().send(mob,msg);
+				invoker=mob;
+
+				int damage = 0;
+				int maxDie =  adjustedLevel(mob);
+				damage += Dice.roll(maxDie,8,15);
+				mob.location().send(mob,msg2);
+				if((msg2.wasModified())||(msg.wasModified()))
+					damage = (int)Math.round(Util.div(damage,2.0));
+
+				if(target.location()==mob.location())
+					ExternalPlay.postDamage(mob,target,this,damage,Affect.MASK_GENERAL|Affect.TYP_WATER,Weapon.TYPE_BASHING,"The water blast <DAMAGE> <T-NAME>!");
+            
+            int percentage = Dice.roll(1, 100, 0);
+            if(percentage < 10)
+            {
+      			FullMsg msg3=new FullMsg(mob,target,this,affectType(auto),"<T-NAME> is knocked down by the water cannon");
+               if(mob.location().okAffect(mob,msg3))
+               {
+                  mob.location().send(mob, msg3);                  
+               }
+               affectEnvStats(target, target.envStats());
+            }
+			}
+		}
+		else
+			return maliciousFizzle(mob,target,"<S-NAME> incant(s) and point(s) at <T-NAMESELF>, but flub(s) the spell.");
+
+
+		// return whether it worked
+		return success;
+	}
+}
