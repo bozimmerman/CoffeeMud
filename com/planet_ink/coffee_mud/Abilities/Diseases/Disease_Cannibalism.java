0240be43facd7cb7acb8c7647d3c970605416c17
bozimmerman <bo@zimmers.net>
2003-11-04 02:48:06 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@3137 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Abilities/Diseases/Disease_Cannibalism.java
@@ -0,0 +1,108 @@
+package com.planet_ink.coffee_mud.Abilities.Diseases;
+import com.planet_ink.coffee_mud.Abilities.StdAbility;
+import com.planet_ink.coffee_mud.Items.*;
+import com.planet_ink.coffee_mud.interfaces.*;
+import com.planet_ink.coffee_mud.common.*;
+import com.planet_ink.coffee_mud.utils.*;
+import java.util.*;
+
+public class Disease_Cannibalism extends Disease
+{
+	public String ID() { return "Disease_Cannibalism"; }
+	public String name(){ return "Cannibalism";}
+	public String displayText(){ return "(Cannibalism)";}
+	protected int canAffectCode(){return CAN_MOBS;}
+	protected int canTargetCode(){return CAN_MOBS;}
+	public int quality(){return Ability.MALICIOUS;}
+	public boolean putInCommandlist(){return false;}
+	public Environmental newInstance(){	return new Disease_Cannibalism();}
+
+	protected int DISEASE_TICKS(){return 999999;}
+	protected int DISEASE_DELAY(){return 100;}
+	protected String DISEASE_DONE()
+   {
+      String desiredMeat = "";
+      if(affected instanceof MOB)
+      {
+         MOB mob = (MOB) affected;
+         desiredMeat = mob.charStats().raceName();
+      }
+      else
+      {
+         desiredMeat = "your race's";
+      }
+      return "<S-NAME> no longer hunger for "+ affected +" meat.";
+   }
+	protected String DISEASE_START()
+   {
+      String desiredMeat = "";
+      if(affected instanceof MOB)
+      {
+         MOB mob = (MOB) affected;
+         desiredMeat = mob.charStats().raceName();
+      }
+      else
+      {
+         desiredMeat = "your race's";
+      }
+      return "^G<S-NAME> hunger(s) for "+ affected +" meat.^?";
+   }
+	protected String DISEASE_AFFECT(){return "";}
+	public int abilityCode(){return DiseaseAffect.SPREAD_CONSUMPTION;}
+	
+	public void unInvoke()
+	{
+		if(affected==null)
+			return;
+		if(affected instanceof MOB)
+		{
+			MOB mob=(MOB)affected;
+
+			super.unInvoke();
+			if(canBeUninvoked())
+				mob.tell(mob,null,this,DISEASE_DONE());
+		}
+		else
+			super.unInvoke();
+	}
+   
+	public boolean okAffect(Environmental myHost, Affect msg)
+	{
+		if((affected!=null)&&(affected instanceof MOB))
+		{
+         MOB source=(MOB)msg.source();
+         if(source == null)
+            return false;
+			MOB mob=(MOB)affected;
+         if(msg.targetCode() == Affect.TYP_EAT)
+         {
+            Environmental food = msg.target();
+            if(food instanceof StdFood)
+            {
+               if(food.name().toLowerCase().indexOf(mob.charStats().raceName()) < 0)
+               {
+                  FullMsg newMessage=new FullMsg(mob,food,this,0,"^S<S-NAME> attempt(s) to eat "+ food.Name() +", but can't stomach it....^?");
+                  if(mob.location().okAffect(mob,newMessage))
+                  {
+                     mob.location().send(mob,newMessage);
+                  }
+                  return false;
+               }
+            }
+         }
+      }
+		if((affected!=null)&&(affected instanceof MOB))
+		{
+			MOB mob=(MOB)affected;
+			if(msg.amISource(mob)
+			   &&(msg.tool()!=null)
+			   &&(msg.tool().ID().equals("Spell_Hungerless")))
+			{
+				mob.tell("You don't feel any less hungry.");
+				return false;
+			}
+		}
+
+      return super.okAffect(myHost,msg);
+	}
+}
