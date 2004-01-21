3752dc86c238411c63b4c721f8d68d09ee7d6566
bozimmerman <bo@zimmers.net>
2004-01-21 17:37:06 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@3307 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/web/macros/ItemData.java
+++ com/planet_ink/coffee_mud/web/macros/ItemData.java
@@ -124,7 +124,7 @@ public class ItemData extends StdWebMacro
 			{
 				int wclass=Util.s_int(httpReq.getRequestParameter("WEAPONCLASS"));
 				int reach=Util.s_int(httpReq.getRequestParameter("MINRANGE"));
-System.out.println("BLAH/"+I.name()+"/"+level+"/"+material+"/"+weight+"/"+hands+"/"+wclass+"/"+reach+"/"+0);				
+
 				vals=SaucerSupport.timsItemAdjustments(I,
 													   level,
 													   material,
@@ -146,7 +146,6 @@ System.out.println("BLAH/"+I.name()+"/"+level+"/"+material+"/"+weight+"/"+hands+
 						else
 							break;
 				}
-System.out.println("BLAH/"+I.name()+"/"+level+"/"+material+"/"+weight+"/"+hands+"/"+0+"/"+0+"/"+worndata);				
 				vals=SaucerSupport.timsItemAdjustments(I,
 													   level,
 													   material,
@@ -160,7 +159,6 @@ System.out.println("BLAH/"+I.name()+"/"+level+"/"+material+"/"+weight+"/"+hands+
 			{
 				String key=(String)e.nextElement();
 				String val=(String)vals.get(key);
-System.out.println("BLAH2/"+I.name()+"/"+key+"/"+val);
 				httpReq.addRequestParameters(key,val);
 			}
 		}
