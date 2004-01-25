b0581a74954a6940defa03ccd10f95c5b43ff43a
bozimmerman <bo@zimmers.net>
2004-01-25 21:32:00 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@3325 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Behaviors/Conquerable.java
+++ com/planet_ink/coffee_mud/Behaviors/Conquerable.java
@@ -1,5 +1,16 @@
 package com.planet_ink.coffee_mud.Behaviors;
+import com.planet_ink.coffee_mud.interfaces.*;
+import com.planet_ink.coffee_mud.common.*;
+import com.planet_ink.coffee_mud.utils.*;
 
-public class Conquerable
+import java.io.*;
+import java.util.*;
+
+
+public class Conquerable extends Arrest
 {
+	public String ID(){return "Conquerable";}
+	public Behavior newInstance(){ return new Conquerable();}
+	protected boolean defaultModifiableNames(){return false;}
+	public String getParms(){return "custom";}
 }
