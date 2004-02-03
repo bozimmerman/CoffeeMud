c69140ce1342f4aedd67bde4826dbd109632f3b0
bozimmerman <bo@zimmers.net>
2004-02-03 22:02:46 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@3373 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/interfaces/Command.java
+++ com/planet_ink/coffee_mud/interfaces/Command.java
@@ -3,8 +3,9 @@ package com.planet_ink.coffee_mud.interfaces;
 import java.util.Vector;
 public interface Command extends Comparable
 {
-	public Vector getAccessWords();
+	public String[] getAccessWords();
 	public int ticksToExecute();
 	public boolean canBeOrdered();
-	public boolean execute(MOB mob, Vector commands);
+	public boolean execute(MOB mob, Vector commands)
+		throws java.io.Exception;
 }
