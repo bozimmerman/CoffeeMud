cc63a518b959716778ace8cd06d9cc187280e523
bozimmerman <bo@zimmers.net>
2004-02-13 21:18:12 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@3467 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Abilities/Common/Masonry.java
+++ com/planet_ink/coffee_mud/Abilities/Common/Masonry.java
@@ -190,6 +190,7 @@ public class Masonry extends CommonSkill
 								||(R.rawDoors()[d].roomID().length()>0))
 									R.rawExits()[d]=room.rawExits()[d];
 							}
+							
 							R.startItemRejuv();
 							for(Enumeration r=CMMap.rooms();r.hasMoreElements();)
 							{
@@ -202,6 +203,29 @@ public class Masonry extends CommonSkill
 											((GridLocale)R2).buildGrid();
 									}
 							}
+							Room R2=CMClass.getLocale("UnderWater");
+							R2.setRoomID(CMMap.getOpenRoomID(R.getArea().ID()));
+							R2.setDisplayText("Under the water");
+							R2.setDescription("You are swimming around under the water.");
+							R2.setArea(R.getArea());
+							R2.rawDoors()[Directions.UP]=R;
+							R2.rawExits()[Directions.UP]=CMClass.getExit("Open");
+							R.rawDoors()[Directions.DOWN]=R2;
+							R2.rawExits()[Directions.DOWN]=CMClass.getExit("Open");
+							for(int a=0;a<R.numEffects();a++)
+							{
+								Ability A=R.fetchEffect(a);
+								if((A!=null)&&(A instanceof LandTitle))
+								{
+									LandTitle A2=(LandTitle)A.newInstance();
+									A2.setLandPrice(((LandTitle)A).landPrice());
+									R2.addNonUninvokableEffect((Ability)A2);
+									break;
+								}
+							}
+							CMClass.DBEngine().DBCreateRoom(R2,R2.ID());
+							CMClass.DBEngine().DBUpdateExits(R2);
+							
 							R.getArea().clearMap();
 							R.getArea().fillInAreaRoom(R);
 							CMClass.DBEngine().DBUpdateRoom(R);
