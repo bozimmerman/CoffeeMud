e55f940aef9e01b55524720e8812457ddbcb0884
bozimmerman <bo@zimmers.net>
2004-03-04 21:19:14 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@3551 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Abilities/Languages/Goblinese.java
@@ -0,0 +1,44 @@
+package com.planet_ink.coffee_mud.Abilities.Languages;
+import com.planet_ink.coffee_mud.Abilities.StdAbility;
+import com.planet_ink.coffee_mud.interfaces.*;
+import com.planet_ink.coffee_mud.common.*;
+import com.planet_ink.coffee_mud.utils.*;
+import java.util.*;
+
+public class Goblinese extends Language
+{
+	public String ID() { return "Goblinese"; }
+	public String name(){ return "Goblinese";}
+	public static Vector wordLists=null;
+	private static boolean mapped=false;
+	public Gnomish()
+	{
+		super();
+		if(!mapped){mapped=true;
+					CMAble.addCharAbilityMapping("All",1,ID(),false);}
+	}
+	public Environmental newInstance(){	return new Goblin();}
+	public Vector translationVector()
+	{
+		if(wordLists==null)
+		{
+			String[] one={"i","klpt","ih","g"};
+			String[] two={"te","il","ag","go"};
+			String[] three={"nik","rem","tit","nip","pop","pon","ipi","wip","pec"};
+			String[] 
+four={"perp","merp","nerp","pein","noog","gobo","koer","werp","terp","tert","grlt","Jrl","gran","kert"};
+			String[] 
+five={"whamb","thwam","nipgo","pungo","upoin","krepe","tungo","pongo","twang","hrgap","splt","krnch","baam","poww"};
+			String[] 
+six={"tawthak","krsplt","palpep","poopoo","dungdung","owwie","greepnak","tengak","grnoc","pisspiss","phlyyytt","plllb","hrangnok","ticktick","nurang"};
+			wordLists=new Vector();
+			wordLists.addElement(one);
+			wordLists.addElement(two);
+			wordLists.addElement(three);
+			wordLists.addElement(four);
+			wordLists.addElement(five);
+			wordLists.addElement(six);
+		}
+		return wordLists;
+	}
+}
