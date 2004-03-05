c0530c79b56fa11cc8254f53ca13c4a7901562a0
bozimmerman <bo@zimmers.net>
2004-03-05 11:21:04 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@3554 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/web/macros/CharClassData.java
+++ com/planet_ink/coffee_mud/web/macros/CharClassData.java
@@ -106,6 +106,8 @@ public class CharClassData extends StdWebMacro
 							str.append(I.name()+", ");
 					}
 				}
+				if(parms.containsKey("BALANCE"))
+					str.append(balanceChart(C));
 				String strstr=str.toString();
 				if(strstr.endsWith(", "))
 					strstr=strstr.substring(0,strstr.length()-2);
@@ -113,5 +115,102 @@ public class CharClassData extends StdWebMacro
 			}
 		}
 		return "";
+	}
+	
+	public String balanceChart(CharClass C)
+	{
+		MOB M=CMClass.getMOB("StdMOB");
+		M.baseEnvStats().setLevel(1);
+		M.baseCharStats().setCurrentClass(C);
+		M.recoverCharStats();
+		C.startCharacter(M,false,false);
+		HashSet seenBefore=new HashSet();
+		int totalgained=0;
+		int totalqualified=0;
+		int uniqueClassSkills=0;
+		int uniqueClassSkillsGained=0;
+		int uncommonClassSkills=0;
+		int uncommonClassSkillsGained=0;
+		int totalCrossClassSkills=0;
+		int totalCrossClassLevelDiffs=0;
+		int maliciousSkills=0;
+		int maliciousSkillsGained=0;
+		int beneficialSkills=0;
+		int beneficialSkillsGained=0;
+		for(int l=1;l<=30;l++)
+		{
+			Vector set=CMAble.getLevelListings(C.ID(),l);
+			for(int s=0;s<set.size();s++)
+			{
+				String able=(String)set.elementAt(s);
+				if(able.equalsIgnoreCase("Skill_Recall")) continue;
+				if(able.equalsIgnoreCase("Skill_Write")) continue;
+				if(able.equalsIgnoreCase("Skill_Swim")) continue;
+				if(CMAble.getQualifyingLevel("All",able)==l) continue;
+				if(seenBefore.contains(able)) continue;
+				seenBefore.add(able);
+				int numOthers=0;
+				int thisCrossClassLevelDiffs=0;
+				for(Enumeration c=CMClass.charClasses();c.hasMoreElements();)
+				{
+					CharClass C2=(CharClass)c.nextElement();
+					if(C2==C) continue;
+					if(!C2.playerSelectable()) continue;
+					int tlvl=CMAble.getQualifyingLevel(C2.ID(),able);
+					if(tlvl>0)
+					{
+						if(tlvl>l)
+							thisCrossClassLevelDiffs+=(tlvl-l);
+						else
+							thisCrossClassLevelDiffs+=(l-tlvl);
+						numOthers++;
+					}
+				}
+				if(numOthers==0)
+				{ 
+					uniqueClassSkills++; 
+					uncommonClassSkills++;
+				}
+				else
+				{
+					totalCrossClassLevelDiffs+=(thisCrossClassLevelDiffs/numOthers);
+					totalCrossClassSkills++;
+				}
+				if(numOthers==1) 
+					uncommonClassSkills++;
+				boolean gained=(M.fetchAbility(able)!=null);
+				if(gained)
+				{
+					totalgained++;
+					if(numOthers==0){ uniqueClassSkillsGained++; uncommonClassSkillsGained++;}
+					if(numOthers==1) uncommonClassSkillsGained++;
+				}
+				else
+					totalqualified++;
+				Ability A=CMClass.getAbility(able);
+				if(A==null) continue;
+				if((A.quality()==Ability.BENEFICIAL_OTHERS)
+				   ||(A.quality()==Ability.BENEFICIAL_SELF))
+				{
+					beneficialSkills++;
+					if(gained) beneficialSkillsGained++;
+				}
+				if(A.quality()==Ability.MALICIOUS)
+				{
+					maliciousSkills++;
+					if(gained) maliciousSkillsGained++;
+				}
+			}
+			M.charStats().getCurrentClass().level(M);
+		}
+		StringBuffer str=new StringBuffer("");
+		str.append("<BR>Rule#1: Avg gained skill/level: "+Util.div(Math.round(100.0*Util.div(totalgained,30)),(long)100));
+		str.append("<BR>Rule#2: Avg qualified skill/level: "+Util.div(Math.round(100.0*Util.div(totalqualified,30)),(long)100));
+		str.append("<BR>Rule#4: Unique class skills gained: "+uniqueClassSkillsGained+"/"+uniqueClassSkills);
+		str.append("<BR>Rule#4: Uncommon class skills gained: "+uncommonClassSkillsGained+"/"+uncommonClassSkills);
+		str.append("<BR>Rule#5: Combat skills gained: "+(maliciousSkillsGained+beneficialSkillsGained)+"/"+(maliciousSkills+beneficialSkills));
+		str.append("<BR>Rule#6: Avg Unique class skill/level: "+Util.div(Math.round(100.0*Util.div(uniqueClassSkills,30)),(long)100));
+		str.append("<BR>Rule#8: Avg Cross class skill/level: "+Util.div(Math.round(100.0*Util.div(totalCrossClassSkills,30)),(long)100));
+		return str.toString();
 	}
 }
