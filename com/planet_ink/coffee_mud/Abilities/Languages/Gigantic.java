c79a7a458d790e8ea9ea58baad7c2e2ba282c316
bozimmerman <bo@zimmers.net>
2004-03-06 19:03:34 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@3581 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Abilities/Languages/Gigantic.java
@@ -0,0 +1,140 @@
+package com.planet_ink.coffee_mud.Abilities.Languages;
+import com.planet_ink.coffee_mud.Abilities.StdAbility;
+import com.planet_ink.coffee_mud.interfaces.*;
+import com.planet_ink.coffee_mud.common.*;
+import com.planet_ink.coffee_mud.utils.*;
+import java.util.*;
+
+public class Gigantic extends Language
+{
+	public String ID() { return "Gigantic"; }
+	public String name(){ return "Gigantic";}
+	public static Vector wordLists=null;
+	private static boolean mapped=false;
+	public Gigantic()
+	{
+		super();
+		if(!mapped){mapped=true;
+					CMAble.addCharAbilityMapping("All",1,ID(),false);}
+	}
+	public Environmental newInstance(){	return new Gigantic();}
+	public Vector translationVector()
+	{
+		if(wordLists==null)
+		{
+			String[] one={"o","est","e","am"};
+			String[] 
+two={"on","dva","sa","is","id","et","bo","ja","te","me","za","ve"};
+			String[] three={"pet","set","tre","mal","maz","mat","ane","dom"};
+			String[] 
+four={"nast","sest","osam","bedu","beda","mene","mame","maja","beli","nesi"};
+			String[] 
+five={"sedam","devat","flanon","dvade","matke","trede","horat","jesam","taram","anaht","maram","nezme"};
+			String[] 
+six={"jedanast","delalime","veralim","dvanast","bahone","zahedon","prasad","trenast","staronast","starde","delaja"};
+			wordLists=new Vector();
+			wordLists.addElement(one);
+			wordLists.addElement(two);
+			wordLists.addElement(three);
+			wordLists.addElement(four);
+			wordLists.addElement(five);
+			wordLists.addElement(six);
+		}
+		return wordLists;
+	}
+	private static final Hashtable hashwords=new Hashtable();
+	public Hashtable translationHash()
+	{
+		if((hashwords!=null)&&(hashwords.size()>0))
+			return hashwords;
+		hashwords.put("0","nola");
+		hashwords.put("1","jedan");
+		hashwords.put("2","dva");
+		hashwords.put("3","tre");
+		hashwords.put("4","stare");
+		hashwords.put("5","pet");
+		hashwords.put("6","sest");
+		hashwords.put("7","sedam");
+		hashwords.put("8","osam");
+		hashwords.put("9","devet");
+		hashwords.put("10","deset");
+		hashwords.put("100","sto");
+		hashwords.put("1000","tesac");
+		hashwords.put("1000000","meljen");
+		hashwords.put("1000000000","meljard");
+		hashwords.put("1000000000000","heljen");
+		hashwords.put("1000000000000000","treljen");
+		hashwords.put("AND","e");
+		hashwords.put("BAD","spatno");
+		hashwords.put("BADLY","spatnoje");
+		hashwords.put("BE","ta jast");
+		hashwords.put("BEAUTY","kresno");
+		hashwords.put("BEAUTIFUL"","kresnoje");
+		hashwords.put("BEAUTIFULLY"","nakresnoje");
+		hashwords.put("BREAD","vodanet");
+		hashwords.put("BUT","ola");
+		hashwords.put("COME","kralestvo");
+		hashwords.put("DAY","kezdanon");
+		hashwords.put("DEBTS","vinarat");
+		hashwords.put("DELIVER","zebav");
+		hashwords.put("DONE","vola");
+		hashwords.put("EARTH","nevar");
+		hashwords.put("EIGHT","osmon");
+		hashwords.put("EVER","vaker");
+		hashwords.put("EVIL","zilonis");
+		hashwords.put("FATHER","atece");
+		hashwords.put("FINE","dobro");
+		hashwords.put("FIVE","peton");
+		hashwords.put("FOR","na");
+		hashwords.put("FORGIVE","adpast");
+		hashwords.put("FOUR","staron");
+		hashwords.put("GIVE","helabet");
+		hashwords.put("GLORY","slavat");
+		hashwords.put("GOOD","dobroje");
+		hashwords.put("HALLOWED","fasveston");
+		hashwords.put("HEAVEN","nevaror");
+		hashwords.put("HIGH","vesako");
+		hashwords.put("HIGHEST","navesakoje");
+		hashwords.put("HIGHLY","vesakoje");
+		hashwords.put("HUNDRED","ston");
+		hashwords.put("IN","na");
+		hashwords.put("INTO","vo");
+		hashwords.put("IS","jesi");
+		hashwords.put("KINGDOM","prijoda");
+		hashwords.put("LEAD","neprived");
+		hashwords.put("MILLION","meljanon");
+		hashwords.put("BILLION","meljardon");
+		hashwords.put("NAME","namet");
+		hashwords.put("NEW","navo");
+		hashwords.put("NEWEST","nanavoje");
+		hashwords.put("NEWLY","navoje");
+		hashwords.put("NICE","hezako");
+		hashwords.put("NICELY","hezakoje");
+		hashwords.put("NICEST","nahezakoje");
+		hashwords.put("NINE","devton");
+		hashwords.put("NOT","nas");
+		hashwords.put("OK","dobro");
+		hashwords.put("ONE","nolten");
+		hashwords.put("OUR","nar");
+		hashwords.put("POWER","mocet");
+		hashwords.put("QUADRILLION","treljanon");
+		hashwords.put("SEVEN","sedmon");
+		hashwords.put("SIX","seston");
+		hashwords.put("TEMPTATION","farsykonot");
+		hashwords.put("TEN","deston");
+		hashwords.put("THE","ta");
+		hashwords.put("THIS","daj");
+		hashwords.put("THOUSAND","tesacon");
+		hashwords.put("THREE","treton");
+		hashwords.put("TRILLION","heljanon");
+		hashwords.put("TWO","parvon");
+		hashwords.put("US","nar");
+		hashwords.put("WELL","nadobroje");
+		hashwords.put("WHO","ketri");
+		hashwords.put("WILL","so-stada");
+		hashwords.put("WORSE","naspatnoje");
+		hashwords.put("YOUR","ar");
+		hashwords.put("YOURS","tar");
+		return hashwords;
+		}
+}
