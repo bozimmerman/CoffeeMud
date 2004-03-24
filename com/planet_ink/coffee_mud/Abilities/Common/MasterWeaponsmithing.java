d284c06ce1c7e396d6b84eeeb18f03671e6f156c
bozimmerman <bo@zimmers.net>
2004-03-24 05:19:38 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@3728 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Abilities/Common/MasterWeaponsmithing.java
@@ -0,0 +1,64 @@
+package com.planet_ink.coffee_mud.Abilities.Common;
+
+import com.planet_ink.coffee_mud.interfaces.*;
+import com.planet_ink.coffee_mud.common.*;
+import com.planet_ink.coffee_mud.utils.*;
+import java.util.*;
+import java.io.File;
+
+public class MasterWeaponsmithing extends Weaponsmithing
+{
+	public String ID() { return "MasterWeaponsmithing"; }
+	public Environmental newInstance(){	return new MasterWeaponsmithing();}
+	public String name(){ return "Master Weaponsmithing";}
+	private static final String[] triggerStrings = {"MASTERWEAPONSMITH","MASTERWEAPONSMITHING","MWEAPONSMITH"};
+	public String[] triggerStrings(){return triggerStrings;}
+	private static boolean mapped2=false;
+	
+	public MasterWeaponsmithing()
+	{
+		super();
+		if(!mapped2){mapped2=true;
+					CMAble.addCharAbilityMapping("All",30,ID(),false);}
+	}
+
+	protected Vector loadRecipes()
+	{
+		Vector V=(Vector)Resources.getResource("MASTERWEAPONSMITHING RECIPES");
+		if(V==null)
+		{
+			StringBuffer str=Resources.getFile("resources"+File.separatorChar+"skills"+File.separatorChar+"masterweaponsmith.txt");
+			V=loadList(str);
+			if(V.size()==0)
+				Log.errOut("MasterWeaponsmithing","Recipes not found!");
+			Resources.submitResource("MASTERWEAPONSMITHING RECIPES",V);
+		}
+		return V;
+	}
+	public boolean canBeLearnedBy(MOB teacher, MOB student)
+	{
+		if(!super.canBeLearnedBy(teacher,student))
+			return false;
+		if(student==null) return true;
+		if(student.fetchAbility("Weaponsmithing")==null)
+		{
+			teacher.tell(student.name()+" has not yet learned weaponsmithing.");
+			student.tell("You need to learn weaponsmithing before you can learn "+name()+".");
+			return false;
+		}
+
+		return true;
+	}
+
+	public boolean invoke(MOB mob, Vector commands, Environmental givenTarget, boolean auto)
+	{
+		randomRecipeFix(mob,loadRecipes(),commands);
+		if(commands.size()==0)
+		{
+			commonTell(mob,"Make what? Enter \"mweaponsmith list\" for a list, \"mweaponsmith scan\", or \"mweaponsmith mend <item>\".");
+			return false;
+		}
+		return super.invoke(mob,commands,givenTarget,auto);
+	}
+	
+}
