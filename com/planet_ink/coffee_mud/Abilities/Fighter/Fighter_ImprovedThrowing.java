4e89edba4a8a23cbcd90cd8e56656e4a64cbf11a
bozimmerman <bo@zimmers.net>
2004-03-26 02:08:26 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@3739 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Abilities/Fighter/Fighter_ImprovedThrowing.java
@@ -0,0 +1,57 @@
+package com.planet_ink.coffee_mud.Abilities.Fighter;
+import com.planet_ink.coffee_mud.Abilities.StdAbility;
+import com.planet_ink.coffee_mud.interfaces.*;
+import com.planet_ink.coffee_mud.common.*;
+import com.planet_ink.coffee_mud.utils.*;
+import java.util.*;
+
+public class Fighter_ImprovedThrowing extends StdAbility
+{
+	public String ID() { return "Fighter_ImprovedThrowing"; }
+	public String name(){ return "Improved Throwing";}
+	public String displayText(){ return "";}
+	protected int canAffectCode(){return CAN_MOBS;}
+	protected int canTargetCode(){return 0;}
+	public int quality(){return Ability.BENEFICIAL_SELF;}
+	public int classificationCode(){return Ability.SKILL;}
+	public boolean isAutoInvoked(){return true;}
+	public boolean canBeUninvoked(){return false;}
+	public Environmental newInstance(){	return new Fighter_ImprovedThrowing();}
+	private boolean activated=false;
+
+	public boolean okMessage(Environmental myHost, CMMsg msg)
+	{
+		if((affected instanceof MOB)
+		&&(msg.amISource((MOB)affected))
+		&&(msg.targetMinor()==CMMsg.TYP_DAMAGE)
+		&&(msg.tool()!=null)
+		&&(msg.tool() instanceof Weapon)
+		&&(msg.value()>0)
+		&&(((Weapon)msg.tool()).weaponClassification()==Weapon.CLASS_THROWN))
+		{
+			if(Dice.rollPercentage()<25) helpProfficiency((MOB)affected);
+			msg.setValue(msg.value()+(int)Math.round(Util.mul(msg.value(),Util.div(profficiency(),100.0))));
+		}
+		return super.okMessage(myHost,msg);
+	}
+
+
+	public void affectEnvStats(Environmental affected, EnvStats affectableStats)
+	{
+		super.affectEnvStats(affected,affectableStats);
+		if(affected instanceof MOB)
+		{
+			Item myWeapon=((MOB)affected).fetchWieldedItem();
+			if((myWeapon!=null)
+			&&(myWeapon instanceof Weapon)
+			&&(((Weapon)myWeapon).weaponClassification()==Weapon.CLASS_THROWN))
+			{
+				activated=true;
+				affectableStats.setAttackAdjustment(affectableStats.attackAdjustment()+(int)Math.round(15.0*(Util.div(profficiency(),100.0))));
+			}
+			else
+				activated=false;
+		}
+	}
+	
+}
