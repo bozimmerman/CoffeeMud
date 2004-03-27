1f72584b1146d5e69fa4af96660b82dcee5a8b8c
bozimmerman <bo@zimmers.net>
2004-03-27 23:12:00 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@3755 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Abilities/Prayers/Prayer_Fidelity.java
@@ -0,0 +1,84 @@
+package com.planet_ink.coffee_mud.Abilities.Prayers;
+
+import com.planet_ink.coffee_mud.interfaces.*;
+import com.planet_ink.coffee_mud.common.*;
+import com.planet_ink.coffee_mud.utils.*;
+import java.util.*;
+
+public class Prayer_Fidelity extends Prayer
+{
+	public String ID() { return "Prayer_Fidelity"; }
+	public String name(){ return "Fidelity";}
+	public String displayText(){return "(Fidelity)";}
+	public int quality(){return Ability.OK_OTHERS;}
+	public Environmental newInstance(){	return new Prayer_Fidelity();}
+	public long flags(){return Ability.FLAG_HOLY;}
+
+	public void unInvoke()
+	{
+		// undo the affects of this spell
+		if((affected==null)||(!(affected instanceof MOB)))
+			return;
+		MOB mob=(MOB)affected;
+
+		super.unInvoke();
+
+		if(canBeUninvoked())
+			mob.tell("Your magical fidelity subsides.");
+	}
+
+	public boolean okMessage(Environmental myHost, CMMsg msg)
+	{
+		if(!super.okMessage(myHost,msg)) return false;
+		if(!(affected instanceof MOB)) return true;
+
+		MOB myChar=(MOB)affected;
+		if((msg.target()!=null)&&(msg.target() instanceof MOB))
+		{
+			MOB mate=(MOB)msg.target();
+			if((msg.amISource(myChar))
+			&&(!myChar.getLiegeID().equals(mate.Name()))
+			&&(myChar.isMarriedToLiege())
+			&&(msg.tool()!=null)
+			&&(msg.tool().ID().equals("Social"))
+			&&(msg.tool().Name().equals("MATE <T-NAME>")
+				||msg.tool().Name().equals("SEX <T-NAME>")))
+			{
+				myChar.tell("You fidelity geas prevents you from doing that.");
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public boolean invoke(MOB mob, Vector commands, Environmental givenTarget, boolean auto)
+	{
+		MOB target=this.getTarget(mob,commands,givenTarget);
+		if(target==null) return false;
+
+		if(!super.invoke(mob,commands,givenTarget,auto))
+			return false;
+
+		boolean success=profficiencyCheck(mob,0,auto);
+		if(success)
+		{
+			// it worked, so build a copy of this ability,
+			// and add it to the affects list of the
+			// affected MOB.  Then tell everyone else
+			// what happened.
+			FullMsg msg=new FullMsg(mob,target,this,affectType(auto),auto?"":"^S<S-NAME> "+prayWord(mob)+" for <T-NAMESELF>.^?");
+			if(mob.location().okMessage(mob,msg))
+			{
+				mob.location().send(mob,msg);
+				mob.location().show(target,null,CMMsg.MSG_OK_VISUAL,"<S-NAME> gain(s) the fidelity geas!");
+				beneficialAffect(mob,target,(Integer.MAX_VALUE/2));
+			}
+		}
+		else
+			return beneficialWordsFizzle(mob,target,"<S-NAME> "+prayWord(mob)+" for <T-NAMESELF>, but the magic fades.");
+
+
+		// return whether it worked
+		return success;
+	}
+}
