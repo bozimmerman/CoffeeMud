db66e986c75778c5341a805f4460078e55295663
bozimmerman <bo@zimmers.net>
2004-04-05 20:04:04 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@3795 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Abilities/Poisons/Poison_Caffeine.java
@@ -0,0 +1,51 @@
+package com.planet_ink.coffee_mud.Abilities.Poisons;
+import com.planet_ink.coffee_mud.Abilities.StdAbility;
+import com.planet_ink.coffee_mud.interfaces.*;
+import com.planet_ink.coffee_mud.common.*;
+import com.planet_ink.coffee_mud.utils.*;
+import java.util.*;
+
+public class Poison_Caffeine extends Poison {
+	public String ID() { return "Poison_Caffeine"; }
+	public String name(){ return "Poison_Hyper";}
+	public String displayText(){ return "(CAFFEINATED!!)";}
+	private static final String[] triggerStrings = {"POISONHYPER"};
+	public String[] triggerStrings(){return triggerStrings;}
+	public Environmental newInstance(){	return new Poison_Caffeine();}
+
+	protected int POISON_TICKS(){return 30;} // 0 means no adjustment!
+	protected int POISON_DELAY(){return 5;}
+	protected String POISON_DONE(){return "The caffeine runs its course.";}
+	protected String POISON_START(){return "^G<S-NAME> seem(s) wired!^?";}
+	protected String POISON_AFFECT(){return "^G<S-NAME> twitch(es) spastically.";}
+	protected String POISON_CAST(){return "^F<S-NAME> caffeinate(s) <T-NAMESELF>!^?";}
+	protected String POISON_FAIL(){return "<S-NAME> attempt(s) to caffinate <T-NAMESELF>, but fail(s).";}
+	protected int POISON_DAMAGE(){return 0;}
+
+	public void affectCharStats(MOB affected, CharStats affectableStats)
+	{
+	    affectableStats.setStat(CharStats.DEXTERITY,affectableStats.getStat(CharStats.DEXTERITY)+1);
+	}
+
+	public void affectEnvStats(Environmental affected, EnvStats affectableStats)
+	{
+	    super.affectEnvStats(affected,affectableStats);
+		affectableStats.setSpeed(affectableStats.speed() + 0.5);
+	    int oldDisposition=affectableStats.disposition();
+	    oldDisposition=oldDisposition&(Integer.MAX_VALUE-EnvStats.IS_SLEEPING-EnvStats.IS_SNEAKING-EnvStats.IS_SITTING);
+	    affectableStats.setDisposition(oldDisposition);
+	}
+
+	public boolean okMessage(Environmental myHost, CMMsg msg)
+	{
+	    if((affected==null)||(!(affected instanceof MOB)))
+	            return true;
+
+	    MOB mob=(MOB)affected;
+	    if(msg.amISource(mob)&&((msg.sourceMinor()==CMMsg.TYP_SIT)||(msg.sourceMinor()==CMMsg.TYP_SLEEP))) {
+	        mob.tell("You're too caffeinated for that!");
+	        return false;
+	    }
+	    return true;
+	}
+}
