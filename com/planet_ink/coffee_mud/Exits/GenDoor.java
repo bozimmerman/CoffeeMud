471c3927f62b0cf48f83a5b16f20aeb59cffdf3a
bozimmerman <bo@zimmers.net>
2004-04-14 22:17:28 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@3845 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Exits/GenDoor.java
@@ -0,0 +1,36 @@
+package com.planet_ink.coffee_mud.Exits;
+import com.planet_ink.coffee_mud.interfaces.*;
+import com.planet_ink.coffee_mud.common.*;
+import com.planet_ink.coffee_mud.utils.*;
+import java.util.*;
+
+public class GenDoor extends GenExit
+{
+	public String ID(){	return "GenDoor";}
+	public String Name(){ return "a door";}
+	public String displayText(){ return "";}
+	public boolean hasADoor(){return true;}
+	public boolean hasALock(){return false;}
+	public boolean defaultsLocked(){return false;}
+	public boolean defaultsClosed(){return true;}
+	public String closedText(){return "a closed door";}
+	public GenDoor()
+	{
+		super();
+		name="a door";
+		displayText="";
+		description="An ordinary wooden door with swinging hinges and a latch.";
+		hasADoor=true;
+		hasALock=false;
+		doorDefaultsClosed=true;
+		doorDefaultsLocked=false;
+		closedText="a closed door";
+		doorName="door";
+		closeName="close";
+		openName="open";
+	}
+	public Environmental newInstance()
+	{
+		return new GenDoor();
+	}
+}
