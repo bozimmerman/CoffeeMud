db37ce6078a846acf53ce4c59b827ae7b922f44a
bozimmerman <bo@zimmers.net>
2004-05-23 10:29:26 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@4050 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/interfaces/ShipComponent.java
@@ -0,0 +1,12 @@
+package com.planet_ink.coffee_mud.interfaces;
+
+public interface ShipComponent extends Item
+{
+	public final static int COMPONENT_MISC=0;
+	public final static int COMPONENT_PANEL=1;
+	public final static int COMPONENT_POWER=2;
+	public final static int COMPONENT_COMPUTER=3;
+	public final static int COMPONENT_ENGINE=4;
+	public final static int COMPONENT_WEAPON=5;
+	public final static int COMPONENT_SENSOR=6;
+}
