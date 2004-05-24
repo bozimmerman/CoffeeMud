75465a180e3f46a85e6298a53b7370c9e339f25f
bozimmerman <bo@zimmers.net>
2004-05-24 22:24:08 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@4062 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Items/MiscTech/GenSSConsole.java
@@ -0,0 +1,56 @@
+package com.planet_ink.coffee_mud.Items.MiscTech;
+import com.planet_ink.coffee_mud.Items.StdItem;
+import com.planet_ink.coffee_mud.interfaces.*;
+import com.planet_ink.coffee_mud.common.*;
+import com.planet_ink.coffee_mud.utils.*;
+import java.util.*;
+
+public class GenSSConsole extends StdItem
+{
+	public String ID(){	return "GenSSConsole";}
+	public GenSSConsole()
+	{
+		super();
+		setName("a generic computer console");
+		baseEnvStats.setWeight(2);
+		setDisplayText("a generic computer console sits here.");
+		setDescription("");
+		baseGoldValue=5;
+		baseEnvStats().setLevel(1);
+		recoverEnvStats();
+		setMaterial(EnvResource.RESOURCE_STEEL);
+	}
+	public Environmental newInstance()
+	{
+		return new GenSSConsole();
+	}
+	public boolean isGeneric(){return true;}
+
+	public String text()
+	{
+		return CoffeeMaker.getPropertiesStr(this,false);
+	}
+
+	public String readableText(){return readableText;}
+	public void setReadableText(String text){readableText=text;}
+	public void setMiscText(String newText)
+	{
+		miscText="";
+		CoffeeMaker.setPropertiesStr(this,newText,false);
+		recoverEnvStats();
+	}
+
+	public String getStat(String code)
+	{ return CoffeeMaker.getGenItemStat(this,code);}
+	public void setStat(String code, String val)
+	{ CoffeeMaker.setGenItemStat(this,code,val);}
+	public String[] getStatCodes(){return CoffeeMaker.GENITEMCODES;}
+	public boolean sameAs(Environmental E)
+	{
+		if(!(E instanceof GenItem)) return false;
+		for(int i=0;i<getStatCodes().length;i++)
+			if(!E.getStat(getStatCodes()[i]).equals(getStat(getStatCodes()[i])))
+				return false;
+		return true;
+	}
+}
