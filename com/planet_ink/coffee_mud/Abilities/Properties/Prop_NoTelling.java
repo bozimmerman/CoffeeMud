96df0f7fb9d9788a6e2f1105c1d48b50eadeddc4
bozimmerman <bo@zimmers.net>
2004-05-26 04:51:40 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@4069 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Abilities/Properties/Prop_NoTelling.java
+++ com/planet_ink/coffee_mud/Abilities/Properties/Prop_NoTelling.java
@@ -5,15 +5,15 @@ import com.planet_ink.coffee_mud.common.*;
 import com.planet_ink.coffee_mud.utils.*;
 import java.util.*;
 
-public class Prop_NoChannel extends Property
+public class Prop_NoTelling extends Property
 {
-	public String ID() { return "Prop_NoChannel"; }
-	public String name(){ return "Channel Neutralizing";}
+	public String ID() { return "Prop_NoTelling"; }
+	public String name(){ return "Tel Neutralizing";}
 	protected int canAffectCode(){return Ability.CAN_ROOMS|Ability.CAN_AREAS;}
-	public Environmental newInstance(){	return new Prop_NoChannel();}
+	public Environmental newInstance(){	return new Prop_NoTelling();}
 
 	public String accountForYourself()
-	{ return "No Channeling Field";	}
+	{ return "No Telling Field";	}
 
 	public boolean okMessage(Environmental myHost, CMMsg msg)
 	{
@@ -21,10 +21,13 @@ public class Prop_NoChannel extends Property
 			return false;
 
 
-		if(((msg.othersMajor()&CMMsg.MASK_CHANNEL)>0)
+		if((msg.sourceMinor()==CMMsg.TYP_TELL)
 		&&((!(affected instanceof Room))||(msg.source()==affected)))
 		{
-			msg.source().tell("This is a no-channel area.");
+			if(affected instanceof MOB)
+				msg.source().tell("Your message drifts into oblivion.");
+			else
+				msg.source().tell("This is a no-tell area.");
 			return false;
 		}
 		return true;
