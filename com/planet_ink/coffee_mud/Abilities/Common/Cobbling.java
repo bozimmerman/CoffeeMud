e3d9e4a0cd60cfcf41a96b28358f9e5a9f600500
bozimmerman <bo@zimmers.net>
2004-05-28 05:48:46 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@4081 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Abilities/Common/Cobbling.java
@@ -0,0 +1,411 @@
+package com.planet_ink.coffee_mud.Abilities.Common;
+
+import com.planet_ink.coffee_mud.interfaces.*;
+import com.planet_ink.coffee_mud.common.*;
+import com.planet_ink.coffee_mud.utils.*;
+import java.util.*;
+import java.io.File;
+
+public class Cobbling extends CraftingSkill
+{
+	public String ID() { return "COBBLING"; }
+	public String name(){ return "COBBLING";}
+	private static final String[] triggerStrings = {"COBBLE","COBBLING"};
+	public String[] triggerStrings(){return triggerStrings;}
+
+	private static final int RCP_FINALNAME=0;
+	private static final int RCP_LEVEL=1;
+	private static final int RCP_TICKS=2;
+	private static final int RCP_WOOD=3;
+	private static final int RCP_VALUE=4;
+	private static final int RCP_CLASSTYPE=5;
+	private static final int RCP_MISCTYPE=6;
+	private static final int RCP_CAPACITY=7;
+	private static final int RCP_ARMORDMG=8;
+	private static final int RCP_CONTAINMASK=9;
+	private static final int RCP_SPELL=10;
+	private Item building=null;
+	private Item fire=null;
+	private boolean mending=false;
+	private boolean refitting=false;
+	private boolean messedUp=false;
+	private static boolean mapped=false;
+	public Cobbling()
+	{
+		super();
+		if(!mapped){mapped=true;
+					CMAble.addCharAbilityMapping("All",1,ID(),false);}
+	}
+	public Environmental newInstance(){	return new Cobbling(); }
+
+	public boolean tick(Tickable ticking, int tickID)
+	{
+		if((affected!=null)&&(affected instanceof MOB)&&(tickID==MudHost.TICK_MOB))
+		{
+			MOB mob=(MOB)affected;
+			if(building==null)
+			{
+				messedUp=true;
+				unInvoke();
+			}
+		}
+		return super.tick(ticking,tickID);
+	}
+
+	protected Vector loadRecipes()
+	{
+		Vector V=(Vector)Resources.getResource("COBBLING RECIPES");
+		if(V==null)
+		{
+			StringBuffer str=Resources.getFile("resources"+File.separatorChar+"skills"+File.separatorChar+"cobbler.txt");
+			V=loadList(str);
+			if(V.size()==0)
+				Log.errOut("Cobbling","Recipes not found!");
+			Resources.submitResource("COBBLING RECIPES",V);
+		}
+		return V;
+	}
+
+	public void unInvoke()
+	{
+		if(canBeUninvoked())
+		{
+			if((affected!=null)&&(affected instanceof MOB))
+			{
+				MOB mob=(MOB)affected;
+				if((building!=null)&&(!aborted))
+				{
+					if(messedUp)
+					{
+						if(mending)
+							commonEmote(mob,"<S-NAME> mess(es) up mending "+building.name()+".");
+						else
+						if(refitting)
+							commonEmote(mob,"<S-NAME> mess(es) up refitting "+building.name()+".");
+						else
+							commonEmote(mob,"<S-NAME> mess(es) up cobbling "+building.name()+".");
+					}
+					else
+					{
+						if(mending)
+							building.setUsesRemaining(100);
+						else
+						if(refitting)
+						{
+							building.baseEnvStats().setHeight(0);
+							building.recoverEnvStats();
+						}
+						else
+							mob.location().addItemRefuse(building,Item.REFUSE_PLAYER_DROP);
+					}
+				}
+				building=null;
+				mending=false;
+			}
+		}
+		super.unInvoke();
+	}
+
+	public boolean canBeLearnedBy(MOB teacher, MOB student)
+	{
+		if(!super.canBeLearnedBy(teacher,student))
+			return false;
+		if(student==null) return true;
+		if(student.fetchAbility("Leatherworking")==null)
+		{
+			teacher.tell(student.name()+" has not yet learned leatherworking.");
+			student.tell("You need to learn leatherworking before you can learn "+name()+".");
+			return false;
+		}
+
+		return true;
+	}
+
+	protected boolean canMend(MOB mob, Environmental E, boolean quiet)
+	{
+		if(!super.canMend(mob,E,quiet)) return false;
+		Item IE=(Item)E;
+		if(!IE.fitsOn(Item.ON_FEET))
+		{
+			if(!quiet)
+				commonTell(mob,"That's not footwear.  That can't be mended.");
+			return false;
+		}
+		if(!(IE instanceof Armor))
+		{
+			if(!quiet)
+				commonTell(mob,"You don't know how to mend that sort of thing.");
+			return false;
+		}
+		return true;
+	}
+
+	public boolean invoke(MOB mob, Vector commands, Environmental givenTarget, boolean auto)
+	{
+		int autoGenerate=0;
+                Item IE=(Item)givenTarget;
+		if((auto)&&(givenTarget==this)&&(commands.size()>0)&&(commands.firstElement() instanceof Integer))
+		{	
+			autoGenerate=((Integer)commands.firstElement()).intValue(); 
+			commands.removeElementAt(0);
+			givenTarget=null;
+		}
+		randomRecipeFix(mob,loadRecipes(),commands,autoGenerate);
+		if(commands.size()==0)
+		{
+			commonTell(mob,"Make what? Enter \"cobble list\" for a list, \"cobble refit <item>\" to resize, \"cobble scan\", or \"cobble mend <item>\".");
+			return false;
+		}
+		Vector recipes=loadRecipes();
+		String str=(String)commands.elementAt(0);
+		String startStr=null;
+		boolean bundle=false;
+		int completion=4;
+		if(str.equalsIgnoreCase("list"))
+		{
+			StringBuffer buf=new StringBuffer("");
+			int toggler=1;
+			int toggleTop=2;
+			for(int r=0;r<toggleTop;r++)
+				buf.append(Util.padRight("Item",33)+" "+Util.padRight("Amt",3)+" ");
+			buf.append("\n\r");
+			for(int r=0;r<recipes.size();r++)
+			{
+				Vector V=(Vector)recipes.elementAt(r);
+				if(V.size()>0)
+				{
+					String item=replacePercent((String)V.elementAt(RCP_FINALNAME),"");
+					int level=Util.s_int((String)V.elementAt(RCP_LEVEL));
+					int wood=Util.s_int((String)V.elementAt(RCP_WOOD));
+					if(level<=mob.envStats().level())
+					{
+						buf.append(Util.padRight(item,33)+" "+Util.padRight(""+wood,3)+((toggler!=toggleTop)?" ":"\n\r"));
+						if(++toggler>toggleTop) toggler=1;
+					}
+				}
+			}
+			if(toggler!=1) buf.append("\n\r");
+			commonTell(mob,buf.toString());
+			return true;
+		}
+		if(str.equalsIgnoreCase("scan"))
+			return publicScan(mob,commands);
+		else
+		if(str.equalsIgnoreCase("mend"))
+		{
+			building=null;
+			mending=false;
+			messedUp=false;
+			Vector newCommands=Util.parse(Util.combine(commands,1));
+			building=getTarget(mob,mob.location(),givenTarget,newCommands,Item.WORN_REQ_UNWORNONLY);
+			if(!canMend(mob, building,false)) return false;
+			mending=true;
+			if(!super.invoke(mob,commands,givenTarget,auto))
+				return false;
+			startStr="<S-NAME> start(s) mending "+building.name()+".";
+			displayText="You are mending "+building.name();
+			verb="mending "+building.name();
+		}
+		else
+		if(str.equalsIgnoreCase("refit"))
+		{
+			building=null;
+			mending=false;
+			refitting=false;
+			messedUp=false;
+			Vector newCommands=Util.parse(Util.combine(commands,1));
+			building=getTarget(mob,mob.location(),givenTarget,newCommands,Item.WORN_REQ_UNWORNONLY);
+			if(building==null) return false;
+			if(!IE.fitsOn(Item.ON_FEET))
+			{
+				commonTell(mob,"That's not footwear.  That can't be refitted.");
+				return false;
+			}
+			if(!(building instanceof Armor))
+		    {
+				commonTell(mob,"You don't know how to refit that sort of thing.");
+				return false;
+			}
+			if(((Item)building).envStats().height()==0)
+			{
+				commonTell(mob,building.name()+" is already the right size.");
+				return false;
+			}
+			refitting=true;
+			if(!super.invoke(mob,commands,givenTarget,auto))
+				return false;
+			startStr="<S-NAME> start(s) refitting "+building.name()+".";
+			displayText="You are refitting "+building.name();
+			verb="refitting "+building.name();
+		}
+		else
+		{
+			building=null;
+			mending=false;
+			messedUp=false;
+			int amount=-1;
+			if((commands.size()>1)&&(Util.isNumber((String)commands.lastElement())))
+			{
+				amount=Util.s_int((String)commands.lastElement());
+				commands.removeElementAt(commands.size()-1);
+			}
+			String recipeName=Util.combine(commands,0);
+			Vector foundRecipe=null;
+			Vector matches=matchingRecipeNames(recipes,recipeName);
+			for(int r=0;r<matches.size();r++)
+			{
+				Vector V=(Vector)matches.elementAt(r);
+				if(V.size()>0)
+				{
+					int level=Util.s_int((String)V.elementAt(RCP_LEVEL));
+					if(level<=mob.envStats().level())
+					{
+						foundRecipe=V;
+						break;
+					}
+				}
+			}
+			if(foundRecipe==null)
+			{
+				commonTell(mob,"You don't know how to make a '"+recipeName+"'.  Try \"cobble list\" for a list.");
+				return false;
+			}
+			int woodRequired=Util.s_int((String)foundRecipe.elementAt(RCP_WOOD));
+
+			if(amount>woodRequired) woodRequired=amount;
+			String misctype=(String)foundRecipe.elementAt(this.RCP_MISCTYPE);
+			int[] pm={EnvResource.MATERIAL_METAL,EnvResource.MATERIAL_MITHRIL,EnvResource.MATERIAL_CLOTH,EnvResource.MATERIAL_WOODEN,EnvResource.MATERIAL_LEATHER};
+			int[][] data=fetchFoundResourceData(mob,
+												woodRequired,"metal",pm,
+												0,null,null,
+												misctype.equalsIgnoreCase("BUNDLE"),
+												autoGenerate);
+
+			if(!super.invoke(mob,commands,givenTarget,auto))
+				return false;
+			int lostValue=destroyResources(mob.location(),woodRequired,data[0][FOUND_CODE],0,null,autoGenerate);
+			building=CMClass.getItem((String)foundRecipe.elementAt(RCP_CLASSTYPE));
+			if(building==null)
+			{
+				commonTell(mob,"There's no such thing as a "+foundRecipe.elementAt(RCP_CLASSTYPE)+"!!!");
+				return false;
+			}
+			completion=Util.s_int((String)foundRecipe.elementAt(this.RCP_TICKS))-((mob.envStats().level()-Util.s_int((String)foundRecipe.elementAt(RCP_LEVEL)))*2);
+			String itemName=replacePercent((String)foundRecipe.elementAt(RCP_FINALNAME),EnvResource.RESOURCE_DESCS[(data[0][FOUND_CODE]&EnvResource.RESOURCE_MASK)]).toLowerCase();
+			if(itemName.endsWith("s"))
+				itemName="some "+itemName;
+			else
+				itemName=Util.startWithAorAn(itemName);
+			building.setName(itemName);
+			startStr="<S-NAME> start(s) cobbling "+building.name()+".";
+			displayText="You are cobbling "+building.name();
+			verb="cobbling "+building.name();
+			building.setDisplayText(itemName+" is here");
+			building.setDescription(itemName+". ");
+			building.baseEnvStats().setWeight(woodRequired);
+			building.setBaseValue(Util.s_int((String)foundRecipe.elementAt(RCP_VALUE)));
+			building.setMaterial(data[0][FOUND_CODE]);
+			int hardness=EnvResource.RESOURCE_DATA[data[0][FOUND_CODE]&EnvResource.RESOURCE_MASK][3]-6;
+			building.baseEnvStats().setLevel(Util.s_int((String)foundRecipe.elementAt(RCP_LEVEL))+(hardness*3));
+			if(building.baseEnvStats().level()<1) building.baseEnvStats().setLevel(1);
+			int capacity=Util.s_int((String)foundRecipe.elementAt(RCP_CAPACITY));
+			int canContain=Util.s_int((String)foundRecipe.elementAt(RCP_CONTAINMASK));
+			int armordmg=Util.s_int((String)foundRecipe.elementAt(RCP_ARMORDMG));
+			building.setSecretIdentity("This is the work of "+mob.Name()+".");
+			String spell=(foundRecipe.size()>RCP_SPELL)?((String)foundRecipe.elementAt(RCP_SPELL)).trim():"";
+			bundle=misctype.equalsIgnoreCase("BUNDLE");
+			if(bundle) building.setBaseValue(lostValue);
+			if(spell.length()>0)
+			{
+				String parm="";
+				if(spell.indexOf(";")>0)
+				{
+					parm=spell.substring(spell.indexOf(";")+1);
+					spell=spell.substring(0,spell.indexOf(";"));
+				}
+				Ability A=CMClass.getAbility(spell);
+				A.setMiscText(parm);
+				if(A!=null)	building.addNonUninvokableEffect(A);
+			}
+			if(building instanceof Armor)
+			{
+				((Armor)building).setRawProperLocationBitmap(0);
+				double hardBonus=0.0;
+				for(int wo=1;wo<Item.wornLocation.length;wo++)
+				{
+					String WO=Item.wornLocation[wo].toUpperCase();
+					if(misctype.equalsIgnoreCase(WO))
+					{
+						hardBonus+=Item.wornWeights[wo];
+						((Armor)building).setRawProperLocationBitmap(Util.pow(2,wo-1));
+						((Armor)building).setRawLogicalAnd(false);
+					}
+					else
+					if((misctype.toUpperCase().indexOf(WO+"||")>=0)
+					||(misctype.toUpperCase().endsWith("||"+WO)))
+					{
+						if(hardBonus==0.0)
+							hardBonus+=Item.wornWeights[wo];
+						((Armor)building).setRawProperLocationBitmap(building.rawProperLocationBitmap()|Util.pow(2,wo-1));
+						((Armor)building).setRawLogicalAnd(false);
+					}
+					else
+					if((misctype.toUpperCase().indexOf(WO+"&&")>=0)
+					||(misctype.toUpperCase().endsWith("&&"+WO)))
+					{
+						hardBonus+=Item.wornWeights[wo];
+						((Armor)building).setRawProperLocationBitmap(building.rawProperLocationBitmap()|Util.pow(2,wo-1));
+						((Armor)building).setRawLogicalAnd(true);
+					}
+				}
+				int hardPoints=(int)Math.round(Util.mul(hardBonus,hardness));
+				((Armor)building).baseEnvStats().setArmor(armordmg+hardPoints+(abilityCode()-1));
+			}
+			if(building instanceof Container)
+				if(capacity>0)
+				{
+					((Container)building).setCapacity(capacity+woodRequired);
+					((Container)building).setContainTypes(canContain);
+				}
+			building.recoverEnvStats();
+			building.text();
+			building.recoverEnvStats();
+		}
+
+
+
+		messedUp=!profficiencyCheck(mob,0,auto);
+		if(completion<6) completion=6;
+
+		if(bundle)
+		{
+			messedUp=false;
+			completion=1;
+			verb="bundling "+EnvResource.RESOURCE_DESCS[building.material()&EnvResource.RESOURCE_MASK].toLowerCase();
+			startStr="<S-NAME> start(s) "+verb+".";
+			displayText="You are "+verb;
+		}
+
+		if(autoGenerate>0)
+		{
+			commands.addElement(building);
+			return true;
+		}
+
+		FullMsg msg=new FullMsg(mob,building,this,CMMsg.MSG_NOISYMOVEMENT,startStr);
+		if(mob.location().okMessage(mob,msg))
+		{
+			mob.location().send(mob,msg);
+			building=(Item)msg.target();
+			beneficialAffect(mob,mob,completion);
+		}
+		else
+		if(bundle)
+		{
+			messedUp=false;
+			aborted=false;
+			unInvoke();
+		}
+		return true;
+	}
+}
