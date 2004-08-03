414ee09163bcd69c5e1c91af51950b413a18e6be
bozimmerman <bo@zimmers.net>
2004-08-03 01:32:08 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@4174 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Commands/List.java
+++ com/planet_ink/coffee_mud/Commands/List.java
@@ -561,189 +561,6 @@ public class List extends StdCommand
 		return msg;
 	}
 
-	private void fillCheckDeviations(Room R, String type, Vector check)
-	{
-		if(type.equalsIgnoreCase("mobs")||type.equalsIgnoreCase("both"))
-		{
-			for(int m=0;m<R.numInhabitants();m++)
-			{
-				MOB M=R.fetchInhabitant(m);
-				if((M!=null)&&(M.isEligibleMonster()))
-					check.addElement(M);
-			}
-		}
-		if(type.equalsIgnoreCase("items")||type.equalsIgnoreCase("both"))
-		{
-			for(int i=0;i<R.numItems();i++)
-			{
-				Item I=R.fetchItem(i);
-				if((I!=null)
-				&&((I instanceof Armor)||(I instanceof Weapon)))
-					check.addElement(I);
-			}
-			for(int m=0;m<R.numInhabitants();m++)
-			{
-				MOB M=R.fetchInhabitant(m);
-				if(M!=null)
-				{
-					for(int i=0;i<M.inventorySize();i++)
-					{
-						Item I=M.fetchInventory(i);
-						if((I!=null)
-						&&((I instanceof Armor)||(I instanceof Weapon)))
-							check.addElement(I);
-					}
-					ShopKeeper sk=CoffeeUtensils.getShopKeeper(M);
-					if(sk!=null)
-					{
-						Vector V=sk.getBaseInventory();
-						for(int i=0;i<V.size();i++)
-							if(V.elementAt(i) instanceof Item)
-							{
-								Item I=(Item)V.elementAt(i);
-								if((I instanceof Armor)||(I instanceof Weapon))
-									check.addElement(I);
-							}
-					}
-				}
-			}
-		}
-	}
-
-	private String getDeviation(int val, Hashtable vals, String key)
-	{
-		if(!vals.containsKey(key))
-			return "N/A";
-		int val2=Util.s_int((String)vals.get(key));
-		return getDeviation(val,val2);
-	}
-	private String getDeviation(int val, int val2)
-	{
-		if(val==val2) return "0%";
-		int oval=val-val2;
-		if(oval>0) return "+"+oval;
-		else return ""+oval;
-	}
-
-	public StringBuffer deviations(MOB mob, String rest)
-	{
-		Vector V=Util.parse(rest);
-		if((V.size()==0)
-		||((!((String)V.firstElement()).equalsIgnoreCase("mobs"))
-		   &&(!((String)V.firstElement()).equalsIgnoreCase("items"))
-		   &&(!((String)V.firstElement()).equalsIgnoreCase("both"))))
-			return new StringBuffer("You must specify whether you want deviations on MOBS, ITEMS, or BOTH.");
-
-		String type=((String)V.firstElement()).toLowerCase();
-		if(V.size()==1)
-			return new StringBuffer("You must also specify a mob or item name, or the word room, or the word area.");
-
-		String where=((String)V.elementAt(1)).toLowerCase();
-		Environmental E=mob.location().fetchFromMOBRoomFavorsItems(mob,null,where,Item.WORN_REQ_ANY);
-		Vector check=new Vector();
-		if(where.equalsIgnoreCase("room"))
-			fillCheckDeviations(mob.location(),type,check);
-		else
-		if(where.equalsIgnoreCase("area"))
-		{
-			for(Enumeration r=mob.location().getArea().getMap();r.hasMoreElements();)
-			{
-				Room R=(Room)r.nextElement();
-				fillCheckDeviations(R,type,check);
-			}
-		}
-		else
-		if(E==null)
-			return new StringBuffer("'"+where+"' is an unknown item or mob name.");
-		else
-		if(type.equals("items")
-		&&(!(E instanceof Weapon))
-		&&(!(E instanceof Armor)))
-			return new StringBuffer("'"+where+"' is not a weapon or armor item.");
-		else
-		if(type.equals("mobs")
-		&&(!(E instanceof MOB)))
-			return new StringBuffer("'"+where+"' is not a MOB.");
-		else
-		if((!(E instanceof Weapon))
-		&&(!(E instanceof Armor))
-		&&(!(E instanceof MOB)))
-			return new StringBuffer("'"+where+"' is not a MOB, or Weapon, or Item.");
-		else
-			check.addElement(E);
-		StringBuffer str=new StringBuffer("");
-		str.append("Deviations Report:\n\r");
-		str.append(Util.padRight("Name",20)+" ");
-		str.append(Util.padRight("Lvl",5)+" ");
-		str.append(Util.padRight("Att",5)+" ");
-		str.append(Util.padRight("Dmg",5)+" ");
-		str.append(Util.padRight("Val",5)+" ");
-		str.append(Util.padRight("Weigt",5)+" ");
-		str.append(Util.padRight("Speed",5)+" ");
-		str.append(Util.padRight("MinRg",5)+" ");
-		str.append(Util.padRight("MaxRg",5)+" ");
-		str.append("\n\r");
-		for(int c=0;c<check.size();c++)
-		{
-			if(check.elementAt(c) instanceof Item)
-			{
-				Item I=(Item)check.elementAt(c);
-				Weapon W=null;
-				if(I instanceof Weapon)
-					W=(Weapon)I;
-				Hashtable vals=CoffeeMaker.timsItemAdjustments(
-								I,I.envStats().level(),I.material(),I.baseEnvStats().weight(),
-								I.rawLogicalAnd()?2:1,
-								(W==null)?0:W.weaponClassification(),
-								I.maxRange(),
-								I.rawProperLocationBitmap());
-				str.append(Util.padRight(I.name(),20)+" ");
-				str.append(Util.padRight(""+I.envStats().level(),5)+" ");
-				str.append(Util.padRight(""+getDeviation(
-											I.baseEnvStats().attackAdjustment(),
-											vals,"ATTACK"),5)+" ");
-				str.append(Util.padRight(""+getDeviation(
-											I.baseEnvStats().damage(),
-											vals,"DAMAGE"),5)+" ");
-				str.append(Util.padRight(""+getDeviation(
-											I.baseGoldValue(),
-											vals,"VALUE"),5)+" ");
-				str.append(Util.padRight(""+getDeviation(
-											I.baseEnvStats().weight(),
-											vals,"WEIGHT"),5)+" ");
-				str.append(Util.padRight("N/A",5)+" ");
-				str.append(Util.padRight(""+getDeviation(
-											I.minRange(),
-											vals,"MINRANGE"),5)+" ");
-				str.append(Util.padRight(""+getDeviation(
-											I.maxRange(),
-											vals,"MAXRANGE"),5)+" ");
-				str.append("\n\r");
-			}
-			else
-			{
-				MOB M=(MOB)check.elementAt(c);
-				str.append(Util.padRight(M.name(),20)+" ");
-				str.append(Util.padRight(""+M.envStats().level(),5)+" ");
-				str.append(Util.padRight(""+getDeviation(
-											M.baseEnvStats().attackAdjustment(),
-											M.baseCharStats().getCurrentClass().getLevelAttack(M)),5)+" ");
-				str.append(Util.padRight(""+getDeviation(
-											M.baseEnvStats().damage(),
-											M.baseCharStats().getCurrentClass().getLevelDamage(M)),5)+" ");
-				str.append(Util.padRight("N/A",5)+" ");
-				str.append(Util.padRight("N/A",5)+" ");
-				str.append(Util.padRight(""+getDeviation(
-											(int)Math.round(M.baseEnvStats().speed()),
-											(int)Math.round(M.baseCharStats().getCurrentClass().getLevelSpeed(M))),5)+" ");
-				str.append(Util.padRight("N/A",5)+" ");
-				str.append(Util.padRight("N/A",5)+" ");
-				str.append("\n\r");
-			}
-		}
-		return str;
-	}
-
 	private String reallyFindOneWays(MOB mob, Vector commands)
 	{
 		StringBuffer str=new StringBuffer("");
@@ -881,7 +698,7 @@ public class List extends StdCommand
 		/*17*/{"PROPERTIES","CMDMOBS","CMDITEMS","CMDROOMS","CMDAREAS","CMDEXITS","CMDRACES","CMDCLASSES"},
 		/*18*/{"THIEFSKILLS","CMDMOBS","CMDITEMS","CMDROOMS","CMDAREAS","CMDEXITS","CMDRACES","CMDCLASSES"},
 		/*19*/{"COMMON","CMDMOBS","CMDITEMS","CMDROOMS","CMDAREAS","CMDEXITS","CMDRACES","CMDCLASSES"},
-		/*20*/{"DEVIATIONS","CMDITEMS"},
+		/*20*/{"",""},
 		/*21*/{"SKILLS","CMDMOBS","CMDITEMS","CMDROOMS","CMDAREAS","CMDEXITS","CMDRACES","CMDCLASSES"},
 		/*22*/{"QUESTS","CMDQUESTS"},
 		/*23*/{"DISEASES","CMDMOBS","CMDITEMS","CMDROOMS","CMDAREAS","CMDEXITS","CMDRACES","CMDCLASSES"},
@@ -927,14 +744,15 @@ public class List extends StdCommand
 			{
 				StringBuffer str=new StringBuffer("");
 				for(int v=0;v<V.size();v++)
-				{
-					str.append((String)V.elementAt(v));
-					if(v==(V.size()-2))
-						str.append(", or ");
-					else
-					if(v<(V.size()-1))
-						str.append(", ");
-				}
+					if(((String)V.elementAt(v)).length()>0)
+					{
+						str.append((String)V.elementAt(v));
+						if(v==(V.size()-2))
+							str.append(", or ");
+						else
+						if(v<(V.size()-1))
+							str.append(", ");
+					}
 				mob.tell("You cannot list '"+listWord+"'.  Try "+str.toString()+".");
 			}
 			return;
@@ -961,7 +779,7 @@ public class List extends StdCommand
 		case 17: s.rawPrintln(CMLister.reallyList(CMClass.abilities(),Ability.PROPERTY).toString()); break;
 		case 18: s.rawPrintln(CMLister.reallyList(CMClass.abilities(),Ability.THIEF_SKILL).toString()); break;
 		case 19: s.rawPrintln(CMLister.reallyList(CMClass.abilities(),Ability.COMMON_SKILL).toString()); break;
-		case 20: s.rawPrintln(deviations(mob,Util.combine(commands,1)).toString()); break;
+		case 20: break;
 		case 21: s.rawPrintln(CMLister.reallyList(CMClass.abilities(),Ability.SKILL).toString()); break;
 		case 22: s.println(listQuests().toString()); break;
 		case 23: s.rawPrintln(CMLister.reallyList(CMClass.abilities(),Ability.DISEASE).toString()); break;
