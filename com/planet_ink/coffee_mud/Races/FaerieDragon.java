bd21e2381ec815b54d6ca845029c93c483963d01
bozimmerman <bo@zimmers.net>
2004-09-03 02:03:38 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@4250 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Races/FaerieDragon.java
@@ -0,0 +1,123 @@
+package com.planet_ink.coffee_mud.Races;
+
+import com.planet_ink.coffee_mud.interfaces.*;
+import com.planet_ink.coffee_mud.common.*;
+import com.planet_ink.coffee_mud.utils.*;
+import java.util.*;
+
+public class FaerieDragon extends StdRace
+{
+	public String ID(){	return "Faerie Dragon"; }
+	public String name(){ return "Faerie Dragon"; }
+	public int shortestMale(){return 12;}
+	public int shortestFemale(){return 14;}
+	public int heightVariance(){return 12;}
+	public int lightestWeight(){return 10;}
+	public int weightVariance(){return 15;}
+	public long forbiddenWornBits(){return Item.WIELD|Item.ON_WAIST|Item.ON_BACK|Item.ABOUT_BODY|Item.ON_FEET|Item.ON_HANDS;}
+	public String racialCategory(){return "Dragon";}
+	private String[]racialAbilityNames={"Spell_Invisibility","Spell_FaerieFire","WingFlying"};
+	private int[]racialAbilityLevels={5,5,1};
+	private int[]racialAbilityProfficiencies={100,100,100};
+	private boolean[]racialAbilityQuals={false,false,false};
+	protected String[] racialAbilityNames(){return racialAbilityNames;}
+	protected int[] racialAbilityLevels(){return racialAbilityLevels;}
+	protected int[] racialAbilityProfficiencies(){return racialAbilityProfficiencies;}
+	protected boolean[] racialAbilityQuals(){return racialAbilityQuals;}
+
+	//                                an ey ea he ne ar ha to le fo no gi mo wa ta wi
+	private static final int[] parts={0 ,2 ,2 ,1 ,1 ,0 ,0 ,1 ,4 ,4 ,1 ,0 ,1 ,1 ,1 ,2 };
+	public int[] bodyMask(){return parts;}
+
+	protected static Vector resources=new Vector();
+	public int availability(){return Race.AVAILABLE_MAGICONLY;}
+
+	public void affectEnvStats(Environmental affected, EnvStats affectableStats)
+	{
+		super.affectEnvStats(affected,affectableStats);
+		affectableStats.setSensesMask(affectableStats.sensesMask()|EnvStats.CAN_SEE_INFRARED);
+		affectableStats.setDisposition(affectableStats.disposition()|EnvStats.IS_FLYING);
+	}
+	public void affectCharStats(MOB affectedMOB, CharStats affectableStats)
+	{
+		super.affectCharStats(affectedMOB, affectableStats);
+		affectableStats.setStat(CharStats.STRENGTH,affectableStats.getStat(CharStats.STRENGTH)-2);
+		affectableStats.setStat(CharStats.DEXTERITY,affectableStats.getStat(CharStats.DEXTERITY)+5);
+		affectableStats.setStat(CharStats.INTELLIGENCE,affectableStats.getStat(CharStats.INTELLIGENCE)+2);
+	}
+	public void startRacing(MOB mob, boolean verifyOnly)
+	{
+		super.startRacing(mob,verifyOnly);
+		if(!verifyOnly)
+		{
+			Ability A=CMClass.getAbility("Draconic");
+			if(A!=null)
+			{
+				mob.addAbility(A);
+				A.autoInvocation(mob);
+				if(mob.isMonster())
+					A.invoke(mob,mob,false);
+			}
+		}
+	}
+	public Weapon myNaturalWeapon()
+	{
+		if(naturalWeapon==null)
+		{
+			naturalWeapon=CMClass.getWeapon("StdWeapon");
+			naturalWeapon.setName("tiny talons");
+			naturalWeapon.setWeaponType(Weapon.TYPE_PIERCING);
+		}
+		return naturalWeapon;
+	}
+	public String healthText(MOB mob)
+	{
+		double pct=(Util.div(mob.curState().getHitPoints(),mob.maxState().getHitPoints()));
+
+		if(pct<.10)
+			return "^r" + mob.name() + "^r is raging in bloody pain!^N";
+		else
+		if(pct<.20)
+			return "^r" + mob.name() + "^r is covered in blood.^N";
+		else
+		if(pct<.30)
+			return "^r" + mob.name() + "^r is bleeding badly from lots of wounds.^N";
+		else
+		if(pct<.50)
+			return "^y" + mob.name() + "^y has some bloody wounds and gashed scales.^N";
+		else
+		if(pct<.60)
+			return "^p" + mob.name() + "^p has a few bloody wounds.^N";
+		else
+		if(pct<.70)
+			return "^p" + mob.name() + "^p is cut and bruised heavily.^N";
+		else
+		if(pct<.90)
+			return "^g" + mob.name() + "^g has a few bruises and scratched scales.^N";
+		else
+		if(pct<.99)
+			return "^g" + mob.name() + "^g has a few small bruises.^N";
+		else
+			return "^c" + mob.name() + "^c is in perfect health.^N";
+	}
+	public Vector myResources()
+	{
+		synchronized(resources)
+		{
+			if(resources.size()==0)
+			{
+				resources.addElement(makeResource
+				("a "+name().toLowerCase()+" claw",EnvResource.RESOURCE_BONE));
+				for(int i=0;i<3;i++)
+					resources.addElement(makeResource
+					("a strip of "+name().toLowerCase()+" scales",EnvResource.RESOURCE_SCALES));
+				for(int i=0;i<2;i++)
+					resources.addElement(makeResource
+					("a pound of "+name().toLowerCase()+" meat",EnvResource.RESOURCE_MEAT));
+				resources.addElement(makeResource
+				("some "+name().toLowerCase()+" blood",EnvResource.RESOURCE_BLOOD));
+			}
+		}
+		return resources;
+	}
+}
