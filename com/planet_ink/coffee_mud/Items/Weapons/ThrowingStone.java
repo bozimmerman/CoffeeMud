3d0cf6f9c3790566c4ed9f2cd62ebd0b6edee5a2
bozimmerman <bo@zimmers.net>
2004-10-17 22:58:34 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@4379 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Items/Weapons/ThrowingStone.java
@@ -0,0 +1,27 @@
+/*
+ * Created on Oct 17, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package com.planet_ink.coffee_mud.Items.Weapons;
+
+/**
+ * @author Administrator
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class ThrowingStone
+{
+
+    /**
+     * 
+     */
+    public ThrowingStone()
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+}
---
+++ com/planet_ink/coffee_mud/MOBS/AngryCitizen.java
@@ -0,0 +1,27 @@
+/*
+ * Created on Oct 17, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package com.planet_ink.coffee_mud.MOBS;
+
+/**
+ * @author Administrator
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class LocalCitizen
+{
+
+    /**
+     * 
+     */
+    public LocalCitizen()
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+}
