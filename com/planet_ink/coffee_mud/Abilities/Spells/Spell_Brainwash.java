4d75e9c129d134850e60ba26d08a7a144571267d
bozimmerman <bo@zimmers.net>
2004-10-18 16:22:02 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@4389 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Abilities/Spells/Spell_Brainwash.java
@@ -0,0 +1,27 @@
+/*
+ * Created on Oct 18, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package com.planet_ink.coffee_mud.Abilities.Spells;
+
+/**
+ * @author Administrator
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class Spell_Brainwash
+{
+
+    /**
+     * 
+     */
+    public Spell_Brainwash()
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+}
