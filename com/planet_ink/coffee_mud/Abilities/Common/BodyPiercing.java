76dc1421c5e116e1aaca6624f2e5a845993ac902
bozimmerman <bo@zimmers.net>
2004-10-21 01:33:02 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@4398 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Abilities/Common/BodyPiercing.java
@@ -0,0 +1,27 @@
+/*
+ * Created on Oct 20, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package com.planet_ink.coffee_mud.Abilities.Common;
+
+/**
+ * @author Administrator
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class BodyPiercing
+{
+
+    /**
+     * 
+     */
+    public BodyPiercing()
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+}
