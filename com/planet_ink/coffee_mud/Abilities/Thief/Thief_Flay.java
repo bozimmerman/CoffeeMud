51c59d515d649f2311922fc2be0761e886483505
bozimmerman <bo@zimmers.net>
2004-10-21 00:00:04 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@4397 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Abilities/Thief/Thief_Flay.java
@@ -0,0 +1,27 @@
+/*
+ * Created on Oct 20, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package com.planet_ink.coffee_mud.Abilities.Thief;
+
+/**
+ * @author Administrator
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class Thief_Flay
+{
+
+    /**
+     * 
+     */
+    public Thief_Flay()
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+}
