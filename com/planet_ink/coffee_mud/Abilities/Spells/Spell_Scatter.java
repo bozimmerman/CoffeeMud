8f805084b4c00ff275b4d91e84b87e29e2e5316d
bozimmerman <bo@zimmers.net>
2004-12-06 19:57:16 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@4452 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Abilities/Spells/Spell_Scatter.java
@@ -0,0 +1,135 @@
+package com.planet_ink.coffee_mud.Abilities.Spells;
+
+import com.planet_ink.coffee_mud.interfaces.*;
+import com.planet_ink.coffee_mud.common.*;
+import com.planet_ink.coffee_mud.utils.*;
+import java.util.*;
+
+/* 
+   Copyright 2000-2004 Bo Zimmerman
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+public class Spell_Scatter extends Spell
+{
+	public String ID() { return "Spell_Scatter"; }
+	public String name(){return "Scatter";}
+	protected int canTargetCode(){return CAN_MOBS|CAN_ITEMS;}
+	public int classificationCode(){return Ability.SPELL|Ability.DOMAIN_CONJURATION;}
+
+	public boolean invoke(MOB mob, Vector commands, Environmental givenTarget, boolean auto, int asLevel)
+	{
+		MOB mobTarget=getTarget(mob,commands,givenTarget,true,false);
+		Item target=null;
+		if(mobTarget!=null)
+		{
+			Vector goodPossibilities=new Vector();
+			Vector possibilities=new Vector();
+			for(int i=0;i<mobTarget.inventorySize();i++)
+			{
+				Item item=mobTarget.fetchInventory(i);
+				if((item!=null)
+				   &&(item.subjectToWearAndTear()))
+				{
+					if(item.amWearingAt(Item.INVENTORY))
+						possibilities.addElement(item);
+					else
+						goodPossibilities.addElement(item);
+				}
+				if(goodPossibilities.size()>0)
+					target=(Item)goodPossibilities.elementAt(Dice.roll(1,goodPossibilities.size(),-1));
+				else
+				if(possibilities.size()>0)
+					target=(Item)possibilities.elementAt(Dice.roll(1,possibilities.size(),-1));
+			}
+			if(target==null)
+				return maliciousFizzle(mob,mobTarget,"<S-NAME> attempt(s) a scattering spell at <T-NAMESELF>, but nothing happens.");
+		}
+
+		if(target==null)
+			target=getTarget(mob,mob.location(),givenTarget,commands,Item.WORN_REQ_ANY);
+
+		if(target==null) return false;
+
+		if(!super.invoke(mob,commands,givenTarget,auto,asLevel))
+			return false;
+
+		boolean success=profficiencyCheck(mob,0,auto);
+
+		if(success)
+		{
+			// it worked, so build a copy of this ability,
+			// and add it to the affects list of the
+			// affected MOB.  Then tell everyone else
+			// what happened.
+			FullMsg msg=new FullMsg(mob,target,this,affectType(auto),auto?"<T-NAME> starts vibrating!":"^S<S-NAME> utter(s) a shattering spell, causing <T-NAMESELF> to vibrate and resonate.^?");
+			FullMsg msg2=new FullMsg(mob,mobTarget,this,affectType(auto),null);
+			if((mob.location().okMessage(mob,msg))&&((mobTarget==null)||(mob.location().okMessage(mob,msg2))))
+			{
+				mob.location().send(mob,msg);
+				if(mobTarget!=null)
+					mob.location().send(mob,msg2);
+				if(msg.value()<=0)
+				{
+					int damage=100+mob.envStats().level()-target.envStats().level();
+					if(Sense.isABonusItems(target))
+						damage=(int)Math.round(Util.div(damage,2.0));
+					switch(target.material()&EnvResource.MATERIAL_MASK)
+					{
+					case EnvResource.MATERIAL_PAPER:
+					case EnvResource.MATERIAL_CLOTH:
+					case EnvResource.MATERIAL_VEGETATION:
+					case EnvResource.MATERIAL_PLASTIC:
+					case EnvResource.MATERIAL_LEATHER:
+					case EnvResource.MATERIAL_FLESH:
+						damage=(int)Math.round(Util.div(damage,3.0));
+						break;
+					case EnvResource.MATERIAL_WOODEN:
+						damage=(int)Math.round(Util.div(damage,1.5));
+						break;
+					case EnvResource.MATERIAL_GLASS:
+					case EnvResource.MATERIAL_ROCK:
+						damage=(int)Math.round(Util.mul(damage,2.0));
+						break;
+					case EnvResource.MATERIAL_PRECIOUS:
+						break;
+					case EnvResource.MATERIAL_ENERGY:
+						damage=0;
+						break;
+					}
+					target.setUsesRemaining(target.usesRemaining()-damage);
+					if(target.usesRemaining()>0)
+						target.recoverEnvStats();
+					else
+					{
+						target.setUsesRemaining(100);
+						if(mobTarget==null)
+							mob.location().show(mob,target,CMMsg.MSG_OK_VISUAL,"<T-NAME> is destroyed!");
+						else
+							mob.location().show(mobTarget,target,CMMsg.MSG_OK_VISUAL,"<T-NAME>, possessed by <S-NAME>, is destroyed!");
+						target.unWear();
+						target.destroy();
+						mob.location().recoverRoomStats();
+					}
+				}
+			}
+		}
+		else
+			return maliciousFizzle(mob,null,"<S-NAME> attempt(s) a shattering spell, but nothing happens.");
+
+
+		// return whether it worked
+		return success;
+	}
+}
+
