0dd7f66e7dc764222c91791ca6d96b721363c484
bozimmerman <bo@zimmers.net>
2004-12-06 10:14:26 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@4450 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Items/MiscMagic/DeckOfCards.java
@@ -0,0 +1,95 @@
+package com.planet_ink.coffee_mud.Items.MiscMagic;
+import com.planet_ink.coffee_mud.interfaces.*;
+import com.planet_ink.coffee_mud.common.*;
+import com.planet_ink.coffee_mud.utils.*;
+import com.planet_ink.coffee_mud.Items.Basic.StdContainer;
+import java.util.*;
+import com.planet_ink.coffee_mud.system.*;
+
+/* 
+   Copyright 2000-2004 Bo Zimmerman
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+public class DeckOfCards extends StdContainer implements MiscMagic
+{
+	public String ID(){	return "DeckOfCards";}
+	boolean alreadyFilled=false;
+	public DeckOfCards()
+	{
+		super();
+		name="A deck of cards";
+		displayText="A deck of cards has been left here.";
+		secretIdentity="A magical deck of cards.  Say \"Shuffle\" to me.";
+		recoverEnvStats();
+	}
+
+	protected Item makeResource(int ability)
+	{
+		Item I=CMClass.getItem("PlayingCard");
+		I.baseEnvStats().setAbility(ability);
+		I.recoverEnvStats();
+		I.setContainer(this);
+		if(owner() instanceof Room)
+			((Room)owner()).addItem(I);
+		else
+		if(owner() instanceof MOB)
+			((MOB)owner()).addInventory(I);
+		return I;
+	}
+
+	public boolean okMessage(Environmental myHost, CMMsg msg)
+	{
+		if((!alreadyFilled)&&(owner()!=null))
+		{
+			alreadyFilled=true;
+			if(getContents().size()==0)
+			{
+			    int[] suits={0,16,32,48};
+			    int[] cards={1,2,3,4,5,6,7,8,9,10,11,12,13};
+			    for(int i=0;i<suits.length;i++)
+			        for(int ii=0;ii<cards.length;ii++)
+			            makeResource(suits[i]+cards[ii]);
+			}
+		}
+		if((msg.amITarget(this))
+		&&(msg.targetMinor()==CMMsg.TYP_SPEAK)
+		&&(msg.targetMessage()!=null)
+		&&(msg.targetMessage().toUpperCase().indexOf("SHUFFLE")>0))
+		{
+		    Room R=CoffeeUtensils.roomLocation(this);
+	        Vector V=getContents();
+	        Environmental own=owner();
+	        if(V.size()==0)
+	            msg.source().tell("There are no cards left in the deck");
+	        else
+		    if(R!=null)
+		    {
+		        R.show(msg.source(),null,this,CMMsg.MASK_GENERAL|CMMsg.MSG_QUIETMOVEMENT,"<S-NAME> shuffle(s) <O-NAMESELF>.");
+		        for(int i=0;i<V.size()*5;i++)
+		        {
+		            Item I=(Item)V.elementAt(Dice.roll(1,V.size(),-1));
+		            I.removeFromOwnerContainer();
+		            I.setContainer(this);
+		            if(own instanceof MOB)
+		                ((MOB)own).addInventory(I);
+		            else
+		            if(own instanceof Room)
+		                R.addItemRefuse(I,Item.REFUSE_PLAYER_DROP);
+		        }
+		    }
+		    return false;
+		}
+		return super.okMessage(myHost,msg);
+	}
+}
---
+++ com/planet_ink/coffee_mud/Items/MiscMagic/PlayingCard.java
@@ -0,0 +1,78 @@
+package com.planet_ink.coffee_mud.Items.MiscMagic;
+import com.planet_ink.coffee_mud.interfaces.*;
+import com.planet_ink.coffee_mud.common.*;
+import com.planet_ink.coffee_mud.utils.*;
+import com.planet_ink.coffee_mud.Items.Basic.StdItem;
+import java.util.*;
+import com.planet_ink.coffee_mud.system.*;
+
+/* 
+   Copyright 2000-2004 Bo Zimmerman
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+public class PlayingCard extends StdItem implements MiscMagic
+{
+	public String ID(){	return "PlayingCard";}
+	private int oldAbility=0;
+	
+	public PlayingCard()
+	{
+		super();
+		name="A card";
+		displayText="A card lies here.";
+		secretIdentity="";
+		baseEnvStats().setWeight(0);
+		setBaseValue(0);
+		recoverEnvStats();
+	}
+
+	public void recoverEnvStats()
+	{
+	    super.recoverEnvStats();
+	    if(oldAbility!=envStats.ability())
+	    {
+	        oldAbility=envStats().ability();
+	        int suit=envStats.ability()&(16+32);
+	        int card=envStats.ability()&(1+2+4+8);
+	        String suitStr=null;
+	        switch(suit)
+	        {
+        	case 0: suitStr="spades"; break;
+        	case 16: suitStr="clubs"; break;
+        	case 32: suitStr="hearts"; break;
+        	case 48: suitStr="diamonds"; break;
+	        }
+	        String cardStr=null;
+	        switch(card)
+	        {
+	        	case 1: cardStr="ace"; break;
+	        	case 11: cardStr="jack"; break;
+	        	case 12: cardStr="queen"; break;
+	        	case 13: cardStr="king"; break;
+	        	case 2:case 3:case 4:case 5:case 6:case 7:case 8:case 9:case 10:
+	        	    cardStr=""+card; break;
+	        }
+	        if((suitStr==null)||(cardStr==null))
+	        {
+	    		name="A mangled card";
+	    		displayText="A mangled playing card lies here.";
+	        }
+	        else
+	        {
+	            name="the "+cardStr+" of "+suitStr;
+	            displayText="a playing card, "+name+", lies here";
+	        }
+	    }
+	}
+}
