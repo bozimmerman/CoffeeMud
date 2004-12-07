d01a33fd399d85e02b932e8cd2e7a3332172d04a
bozimmerman <bo@zimmers.net>
2004-12-07 01:18:08 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@4453 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Commands/Cloak.java
@@ -0,0 +1,27 @@
+/*
+ * Created on Dec 6, 2004
+ *
+ * TODO To change the template for this generated file go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+package com.planet_ink.coffee_mud.Commands;
+
+/**
+ * @author Administrator
+ *
+ * TODO To change the template for this generated type comment go to
+ * Window - Preferences - Java - Code Style - Code Templates
+ */
+public class Cloak
+{
+
+    /**
+     * 
+     */
+    public Cloak()
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+}
