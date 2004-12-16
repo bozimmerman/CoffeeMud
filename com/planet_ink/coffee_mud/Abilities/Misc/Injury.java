cbaea45d390409ee44f5049efcab30077fda6082
bozimmerman <bo@zimmers.net>
2004-12-16 05:19:26 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@4473 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Abilities/Misc/Injury.java
@@ -0,0 +1,50 @@
+package com.planet_ink.coffee_mud.Abilities.Misc;
+
+import com.planet_ink.coffee_mud.Abilities.StdAbility;
+import com.planet_ink.coffee_mud.interfaces.*;
+import com.planet_ink.coffee_mud.common.*;
+import com.planet_ink.coffee_mud.utils.*;
+
+import java.util.*;
+
+/* 
+   Copyright 2000-2004 Bo Zimmerman
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+public class Injury extends StdAbility
+{
+	public String ID() { return "Injury"; }
+	public String name(){ return "Injury";}
+
+	public String displayText(){
+		StringBuffer buf=new StringBuffer("");
+		for(int i=0;i<missingLimbNameSet().size();i++)
+			buf.append(", "+missingLimbNameSet().elementAt(i));
+		if(buf.length()==0) return "";
+		return "(Missing your"+buf.substring(1)+")";
+	}
+	protected int canAffectCode(){return CAN_MOBS;}
+	protected int canTargetCode(){return CAN_MOBS;}
+	public int quality(){return Ability.INDIFFERENT;}
+	public boolean putInCommandlist(){return false;}
+	private static final String[] triggerStrings = {"INJURE"};
+	public String[] triggerStrings(){return triggerStrings;}
+	public boolean canBeUninvoked(){return false;}
+	public int classificationCode(){return Ability.PROPERTY;}
+	public int usageType(){return USAGE_MOVEMENT|USAGE_MANA;}
+	private String[][] injuries=new String[Race.BODY_PARTS];
+
+	
+}
