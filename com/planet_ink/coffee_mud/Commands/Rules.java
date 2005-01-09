e8fbe896eb8dbf39401f60e1958e5ddbdd742fb6
bozimmerman <bo@zimmers.net>
2005-01-09 10:37:52 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@4529 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Commands/Rules.java
@@ -0,0 +1,43 @@
+package com.planet_ink.coffee_mud.Commands;
+import com.planet_ink.coffee_mud.interfaces.*;
+import com.planet_ink.coffee_mud.common.*;
+import com.planet_ink.coffee_mud.utils.*;
+import java.util.*;
+import java.io.*;
+
+/* 
+   Copyright 2000-2004 Bo Zimmerman
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+public class Rules extends StdCommand
+{
+	public Rules(){}
+
+	private String[] access={"RULES"};
+	public String[] getAccessWords(){return access;}
+	public boolean execute(MOB mob, Vector commands)
+		throws java.io.IOException
+	{
+		StringBuffer credits=Resources.getFileResource("text"+File.separatorChar+"rules.txt");
+		if((credits!=null)&&(mob.session()!=null))
+			mob.session().colorOnlyPrintln(credits.toString());
+		else
+			mob.tell("This mud has no rules.  Welcome to chaos.");
+		return false;
+	}
+	public int ticksToExecute(){return 0;}
+	public boolean canBeOrdered(){return true;}
+
+	public int compareTo(Object o){ return CMClass.classID(this).compareToIgnoreCase(CMClass.classID(o));}
+}
