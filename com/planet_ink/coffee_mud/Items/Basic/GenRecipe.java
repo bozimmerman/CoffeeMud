8db74a37f83b64e60a319258fd27495f18fead92
bozimmerman <bo@zimmers.net>
2005-02-10 09:47:26 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@4562 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Items/Basic/GenRecipe.java
@@ -0,0 +1,99 @@
+package com.planet_ink.coffee_mud.Items.Basic;
+
+
+import com.planet_ink.coffee_mud.interfaces.*;
+import com.planet_ink.coffee_mud.common.*;
+import com.planet_ink.coffee_mud.utils.*;
+
+import java.util.*;
+
+/* 
+   Copyright 2000-2005 Bo Zimmerman
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+public class GenRecipe extends GenReadable
+{
+	public String ID(){	return "GenRecipe";}
+	protected String commonSkillID="";
+	protected String recipeLine="";
+	public GenRecipe()
+	{
+		super();
+		setName("a generic recipe");
+		setDisplayText("a generic recipe sits here.");
+		setMaterial(EnvResource.RESOURCE_PAPER);
+		recoverEnvStats();
+	}
+
+
+	public boolean isGeneric(){return true;}
+	public void recoverEnvStats(){Sense.setReadable(this,true); super.recoverEnvStats();}
+	public String getCommonSkillID(){return commonSkillID;}
+	public void setCommonSkillID(String ID){commonSkillID=ID;}
+	public String getRecipeCodeLine(){return recipeLine;}
+	public void setRecipeCodeLine(String line){recipeLine=line;}
+	
+	private static String[] MYCODES={"SKILLID","RECIPE"};
+	public String getStat(String code)
+	{
+		if(CoffeeMaker.getGenItemCodeNum(code)>=0)
+			return CoffeeMaker.getGenItemStat(this,code);
+		else
+		switch(getCodeNum(code))
+		{
+		case 0: return ""+getCommonSkillID();
+		case 1: return ""+getRecipeCodeLine();
+		}
+		return "";
+	}
+	public void setStat(String code, String val)
+	{
+		if(CoffeeMaker.getGenItemCodeNum(code)>=0)
+			CoffeeMaker.setGenItemStat(this,code,val);
+		else
+		switch(getCodeNum(code))
+		{
+		case 0: setCommonSkillID(val); break;
+		case 1: setRecipeCodeLine(val); break;
+		}
+	}
+	protected int getCodeNum(String code){
+		for(int i=0;i<MYCODES.length;i++)
+			if(code.equalsIgnoreCase(MYCODES[i])) return i;
+		return -1;
+	}
+	private static String[] codes=null;
+	public String[] getStatCodes()
+	{
+		if(codes!=null) return codes;
+		String[] superCodes=CoffeeMaker.GENITEMCODES;
+		codes=new String[superCodes.length+MYCODES.length];
+		int i=0;
+		for(;i<superCodes.length;i++)
+			codes[i]=superCodes[i];
+		for(int x=0;x<MYCODES.length;i++,x++)
+			codes[i]=MYCODES[x];
+		return codes;
+	}
+	public boolean sameAs(Environmental E)
+	{
+		if(!(E instanceof GenRecipe)) return false;
+		String[] codes=getStatCodes();
+		for(int i=0;i<codes.length;i++)
+			if(!E.getStat(codes[i]).equals(getStat(codes[i])))
+				return false;
+		return true;
+	}
+}
+
