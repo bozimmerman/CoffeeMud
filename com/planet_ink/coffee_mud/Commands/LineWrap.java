171b5f3336bc6afbcfed119b862eaac32dda1a5d
bozimmerman <bo@zimmers.net>
2005-03-08 02:03:24 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@4590 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Commands/LineWrap.java
@@ -0,0 +1,63 @@
+package com.planet_ink.coffee_mud.Commands;
+import com.planet_ink.coffee_mud.interfaces.*;
+import com.planet_ink.coffee_mud.common.*;
+import com.planet_ink.coffee_mud.utils.*;
+import java.util.*;
+
+/* 
+   Copyright 2000-2005 Bo Zimmerman
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+public class LineWrap extends StdCommand
+{
+	public LineWrap(){}
+
+	private String[] access={"LINEWRAP"};
+	public String[] getAccessWords(){return access;}
+	
+	public boolean execute(MOB mob, Vector commands)
+		throws java.io.IOException
+	{
+	    if((mob==null)||(mob.playerStats()==null))
+	        return false;
+	    
+		if(commands.size()<2)
+		{
+		    String wrap=(mob.playerStats().getWrap()!=0)?(""+mob.playerStats().getWrap()):"Disabled";
+			mob.tell("Change your line wrap to what? Your current line wrap setting is: "+wrap+". Enter a number larger than 10 or 'disable'.");
+			return false;
+		}
+		String newWrap=Util.combine(commands,1);
+		int newVal=mob.playerStats().getWrap();
+		if((Util.isInteger(newWrap))&&(Util.s_int(newWrap)>10))
+		    newVal=Util.s_int(newWrap);
+		else
+		if("DISABLED".startsWith(newWrap.toUpperCase()))
+		    newVal=0;
+		else
+		{
+			mob.tell("'"+newWrap+"' is not a valid setting. Enter a number larger than 10 or 'disable'.");
+		    return false;
+		}
+		mob.playerStats().setWrap(newVal);
+	    String wrap=(mob.playerStats().getWrap()!=0)?(""+mob.playerStats().getWrap()):"Disabled";
+		mob.tell("Your new line wrap setting is: "+wrap+".");
+		return false;
+	}
+	public int ticksToExecute(){return 0;}
+	public boolean canBeOrdered(){return true;}
+
+	public int compareTo(Object o){ return CMClass.classID(this).compareToIgnoreCase(CMClass.classID(o));}
+}
+
