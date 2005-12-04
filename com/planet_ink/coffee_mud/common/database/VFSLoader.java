61ebc1643f4eb40ad76bfb3d9be61dd5aa9aaafb
bozimmerman <bo@zimmers.net>
2005-12-04 16:45:24 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@5097 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/common/database/VFSLoader.java
@@ -0,0 +1,12 @@
+package com.planet_ink.coffee_mud.system.database;
+
+public class VFSLoader
+{
+
+    public VFSLoader()
+    {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+}
