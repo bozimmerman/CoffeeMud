e75bab4d91da84ca806eda2b58449ba2fd2878d4
bozimmerman <bo@zimmers.net>
2005-12-11 18:59:40 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@5282 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Common/interfaces/CMIntegerGrouper.java
@@ -0,0 +1,52 @@
+package com.planet_ink.coffee_mud.Common.interfaces;
+import com.planet_ink.coffee_mud.core.interfaces.*;
+import com.planet_ink.coffee_mud.core.*;
+import com.planet_ink.coffee_mud.Abilities.interfaces.*;
+import com.planet_ink.coffee_mud.Areas.interfaces.*;
+import com.planet_ink.coffee_mud.Behaviors.interfaces.*;
+import com.planet_ink.coffee_mud.CharClasses.interfaces.*;
+import com.planet_ink.coffee_mud.Commands.interfaces.*;
+import com.planet_ink.coffee_mud.Common.interfaces.*;
+import com.planet_ink.coffee_mud.Exits.interfaces.*;
+import com.planet_ink.coffee_mud.Items.interfaces.*;
+import com.planet_ink.coffee_mud.Locales.interfaces.*;
+import com.planet_ink.coffee_mud.MOBS.interfaces.*;
+import com.planet_ink.coffee_mud.Races.interfaces.*;
+import java.util.*;
+
+/* 
+   Copyright 2000-2005 Bo Zimmerman
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+public interface CMIntegerGrouper extends CMObject
+{
+    public static final int NEXT_FLAG=(Integer.MAX_VALUE/2)+1;
+    public static final int NEXT_BITS=NEXT_FLAG-1;
+    public static final long NEXT_FLAGL=(Long.MAX_VALUE/2)+1;
+    public static final long NEXT_BITSL=NEXT_FLAGL-1;
+    
+    public String text();
+    public CMIntegerGrouper parseText(String txt);
+    public boolean contains(long x);
+    public int roomCount();
+    public void growarrayx(int here);
+    public void growarrayy(int here);
+    public void shrinkarrayx(int here);
+    public void shrinkarrayy(int here);
+    public void consolodatex();
+    public void consolodatey();
+    public CMIntegerGrouper add(long x);
+    public void addy(long x);
+    public void addx(int x);
+}
---
+++ com/planet_ink/coffee_mud/Common/interfaces/CoffeeTableRow.java
@@ -0,0 +1,62 @@
+package com.planet_ink.coffee_mud.Common.interfaces;
+import com.planet_ink.coffee_mud.core.interfaces.*;
+import com.planet_ink.coffee_mud.core.*;
+import com.planet_ink.coffee_mud.Abilities.interfaces.*;
+import com.planet_ink.coffee_mud.Areas.interfaces.*;
+import com.planet_ink.coffee_mud.Behaviors.interfaces.*;
+import com.planet_ink.coffee_mud.CharClasses.interfaces.*;
+import com.planet_ink.coffee_mud.Commands.interfaces.*;
+import com.planet_ink.coffee_mud.Common.interfaces.*;
+import com.planet_ink.coffee_mud.Exits.interfaces.*;
+import com.planet_ink.coffee_mud.Items.interfaces.*;
+import com.planet_ink.coffee_mud.Locales.interfaces.*;
+import com.planet_ink.coffee_mud.MOBS.interfaces.*;
+import com.planet_ink.coffee_mud.Races.interfaces.*;
+
+/* 
+   Copyright 2000-2005 Bo Zimmerman
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+public interface CoffeeTableRow extends CMObject
+{
+    public final int STAT_LOGINS=0;
+    public final int STAT_TICKSONLINE=1;
+    public final int STAT_NEWPLAYERS=2;
+    public final int STAT_LEVELSGAINED=3;
+    public final int STAT_DEATHS=4;
+    public final int STAT_PKDEATHS=5;
+    public final int STAT_MARRIAGES=6;
+    public final int STAT_BIRTHS=7;
+    public final int STAT_DIVORCES=8;
+    public final int STAT_CLASSCHANGE=9;
+    public final int STAT_PURGES=10;
+    public final int STAT_SKILLUSE=11;
+    public final int STAT_TOTAL=12;
+    
+    public final int STAT_SPECIAL_NUMONLINE=1000;
+    
+    public long startTime();
+    public long endTime();
+    public void setStartTime(long time);
+    public void setEndTime(long time);
+    public long highestOnline();
+    public long numberOnlineTotal();
+    public long numberOnlineCounter();
+    public String data();
+    public void bumpVal(String s, int type);
+    public void bumpVal(Environmental E, int type);
+    public void totalUp(String code, long[] tot);
+    public void populate(long start, long end, String data);
+
+}
