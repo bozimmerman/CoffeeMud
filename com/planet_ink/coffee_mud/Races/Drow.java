36003b36dd394c25694147a7de575bba0fe04dbd
bozimmerman <bo@zimmers.net>
2006-04-24 16:23:23 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@5478 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Races/Drow.java
@@ -0,0 +1,175 @@
+package com.planet_ink.coffee_mud.Races;
+import java.util.Vector;
+
+import com.planet_ink.coffee_mud.Areas.interfaces.Area;
+import com.planet_ink.coffee_mud.Common.interfaces.CharStats;
+import com.planet_ink.coffee_mud.Common.interfaces.EnvStats;
+import com.planet_ink.coffee_mud.Items.interfaces.Armor;
+import com.planet_ink.coffee_mud.Items.interfaces.RawMaterial;
+import com.planet_ink.coffee_mud.Items.interfaces.Weapon;
+import com.planet_ink.coffee_mud.Locales.interfaces.Room;
+import com.planet_ink.coffee_mud.MOBS.interfaces.MOB;
+import com.planet_ink.coffee_mud.core.CMClass;
+import com.planet_ink.coffee_mud.core.CMath;
+import com.planet_ink.coffee_mud.core.interfaces.Environmental;
+
+/*
+   Copyright 2006 Lee Fox
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+public class Drow extends Elf
+{
+	public String ID(){	return "Drow"; }
+	public String name(){ return "Drow"; }
+	public int shortestMale(){return 59;}
+	public int shortestFemale(){return 59;}
+	public int heightVariance(){return 12;}
+	public int lightestWeight(){return 80;}
+	public int weightVariance(){return 80;}
+	public long forbiddenWornBits(){return 0;}
+	public String racialCategory(){return "Elf";}
+	private String[]culturalAbilityNames={"Drowish","Foraging"};
+	private int[]culturalAbilityProfficiencies={75,50};
+	public String[] culturalAbilityNames(){return culturalAbilityNames;}
+	public int[] culturalAbilityProfficiencies(){return culturalAbilityProfficiencies;}
+
+	//                                an ey ea he ne ar ha to le fo no gi mo wa ta wi
+	private static final int[] parts={0 ,2 ,2 ,1 ,1 ,2 ,2 ,1 ,2 ,2 ,1 ,0 ,1 ,1 ,0 ,0 };
+	public int[] bodyMask(){return parts;}
+
+	private int[] agingChart={0,2,20,110,175,263,350,390,430};
+	public int[] getAgingChart(){return agingChart;}
+
+	protected static Vector resources=new Vector();
+	public int availabilityCode(){return Area.THEME_FANTASY;}
+
+	public void affectEnvStats(Environmental affected, EnvStats affectableStats)
+	{
+		super.affectEnvStats(affected,affectableStats);
+		if((affected instanceof MOB)&&(((MOB)affected).location()!=null)) {
+			MOB mob=(MOB)affected;
+			Room room=mob.location();
+			if(mob.location().getArea().getClimateObj().canSeeTheSun(room)) {
+				affectableStats.setArmor(affectableStats.armor() * 2);
+			}
+		}
+
+		affectableStats.setSensesMask(affectableStats.sensesMask()|EnvStats.CAN_SEE_INFRARED|EnvStats.CAN_SEE_DARK);
+	}
+	public void affectCharStats(MOB affectedMOB, CharStats affectableStats)
+	{
+		super.affectCharStats(affectedMOB, affectableStats);
+		affectableStats.setStat(CharStats.STAT_INTELLIGENCE,affectableStats.getStat(CharStats.STAT_INTELLIGENCE)+1);
+		affectableStats.setStat(CharStats.STAT_MAX_INTELLIGENCE_ADJ,affectableStats.getStat(CharStats.STAT_MAX_INTELLIGENCE_ADJ)+1);
+		affectableStats.setStat(CharStats.STAT_DEXTERITY,affectableStats.getStat(CharStats.STAT_DEXTERITY)+2);
+		affectableStats.setStat(CharStats.STAT_MAX_DEXTERITY_ADJ,affectableStats.getStat(CharStats.STAT_MAX_DEXTERITY_ADJ)+2);
+		affectableStats.setStat(CharStats.STAT_CONSTITUTION,affectableStats.getStat(CharStats.STAT_CONSTITUTION)-1);
+		affectableStats.setStat(CharStats.STAT_MAX_CONSTITUTION_ADJ,affectableStats.getStat(CharStats.STAT_MAX_CONSTITUTION_ADJ)-1);
+		affectableStats.setStat(CharStats.STAT_CHARISMA,affectableStats.getStat(CharStats.STAT_CHARISMA)-2);
+		affectableStats.setStat(CharStats.STAT_MAX_CHARISMA_ADJ,affectableStats.getStat(CharStats.STAT_MAX_CHARISMA_ADJ)-2);
+		affectableStats.setStat(CharStats.STAT_SAVE_MAGIC,affectableStats.getStat(CharStats.STAT_SAVE_MAGIC)+20);
+
+		if(affectedMOB.location()!=null) {
+			MOB mob=affectedMOB;
+			Room room=mob.location();
+			if(mob.location().getArea().getClimateObj().canSeeTheSun(room)) {
+				affectableStats.setStat(CharStats.STAT_DEXTERITY, affectableStats.getStat(CharStats.STAT_DEXTERITY) /2);
+			}
+		}
+	}
+	public Vector outfit(MOB myChar)
+	{
+		if(outfitChoices==null)
+		{
+			outfitChoices=new Vector();
+			// Have to, since it requires use of special constructor
+			Armor s1=CMClass.getArmor("GenShirt");
+			s1.setName("a delicate green shirt");
+			s1.setDisplayText("a delicate green shirt sits gracefully here.");
+			s1.setDescription("Obviously fine craftmenship, with sharp folds and intricate designs.");
+			s1.text();
+			outfitChoices.addElement(s1);
+
+			Armor s2=CMClass.getArmor("GenShoes");
+			s2.setName("a pair of sandals");
+			s2.setDisplayText("a pair of sandals lie here.");
+			s2.setDescription("Obviously fine craftmenship, these light leather sandals have tiny woodland drawings in them.");
+			s2.text();
+			outfitChoices.addElement(s2);
+
+			Armor p1=CMClass.getArmor("GenPants");
+			p1.setName("some delicate leggings");
+			p1.setDisplayText("a pair delicate brown leggings sit here.");
+			p1.setDescription("Obviously fine craftmenship, with sharp folds and intricate designs.  They look perfect for dancing in!");
+			p1.text();
+			outfitChoices.addElement(p1);
+		}
+		return outfitChoices;
+	}
+	public Weapon myNaturalWeapon()
+	{ return funHumanoidWeapon();	}
+
+	public String healthText(MOB mob)
+	{
+		double pct=(CMath.div(mob.curState().getHitPoints(),mob.maxState().getHitPoints()));
+
+		if(pct<.10)
+			return "^r" + mob.name() + "^r is facing mortality!^N";
+		else
+		if(pct<.20)
+			return "^r" + mob.name() + "^r is covered in blood.^N";
+		else
+		if(pct<.30)
+			return "^r" + mob.name() + "^r is bleeding badly from lots of wounds.^N";
+		else
+		if(pct<.40)
+			return "^y" + mob.name() + "^y has numerous bloody wounds and gashes.^N";
+		else
+		if(pct<.50)
+			return "^y" + mob.name() + "^y has some bloody wounds and gashes.^N";
+		else
+		if(pct<.60)
+			return "^p" + mob.name() + "^p has a few bloody wounds.^N";
+		else
+		if(pct<.70)
+			return "^p" + mob.name() + "^p is cut and bruised.^N";
+		else
+		if(pct<.80)
+			return "^g" + mob.name() + "^g has some minor cuts and bruises.^N";
+		else
+		if(pct<.90)
+			return "^g" + mob.name() + "^g has a few bruises and scratches.^N";
+		else
+		if(pct<.99)
+			return "^g" + mob.name() + "^g has a few small bruises.^N";
+		else
+			return "^c" + mob.name() + "^c is in perfect health.^N";
+	}
+	public Vector myResources()
+	{
+		synchronized(resources)
+		{
+			if(resources.size()==0)
+			{
+				resources.addElement(makeResource
+				("a pair of "+name().toLowerCase()+" ears",RawMaterial.RESOURCE_MEAT));
+				resources.addElement(makeResource
+				("some "+name().toLowerCase()+" blood",RawMaterial.RESOURCE_BLOOD));
+				resources.addElement(makeResource
+				("a pile of "+name().toLowerCase()+" bones",RawMaterial.RESOURCE_BONE));
+			}
+		}
+		return resources;
+	}
+}
