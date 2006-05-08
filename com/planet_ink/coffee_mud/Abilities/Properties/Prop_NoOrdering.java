469b04e241a3ea3e729edbc142cc9465ee6ae93f
bozimmerman <bo@zimmers.net>
2006-05-08 15:25:24 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@5516 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Abilities/Properties/Prop_NoOrdering.java
@@ -0,0 +1,59 @@
+package com.planet_ink.coffee_mud.Abilities.Properties;
+import com.planet_ink.coffee_mud.core.interfaces.*;
+import com.planet_ink.coffee_mud.core.*;
+import com.planet_ink.coffee_mud.Abilities.interfaces.*;
+import com.planet_ink.coffee_mud.Areas.interfaces.*;
+import com.planet_ink.coffee_mud.Behaviors.interfaces.*;
+import com.planet_ink.coffee_mud.CharClasses.interfaces.*;
+import com.planet_ink.coffee_mud.Commands.interfaces.*;
+import com.planet_ink.coffee_mud.Common.interfaces.*;
+import com.planet_ink.coffee_mud.Exits.interfaces.*;
+import com.planet_ink.coffee_mud.Items.interfaces.*;
+import com.planet_ink.coffee_mud.Locales.interfaces.*;
+import com.planet_ink.coffee_mud.MOBS.interfaces.*;
+import com.planet_ink.coffee_mud.Races.interfaces.*;
+
+
+/* 
+   Copyright 2000-2006 Bo Zimmerman
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+public class Prop_NoOrdering extends Property
+{
+	public String ID() { return "Prop_NoOrdering"; }
+	public String name(){ return "Group/Ordering Neutralizing";}
+	protected int canAffectCode(){return Ability.CAN_ROOMS|Ability.CAN_AREAS|Ability.CAN_MOBS;}
+
+	public boolean okMessage(Environmental myHost, CMMsg msg)
+	{
+		if(!super.okMessage(myHost,msg))
+			return false;
+
+		if((msg.targetMinor()==CMMsg.TYP_ORDER)
+		&&(msg.source().location()!=null)
+		&&(msg.target() instanceof MOB)
+		&&((msg.source().location()==affected)
+		   ||((affected instanceof Area)&&(((Area)affected).inMetroArea(msg.source().location().getArea())))
+		   ||(msg.target()==affected))
+		&&(!CMSecurity.isAllowed(msg.source(),msg.source().location(),"CMDMOBS")))
+		{
+			if(affected instanceof MOB)
+				msg.source().tell("You don't feel very commanding around here.");
+			else
+				msg.source().tell(msg.source(),msg.target(),null,"<T-NAME> isn't paying any attention to you.");
+			return false;
+		}
+		return true;
+	}
+}
