0e386ceecc01f44668b4a58e2b7b377632a04ddb
bozimmerman <bo@zimmers.net>
2006-05-09 03:16:06 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@5519 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Common/DefaultClan.java
+++ com/planet_ink/coffee_mud/Common/DefaultClan.java
@@ -14,7 +14,6 @@ import com.planet_ink.coffee_mud.Libraries.interfaces.XMLLibrary;
 import com.planet_ink.coffee_mud.Locales.interfaces.*;
 import com.planet_ink.coffee_mud.MOBS.interfaces.*;
 import com.planet_ink.coffee_mud.Races.interfaces.*;
-import com.sun.rsasign.v;
 
 import java.util.*;
 
