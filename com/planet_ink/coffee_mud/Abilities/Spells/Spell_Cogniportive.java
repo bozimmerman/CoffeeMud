2aaec3ebe6c7c3ab7dfc31f962cb336594c76a35
bozimmerman <bo@zimmers.net>
2006-07-04 16:00:38 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@5654 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Abilities/Spells/Spell_Cogniportive.java
+++ com/planet_ink/coffee_mud/Abilities/Spells/Spell_Cogniportive.java
@@ -191,8 +191,9 @@ public class Spell_Cogniportive extends Spell
 						&&(((String)V.firstElement()).equalsIgnoreCase("HOME")))
 						{
 							String str=CMParms.combine(V,1);
-							if(CMLib.english().containsString(affected.name(),str)
-							||CMLib.english().containsString(affected.displayText(),str))
+							if((str.length()>0)
+							&&((CMLib.english().containsString(affected.name(),str)
+									||CMLib.english().containsString(affected.displayText(),str))))
 								msg.addTrailerMsg(CMClass.getMsg(msg.source(),affected,msg.target(),CMMsg.NO_EFFECT,null,CMMsg.MASK_ALWAYS|CMMsg.TYP_WAND_USE,msg.sourceMessage(),CMMsg.NO_EFFECT,null));
 						}
 					}
