bcb48680226559bba09ca14fba4b68a58906251a
bozimmerman <bo@zimmers.net>
2006-07-20 01:14:53 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@5721 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Common/interfaces/Quest.java
+++ com/planet_ink/coffee_mud/Common/interfaces/Quest.java
@@ -126,7 +126,11 @@ public interface Quest extends Tickable
 	public final static String[] QOBJS={"LOADEDMOBS", "LOADEDITEMS", "AREA", "ROOM", "MOBGROUP", "ITEMGROUP", "ROOMGROUP",
 		 								"ITEM", "ENVOBJ", "STUFF"};
 	//TODO: add the following:
-	//LOAD=alibies/mob-item.aliby {AGENT} {ACTION}
+	// LOAD ALIBIES=/resources/quests/alibies/mine.alibies {ACTIONGROUP-ACTION} {TOOLGROUP-TOOL}
+	// (above file has [ALIBY] cmd cmd [ALIBY] followed by quest script, etc..
+	// SET MOB AGENT
+	// SET MOBGROUP ACTIONGROUP
+	// etc..
 	// EVIDENCE, ALIBY sets need to figure in, and be able to load from lists dependent on ACTION and/or FACTION
 	// some way to add behaviors/props to implement random alibies... thats the last thing I think. -- yes, it is!
 	public static final String[] MYSTERY_QCODES={"FACTION","FACTIONGROUP",
