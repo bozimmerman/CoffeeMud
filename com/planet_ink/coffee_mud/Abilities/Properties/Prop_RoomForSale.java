5130987070b8d22bcd22921babdd733ab543b438
bozimmerman <bo@zimmers.net>
2006-08-24 19:06:07 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@5774 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Abilities/Properties/Prop_RoomForSale.java
+++ com/planet_ink/coffee_mud/Abilities/Properties/Prop_RoomForSale.java
@@ -406,7 +406,7 @@ public class Prop_RoomForSale extends Property implements LandTitle
 				Item I=R.fetchItem(i);
 				if((I.expirationDate()!=0)
 	            &&((I.savable())||(I.Name().equalsIgnoreCase("id")))
-				&&(!(I instanceof DeadBody)))
+				&&((!(I instanceof DeadBody))||(((DeadBody)I).playerCorpse())))
 				{
 					I.setExpirationDate(0);
 					updateItems=true;
