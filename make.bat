6dc8310a5266817ae1ad2b38252f635a5e358721
bozimmerman <bo@zimmers.net>
2006-09-27 00:19:52 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@5936 0d6f1817-ed0e-0410-87c9-987e46238f29

--- make.bat
+++ make.bat
@@ -1,4 +1,4 @@
-SET Java_Home=C:\java\j2sdk1.4.2_10
+REM SET Java_Home=C:\java\j2sdk1.4.2_10
 set CLASSPATH=.;%Java_Home%\lib\dt.jar;%Java_Home%\lib\tools.jar;.\lib\js.jar;.\lib\jzlib.jar
 SET JAVACPATH=%Java_Home%\bin\javac -g -nowarn -deprecation
 
