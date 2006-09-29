21fadb3ac31113be4de04b17b52dfd043890005d
bozimmerman <bo@zimmers.net>
2006-09-29 01:33:34 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@5946 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Items/Basic/GenFatWallpaper.java
@@ -0,0 +1,98 @@
+package com.planet_ink.coffee_mud.Items.Basic;
+import com.planet_ink.coffee_mud.core.interfaces.*;
+import com.planet_ink.coffee_mud.core.*;
+import com.planet_ink.coffee_mud.Abilities.interfaces.*;
+import com.planet_ink.coffee_mud.Areas.interfaces.*;
+import com.planet_ink.coffee_mud.Behaviors.interfaces.*;
+import com.planet_ink.coffee_mud.CharClasses.interfaces.*;
+import com.planet_ink.coffee_mud.Commands.interfaces.*;
+import com.planet_ink.coffee_mud.Common.interfaces.*;
+import com.planet_ink.coffee_mud.Exits.interfaces.*;
+import com.planet_ink.coffee_mud.Items.interfaces.*;
+import com.planet_ink.coffee_mud.Locales.interfaces.*;
+import com.planet_ink.coffee_mud.MOBS.interfaces.*;
+import com.planet_ink.coffee_mud.Races.interfaces.*;
+
+import java.util.*;
+
+
+/* 
+   Copyright 2000-2006 Bo Zimmerman
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+public class GenFatWallpaper extends GenWallpaper
+{
+	public String ID(){	return "GenFatWallpaper";}
+	protected String	displayText="";
+	public String displayText(){ return displayText();}
+	public void setDisplayText(String newText){displayText=newText;}
+	protected long expirationDate=0;
+	public long expirationDate(){return expirationDate;}
+	public void setExpirationDate(long time){expirationDate=time;}
+	public boolean okMessage(Environmental myHost, CMMsg msg)
+	{
+		if(msg.amITarget(this)
+		&&((msg.targetMinor()==CMMsg.TYP_EXPIRE)||(msg.targetMinor()==CMMsg.TYP_DEATH)))
+			return true;
+		return super.okMessage(myHost,msg);
+	}
+	public void executeMsg(Environmental myHost, CMMsg msg)
+	{
+		if(msg.amITarget(this)
+		&&((msg.targetMinor()==CMMsg.TYP_EXPIRE)||(msg.targetMinor()==CMMsg.TYP_DEATH)))
+			destroy();
+		super.executeMsg(myHost,msg);
+	}
+	private static final String[] CODES={"DISPLAY"};
+	public String[] getStatCodes(){
+		String[] THINCODES=super.getStatCodes();
+		String[] codes=new String[THINCODES.length+1];
+		for(int c=0;c<THINCODES.length;c++)
+			codes[c]=THINCODES[c];
+		codes[THINCODES.length]="DISPLAY";
+		return codes;
+	}
+	protected int getCodeNum(String code){
+		for(int i=0;i<CODES.length;i++)
+			if(code.equalsIgnoreCase(CODES[i])) return i;
+		return -1;
+	}
+	public String getStat(String code){
+		if(super.getCodeNum(code)>=0) return super.getStat(code);
+		switch(getCodeNum(code))
+		{
+		case 0: return displayText();
+		}
+		return "";
+	}
+	public void setStat(String code, String val)
+	{
+		if(super.getCodeNum(code)>=0)
+			super.setStat(code,val);
+		else
+		switch(getCodeNum(code))
+		{
+		case 0: setDisplayText(val); break;
+		}
+	}
+	public boolean sameAs(Environmental E)
+	{
+		if(!(E instanceof GenFatWallpaper)) return false;
+		if(!super.sameAs(E)) return false;
+		for(int i=0;i<CODES.length;i++)
+			if(!E.getStat(CODES[i]).equals(getStat(CODES[i])))
+				return false;
+		return true;
+	}
+}
