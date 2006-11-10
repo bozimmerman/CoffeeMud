e7a453216c56a16fb1aa40dfa2e2ef921c4f15f0
bozimmerman <bo@zimmers.net>
2006-11-10 19:41:20 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@6137 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/WebMacros/CheckAuthCode.java
+++ com/planet_ink/coffee_mud/WebMacros/CheckAuthCode.java
@@ -34,42 +34,55 @@ public class CheckAuthCode extends StdWebMacro
 {
 	public String name()	{return this.getClass().getName().substring(this.getClass().getName().lastIndexOf('.')+1);}
 
+	public Hashtable getAuths(ExternalHTTPRequests httpReq)
+	{
+		String login=Authenticate.getLogin(httpReq);
+		if(!Authenticate.authenticated(httpReq,login,Authenticate.getPassword(httpReq)))
+			return null;
+		MOB mob=CMLib.map().getLoadPlayer(login);
+		if(mob==null) return null;
+		Hashtable auths=(Hashtable)httpReq.getRequestObjects().get("AUTHS_"+login.toUpperCase().trim());
+		if(auths==null)
+		{
+			auths=new Hashtable();
+			boolean subOp=false;
+			boolean sysop=CMSecurity.isASysOp(mob);
+			
+			String AREA=httpReq.getRequestParameter("AREA");
+			Room R=null;
+			for(Enumeration a=CMLib.map().areas();a.hasMoreElements();)
+			{
+				Area A=(Area)a.nextElement();
+				if((AREA==null)||(AREA.length()==0)||(AREA.equals(A.Name())))
+					if(A.amISubOp(mob.Name()))
+					{ 
+						if(R==null) R=A.getRandomProperRoom();
+						subOp=true; 
+						break;
+					}
+			}
+			auths.put("ANYMODAREAS",""+((subOp&&(CMSecurity.isAllowedAnywhere(mob,"CMDROOMS")||CMSecurity.isAllowedAnywhere(mob,"CMDAREAS")))
+														   ||CMSecurity.isAllowedEverywhere(mob,"CMDROOMS")||CMSecurity.isAllowedEverywhere(mob,"CMDAREAS")));
+			auths.put("ALLMODAREAS",""+(CMSecurity.isAllowedEverywhere(mob,"CMDROOMS")||CMSecurity.isAllowedEverywhere(mob,"CMDAREAS")));
+	        auths.put("ANYFILEBROWSE",""+CMSecurity.hasAccessibleDir(mob,mob.location()));
+			auths.put("SYSOP",""+sysop);
+			auths.put("SUBOP",""+(sysop||subOp));
+	        
+			Vector V=CMSecurity.getSecurityCodes(mob,R);
+			for(int v=0;v<V.size();v++)
+				auths.put("AUTH_"+((String)V.elementAt(v)),"true");
+			httpReq.getRequestObjects().put("AUTHS_"+login.toUpperCase().trim(),auths);
+		}
+		return auths;
+	}
+	
 	public String runMacro(ExternalHTTPRequests httpReq, String parm)
 	{
 		Hashtable parms=parseParms(parm);
-		Hashtable auths=new Hashtable();
-		String login=Authenticate.getLogin(httpReq);
-		if(!Authenticate.authenticated(httpReq,login,Authenticate.getPassword(httpReq)))
-			return "false";
-		MOB mob=CMLib.map().getLoadPlayer(login);
-		if(mob==null) return "false";
-		boolean subOp=false;
-		boolean sysop=CMSecurity.isASysOp(mob);
-		
-		String AREA=httpReq.getRequestParameter("AREA");
-		Room R=null;
-		for(Enumeration a=CMLib.map().areas();a.hasMoreElements();)
-		{
-			Area A=(Area)a.nextElement();
-			if((AREA==null)||(AREA.length()==0)||(AREA.equals(A.Name())))
-				if(A.amISubOp(mob.Name()))
-				{ 
-					if(R==null) R=A.getRandomProperRoom();
-					subOp=true; 
-					break;
-				}
-		}
-		auths.put("ANYMODAREAS",""+((subOp&&(CMSecurity.isAllowedAnywhere(mob,"CMDROOMS")||CMSecurity.isAllowedAnywhere(mob,"CMDAREAS")))
-													   ||CMSecurity.isAllowedEverywhere(mob,"CMDROOMS")||CMSecurity.isAllowedEverywhere(mob,"CMDAREAS")));
-		auths.put("ALLMODAREAS",""+(CMSecurity.isAllowedEverywhere(mob,"CMDROOMS")||CMSecurity.isAllowedEverywhere(mob,"CMDAREAS")));
-        auths.put("ANYFILEBROWSE",""+CMSecurity.hasAccessibleDir(mob,mob.location()));
-		auths.put("SYSOP",""+sysop);
-		auths.put("SUBOP",""+(sysop||subOp));
-        
-		Vector V=CMSecurity.getSecurityCodes(mob,R);
-		for(int v=0;v<V.size();v++)
-			auths.put("AUTH_"+((String)V.elementAt(v)),"true");
 		boolean finalCondition=false;
+		Hashtable auths=getAuths(httpReq);
+		if(auths==null) return "false";
+		boolean sysop=((String)auths.get("SYSOP")).equalsIgnoreCase("true");
 		for(Enumeration e=parms.keys();e.hasMoreElements();)
 		{
 			String key=(String)e.nextElement();
@@ -77,13 +90,12 @@ public class CheckAuthCode extends StdWebMacro
 			boolean not=false;
 			boolean thisCondition=true;
 			if(key.startsWith("||")) key=key.substring(2);
-			
 			if(key.startsWith("!"))
 			{
 				key=key.substring(1);
 				not=true;
 			}
-			String check=(String)auths.get(key);
+			String check=sysop?"true":(String)auths.get(key);
 			if(not)
 			{
 				if((check==null)&&(equals.length()==0))
@@ -92,7 +104,7 @@ public class CheckAuthCode extends StdWebMacro
 				if(check==null) 
 					thisCondition=true;
 				else
-				if((!check.equalsIgnoreCase(equals))&&(!sysop))
+				if(!check.equalsIgnoreCase(equals))
 					thisCondition=true;
 				else
 					thisCondition=false;
@@ -105,7 +117,7 @@ public class CheckAuthCode extends StdWebMacro
 				if(check==null) 
 					thisCondition=false;
 				else
-				if((!check.equalsIgnoreCase(equals))&&(!sysop))
+				if(!check.equalsIgnoreCase(equals))
 					thisCondition=false;
 				else
 					thisCondition=true;
