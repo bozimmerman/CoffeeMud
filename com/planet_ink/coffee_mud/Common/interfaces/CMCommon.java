e209235ff9db8a53115aaf8a5914c0b4ef19888c
bozimmerman <bo@zimmers.net>
2006-12-01 22:44:01 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@6260 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Common/interfaces/CMCommon.java
@@ -0,0 +1,5 @@
+package com.planet_ink.coffee_mud.Common.interfaces;
+import com.planet_ink.coffee_mud.core.interfaces.*;
+public interface CMCommon extends CMObject {
+
+}
---
+++ com/planet_ink/coffee_mud/Libraries/interfaces/CMLibrary.java
@@ -0,0 +1,6 @@
+package com.planet_ink.coffee_mud.Libraries.interfaces;
+import com.planet_ink.coffee_mud.core.interfaces.*;
+public interface CMLibrary extends CMObject
+{
+
+}
