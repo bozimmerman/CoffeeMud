810e40e48c158aa31b03d226c4328c182eee37f9
bozimmerman <bo@zimmers.net>
2007-09-11 19:52:45 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@6807 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Behaviors/interfaces/Behavior.java
+++ com/planet_ink/coffee_mud/Behaviors/interfaces/Behavior.java
@@ -15,7 +15,7 @@ import com.planet_ink.coffee_mud.Races.interfaces.*;
 import java.util.Vector;
 
 
-/* 
+/*
    Copyright 2000-2007 Bo Zimmerman
 
    Licensed under the Apache License, Version 2.0 (the "License");
@@ -52,34 +52,34 @@ public interface Behavior extends Tickable, MsgListener, CMObject
 	 * @param forMe the object to which this behavior has been added
 	 */
 	public void startBehavior(Environmental forMe);
-	
+
 	/**
 	 * Called after a behavior is added to an Environmental object.
 	 * The point is to register a quest objet, should it be needed or
 	 * be useful to the quest.
 	 * @see com.planet_ink.coffee_mud.common.interfaces.Quest
-	 * @param Quest Q the Quest object
+	 * @param Q the Quest object
 	 */
     public void registerDefaultQuest(Quest Q);
-    
+
 	/**
-	 * Returns the raw parameter string for this behavior.  
+	 * Returns the raw parameter string for this behavior.
 	 * Parameters are meant to modify or specify specific behavior of this
 	 * Behavior.
 	 * @see Behavior#setParms(String)
 	 * @return the parameter string for this behavior
 	 */
 	public String getParms();
-	
+
 	/**
-	 * Sets the raw parameter string for this behavior.  
+	 * Sets the raw parameter string for this behavior.
 	 * Parameters are meant to modify or specify specific behavior of this
 	 * Behavior.
 	 * @see Behavior#getParms()
 	 * @param parameters the parameter string for this behavior
 	 */
 	public void setParms(String parameters);
-	
+
 	/**
 	 * Unimplemented as of yet, but will hold a string telling the system what
 	 * the proper format of any parms data.  Will use the CMParms.MTFORMAT_*
@@ -90,25 +90,25 @@ public interface Behavior extends Tickable, MsgListener, CMObject
 	 * @return the format expected for the parms field
 	 */
 	public String parmsFormat();
-	
+
 	/**
-	 * Returns a string list of any external files which 
+	 * Returns a string list of any external files which
 	 * may be required to make this ability work.  Usually
 	 * derived from the parameters.
-	 * 
+	 *
 	 * Files returned by this method should not be base distrib files!
 	 * @see Behavior#setParms(String)
 	 * @return a list of the path/names of files used by this behavior
 	 */
 	public Vector externalFiles();
-	
+
 	/**
 	 * Sets whether this behavior can be saved as a permanent aspect of
 	 * its host.
 	 * @see Behavior#isSavable()
 	 * @param truefalse whether this behavior can be saved as part of its host.
 	 */
-	
+
     public void setSavable(boolean truefalse);
 	/**
 	 * Returns whether this behavior can be saved as a permanent aspect of
@@ -117,20 +117,20 @@ public interface Behavior extends Tickable, MsgListener, CMObject
 	 * @return truefalse whether this behavior can be saved as part of its host.
 	 */
     public boolean isSavable();
-    
+
     /**
      * Returns whether this behavior is capable of enhancing the given type
-     * of object designated by E.  It derives from the protected 
-     * Behavior.canImproveCode() method.  
+     * of object designated by E.  It derives from the protected
+     * Behavior.canImproveCode() method.
      * @see com.planet_ink.coffee_mud.Behaviors.StdBehavior#canImproveCode()
      * @param E the object to evaluate for this behavior
      * @return whether the given object can be enhanced by this behavior
      */
 	public boolean canImprove(Environmental E);
-	
+
     /**
      * Returns whether this behavior is capable of enhancing the given type
-     * of object designated by the can_code.  It derives from the protected 
+     * of object designated by the can_code.  It derives from the protected
      * Behavior.canImproveCode() method and uses the Behavior.CAN_* constants.
      * @see com.planet_ink.coffee_mud.Behaviors.StdBehavior#canImproveCode()
      * @see Behavior
@@ -138,15 +138,15 @@ public interface Behavior extends Tickable, MsgListener, CMObject
      * @return whether the given type can be enhanced by this behavior
      */
 	public boolean canImprove(int can_code);
-	
+
 	/**
-	 * Returns a bitmap made of up Behavior.FLAG_* constant masks which 
-	 * designates certain aspects about this behavior the rest of the 
+	 * Returns a bitmap made of up Behavior.FLAG_* constant masks which
+	 * designates certain aspects about this behavior the rest of the
 	 * system may need to know.
 	 * @return a bitmap made up of Behavior.FLAG_* constants
 	 */
 	public long flags();
-	
+
 	/**
 	 * Returns the result of a very specific test, namely whether this
 	 * behavior would be the direct cause of a malicious act against
@@ -155,8 +155,8 @@ public interface Behavior extends Tickable, MsgListener, CMObject
 	 * @return whether this behavior would harm the mob
 	 */
 	public boolean grantsAggressivenessTo(MOB M);
-	
-	/** 
+
+	/**
      * Returns an array of the string names of those fields which are modifiable on this object at run-time by
      * builders.
      * @see Behavior#getStat(String)
@@ -164,27 +164,27 @@ public interface Behavior extends Tickable, MsgListener, CMObject
      * @return list of the fields which may be set.
      */
 	public String[] getStatCodes();
-	
+
     /**
-     * An alternative means of retreiving the values of those fields on this object which are modifiable at 
-     * run-time by builders.  See getStatCodes() for possible values for the code passed to this method.  
+     * An alternative means of retreiving the values of those fields on this object which are modifiable at
+     * run-time by builders.  See getStatCodes() for possible values for the code passed to this method.
      * Values returned are always strings, even if the field itself is numeric or a list.
      * @see Behavior#getStatCodes()
      * @param code the name of the field to read.
      * @return the value of the field read
      */
 	public String getStat(String code);
-	
+
     /**
-     * An alternative means of setting the values of those fields on this object which are modifiable at 
-     * run-time by builders.  See getStatCodes() for possible values for the code passed to this method.  
+     * An alternative means of setting the values of those fields on this object which are modifiable at
+     * run-time by builders.  See getStatCodes() for possible values for the code passed to this method.
      * The value passed in is always a string, even if the field itself is numeric or a list.
      * @see Behavior#getStatCodes()
      * @param code the name of the field to set
      * @param val the value to set the field to
      */
 	public void setStat(String code, String val);
-	
+
     /**
      * Whether this object instance is functionally identical to the object passed in.  Works by repeatedly
      * calling getStat on both objects and comparing the values.
@@ -194,7 +194,7 @@ public interface Behavior extends Tickable, MsgListener, CMObject
      * @return whether this object is the same as the one passed in
      */
 	public boolean sameAs(Behavior B);
-	
+
 	/** constant mask for the canImprove() and canImproveCode() methods.  Means it can improve mobs @see Behavior#canImprove(Environmental) */
 	public static final int CAN_MOBS=1;
 	/** constant mask for the canImprove() and canImproveCode() methods.  Means it can improve items @see Behavior#canImprove(Environmental) */
@@ -205,7 +205,7 @@ public interface Behavior extends Tickable, MsgListener, CMObject
 	public static final int CAN_ROOMS=8;
 	/** constant mask for the canImprove() and canImproveCode() methods.  Means it can improve exits @see Behavior#canImprove(Environmental) */
 	public static final int CAN_EXITS=16;
-	
+
 	/** constant mask for the flags() method designating that this behavior makes the host move @see Behavior#flags() */
 	public static final long FLAG_MOBILITY=1;
 	/** constant mask for the flags() method designating that this behavior makes the host a trouble-maker @see Behavior#flags() */
