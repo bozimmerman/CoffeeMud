02953b46ead83c08cfafc74189ea60c4aa9d8d56
bozimmerman <bo@zimmers.net>
2008-07-11 00:09:03 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@7234 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/core/exceptions/ScriptParseException.java
@@ -0,0 +1,8 @@
+package com.planet_ink.coffee_mud.core.exceptions;
+
+public class ScriptParseException extends CoffeeMudException {
+static final long serialVersionUID=0;
+    public ScriptParseException(String s) {
+        super(s);
+    }
+}
