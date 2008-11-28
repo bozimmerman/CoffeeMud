ed2d0630b31389f37dc2b49058df7180377afb57
bozimmerman <bo@zimmers.net>
2008-11-28 07:01:38 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@7627 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Races/Blob.java
@@ -0,0 +1,60 @@
+package com.planet_ink.coffee_mud.Races;
+import com.planet_ink.coffee_mud.core.interfaces.*;
+import com.planet_ink.coffee_mud.core.*;
+import com.planet_ink.coffee_mud.Abilities.interfaces.*;
+import com.planet_ink.coffee_mud.Areas.interfaces.*;
+import com.planet_ink.coffee_mud.Behaviors.interfaces.*;
+import com.planet_ink.coffee_mud.CharClasses.interfaces.*;
+import com.planet_ink.coffee_mud.Commands.interfaces.*;
+import com.planet_ink.coffee_mud.Common.interfaces.*;
+import com.planet_ink.coffee_mud.Exits.interfaces.*;
+import com.planet_ink.coffee_mud.Items.interfaces.*;
+import com.planet_ink.coffee_mud.Locales.interfaces.*;
+import com.planet_ink.coffee_mud.MOBS.interfaces.*;
+import com.planet_ink.coffee_mud.Races.interfaces.*;
+
+
+import java.util.*;
+
+/* 
+   Copyright 2000-2008 Bo Zimmerman
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+@SuppressWarnings("unchecked")
+public class Blob extends Unique
+{
+	public String ID(){	return "Blob"; }
+	public String name(){ return "Blob"; }
+	public int shortestMale(){return 24;}
+	public int shortestFemale(){return 20;}
+	public int heightVariance(){return 12;}
+	public int lightestWeight(){return 200;}
+	public int weightVariance(){return 200;}
+	public long forbiddenWornBits(){return 0;}
+	public String racialCategory(){return "Unique";}
+	public boolean fertile(){return true;}
+
+	public Vector myResources()
+	{
+		synchronized(resources)
+		{
+			if(resources.size()==0)
+			{
+				resources.addElement(makeResource
+				("a palm-full of "+name().toLowerCase(),RawMaterial.RESOURCE_BLOOD));
+			}
+		}
+		return resources;
+	}
+}
