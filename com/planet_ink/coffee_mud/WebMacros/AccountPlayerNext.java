dd20481788009511963cf5f9e28b944af323627d
bozimmerman <bo@zimmers.net>
2010-02-19 03:41:59 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@8244 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/WebMacros/AccountPlayerNext.java
@@ -0,0 +1,76 @@
+package com.planet_ink.coffee_mud.WebMacros;
+import com.planet_ink.coffee_mud.core.interfaces.*;
+import com.planet_ink.coffee_mud.core.*;
+import com.planet_ink.coffee_mud.Abilities.interfaces.*;
+import com.planet_ink.coffee_mud.Areas.interfaces.*;
+import com.planet_ink.coffee_mud.Behaviors.interfaces.*;
+import com.planet_ink.coffee_mud.CharClasses.interfaces.*;
+import com.planet_ink.coffee_mud.Libraries.interfaces.*;
+import com.planet_ink.coffee_mud.Common.interfaces.*;
+import com.planet_ink.coffee_mud.Exits.interfaces.*;
+import com.planet_ink.coffee_mud.Items.interfaces.*;
+import com.planet_ink.coffee_mud.Locales.interfaces.*;
+import com.planet_ink.coffee_mud.MOBS.interfaces.*;
+import com.planet_ink.coffee_mud.Races.interfaces.*;
+import java.util.*;
+
+
+
+/* 
+   Copyright 2000-2010 Bo Zimmerman
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+@SuppressWarnings("unchecked")
+public class AccountPlayerNext extends StdWebMacro
+{
+	public String name(){return this.getClass().getName().substring(this.getClass().getName().lastIndexOf('.')+1);}
+
+	public String runMacro(ExternalHTTPRequests httpReq, String parm)
+	{
+		if(!CMProps.getBoolVar(CMProps.SYSTEMB_MUDSTARTED))
+			return CMProps.getVar(CMProps.SYSTEM_MUDSTATUS);
+
+		Hashtable parms=parseParms(parm);
+		String last=httpReq.getRequestParameter("PLAYER");
+		if(parms.containsKey("RESET"))
+		{	
+			if(last!=null) httpReq.removeRequestParameter("PLAYER");
+			return "";
+		}
+		String accountName=httpReq.getRequestParameter("ACCOUNT");
+		if(last==null) return "";
+		PlayerAccount account=CMLib.players().getLoadAccount(accountName);
+		if(account==null) return "";
+		
+		String lastID="";
+		String sort=httpReq.getRequestParameter("SORTBY");
+		if(sort==null) sort="";
+		Enumeration pe=account.getThinPlayers();
+		for(;pe.hasMoreElements();)
+		{
+			PlayerLibrary.ThinPlayer user=(PlayerLibrary.ThinPlayer)pe.nextElement();
+			if((last==null)||((last.length()>0)&&(last.equals(lastID))&&(!user.name.equals(lastID))))
+			{
+				httpReq.addRequestParameters("PLAYER",user.name);
+				return "";
+			}
+			lastID=user.name;
+		}
+		httpReq.addRequestParameters("PLAYER","");
+		if(parms.containsKey("EMPTYOK"))
+			return "<!--EMPTY-->";
+		return " @break@";
+	}
+
+}
