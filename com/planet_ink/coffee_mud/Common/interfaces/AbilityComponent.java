40cb93cc2b9c0cf1f3ae8a1e730a7a941c62921d
bozimmerman <bo@zimmers.net>
2010-02-19 03:35:30 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@8243 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Common/interfaces/AbilityComponent.java
+++ com/planet_ink/coffee_mud/Common/interfaces/AbilityComponent.java
@@ -7,119 +7,170 @@ import com.planet_ink.coffee_mud.core.CMath;
 /**
  * Descriptor class for ability components.
  * These are managed by by the Ability library.
+ * @see com.planet_ink.coffee_mud.Libraries.interfaces.AbilityMapper#addAbilityComponent(String, java.util.Hashtable)
+ * @see com.planet_ink.coffee_mud.Libraries.interfaces.AbilityMapper#componentCheck(com.planet_ink.coffee_mud.MOBS.interfaces.MOB, Vector)
+ * @see com.planet_ink.coffee_mud.Libraries.interfaces.AbilityMapper#getAbilityComponentMap()
  * @author Bo Zimmerman
- *
  */
 public interface AbilityComponent extends CMCommon
 {
 	/**
-	 * 
-	 * @return
+	 * Returns an enum describing how this component "connects" with the
+	 * following component logically.  As in, is it required
+	 * WITH the following component, or instead of?
+	 * @see com.planet_ink.coffee_mud.Common.interfaces.AbilityComponent.CompConnector
+	 * @see com.planet_ink.coffee_mud.Common.interfaces.AbilityComponent#setConnector(CompConnector)
+	 * @return a connector enum
 	 */
 	public CompConnector getConnector();
 	
 	/**
-	 * 
-	 * @param connector
+	 * Sets an enum describing how this component "connects" with the
+	 * following component logically.  As in, is it required
+	 * WITH the following component, or instead of?
+	 * @see com.planet_ink.coffee_mud.Common.interfaces.AbilityComponent.CompConnector
+	 * @see com.planet_ink.coffee_mud.Common.interfaces.AbilityComponent#getConnector()
+	 * @param connector a connector enum
 	 */
 	public void setConnector(CompConnector connector);
 	
 	/**
-	 * 
-	 * @return
+	 * Returns an enum value describing where an item must be to
+	 * be considered a valid component.  
+	 * @see com.planet_ink.coffee_mud.Common.interfaces.AbilityComponent.CompLocation
+	 * @see com.planet_ink.coffee_mud.Common.interfaces.AbilityComponent#setLocation(CompLocation)
+	 * @return where an item must be to be a component
 	 */
 	public CompLocation getLocation();
 	
 	/**
-	 * 
-	 * @param location
+	 * Sets an enum value describing where an item must be to
+	 * be considered a valid component.  
+	 * @see com.planet_ink.coffee_mud.Common.interfaces.AbilityComponent.CompLocation
+	 * @see com.planet_ink.coffee_mud.Common.interfaces.AbilityComponent#getLocation()
+	 * @param location where an item must be to be a component
 	 */
 	public void setLocation(CompLocation location);
 	
 	/**
-	 * 
-	 * @return
+	 * Gets whether or not this component is consumed upon use
+	 * @see com.planet_ink.coffee_mud.Common.interfaces.AbilityComponent#setConsumed(boolean)
+	 * @return true if consumed, false otherwise
 	 */
 	public boolean isConsumed();
 	
 	/**
-	 * 
-	 * @param isConsumed
+	 * Sets whether or not this component is consumed upon use
+	 * @see com.planet_ink.coffee_mud.Common.interfaces.AbilityComponent#isConsumed()
+	 * @param isConsumed true if consumed, false otherwise
 	 */
 	public void setConsumed(boolean isConsumed);
 	
 	/**
-	 * 
-	 * @return
+	 * Returns the number of items matching this component which must be present.
+	 * @see com.planet_ink.coffee_mud.Common.interfaces.AbilityComponent#setAmount(int)
+	 * @return the number of items matching this component which must be present.
 	 */
 	public int getAmount();
 	
 	/**
-	 * 
-	 * @param amount
+	 * Sets the number of items matching this component which must be present.
+	 * @see com.planet_ink.coffee_mud.Common.interfaces.AbilityComponent#getAmount()
+	 * @param amount the number of items matching this component which must be present
 	 */
 	public void setAmount(int amount);
 	
 	/**
-	 * 
-	 * @return
+	 * Returns the item filter type for determining whether an item is
+	 * a component.  This type can designate a resource, material, or 
+	 * an item name string. 
+	 * @see com.planet_ink.coffee_mud.Common.interfaces.AbilityComponent.CompType
+	 * @see com.planet_ink.coffee_mud.Common.interfaces.AbilityComponent#setType(CompType, Object)
+	 * @return a CompType enum
 	 */
 	public CompType getType();
 	
 	/**
-	 * 
-	 * @param type
-	 * @param typeObj
+	 * Sets the item filter type for determining whether an item is
+	 * a component.  This type can designate a resource, material, or 
+	 * an item name string.  Also sent is either the resource mask,
+	 * material mask, a string for a name filter, or a string with a
+	 * long number in it. 
+	 * @see com.planet_ink.coffee_mud.Common.interfaces.AbilityComponent.CompType
+	 * @see com.planet_ink.coffee_mud.Common.interfaces.AbilityComponent#getType()
+	 * @see com.planet_ink.coffee_mud.Common.interfaces.AbilityComponent#getLongType()
+	 * @see com.planet_ink.coffee_mud.Common.interfaces.AbilityComponent#getStringType()
+	 * @param type the CompType enum
+	 * @param typeObj either a Integer object or a String
 	 */
 	public void setType(CompType type, Object typeObj);
 	
 	/**
-	 * 
-	 * @return
+	 * For resource and material type item component filters, this will
+	 * return the type comparison object as a long value.  This is typically
+	 * a resource or material mask value.
+	 * @see com.planet_ink.coffee_mud.Common.interfaces.AbilityComponent#setType(CompType, Object)
+	 * @return a resource or material mask value
 	 */
 	public long getLongType();
 	
 	/**
-	 * 
-	 * @return
+	 * For resource and material type item component filters, this will
+	 * return the type comparison object as a String value.  This is typically
+	 * a item name filter.
+	 * @see com.planet_ink.coffee_mud.Common.interfaces.AbilityComponent#setType(CompType, Object)
+	 * @return a item name filter
 	 */
 	public String getStringType();
 	
 	/**
-	 * 
-	 * @return
+	 * Returns the compiled zapper mask to determine whether a given agent 
+	 * qualifies this item as a component.
+	 * @see com.planet_ink.coffee_mud.Common.interfaces.AbilityComponent#getMaskStr()
+	 * @see com.planet_ink.coffee_mud.Common.interfaces.AbilityComponent#setMask(String)
+	 * @see com.planet_ink.coffee_mud.Libraries.interfaces.MaskingLibrary
+	 * @return the compiled zapper mask
 	 */
 	@SuppressWarnings("unchecked")
 	public Vector getCompiledMask();
 
 	/**
-	 * 
-	 * @return
+	 * Returns the raw zapper mask to determine whether a given agent 
+	 * qualifies this item as a component.
+	 * @see com.planet_ink.coffee_mud.Common.interfaces.AbilityComponent#getCompiledMask()
+	 * @see com.planet_ink.coffee_mud.Common.interfaces.AbilityComponent#setMask(String)
+	 * @see com.planet_ink.coffee_mud.Libraries.interfaces.MaskingLibrary
+	 * @return the raw zapper mask
 	 */
 	public String getMaskStr();
 
 	/**
-	 * 
-	 * @param maskStr
+	 * Sets the raw zapper mask to determine whether a given agent 
+	 * qualifies this item as a component.
+	 * @see com.planet_ink.coffee_mud.Common.interfaces.AbilityComponent#getCompiledMask()
+	 * @see com.planet_ink.coffee_mud.Common.interfaces.AbilityComponent#getMaskStr()
+	 * @see com.planet_ink.coffee_mud.Libraries.interfaces.MaskingLibrary
+	 * @param maskStr the raw zapper mask
 	 */
 	public void setMask(String maskStr); 
 	
 	/**
-	 * 
+	 * Connector descriptors for connecting component descriptions together
 	 * @author Bo Zimmerman
 	 *
 	 */
 	public enum CompConnector { AND, OR };
 	
 	/**
-	 * 
+	 * Where worn locations for determining where a component must be
 	 * @author Bo Zimmerman
 	 *
 	 */
 	public enum CompLocation { INVENTORY, HELD, WORN };
 	
 	/**
-	 * 
+	 * An component type item filter for determining 
+	 * how to interpret the kind of item to compare
 	 * @author Bo Zimmerman
 	 *
 	 */
