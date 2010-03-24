c7d1e78460d14c9644b1ba90dfecfec85f5220aa
bozimmerman <bo@zimmers.net>
2010-03-24 02:03:44 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@8299 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/WebMacros/ForumInfo.java
@@ -0,0 +1,76 @@
+package com.planet_ink.coffee_mud.WebMacros;
+import com.planet_ink.coffee_mud.core.interfaces.*;
+import com.planet_ink.coffee_mud.core.*;
+import com.planet_ink.coffee_mud.Abilities.interfaces.*;
+import com.planet_ink.coffee_mud.Areas.interfaces.*;
+import com.planet_ink.coffee_mud.Behaviors.interfaces.*;
+import com.planet_ink.coffee_mud.CharClasses.interfaces.*;
+import com.planet_ink.coffee_mud.Libraries.interfaces.*;
+import com.planet_ink.coffee_mud.Common.interfaces.*;
+import com.planet_ink.coffee_mud.Exits.interfaces.*;
+import com.planet_ink.coffee_mud.Items.interfaces.*;
+import com.planet_ink.coffee_mud.Locales.interfaces.*;
+import com.planet_ink.coffee_mud.MOBS.interfaces.*;
+import com.planet_ink.coffee_mud.Races.interfaces.*;
+import java.util.*;
+
+
+
+/* 
+   Copyright 2000-2010 Bo Zimmerman
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+@SuppressWarnings("unchecked")
+public class ForumInfo extends StdWebMacro
+{
+	public String name()	{return this.getClass().getName().substring(this.getClass().getName().lastIndexOf('.')+1);}
+
+	public String runMacro(ExternalHTTPRequests httpReq, String parm)
+	{
+		Hashtable parms=parseParms(parm);
+		String last=httpReq.getRequestParameter("JOURNAL");
+		if(last==null) return " @break@";
+		
+		MOB M = Authenticate.getAuthenticatedMob(httpReq);
+		if((CMLib.journals().isArchonJournalName(last))&&((M==null)||(!CMSecurity.isASysOp(M))))
+		    return " @break@";
+		JournalsLibrary.ForumJournal journal = CMLib.journals().getForumJournal(last);
+		if(journal == null) return " @break@";
+		
+		JournalsLibrary.JournalSummaryStats stats = CMLib.journals().getJournalStats(last);
+		if(journal == null) return " @break@";
+		
+		if(parms.containsKey("POSTS"))
+			return ""+stats.posts;
+		
+		if(parms.containsKey("THREADS"))
+			return ""+stats.threads;
+		
+		if(parms.containsKey("SHORTDESC"))
+			return ""+stats.shortIntro;
+		
+		if(parms.containsKey("LONGDESC"))
+			return ""+stats.longIntro;
+		
+		if(parms.containsKey("IMAGEPATH"))
+		{
+			if((stats.imagePath==null)
+			||(stats.imagePath.trim().length()==0))
+				return "images/lilcm.jpg";
+			return ""+stats.threads;
+		}
+		
+		return "";
+	}
+}
