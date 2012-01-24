35278865c1317f7bc805af4e3a9c5f4d690d563a
bozimmerman <bo@zimmers.net>
2012-01-24 09:09:16 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@9311 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/application/MUD.java
+++ com/planet_ink/coffee_mud/application/MUD.java
@@ -39,7 +39,7 @@ import java.sql.*;
 
 
 /*
-   Copyright 2000-2011 Bo Zimmerman
+   Copyright 2000-2012 Bo Zimmerman
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -1334,7 +1334,7 @@ public class MUD extends Thread implements MudHost
 		{
 			System.out.println();
 			Log.sysOut(Thread.currentThread().getName(),"CoffeeMud v"+HOST_VERSION_MAJOR + "." + HOST_VERSION_MINOR);
-			Log.sysOut(Thread.currentThread().getName(),"(C) 2000-2011 Bo Zimmerman");
+			Log.sysOut(Thread.currentThread().getName(),"(C) 2000-2012 Bo Zimmerman");
 			Log.sysOut(Thread.currentThread().getName(),"http://www.coffeemud.org");
 			HostGroup joinable=null;
             CMLib.hosts().clear();
