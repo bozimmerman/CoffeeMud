e66f97a13d9f3495df125642a28225aa7f6251fc
bozimmerman <bo@zimmers.net>
2012-01-30 08:58:08 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@9315 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/core/collections/FilteredListIterator.java
@@ -0,0 +1,129 @@
+package com.planet_ink.coffee_mud.core.collections;
+import java.util.Iterator;
+import java.util.ListIterator;
+import java.util.NoSuchElementException;
+import java.util.Vector;
+/*
+Copyright 2000-2012 Bo Zimmerman
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+public class FilteredListIterator<K> implements ListIterator<K>
+{
+	private final ListIterator<K>  iter;
+	private Filterer<K> 	filterer;
+	private K 				nextElement = null;
+	private K 				prevElement = null;
+	private boolean 		initialized = false;
+
+	public FilteredListIterator(ListIterator<K> eset, Filterer<K> fil) 
+    {
+		iter=eset;
+		filterer=fil;
+    }
+    
+	public void setFilterer(Filterer<K> fil) 
+    {
+		filterer=fil;
+    }
+
+	private void stageNextElement()
+	{
+		prevElement=nextElement;
+		nextElement = null;
+		while((nextElement==null) && (iter.hasNext()))
+		{
+			nextElement = iter.next();
+			if(filterer.passesFilter(nextElement))
+				return;
+			nextElement = null;
+		}
+	}
+	
+	private void stagePrevElement()
+	{
+		nextElement=prevElement;
+		prevElement = null;
+		while((prevElement==null) && (iter.hasPrevious()))
+		{
+			prevElement = iter.previous();
+			if(filterer.passesFilter(prevElement))
+				return;
+			prevElement = null;
+		}
+	}
+	
+	private void initialize()
+	{
+		if(!initialized)
+		{
+			stageNextElement();
+			stagePrevElement();
+			initialized=true;
+		}
+	}
+	
+    public boolean hasNext() 
+    { 
+    	if(!initialized)
+    		initialize();
+    	return nextElement!=null;
+    }
+    
+    public K next() 
+    {
+    	if(!hasNext())
+    		throw new NoSuchElementException();
+    	K element = nextElement;
+    	stageNextElement();
+    	return element;
+    }
+
+	public void remove() {
+		throw new java.lang.IllegalArgumentException();
+	}
+
+	public void add(K e) {
+		throw new java.lang.IllegalArgumentException();
+	}
+
+	public boolean hasPrevious() {
+    	if(!initialized)
+    		initialize();
+    	return prevElement!=null;
+	}
+
+	@Override
+	public int nextIndex() {
+		throw new java.lang.IllegalArgumentException();
+	}
+
+	@Override
+	public K previous() {
+    	if(!hasPrevious())
+    		throw new NoSuchElementException();
+    	K element = prevElement;
+    	stagePrevElement();
+    	return element;
+	}
+
+	@Override
+	public int previousIndex() {
+		throw new java.lang.IllegalArgumentException();
+	}
+
+	@Override
+	public void set(K e) {
+		throw new java.lang.IllegalArgumentException();
+	}
+}
---
+++ com/planet_ink/coffee_mud/core/collections/ReadOnlyFilteredList.java
@@ -0,0 +1,165 @@
+package com.planet_ink.coffee_mud.core.collections;
+import java.util.*;
+
+/*
+Copyright 2000-2012 Bo Zimmerman
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+public class ReadOnlyFilteredList<K> implements List<K> 
+{
+	private List<K>     list;
+	private Filterer<K> filterer;
+	
+	public ReadOnlyFilteredList(List<K> l, Filterer<K> fill)
+	{
+		list=l;
+		filterer=fill;
+	}
+	
+	@Override
+	public boolean add(K arg0) {
+		throw new java.lang.IllegalArgumentException();
+	}
+
+	@Override
+	public void add(int arg0, K arg1) {
+		throw new java.lang.IllegalArgumentException();
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends K> arg0) {
+		throw new java.lang.IllegalArgumentException();
+	}
+
+	@Override
+	public boolean addAll(int arg0, Collection<? extends K> arg1) {
+		throw new java.lang.IllegalArgumentException();
+	}
+
+	@Override
+	public void clear() {
+		throw new java.lang.IllegalArgumentException();
+	}
+
+	@Override
+	public boolean contains(Object arg0) {
+		for(int x=0;x<size();x++)
+			if((arg0==null)?get(x)==null:arg0.equals(get(x)))
+				return true;
+		return list.contains(arg0);
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> arg0) {
+		for(Object o : arg0)
+			if(!contains(o))
+				return false;
+		return true;
+	}
+
+	@Override
+	public K get(int arg0) {
+		return list.get(arg0);
+	}
+
+	@Override
+	public int indexOf(Object arg0) {
+		for(int x=0;x<size();x++)
+			if((arg0==null)?get(x)==null:arg0.equals(get(x)))
+				return x;
+		return list.lastIndexOf(arg0);
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return list.isEmpty();
+	}
+
+	@Override
+	public Iterator<K> iterator() {
+		return new FilteredIterator<K>(list.iterator(),filterer);
+	}
+
+	@Override
+	public int lastIndexOf(Object arg0) 
+	{
+		for(int x=size()-1;x>=0;x--)
+			if((arg0==null)?get(x)==null:arg0.equals(get(x)))
+				return x;
+		return list.lastIndexOf(arg0);
+	}
+
+	@Override
+	public ListIterator<K> listIterator() {
+		return new FilteredListIterator<K>(list.listIterator(), filterer);
+	}
+
+	@Override
+	public ListIterator<K> listIterator(int arg0) {
+		return new FilteredListIterator<K>(list.listIterator(arg0), filterer);
+	}
+
+	@Override
+	public boolean remove(Object arg0) {
+		throw new java.lang.IllegalArgumentException();
+	}
+
+	@Override
+	public K remove(int arg0) {
+		throw new java.lang.IllegalArgumentException();
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> arg0) {
+		throw new java.lang.IllegalArgumentException();
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> arg0) {
+		throw new java.lang.IllegalArgumentException();
+	}
+
+	@Override
+	public K set(int arg0, K arg1) {
+		throw new java.lang.IllegalArgumentException();
+	}
+
+	@Override
+	public int size() {
+		return list.size();
+	}
+
+	@Override
+	public List<K> subList(int arg0, int arg1) {
+		return new ReadOnlyFilteredList<K>(list.subList(arg0,arg1),filterer);
+	}
+
+	@Override
+	public Object[] toArray() 
+	{
+		final List<K> set=new ArrayList<K>(size());
+		for(final Iterator<K> i=iterator();i.hasNext();)
+			set.add(i.next());
+		return set.toArray();
+	}
+
+	@Override
+	public <T> T[] toArray(T[] arg0) {
+		final List<K> set=new ArrayList<K>(size());
+		for(final Iterator<K> i=iterator();i.hasNext();)
+			set.add(i.next());
+		return set.toArray(arg0);
+	}
+
+}
