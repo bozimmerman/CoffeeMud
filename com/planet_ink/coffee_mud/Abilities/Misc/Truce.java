5dafb2bc999dc325e4e2349ded136a3b4f4c2f65
bozimmerman <bo@zimmers.net>
2012-04-04 03:18:08 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@9406 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Abilities/Misc/Truce.java
@@ -0,0 +1,111 @@
+package com.planet_ink.coffee_mud.Abilities.Misc;
+import com.planet_ink.coffee_mud.core.interfaces.*;
+import com.planet_ink.coffee_mud.core.*;
+import com.planet_ink.coffee_mud.core.collections.*;
+import com.planet_ink.coffee_mud.Abilities.StdAbility;
+import com.planet_ink.coffee_mud.Abilities.interfaces.*;
+import com.planet_ink.coffee_mud.Areas.interfaces.*;
+import com.planet_ink.coffee_mud.Behaviors.interfaces.*;
+import com.planet_ink.coffee_mud.CharClasses.interfaces.*;
+import com.planet_ink.coffee_mud.Commands.interfaces.*;
+import com.planet_ink.coffee_mud.Common.interfaces.*;
+import com.planet_ink.coffee_mud.Exits.interfaces.*;
+import com.planet_ink.coffee_mud.Items.interfaces.*;
+import com.planet_ink.coffee_mud.Locales.interfaces.*;
+import com.planet_ink.coffee_mud.MOBS.interfaces.*;
+import com.planet_ink.coffee_mud.Races.interfaces.*;
+
+
+import java.util.*;
+
+/* 
+   Copyright 2000-2012 Bo Zimmerman
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+@SuppressWarnings("unchecked")
+public class Truce extends StdAbility
+{
+	public String ID() { return "Truce"; }
+	public String name(){return "Truce";}
+	public String displayText(){ return "";}
+	protected int canAffectCode(){return CAN_MOBS;}
+	protected int canTargetCode(){return CAN_MOBS;}
+	public int abstractQuality(){return Ability.QUALITY_INDIFFERENT;}
+	public boolean putInCommandlist(){return false;}
+	private static final String[] triggerStrings = {"DECLARETRUCE"};
+	public String[] triggerStrings(){return triggerStrings;}
+	public int classificationCode(){return Ability.ACODE_SKILL;}
+
+	public boolean okMessage(final Environmental myHost, final CMMsg msg)
+	{
+		if((affected==null)||(!(affected instanceof MOB)))
+			return true;
+
+		MOB mob=(MOB)affected;
+		if((msg.targetMajor(CMMsg.MASK_MALICIOUS))
+		&&(((msg.source()==mob)&&(msg.target()!=null)&&(text().equals(msg.target().Name())))
+			||((msg.target()==mob)&&(text().equals(msg.source().Name()))))
+		&&(!msg.sourceMajor(CMMsg.MASK_ALWAYS)))
+		{
+			msg.source().tell(msg.source(),msg.target(),null,"You have made peace with <T-NAMESELF>.");
+			msg.source().makePeace();
+			if(msg.target() instanceof MOB)
+				((MOB)msg.target()).makePeace();
+			return false;
+		}
+		return super.okMessage(myHost,msg);
+	}
+
+	public boolean invoke(MOB mob, Vector commands, Physical givenTarget, boolean auto, int asLevel)
+	{
+		MOB target=this.getTarget(mob,commands,givenTarget);
+		if(target==null) return false;
+
+		// the invoke method for spells receives as
+		// parameters the invoker, and the REMAINING
+		// command line parameters, divided into words,
+		// and added as String objects to a vector.
+		if(!super.invoke(mob,commands,givenTarget,auto,asLevel))
+			return false;
+
+		boolean success=proficiencyCheck(mob,0,auto);
+		if(success)
+		{
+			// it worked, so build a copy of this ability,
+			// and add it to the affects list of the
+			// affected MOB.  Then tell everyone else
+			// what happened.
+			invoker=mob;
+			CMMsg msg=CMClass.getMsg(mob,target,this,verbalCastCode(mob,target,auto),auto?"":"^S<S-NAME> make(s) a truce with <T-NAMESELF>.^?");
+			if(mob.location().okMessage(mob,msg))
+			{
+				mob.location().send(mob,msg);
+				if(msg.value()<=0)
+				{
+					success=beneficialAffect(mob,target,asLevel,auto?3:0);
+					Ability A=target.fetchEffect(ID());
+					if(A!=null)A.setMiscText(target.Name());
+				}
+				target.makePeace();
+				if(mob.getVictim()==target) 
+					mob.makePeace();
+			}
+		}
+		else
+			return maliciousFizzle(mob,target,auto?"":"^S<S-NAME> tr(ys) to make <T-NAMESELF> fall asleep, but fails.^?");
+
+		// return whether it worked
+		return success;
+	}
+}
