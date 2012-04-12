14317b67427863672c8a73e6164644854247451e
bozimmerman <bo@zimmers.net>
2012-04-12 17:24:59 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@9452 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Abilities/Fighter/Fighter_MonkeyPunch.java
+++ com/planet_ink/coffee_mud/Abilities/Fighter/Fighter_MonkeyPunch.java
@@ -34,76 +34,76 @@ import java.util.*;
 
 public class Fighter_MonkeyPunch extends FighterSkill
 {
-	public String ID() { return "Fighter_MonkeyPunch"; }
-	public String name(){ return "Monkey Punch";}
-	public String displayText(){ return "";}
-	protected int canAffectCode(){return CAN_MOBS;}
-	protected int canTargetCode(){return 0;}
-	public int abstractQuality(){return Ability.QUALITY_MALICIOUS;}
+    public String ID() { return "Fighter_MonkeyPunch"; }
+    public String name(){ return "Monkey Punch";}
+    public String displayText(){ return "";}
+    protected int canAffectCode(){return CAN_MOBS;}
+    protected int canTargetCode(){return 0;}
+    public int abstractQuality(){return Ability.QUALITY_MALICIOUS;}
     public int classificationCode(){return Ability.ACODE_SKILL|Ability.DOMAIN_PUNCHING;}
-	public boolean isAutoInvoked(){return true;}
-	public boolean canBeUninvoked(){return false;}
-	protected Weapon naturalWeapon=null;
+    public boolean isAutoInvoked(){return true;}
+    public boolean canBeUninvoked(){return false;}
+    protected Weapon naturalWeapon=null;
 
-	public boolean tick(Tickable ticking, int tickID)
-	{
-		if(!super.tick(ticking,tickID))
-			return false;
-		if((tickID==Tickable.TICKID_MOB)
-		   &&(affected!=null)
-		   &&(affected instanceof MOB))
-		{
-			MOB mob=(MOB)affected;
-			if((mob.isInCombat())
-			&&(CMLib.flags().aliveAwakeMobileUnbound(mob,true))
-			&&(mob.rangeToTarget()==0)
-			&&(mob.charStats().getBodyPart(Race.BODY_HAND)>1)
-			&&(!anyWeapons(mob)))
-			{
-				if(CMLib.dice().rollPercentage()>95)
-					helpProficiency(mob);
-				if((naturalWeapon==null)
-				||(naturalWeapon.amDestroyed()))
-				{
-					naturalWeapon=CMClass.getWeapon("GenWeapon");
-					naturalWeapon.setName("a monkey punch");
-					naturalWeapon.setWeaponType(Weapon.TYPE_BASHING);
-	                naturalWeapon.basePhyStats().setDamage(5);
-					naturalWeapon.recoverPhyStats();
-				}
-				CMLib.combat().postAttack(mob,mob.getVictim(),naturalWeapon);
-			}
-		}
-		return true;
-	}
+    public boolean tick(Tickable ticking, int tickID)
+    {
+        if(!super.tick(ticking,tickID))
+            return false;
+        if((tickID==Tickable.TICKID_MOB)
+           &&(affected!=null)
+           &&(affected instanceof MOB))
+        {
+            MOB mob=(MOB)affected;
+            if((mob.isInCombat())
+            &&(CMLib.flags().aliveAwakeMobileUnbound(mob,true))
+            &&(mob.rangeToTarget()==0)
+            &&(mob.charStats().getBodyPart(Race.BODY_HAND)>1)
+            &&(!anyWeapons(mob)))
+            {
+                if(CMLib.dice().rollPercentage()>95)
+                    helpProficiency(mob);
+                if((naturalWeapon==null)
+                ||(naturalWeapon.amDestroyed()))
+                {
+                    naturalWeapon=CMClass.getWeapon("GenWeapon");
+                    naturalWeapon.setName("a monkey punch");
+                    naturalWeapon.setWeaponType(Weapon.TYPE_BASHING);
+                    naturalWeapon.basePhyStats().setDamage(5);
+                    naturalWeapon.recoverPhyStats();
+                }
+                CMLib.combat().postAttack(mob,mob.getVictim(),naturalWeapon);
+            }
+        }
+        return true;
+    }
 
-	public boolean okMessage(final Environmental myHost, final CMMsg msg)
-	{
-		if(!super.okMessage(myHost,msg))
-			return false;
+    public boolean okMessage(final Environmental myHost, final CMMsg msg)
+    {
+        if(!super.okMessage(myHost,msg))
+            return false;
 
-		if((affected==null)||(!(affected instanceof MOB)))
-			return true;
+        if((affected==null)||(!(affected instanceof MOB)))
+            return true;
 
-		MOB mob=(MOB)affected;
-		if(msg.amISource(mob)
-		&&(msg.targetMinor()==CMMsg.TYP_DAMAGE)
-		&&(msg.tool() instanceof Weapon)
-		&&(msg.tool()==naturalWeapon))
-			msg.setValue(msg.value()+naturalWeapon.basePhyStats().damage()+super.getXLEVELLevel(mob));
-		return true;
-	}
-	
-	public boolean anyWeapons(MOB mob)
-	{
-		for(int i=0;i<mob.numItems();i++)
-		{
-			Item I=mob.getItem(i);
-			if((I!=null)
-			   &&((I.amWearingAt(Wearable.WORN_WIELD))
-			      ||(I.amWearingAt(Wearable.WORN_HELD))))
-				return true;
-		}
-		return false;
-	}
+        MOB mob=(MOB)affected;
+        if(msg.amISource(mob)
+        &&(msg.targetMinor()==CMMsg.TYP_DAMAGE)
+        &&(msg.tool() instanceof Weapon)
+        &&(msg.tool()==naturalWeapon))
+            msg.setValue(msg.value()+naturalWeapon.basePhyStats().damage()+super.getXLEVELLevel(mob));
+        return true;
+    }
+    
+    public boolean anyWeapons(MOB mob)
+    {
+        for(int i=0;i<mob.numItems();i++)
+        {
+            Item I=mob.getItem(i);
+            if((I!=null)
+               &&((I.amWearingAt(Wearable.WORN_WIELD))
+                  ||(I.amWearingAt(Wearable.WORN_HELD))))
+                return true;
+        }
+        return false;
+    }
 }
