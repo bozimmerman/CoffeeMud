24efe2b9e84aa9159880b7690e642e88379f3866
bozimmerman <bo@zimmers.net>
2012-04-22 06:58:18 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@9495 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Commands/As.java
+++ com/planet_ink/coffee_mud/Commands/As.java
@@ -34,56 +34,56 @@ import java.util.*;
 @SuppressWarnings("unchecked")
 public class As extends StdCommand
 {
-	public As(){}
+    public As(){}
 
-	private final String[] access={"AS"};
-	public String[] getAccessWords(){return access;}
+    private final String[] access={"AS"};
+    public String[] getAccessWords(){return access;}
 
-	public boolean execute(MOB mob, Vector commands, int metaFlags)
-		throws java.io.IOException
-	{
-		commands.removeElementAt(0);
-		if(commands.size()<2)
-		{
-			mob.tell("As whom do what?");
-			return false;
-		}
-		String cmd=(String)commands.firstElement();
-		commands.removeElementAt(0);
-		if((!CMSecurity.isAllowed(mob,mob.location(),"AS"))||(mob.isMonster()))
-		{
-			mob.tell("You aren't powerful enough to do that.");
-			return false;
-		}
-		final Session mySession=mob.session();
-		MOB M=CMLib.players().getLoadPlayer(cmd);
-		if(M==null)
-			M=mob.location().fetchInhabitant(cmd);
-		if(M==null)
-		{
-		    try
-		    {
-		    	List<MOB> targets=CMLib.map().findInhabitants(CMLib.map().rooms(), mob, cmd, 50);
-		    	if(targets.size()>0) 
-		    		M=(MOB)targets.get(CMLib.dice().roll(1,targets.size(),-1));
-		    }
-		    catch(NoSuchElementException e){}
-		}
-		if(M==null)
-		{
-			mob.tell("You don't know of anyone by that name.");
-			return false;
-		}
-		if(M.soulMate()!=null)
-		{
-		    mob.tell(M.Name()+" is being possessed at the moment.");
-		    return false;
-		}
-		if((CMSecurity.isASysOp(M))&&(!CMSecurity.isASysOp(mob)))
-		{
-		    mob.tell("You aren't powerful enough to do that.");
-		    return false;
-		}
+    public boolean execute(MOB mob, Vector commands, int metaFlags)
+        throws java.io.IOException
+    {
+        commands.removeElementAt(0);
+        if(commands.size()<2)
+        {
+            mob.tell("As whom do what?");
+            return false;
+        }
+        String cmd=(String)commands.firstElement();
+        commands.removeElementAt(0);
+        if((!CMSecurity.isAllowed(mob,mob.location(),"AS"))||(mob.isMonster()))
+        {
+            mob.tell("You aren't powerful enough to do that.");
+            return false;
+        }
+        final Session mySession=mob.session();
+        MOB M=CMLib.players().getLoadPlayer(cmd);
+        if(M==null)
+            M=mob.location().fetchInhabitant(cmd);
+        if(M==null)
+        {
+            try
+            {
+                List<MOB> targets=CMLib.map().findInhabitants(CMLib.map().rooms(), mob, cmd, 50);
+                if(targets.size()>0) 
+                    M=(MOB)targets.get(CMLib.dice().roll(1,targets.size(),-1));
+            }
+            catch(NoSuchElementException e){}
+        }
+        if(M==null)
+        {
+            mob.tell("You don't know of anyone by that name.");
+            return false;
+        }
+        if(M.soulMate()!=null)
+        {
+            mob.tell(M.Name()+" is being possessed at the moment.");
+            return false;
+        }
+        if((CMSecurity.isASysOp(M))&&(!CMSecurity.isASysOp(mob)))
+        {
+            mob.tell("You aren't powerful enough to do that.");
+            return false;
+        }
         if(!M.isMonster())
         {
             if(!CMSecurity.isAllowedEverywhere(mob,"ORDER"))
@@ -94,64 +94,64 @@ public class As extends StdCommand
         }
         if(M==mob)
         {
-    		if(((String)commands.firstElement()).equalsIgnoreCase("here")
-			   ||((String)commands.firstElement()).equalsIgnoreCase("."))
-			{
-				commands.removeElementAt(0);
-			}
-    		M.doCommand(commands,metaFlags|Command.METAFLAG_AS);
-    		return false;
+            if(((String)commands.firstElement()).equalsIgnoreCase("here")
+               ||((String)commands.firstElement()).equalsIgnoreCase("."))
+            {
+                commands.removeElementAt(0);
+            }
+            M.doCommand(commands,metaFlags|Command.METAFLAG_AS);
+            return false;
         }
-		Room oldRoom=M.location();
-		boolean inside=(oldRoom!=null)?oldRoom.isInhabitant(M):false;
-		boolean dead=M.amDead();
-		final Session hisSession=M.session();
+        Room oldRoom=M.location();
+        boolean inside=(oldRoom!=null)?oldRoom.isInhabitant(M):false;
+        boolean dead=M.amDead();
+        final Session hisSession=M.session();
         synchronized(mySession)
         {
-			//int myBitmap=mob.getBitmap();
-			//int oldBitmap=M.getBitmap();
-			M.setSession(mySession);
-			mySession.setMob(M);
-			M.setSoulMate(mob);
-	        //mySession.initTelnetMode(oldBitmap);
-			if(((String)commands.firstElement()).equalsIgnoreCase("here")
-			   ||((String)commands.firstElement()).equalsIgnoreCase("."))
-			{
-			    if((M.location()!=mob.location())&&(!mob.location().isInhabitant(M)))
-					mob.location().bringMobHere(M,false);
-				commands.removeElementAt(0);
-			}
-			if(dead) M.bringToLife();
-			if((M.location()==null)&&(oldRoom==null)&&(mob.location()!=null))
-			{
-			    inside=false;
-				mob.location().bringMobHere(M,false);
-			}
+            //int myBitmap=mob.getBitmap();
+            //int oldBitmap=M.getBitmap();
+            M.setSession(mySession);
+            mySession.setMob(M);
+            M.setSoulMate(mob);
+            //mySession.initTelnetMode(oldBitmap);
+            if(((String)commands.firstElement()).equalsIgnoreCase("here")
+               ||((String)commands.firstElement()).equalsIgnoreCase("."))
+            {
+                if((M.location()!=mob.location())&&(!mob.location().isInhabitant(M)))
+                    mob.location().bringMobHere(M,false);
+                commands.removeElementAt(0);
+            }
+            if(dead) M.bringToLife();
+            if((M.location()==null)&&(oldRoom==null)&&(mob.location()!=null))
+            {
+                inside=false;
+                mob.location().bringMobHere(M,false);
+            }
         }
-		M.doCommand(commands,metaFlags|Command.METAFLAG_AS);
+        M.doCommand(commands,metaFlags|Command.METAFLAG_AS);
         synchronized(mySession)
         {
-			if(M.playerStats()!=null) M.playerStats().setLastUpdated(0);
-			if((oldRoom!=null)&&(inside)&&(!oldRoom.isInhabitant(M)))
-				oldRoom.bringMobHere(M,false);
-			else
-			if((oldRoom==null)||(!inside))
-			{
-				if(M.location()!=null)
-					M.location().delInhabitant(M);
-				M.setLocation(oldRoom);
-			}
-			M.setSoulMate(null);
-			M.setSession(hisSession);
-			mySession.setMob(mob);
+            if(M.playerStats()!=null) M.playerStats().setLastUpdated(0);
+            if((oldRoom!=null)&&(inside)&&(!oldRoom.isInhabitant(M)))
+                oldRoom.bringMobHere(M,false);
+            else
+            if((oldRoom==null)||(!inside))
+            {
+                if(M.location()!=null)
+                    M.location().delInhabitant(M);
+                M.setLocation(oldRoom);
+            }
+            M.setSoulMate(null);
+            M.setSession(hisSession);
+            mySession.setMob(mob);
         }
         //mySession.initTelnetMode(myBitmap);
-		if(dead) M.removeFromGame(true,true);
-		return false;
-	}
-	
-	public boolean canBeOrdered(){return true;}
-	public boolean securityCheck(MOB mob){return CMSecurity.isAllowedAnywhere(mob,"AS");}
+        if(dead) M.removeFromGame(true,true);
+        return false;
+    }
+    
+    public boolean canBeOrdered(){return true;}
+    public boolean securityCheck(MOB mob){return CMSecurity.isAllowedAnywhere(mob,"AS");}
 
-	
+    
 }
