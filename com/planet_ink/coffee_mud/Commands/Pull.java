d01165be3d41099c6b150c817a1a6d894aea7efc
bozimmerman <bo@zimmers.net>
2012-05-02 18:11:48 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@9531 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Commands/Pull.java
+++ com/planet_ink/coffee_mud/Commands/Pull.java
@@ -35,72 +35,74 @@ import java.util.*;
 @SuppressWarnings("unchecked")
 public class Pull extends Go
 {
-	public Pull(){}
+    public Pull(){}
 
-	private final String[] access={"PULL"};
-	public String[] getAccessWords(){return access;}
-	public boolean execute(MOB mob, Vector commands, int metaFlags)
-		throws java.io.IOException
-	{
-		
-		Environmental openThis=null;
-		String dir="";
-		int dirCode=-1;
-		Environmental E=null;
-		if(commands.size()>1)
-		{
-			dirCode=Directions.getGoodDirectionCode((String)commands.lastElement());
-			if(dirCode>=0)
-			{
-			    if((mob.location().getRoomInDir(dirCode)==null)
-			    ||(mob.location().getExitInDir(dirCode)==null)
-			    ||(!mob.location().getExitInDir(dirCode).isOpen()))
-			    {
-			        mob.tell("You can't pull anything that way.");
-			        return false;
-			    }
-			    E=mob.location().getRoomInDir(dirCode);
-			    dir=" "+Directions.getDirectionName(dirCode);
-			    commands.removeElementAt(commands.size()-1);
-			}
-		}
-		if(dir.length()==0)
-		{
-			dirCode=Directions.getGoodDirectionCode((String)commands.lastElement());
-			if(dirCode>=0)
-				openThis=mob.location().getExitInDir(dirCode);
-		}
-		String whatToOpen=CMParms.combine(commands,1);
-		if(openThis==null)
-			openThis=mob.location().fetchFromMOBRoomFavorsItems(mob,null,whatToOpen,Wearable.FILTER_ANY);
-		if((openThis==null)||(!CMLib.flags().canBeSeenBy(openThis,mob)))
-		{
-			mob.tell("You don't see '"+whatToOpen+"' here.");
-			return false;
-		}
-		CMMsg msg=CMClass.getMsg(mob,openThis,E,CMMsg.MSG_PULL,"<S-NAME> pull(s) <T-NAME>"+dir+".");
-		if(mob.location().okMessage(mob,msg))
-		{
-			mob.location().send(mob,msg);
-		    if((dir.length()>0)&&(msg.tool() instanceof Room))
-		    {
-		        Room R=(Room)msg.tool();
-		        dirCode=CMLib.tracking().findRoomDir(mob,R);
-		        if((dirCode>=0)&&(CMLib.tracking().walk(mob,dirCode,false,false,false,false)))
-		        {
-			        if(openThis instanceof Item)
-			            R.moveItemTo((Item)openThis,ItemPossessor.Expire.Player_Drop,ItemPossessor.Move.Followers);
-			        else
-			        if(openThis instanceof MOB)
-			        	CMLib.tracking().walk((MOB)openThis,dirCode,((MOB)openThis).isInCombat(),false,true,true);
-		        }
-		    }
-		}
-		return false;
-	}
+    private final String[] access={"PULL"};
+    public String[] getAccessWords(){return access;}
+    public boolean execute(MOB mob, Vector commands, int metaFlags)
+        throws java.io.IOException
+    {
+        
+        Environmental openThis=null;
+        String dir="";
+        int dirCode=-1;
+        Environmental E=null;
+        if(commands.size()>1)
+        {
+            dirCode=Directions.getGoodDirectionCode((String)commands.lastElement());
+            if(dirCode>=0)
+            {
+                if((mob.location().getRoomInDir(dirCode)==null)
+                ||(mob.location().getExitInDir(dirCode)==null)
+                ||(!mob.location().getExitInDir(dirCode).isOpen()))
+                {
+                    mob.tell("You can't pull anything that way.");
+                    return false;
+                }
+                E=mob.location().getRoomInDir(dirCode);
+                dir=" "+Directions.getDirectionName(dirCode);
+                commands.removeElementAt(commands.size()-1);
+            }
+        }
+        if(dir.length()==0)
+        {
+            dirCode=Directions.getGoodDirectionCode((String)commands.lastElement());
+            if(dirCode>=0)
+                openThis=mob.location().getExitInDir(dirCode);
+        }
+        String itemName=CMParms.combine(commands,1);
+        if(openThis==null)
+            openThis=mob.location().fetchFromRoomFavorItems(null,itemName);
+        if(openThis==null)
+            openThis=mob.location().fetchFromMOBRoomFavorsItems(mob,null,itemName,Wearable.FILTER_ANY);
+        if((openThis==null)||(!CMLib.flags().canBeSeenBy(openThis,mob)))
+        {
+            mob.tell("You don't see '"+itemName+"' here.");
+            return false;
+        }
+        CMMsg msg=CMClass.getMsg(mob,openThis,E,CMMsg.MSG_PULL,"<S-NAME> pull(s) <T-NAME>"+dir+".");
+        if(mob.location().okMessage(mob,msg))
+        {
+            mob.location().send(mob,msg);
+            if((dir.length()>0)&&(msg.tool() instanceof Room))
+            {
+                Room R=(Room)msg.tool();
+                dirCode=CMLib.tracking().findRoomDir(mob,R);
+                if((dirCode>=0)&&(CMLib.tracking().walk(mob,dirCode,false,false,false,false)))
+                {
+                    if(openThis instanceof Item)
+                        R.moveItemTo((Item)openThis,ItemPossessor.Expire.Player_Drop,ItemPossessor.Move.Followers);
+                    else
+                    if(openThis instanceof MOB)
+                        CMLib.tracking().walk((MOB)openThis,dirCode,((MOB)openThis).isInCombat(),false,true,true);
+                }
+            }
+        }
+        return false;
+    }
     public double combatActionsCost(final MOB mob, final List<String> cmds){return CMProps.getCombatActionCost(ID());}
     public double actionsCost(final MOB mob, final List<String> cmds){return CMProps.getActionCost(ID());}
-	public boolean canBeOrdered(){return true;}
+    public boolean canBeOrdered(){return true;}
 
-	
+    
 }
--- com/planet_ink/coffee_mud/Commands/Push.java
+++ com/planet_ink/coffee_mud/Commands/Push.java
@@ -35,81 +35,83 @@ import java.util.*;
 @SuppressWarnings("unchecked")
 public class Push extends Go
 {
-	public Push(){}
+    public Push(){}
 
-	private final String[] access={"PUSH"};
-	public String[] getAccessWords(){return access;}
-	public boolean execute(MOB mob, Vector commands, int metaFlags)
-		throws java.io.IOException
-	{
-		Environmental pushThis=null;
-		String dir="";
-		int dirCode=-1;
-		Environmental E=null;
-		if(commands.size()>1)
-		{
-			dirCode=Directions.getGoodDirectionCode((String)commands.lastElement());
-			if(dirCode>=0)
-			{
-			    if((mob.location().getRoomInDir(dirCode)==null)
-			    ||(mob.location().getExitInDir(dirCode)==null)
-			    ||(!mob.location().getExitInDir(dirCode).isOpen()))
-			    {
-			        mob.tell("You can't push anything that way.");
-			        return false;
-			    }
-			    E=mob.location().getRoomInDir(dirCode);
-			    dir=" "+Directions.getDirectionName(dirCode);
-			    commands.removeElementAt(commands.size()-1);
-			}
-		}
-		if(dir.length()==0)
-		{
-			dirCode=Directions.getGoodDirectionCode((String)commands.lastElement());
-			if(dirCode>=0)
-				pushThis=mob.location().getExitInDir(dirCode);
-		}
-		String whatToOpen=CMParms.combine(commands,1);
-		if(pushThis==null)
-			pushThis=mob.location().fetchFromMOBRoomFavorsItems(mob,null,whatToOpen,Wearable.FILTER_ANY);
+    private final String[] access={"PUSH"};
+    public String[] getAccessWords(){return access;}
+    public boolean execute(MOB mob, Vector commands, int metaFlags)
+        throws java.io.IOException
+    {
+        Environmental pushThis=null;
+        String dir="";
+        int dirCode=-1;
+        Environmental E=null;
+        if(commands.size()>1)
+        {
+            dirCode=Directions.getGoodDirectionCode((String)commands.lastElement());
+            if(dirCode>=0)
+            {
+                if((mob.location().getRoomInDir(dirCode)==null)
+                ||(mob.location().getExitInDir(dirCode)==null)
+                ||(!mob.location().getExitInDir(dirCode).isOpen()))
+                {
+                    mob.tell("You can't push anything that way.");
+                    return false;
+                }
+                E=mob.location().getRoomInDir(dirCode);
+                dir=" "+Directions.getDirectionName(dirCode);
+                commands.removeElementAt(commands.size()-1);
+            }
+        }
+        if(dir.length()==0)
+        {
+            dirCode=Directions.getGoodDirectionCode((String)commands.lastElement());
+            if(dirCode>=0)
+                pushThis=mob.location().getExitInDir(dirCode);
+        }
+        String itemName=CMParms.combine(commands,1);
+        if(pushThis==null)
+            pushThis=mob.location().fetchFromRoomFavorItems(null,itemName);
+        if(pushThis==null)
+            pushThis=mob.location().fetchFromMOBRoomFavorsItems(mob,null,itemName,Wearable.FILTER_ANY);
 
-		if((pushThis==null)||(!CMLib.flags().canBeSeenBy(pushThis,mob)))
-		{
-			mob.tell("You don't see '"+whatToOpen+"' here.");
-			return false;
-		}
-		int malmask=(pushThis instanceof MOB)?CMMsg.MASK_MALICIOUS:0;
-		String msgStr = "<S-NAME> push(es) <T-NAME>"+dir+".";
-		CMMsg msg=CMClass.getMsg(mob,pushThis,E,CMMsg.MSG_PUSH|malmask,msgStr);
-		if(mob.location().okMessage(mob,msg))
-		{
-		    mob.location().send(mob,msg);
-		    if((dir.length()>0)&&(msg.tool() instanceof Room))
-		    {
-		        Room R=(Room)msg.tool();
-		        if(R.okMessage(mob,msg))
-		        {
-    		        dirCode=CMLib.tracking().findRoomDir(mob,R);
-    		        if(dirCode>=0)
-    		        {
-    		            if(msg.othersMessage().equals(msgStr))
-    		                msg.setOthersMessage("<S-NAME> push(es) <T-NAME> into here.");
+        if((pushThis==null)||(!CMLib.flags().canBeSeenBy(pushThis,mob)))
+        {
+            mob.tell("You don't see '"+itemName+"' here.");
+            return false;
+        }
+        int malmask=(pushThis instanceof MOB)?CMMsg.MASK_MALICIOUS:0;
+        String msgStr = "<S-NAME> push(es) <T-NAME>"+dir+".";
+        CMMsg msg=CMClass.getMsg(mob,pushThis,E,CMMsg.MSG_PUSH|malmask,msgStr);
+        if(mob.location().okMessage(mob,msg))
+        {
+            mob.location().send(mob,msg);
+            if((dir.length()>0)&&(msg.tool() instanceof Room))
+            {
+                Room R=(Room)msg.tool();
+                if(R.okMessage(mob,msg))
+                {
+                    dirCode=CMLib.tracking().findRoomDir(mob,R);
+                    if(dirCode>=0)
+                    {
+                        if(msg.othersMessage().equals(msgStr))
+                            msg.setOthersMessage("<S-NAME> push(es) <T-NAME> into here.");
                         R.sendOthers(mob,msg);
-    			        if(pushThis instanceof Item)
-    			            R.moveItemTo((Item)pushThis,ItemPossessor.Expire.Player_Drop,ItemPossessor.Move.Followers);
-    			        else
-    			        if(pushThis instanceof MOB)
-    			        	CMLib.tracking().walk((MOB)pushThis,dirCode,((MOB)pushThis).isInCombat(),false,true,true);
-    		        }
-		        }
-		    }
-		            
-		}
-		return false;
-	}
+                        if(pushThis instanceof Item)
+                            R.moveItemTo((Item)pushThis,ItemPossessor.Expire.Player_Drop,ItemPossessor.Move.Followers);
+                        else
+                        if(pushThis instanceof MOB)
+                            CMLib.tracking().walk((MOB)pushThis,dirCode,((MOB)pushThis).isInCombat(),false,true,true);
+                    }
+                }
+            }
+                    
+        }
+        return false;
+    }
     public double combatActionsCost(final MOB mob, final List<String> cmds){return CMProps.getCombatActionCost(ID());}
     public double actionsCost(final MOB mob, final List<String> cmds){return CMProps.getActionCost(ID());}
-	public boolean canBeOrdered(){return true;}
+    public boolean canBeOrdered(){return true;}
 
-	
+    
 }
