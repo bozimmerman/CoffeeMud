9e1e8ac85973419a18a08e1b752e4273eeb3d934
bozimmerman <bo@zimmers.net>
2012-05-16 02:25:55 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@9539 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Abilities/Spells/Spell_StoreSpell.java
+++ com/planet_ink/coffee_mud/Abilities/Spells/Spell_StoreSpell.java
@@ -36,9 +36,9 @@ import java.util.*;
 @SuppressWarnings("unchecked")
 public class Spell_StoreSpell extends Spell
 {
-	public String ID() { return "Spell_StoreSpell"; }
+    public String ID() { return "Spell_StoreSpell"; }
     public String Name(){return "Store Spell";}
-	public String name(){
+    public String name(){
         if((affected!=null)&&(CMLib.flags().isInTheGame(affected,true)))
         {
             if(spellName.length()==0)
@@ -56,190 +56,205 @@ public class Spell_StoreSpell extends Spell
         }
         return Name();
     }
-	protected int canTargetCode(){return CAN_ITEMS;}
-	public int classificationCode(){return Ability.ACODE_SPELL|Ability.DOMAIN_ENCHANTMENT;}
-	protected int overrideMana(){return overridemana;}
+    protected int canTargetCode(){return CAN_ITEMS;}
+    public int classificationCode(){return Ability.ACODE_SPELL|Ability.DOMAIN_ENCHANTMENT;}
+    protected int overrideMana(){return overridemana;}
     public int abstractQuality(){ return Ability.QUALITY_INDIFFERENT;}
-	public String spellName="";
-	protected int overridemana=-1;
+    public String spellName="";
+    protected int overridemana=-1;
 
-	public void waveIfAble(MOB mob,
-						   Physical afftarget,
-						   String message,
-						   Item me)
-	{
-		if((mob.isMine(me))&&(!me.amWearingAt(Wearable.IN_INVENTORY))&&(message!=null))
-		{
-			Physical target=null;
-			if((mob.location()!=null))
-				target=afftarget;
-			String name=CMStrings.removeColors(me.name().toUpperCase());
-			if(name.startsWith("A ")) name=name.substring(2).trim();
-			if(name.startsWith("AN ")) name=name.substring(3).trim();
-			if(name.startsWith("THE ")) name=name.substring(4).trim();
-			if(name.startsWith("SOME ")) name=name.substring(5).trim();
-			int x=message.toUpperCase().indexOf(name);
-			if(x>=0)
-			{
-				message=message.substring(x+name.length());
-				int y=message.indexOf('\'');
-				if(y>=0) message=message.substring(0,y);
-				message=message.trim();
-				x=text().indexOf('/');
-				int charges=0;
-				Ability A=null;
-				if(x>0){
-					charges=CMath.s_int(text().substring(x+1));
-					A=CMClass.getAbility(text().substring(0,x));
-				}
-				if(A==null)
-					mob.tell("Something seems wrong with "+me.name()+".");
-				else
-				if(charges<=0)
-				{
-					mob.tell(me.name()+" seems spent.");
-					me.delEffect(this);
-				}
-				else
-				{
-					setMiscText(A.ID()+"/"+(charges-1));
-					A=(Ability)A.newInstance();
-					Vector V=new Vector();
-					if(target!=null)
-						V.addElement(target.name());
-					V.addElement(message);
-					mob.location().show(mob,null,CMMsg.MSG_OK_VISUAL,me.name()+" glows brightly.");
-					A.invoke(mob, V, target, true,0);
-				}
-			}
-		}
-	}
+    public String getSpeakableName(String name)
+    {
+        name=CMStrings.removeColors(name.toUpperCase());
+        if(name.startsWith("A ")) name=name.substring(2).trim();
+        if(name.startsWith("AN ")) name=name.substring(3).trim();
+        if(name.startsWith("THE ")) name=name.substring(4).trim();
+        if(name.startsWith("SOME ")) name=name.substring(5).trim();
+        return name;
+    }
 
-	public void executeMsg(final Environmental myHost, final CMMsg msg)
-	{
-		MOB mob=msg.source();
+    public void waveIfAble(MOB mob,
+                           Physical afftarget,
+                           String message,
+                           Item me)
+    {
+        if((mob.isMine(me))&&(!me.amWearingAt(Wearable.IN_INVENTORY))&&(message!=null))
+        {
+            Physical target=null;
+            if((mob.location()!=null))
+                target=afftarget;
+            final String name=getSpeakableName(me.name());
+            int x=message.toUpperCase().indexOf(name);
+            if(x>=0)
+            {
+                message=message.substring(x+name.length());
+                int y=message.indexOf('\'');
+                if(y>=0) message=message.substring(0,y);
+                message=message.trim();
+                x=text().indexOf('/');
+                int charges=0;
+                Ability A=null;
+                if(x>0){
+                    charges=CMath.s_int(text().substring(x+1));
+                    A=CMClass.getAbility(text().substring(0,x));
+                }
+                if(A==null)
+                    mob.tell("Something seems wrong with "+me.name()+".");
+                else
+                if(charges<=0)
+                {
+                    mob.tell(me.name()+" seems spent.");
+                    me.delEffect(this);
+                }
+                else
+                {
+                    setMiscText(A.ID()+"/"+(charges-1));
+                    A=(Ability)A.newInstance();
+                    Vector V=new Vector();
+                    if(target!=null)
+                        V.addElement(target.name());
+                    V.addElement(message);
+                    mob.location().show(mob,null,CMMsg.MSG_OK_VISUAL,me.name()+" glows brightly.");
+                    A.invoke(mob, V, target, true,0);
+                }
+            }
+        }
+    }
 
-		switch(msg.targetMinor())
-		{
-		case CMMsg.TYP_WAND_USE:
-			if((msg.amITarget(affected))&&(affected instanceof Item)&&((msg.tool()==null)||(msg.tool() instanceof Physical)))
-				waveIfAble(mob,(Physical)msg.tool(),msg.targetMessage(),(Item)affected);
-			break;
-		case CMMsg.TYP_SPEAK:
-			if(msg.sourceMinor()==CMMsg.TYP_SPEAK)
-			{
-				boolean alreadyWanding=false;
-				final List<CMMsg> trailers =msg.trailerMsgs();
-				if(trailers!=null)
-					for(CMMsg msg2 : trailers)
-						if(msg2.targetMinor()==CMMsg.TYP_WAND_USE)
-							alreadyWanding=true;
-				if(!alreadyWanding)
-					msg.addTrailerMsg(CMClass.getMsg(msg.source(),affected,msg.target(),CMMsg.NO_EFFECT,null,CMMsg.MASK_ALWAYS|CMMsg.TYP_WAND_USE,CMStrings.getSayFromMessage(msg.sourceMessage()),CMMsg.NO_EFFECT,null));
-			}
-			break;
-		default:
-			break;
-		}
-		super.executeMsg(myHost,msg);
-	}
+    public void executeMsg(final Environmental myHost, final CMMsg msg)
+    {
+        MOB mob=msg.source();
 
-	public boolean invoke(MOB mob, Vector commands, Physical givenTarget, boolean auto, int asLevel)
-	{
-		if(commands.size()<2)
-		{
-			mob.tell("Store which spell onto what?");
-			return false;
-		}
-		Physical target=mob.location().fetchFromMOBRoomFavorsItems(mob,null,(String)commands.lastElement(),Wearable.FILTER_UNWORNONLY);
-		if((target==null)||(!CMLib.flags().canBeSeenBy(target,mob)))
-		{
-			mob.tell("You don't see '"+((String)commands.lastElement())+"' here.");
-			return false;
-		}
-		if(!(target instanceof Item))
-		{
-			mob.tell("You can't enchant '"+target.name()+"'.");
-			return false;
-		}
+        switch(msg.targetMinor())
+        {
+        case CMMsg.TYP_WAND_USE:
+            if((msg.amITarget(affected))&&(affected instanceof Item)&&((msg.tool()==null)||(msg.tool() instanceof Physical)))
+                waveIfAble(mob,(Physical)msg.tool(),msg.targetMessage(),(Item)affected);
+            break;
+        case CMMsg.TYP_SPEAK:
+            if((msg.sourceMinor()==CMMsg.TYP_SPEAK)
+            &&(msg.targetMessage()!=null)
+            &&(affected != null))
+            {
+                boolean alreadyWanding=false;
+                final List<CMMsg> trailers =msg.trailerMsgs();
+                if(trailers!=null)
+                    for(CMMsg msg2 : trailers)
+                        if(msg2.targetMinor()==CMMsg.TYP_WAND_USE)
+                            alreadyWanding=true;
+                if(!alreadyWanding)
+                {
+                    final String name=getSpeakableName(affected.name());
+                    int x=msg.targetMessage().toUpperCase().indexOf(name);
+                    if(x>=0)
+                    {
+                        msg.addTrailerMsg(CMClass.getMsg(msg.source(),affected,msg.target(),CMMsg.NO_EFFECT,null,CMMsg.MASK_ALWAYS|CMMsg.TYP_WAND_USE,CMStrings.getSayFromMessage(msg.sourceMessage()),CMMsg.NO_EFFECT,null));
+                    }
+                }
+            }
+            break;
+        default:
+            break;
+        }
+        super.executeMsg(myHost,msg);
+    }
 
-		Item item=(Item)target;
+    public boolean invoke(MOB mob, Vector commands, Physical givenTarget, boolean auto, int asLevel)
+    {
+        if(commands.size()<2)
+        {
+            mob.tell("Store which spell onto what?");
+            return false;
+        }
+        Physical target=mob.location().fetchFromMOBRoomFavorsItems(mob,null,(String)commands.lastElement(),Wearable.FILTER_UNWORNONLY);
+        if((target==null)||(!CMLib.flags().canBeSeenBy(target,mob)))
+        {
+            mob.tell("You don't see '"+((String)commands.lastElement())+"' here.");
+            return false;
+        }
+        if(!(target instanceof Item))
+        {
+            mob.tell("You can't enchant '"+target.name()+"'.");
+            return false;
+        }
 
-		commands.removeElementAt(commands.size()-1);
+        Item item=(Item)target;
 
-		String spellName=CMParms.combine(commands,0).trim();
-		Spell wandThis=null;
+        commands.removeElementAt(commands.size()-1);
+
+        String spellName=CMParms.combine(commands,0).trim();
+        Spell wandThis=null;
         for(Enumeration<Ability> a=mob.allAbilities();a.hasMoreElements();)
         {
             Ability A=a.nextElement();
-			if((A!=null)
-			&&(A instanceof Spell)
-			&&((!A.isSavable())||(CMLib.ableMapper().qualifiesByLevel(mob,A)))
-			&&(A.name().toUpperCase().startsWith(spellName.toUpperCase()))
-			&&(!A.ID().equals(this.ID())))
-				wandThis=(Spell)A;
-		}
-		if(wandThis==null)
-		{
-			mob.tell("You don't know how to enchant anything with '"+spellName+"'.");
-			return false;
-		}
+            if((A!=null)
+            &&(A instanceof Spell)
+            &&((!A.isSavable())||(CMLib.ableMapper().qualifiesByLevel(mob,A)))
+            &&(A.name().toUpperCase().startsWith(spellName.toUpperCase()))
+            &&(!A.ID().equals(this.ID())))
+                wandThis=(Spell)A;
+        }
+        if(wandThis==null)
+        {
+            mob.tell("You don't know how to enchant anything with '"+spellName+"'.");
+            return false;
+        }
         if((CMLib.ableMapper().lowestQualifyingLevel(wandThis.ID())>24)
         ||(((StdAbility)wandThis).usageCost(null,true)[0]>45))
-		{
-			mob.tell("That spell is too powerful to store.");
-			return false;
-		}
-		Ability A=item.fetchEffect(ID());
-		if((A!=null)&&(A.text().length()>0)&&(!A.text().startsWith(wandThis.ID()+"/")))
-		{
-			mob.tell("'"+item.name()+"' already has a different spell stored in it.");
-			return false;
-		}
-		else
-		if(A==null)
-		{
-			A=(Ability)copyOf();
-			A.setMiscText(wandThis.ID()+"/0");
-		}
-		int charges=0;
-		int x=A.text().indexOf('/');
-		if(x>=0) charges=CMath.s_int(A.text().substring(x+1));
-		overridemana=-1;
-		int mana=usageCost(mob,true)[0]+wandThis.usageCost(mob,true)[0];
-		if(mana>mob.maxState().getMana())
-			mana=mob.maxState().getMana();
-		overridemana=mana;
+        {
+            mob.tell("That spell is too powerful to store.");
+            return false;
+        }
+        Ability A=item.fetchEffect(ID());
+        if((A!=null)&&(A.text().length()>0)&&(!A.text().startsWith(wandThis.ID()+"/")))
+        {
+            mob.tell("'"+item.name()+"' already has a different spell stored in it.");
+            return false;
+        }
+        else
+        if(A==null)
+        {
+            A=(Ability)copyOf();
+            A.setMiscText(wandThis.ID()+"/0");
+        }
+        int charges=0;
+        int x=A.text().indexOf('/');
+        if(x>=0) charges=CMath.s_int(A.text().substring(x+1));
+        overridemana=-1;
+        int mana=usageCost(mob,true)[0]+wandThis.usageCost(mob,true)[0];
+        if(mana>mob.maxState().getMana())
+            mana=mob.maxState().getMana();
+        overridemana=mana;
 
-		// lose all the mana!
-		if(!super.invoke(mob,commands,givenTarget,auto,asLevel))
-			return false;
-		overridemana=-1;
+        // lose all the mana!
+        if(!super.invoke(mob,commands,givenTarget,auto,asLevel))
+            return false;
+        overridemana=-1;
 
-		boolean success=proficiencyCheck(mob,0,auto);
+        boolean success=proficiencyCheck(mob,0,auto);
 
-		if(success)
-		{
-			setMiscText(wandThis.ID());
-			CMMsg msg=CMClass.getMsg(mob,target,this,verbalCastCode(mob,target,auto),"^S<S-NAME> move(s) <S-HIS-HER> fingers around <T-NAMESELF>, incanting softly.^?");
-			if(mob.location().okMessage(mob,msg))
-			{
-				mob.location().send(mob,msg);
-				if(target.fetchEffect(ID())==null)
-				{
-					A.setInvoker(mob);
-					target.addNonUninvokableEffect(A);
-				}
-				A.setMiscText(wandThis.ID()+"/"+(charges+1));
-				mob.location().show(mob,target,null,CMMsg.MSG_OK_VISUAL,"<T-NAME> glow(s) softly.");
-			}
+        if(success)
+        {
+            setMiscText(wandThis.ID());
+            CMMsg msg=CMClass.getMsg(mob,target,this,verbalCastCode(mob,target,auto),"^S<S-NAME> move(s) <S-HIS-HER> fingers around <T-NAMESELF>, incanting softly.^?");
+            if(mob.location().okMessage(mob,msg))
+            {
+                mob.location().send(mob,msg);
+                if(target.fetchEffect(ID())==null)
+                {
+                    A.setInvoker(mob);
+                    target.addNonUninvokableEffect(A);
+                }
+                A.setMiscText(wandThis.ID()+"/"+(charges+1));
+                mob.location().show(mob,target,null,CMMsg.MSG_OK_VISUAL,"<T-NAME> glow(s) softly.");
+            }
 
-		}
-		else
-			beneficialWordsFizzle(mob,target,"<S-NAME> move(s) <S-HIS-HER> fingers around <T-NAMESELF>, incanting softly, and looking very frustrated.");
+        }
+        else
+            beneficialWordsFizzle(mob,target,"<S-NAME> move(s) <S-HIS-HER> fingers around <T-NAMESELF>, incanting softly, and looking very frustrated.");
 
 
-		// return whether it worked
-		return success;
-	}
+        // return whether it worked
+        return success;
+    }
 }
