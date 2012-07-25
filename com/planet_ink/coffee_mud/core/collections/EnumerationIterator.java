8d611709612ec30c06ac51e4fef4a1ee67abf5fd
bozimmerman <bo@zimmers.net>
2012-07-25 14:59:31 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@9635 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/core/collections/EnumerationIterator.java
@@ -0,0 +1,50 @@
+package com.planet_ink.coffee_mud.core.collections;
+import java.util.Enumeration;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+/*
+Copyright 2000-2012 Bo Zimmerman
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+public class EnumerationIterator<K> implements Iterator<K>
+{
+	private final Enumeration<K> e;
+	
+	@SuppressWarnings("unchecked")
+	public EnumerationIterator(Enumeration<K> e) 
+	{
+		if(e==null)
+			this.e=(Enumeration<K>)EmptyEnumeration.INSTANCE;
+		else
+			this.e=e;
+		hasNext();
+	}
+	
+	public boolean hasNext() 
+	{ 
+		return e.hasMoreElements();
+	}
+	
+	public K next() 
+	{
+		return e.nextElement();
+	}
+
+	@Override
+    public void remove() {
+		throw new java.lang.UnsupportedOperationException();
+    }
+	
+	
+}
