2d588d6340ecdcd4ecc77685585470f11f6e1b90
bozimmerman <bo@zimmers.net>
2012-11-21 08:45:14 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@9935 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Abilities/Spells/Spell_Light.java
+++ com/planet_ink/coffee_mud/Abilities/Spells/Spell_Light.java
@@ -90,15 +90,16 @@ public class Spell_Light extends Spell
 
 		boolean success=proficiencyCheck(mob,0,auto);
 
-		if(success)
+		final Room room=mob.location();
+		if((success)&&(room!=null))
 		{
-    		CMMsg msg=CMClass.getMsg(mob,target,this,verbalCastCode(mob,target,auto),auto?"^S<S-NAME> attain(s) a light above <S-HIS-HER> head!":"^S<S-NAME> invoke(s) a white light above <S-HIS-HER> head!^?");
-    		if(mob.location().okMessage(mob,msg))
-    		{
-    			mob.location().send(mob,msg);
-    			beneficialAffect(mob,target,asLevel,0);
-    			mob.location().recoverRoomStats(); // attempt to handle followers
-    		}
+			CMMsg msg=CMClass.getMsg(mob,target,this,verbalCastCode(mob,target,auto),auto?"^S<S-NAME> attain(s) a light above <S-HIS-HER> head!":"^S<S-NAME> invoke(s) a white light above <S-HIS-HER> head!^?");
+			if(room.okMessage(mob,msg))
+			{
+				room.send(mob,msg);
+				beneficialAffect(mob,target,asLevel,0);
+				room.recoverRoomStats();
+			}
 		}
 		else
 			beneficialWordsFizzle(mob,mob.location(),"<S-NAME> attempt(s) to invoke light, but fail(s).");
