60f75b009a67211c41f5eeec23c73a338f8f5b84
bozimmerman <bo@zimmers.net>
2013-01-18 06:19:08 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@10154 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/core/collections/MultiListEnumeration.java
+++ com/planet_ink/coffee_mud/core/collections/MultiListEnumeration.java
@@ -27,7 +27,6 @@ public class MultiListEnumeration<K> implements Enumeration<K>
 	private volatile Iterator<Iterable<K>> listIter = null; 
 	private volatile Iterator<K> iter = null; 
 	
-	@SafeVarargs
 	public MultiListEnumeration(final Iterable<K>... esets) 
 	{
 		if((esets!=null)&&(esets.length>0))
