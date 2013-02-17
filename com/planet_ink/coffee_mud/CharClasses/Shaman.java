e9a78066912fb4df36d52b38c35afe0a2338303f
bozimmerman <bo@zimmers.net>
2013-02-17 05:41:58 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@10221 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/CharClasses/Shaman.java
+++ com/planet_ink/coffee_mud/CharClasses/Shaman.java
@@ -129,7 +129,12 @@ public class Shaman extends Cleric
 		CMLib.ableMapper().addCharAbilityMapping(ID(),19,"Prayer_FleshRock",false);
 
 		CMLib.ableMapper().addCharAbilityMapping(ID(),20,"Prayer_MassMobility",true,CMParms.parseSemicolons("Prayer_ProtParalyzation",true));
-		CMLib.ableMapper().addCharAbilityMapping(ID(),20,"Prayer_AcidHealing",false);
+		//CMLib.ableMapper().addCharAbilityMapping(ID(),25,"Prayer_SelfResurrection",false);
+		//CMLib.ableMapper().addCharAbilityMapping(ID(),25,"Prayer_CommuneWithDead",false); // (MudChat with corpses + Mood)
+		//CMLib.ableMapper().addCharAbilityMapping(ID(),25,"Prayer_SpeakWithAnimal",false); // (MudChat with animals + Mood)
+		//CMLib.ableMapper().addCharAbilityMapping(ID(),25,"Prayer_SpiritHelper",false); // (Always same spirit animal, gives power)
+		//CMLib.ableMapper().addCharAbilityMapping(ID(),25,"Prayer_Awaken",false); // (Cancels sleep spells and wakes the sleeping)
+		//CMLib.ableMapper().addCharAbilityMapping(ID(),25,"Prayer_SummonAnimalSpirits",false); // (Summons animal spirits to fight)
 
 		CMLib.ableMapper().addCharAbilityMapping(ID(),21,"Prayer_DrunkenStupor",false);
 		CMLib.ableMapper().addCharAbilityMapping(ID(),21,"Prayer_MoralBalance",false);
@@ -144,7 +149,7 @@ public class Shaman extends Cleric
 		CMLib.ableMapper().addCharAbilityMapping(ID(),24,"Prayer_NeutralizeLand",false);
 
 		CMLib.ableMapper().addCharAbilityMapping(ID(),25,"Prayer_SummonElemental",0,"EARTH",false);
-		//CMLib.ableMapper().addCharAbilityMapping(ID(),25,"Prayer_SelfResurrection",false);
+		CMLib.ableMapper().addCharAbilityMapping(ID(),25,"Prayer_AcidHealing",false);
 		
 		CMLib.ableMapper().addCharAbilityMapping(ID(),30,"Prayer_HolyDay",true);
 	}
