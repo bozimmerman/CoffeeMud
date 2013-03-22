401619fc7cb36759e5f7c5d57a10e87d880500fd
bozimmerman <bo@zimmers.net>
2013-03-22 03:57:23 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@10321 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/miniweb/http/MWFileCache.java
+++ com/planet_ink/miniweb/http/MWFileCache.java
@@ -4,7 +4,6 @@ import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.Date;
