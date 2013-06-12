79d54347aa9538a78aaa1a538f8d85110592a1cf
bozimmerman <bo@zimmers.net>
2013-06-12 20:46:10 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@10369 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/core/threads/TockClient.java
+++ com/planet_ink/coffee_mud/core/threads/TockClient.java
@@ -1,18 +1,6 @@
 package com.planet_ink.coffee_mud.core.threads;
-import com.planet_ink.coffee_mud.core.interfaces.*;
-import com.planet_ink.coffee_mud.core.*;
-import com.planet_ink.coffee_mud.core.collections.*;
-import com.planet_ink.coffee_mud.Abilities.interfaces.*;
-import com.planet_ink.coffee_mud.Areas.interfaces.*;
-import com.planet_ink.coffee_mud.Behaviors.interfaces.*;
-import com.planet_ink.coffee_mud.CharClasses.interfaces.*;
-import com.planet_ink.coffee_mud.Commands.interfaces.*;
-import com.planet_ink.coffee_mud.Common.interfaces.*;
-import com.planet_ink.coffee_mud.Exits.interfaces.*;
-import com.planet_ink.coffee_mud.Items.interfaces.*;
-import com.planet_ink.coffee_mud.Locales.interfaces.*;
-import com.planet_ink.coffee_mud.MOBS.interfaces.*;
-import com.planet_ink.coffee_mud.Races.interfaces.*;
+import com.planet_ink.coffee_mud.core.Log;
+import com.planet_ink.coffee_mud.core.interfaces.Tickable;
 
 
 /* 
