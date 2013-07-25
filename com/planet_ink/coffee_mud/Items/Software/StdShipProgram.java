53a17c2cdd954607dea7410779493d9f848d5511
bozimmerman <bo@zimmers.net>
2013-07-25 21:12:20 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@10780 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Items/Software/StdShipProgram.java
+++ com/planet_ink/coffee_mud/Items/Software/StdShipProgram.java
@@ -36,7 +36,7 @@ import java.util.*;
    See the License for the specific language governing permissions and
    limitations under the License.
 */
-public class StdShipProgram extends StdProgram implements ArchonOnly
+public class StdShipProgram extends StdProgram
 {
 	public String ID(){	return "StdShipProgram";}
 	
