753da0932ffbb805516b766c90abd5d849ec72d5
bozimmerman <bo@zimmers.net>
2013-11-16 05:55:44 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@11105 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Commands/Retire.java
+++ com/planet_ink/coffee_mud/Commands/Retire.java
@@ -67,7 +67,7 @@ public class Retire extends StdCommand
 					{
 						Log.sysOut("Retire","Retired: "+mob.Name());
 						CMLib.players().obliteratePlayer(mob,true,false);
-						mob.session().logout(true);
+						session.logout(true);
 					}
 					else
 					session.prompt(new InputCallback(InputCallback.Type.PROMPT,"") {
@@ -82,7 +82,7 @@ public class Retire extends StdCommand
 						{
 							Log.sysOut("Retire","Retired: "+mob.Name()+": "+this.input);
 							CMLib.players().obliteratePlayer(mob,true,false);
-							mob.session().logout(true);
+							session.logout(true);
 						}
 					});
 				}
