88bb35d9e4289fdebbd94cc8b4b8007ced4c0ee9
bozimmerman <bo@zimmers.net>
2014-06-13 06:24:29 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@12030 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/MOBS/Crocodile.java
@@ -0,0 +1,64 @@
+package com.planet_ink.coffee_mud.MOBS;
+import com.planet_ink.coffee_mud.core.interfaces.*;
+import com.planet_ink.coffee_mud.core.*;
+import com.planet_ink.coffee_mud.core.collections.*;
+import com.planet_ink.coffee_mud.Abilities.interfaces.*;
+import com.planet_ink.coffee_mud.Areas.interfaces.*;
+import com.planet_ink.coffee_mud.Behaviors.interfaces.*;
+import com.planet_ink.coffee_mud.CharClasses.interfaces.*;
+import com.planet_ink.coffee_mud.Commands.interfaces.*;
+import com.planet_ink.coffee_mud.Common.interfaces.*;
+import com.planet_ink.coffee_mud.Exits.interfaces.*;
+import com.planet_ink.coffee_mud.Items.interfaces.*;
+import com.planet_ink.coffee_mud.Locales.interfaces.*;
+import com.planet_ink.coffee_mud.MOBS.interfaces.*;
+import com.planet_ink.coffee_mud.Races.interfaces.*;
+
+import java.util.*;
+
+/*
+   Copyright 2000-2014 Bo Zimmerman
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+	   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+public class Crocodile extends StdMOB
+{
+	@Override public String ID(){return "Crocodile";}
+	public Crocodile()
+	{
+		super();
+		username="a crocodile";
+		setDescription("A long green lizard with long ferocious rows of teeth.");
+		setDisplayText("A crocodile is waiting patiently for prey.");
+		CMLib.factions().setAlignment(this,Faction.Align.NEUTRAL);
+		setMoney(0);
+
+		basePhyStats().setDamage(30);
+
+		baseCharStats().setStat(CharStats.STAT_INTELLIGENCE,1);
+
+		basePhyStats().setAbility(0);
+		basePhyStats().setLevel(5);
+		basePhyStats().setArmor(70);
+
+		baseCharStats().setMyRace(CMClass.getRace("Crocodile"));
+		baseCharStats().getMyRace().startRacing(this,false);
+		baseState.setHitPoints(CMLib.dice().rollHP(basePhyStats.level(), 20));
+
+		recoverMaxState();
+		resetToMaxState();
+		recoverPhyStats();
+		recoverCharStats();
+	}
+
+}
