84d54396179892242116747ae6c904033a8dc903
bozimmerman <bo@zimmers.net>
2014-11-04 06:40:51 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@12425 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/WebMacros/CrossBaseClassAbilities.java
+++ com/planet_ink/coffee_mud/WebMacros/CrossBaseClassAbilities.java
@@ -15,6 +15,7 @@ import com.planet_ink.coffee_mud.Items.interfaces.*;
 import com.planet_ink.coffee_mud.Locales.interfaces.*;
 import com.planet_ink.coffee_mud.MOBS.interfaces.*;
 import com.planet_ink.coffee_mud.Races.interfaces.*;
+
 import java.util.*;
 
 /*
@@ -41,102 +42,134 @@ public class CrossBaseClassAbilities extends StdWebMacro
 	public String runMacro(HTTPRequest httpReq, String parm)
 	{
 		final StringBuffer buf=new StringBuffer("");
-		String baseClass=httpReq.getUrlParameter("BASECLASS");
-		if(baseClass==null) 
-			return " @break@";
-		if(baseClass.length()>0)
+		final java.util.Map<String,String> parms=parseParms(parm);
+		if(parms.containsKey("DOMAINSKILLSBYCLASS"))
 		{
-			final Vector charClasses=new Vector();
-			for(final Enumeration c=CMClass.charClasses();c.hasMoreElements();)
+			final String className=parms.get("CHARCLASS");
+			if(className==null)
+				return " @break@";
+			final String domainStr=parms.get("DOMAIN");
+			if(domainStr==null)
+				return " @break@";
+			final CharClass C=CMClass.findCharClass(className);
+			if(C==null)
+				return " @break@";
+			int domain=CMParms.indexOf(Ability.DOMAIN_DESCS, domainStr.toUpperCase().trim());
+			if(domain<0)
+				return " @break@";
+			domain = domain << 5;
+			int levelCap=C.getLevelCap();
+			if(levelCap < 0)
+				levelCap = CMProps.getIntVar(CMProps.Int.LASTPLAYERLEVEL);
+			List<AbilityMapper.AbilityMapping> list = CMLib.ableMapper().getUpToLevelListings(C.ID(), levelCap, true, false);
+			int ct=0;
+			for(AbilityMapper.AbilityMapping mapping : list)
 			{
-				final CharClass C=(CharClass)c.nextElement();
-				if((CMProps.isTheme(C.availabilityCode()))
-				&&(C.baseClass().equals(baseClass))
-				&&(!charClasses.contains(C.ID())))
-					charClasses.addElement(C.ID());
+				Ability A=(Ability)CMClass.getPrototypeByID(CMClass.CMObjectType.ABILITY, mapping.abilityID);
+				if((A.classificationCode()&Ability.ALL_DOMAINS)==domain)
+					ct++;
 			}
-
-			final Vector abilities=new Vector();
-			final Vector levelssum=new Vector();
-			final Vector numberare=new Vector();
-			for(int c=0;c<charClasses.size();c++)
+			buf.append(Integer.toString(ct));
+		}
+		else
+		{
+			String baseClass=httpReq.getUrlParameter("BASECLASS");
+			if(baseClass==null) 
+				return " @break@";
+			if(baseClass.length()>0)
 			{
-				final String className=(String)charClasses.elementAt(c);
-				for(final Enumeration<Ability> a=CMClass.abilities();a.hasMoreElements();)
+				final Vector charClasses=new Vector();
+				for(final Enumeration c=CMClass.charClasses();c.hasMoreElements();)
 				{
-					final Ability A=a.nextElement();
-					final int level=CMLib.ableMapper().getQualifyingLevel(className,true,A.ID());
-					if(level>=0)
+					final CharClass C=(CharClass)c.nextElement();
+					if((CMProps.isTheme(C.availabilityCode()))
+					&&(C.baseClass().equals(baseClass))
+					&&(!charClasses.contains(C.ID())))
+						charClasses.addElement(C.ID());
+				}
+
+				final Vector abilities=new Vector();
+				final Vector levelssum=new Vector();
+				final Vector numberare=new Vector();
+				for(int c=0;c<charClasses.size();c++)
+				{
+					final String className=(String)charClasses.elementAt(c);
+					for(final Enumeration<Ability> a=CMClass.abilities();a.hasMoreElements();)
 					{
-						final int dex=abilities.indexOf(A.ID());
-						if(dex<0)
+						final Ability A=a.nextElement();
+						final int level=CMLib.ableMapper().getQualifyingLevel(className,true,A.ID());
+						if(level>=0)
 						{
-							abilities.addElement(A.ID());
-							levelssum.addElement(Integer.valueOf(level));
-							numberare.addElement(Integer.valueOf(1));
-						}
-						else
-						{
-							final Integer I=(Integer)levelssum.elementAt(dex);
-							levelssum.setElementAt(Integer.valueOf(I.intValue()+level),dex);
-							final Integer I2=(Integer)numberare.elementAt(dex);
-							numberare.setElementAt(Integer.valueOf(I2.intValue()+1),dex);
+							final int dex=abilities.indexOf(A.ID());
+							if(dex<0)
+							{
+								abilities.addElement(A.ID());
+								levelssum.addElement(Integer.valueOf(level));
+								numberare.addElement(Integer.valueOf(1));
+							}
+							else
+							{
+								final Integer I=(Integer)levelssum.elementAt(dex);
+								levelssum.setElementAt(Integer.valueOf(I.intValue()+level),dex);
+								final Integer I2=(Integer)numberare.elementAt(dex);
+								numberare.setElementAt(Integer.valueOf(I2.intValue()+1),dex);
+							}
 						}
 					}
 				}
-			}
 
-			final Vector sortedAbilities=new Vector();
-			while(abilities.size()>0)
-			{
-				double lowAvg=Double.MAX_VALUE;
-				int lowDex=-1;
-				for(int i=0;i<abilities.size();i++)
+				final Vector sortedAbilities=new Vector();
+				while(abilities.size()>0)
 				{
-					final Integer I=(Integer)levelssum.elementAt(i);
-					final Integer I2=(Integer)numberare.elementAt(i);
-					final double avg=CMath.div(I.intValue(),I2.intValue());
-					if(avg<lowAvg)
+					double lowAvg=Double.MAX_VALUE;
+					int lowDex=-1;
+					for(int i=0;i<abilities.size();i++)
 					{
-						lowAvg=avg;
-						lowDex=i;
+						final Integer I=(Integer)levelssum.elementAt(i);
+						final Integer I2=(Integer)numberare.elementAt(i);
+						final double avg=CMath.div(I.intValue(),I2.intValue());
+						if(avg<lowAvg)
+						{
+							lowAvg=avg;
+							lowDex=i;
+						}
+					}
+					if(lowDex>=0)
+					{
+						sortedAbilities.addElement(abilities.elementAt(lowDex));
+						abilities.removeElementAt(lowDex);
+						levelssum.removeElementAt(lowDex);
+						numberare.removeElementAt(lowDex);
 					}
 				}
-				if(lowDex>=0)
-				{
-					sortedAbilities.addElement(abilities.elementAt(lowDex));
-					abilities.removeElementAt(lowDex);
-					levelssum.removeElementAt(lowDex);
-					numberare.removeElementAt(lowDex);
-				}
-			}
 
-			buf.append("<BR><BR><BR><B><H3>"+baseClass+"</H3></B>\n\r");
-			buf.append("<TABLE WIDTH=100% CELLSPACING=0 CELLPADDING=0 BORDER=1>\n\r");
-			buf.append("<TR>");
-			buf.append("<TD><B><FONT COLOR=WHITE>Skill</FONT></B></TD>");
-			for(int c=0;c<charClasses.size();c++)
-			{
-				final String charClass=(String)charClasses.elementAt(c);
-				buf.append("<TD><B><FONT COLOR=WHITE>"+charClass+"</FONT></B></TD>");
-			}
-			buf.append("</TR>\n\r");
-			for(int a=0;a<sortedAbilities.size();a++)
-			{
-				final String able=(String)sortedAbilities.elementAt(a);
-				buf.append("<TR><TD><B><FONT COLOR=WHITE>"+able+"</FONT></B></TD>");
+				buf.append("<BR><BR><BR><B><H3>"+baseClass+"</H3></B>\n\r");
+				buf.append("<TABLE WIDTH=100% CELLSPACING=0 CELLPADDING=0 BORDER=1>\n\r");
+				buf.append("<TR>");
+				buf.append("<TD><B><FONT COLOR=WHITE>Skill</FONT></B></TD>");
 				for(int c=0;c<charClasses.size();c++)
 				{
 					final String charClass=(String)charClasses.elementAt(c);
-					final int level=CMLib.ableMapper().getQualifyingLevel(charClass,true,able);
-					if(level>=0)
-						buf.append("<TD><FONT COLOR=CYAN>"+level+"</FONT></TD>");
-					else
-						buf.append("<TD><BR></TD>");
+					buf.append("<TD><B><FONT COLOR=WHITE>"+charClass+"</FONT></B></TD>");
 				}
 				buf.append("</TR>\n\r");
+				for(int a=0;a<sortedAbilities.size();a++)
+				{
+					final String able=(String)sortedAbilities.elementAt(a);
+					buf.append("<TR><TD><B><FONT COLOR=WHITE>"+able+"</FONT></B></TD>");
+					for(int c=0;c<charClasses.size();c++)
+					{
+						final String charClass=(String)charClasses.elementAt(c);
+						final int level=CMLib.ableMapper().getQualifyingLevel(charClass,true,able);
+						if(level>=0)
+							buf.append("<TD><FONT COLOR=CYAN>"+level+"</FONT></TD>");
+						else
+							buf.append("<TD><BR></TD>");
+					}
+					buf.append("</TR>\n\r");
+				}
+				buf.append("</TABLE>");
 			}
-			buf.append("</TABLE>");
 		}
 		return clearWebMacros(buf);
 	}
