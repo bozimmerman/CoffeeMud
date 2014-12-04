e91a221e345dc8e369939ac40d7a55f66549b128
bozimmerman <bo@zimmers.net>
2014-12-04 10:33:10 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@12685 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/WebMacros/AreaIDEncoded.java
@@ -0,0 +1,64 @@
+package com.planet_ink.coffee_mud.WebMacros;
+
+import com.planet_ink.coffee_web.interfaces.*;
+import com.planet_ink.coffee_mud.core.interfaces.*;
+import com.planet_ink.coffee_mud.core.*;
+import com.planet_ink.coffee_mud.core.collections.*;
+import com.planet_ink.coffee_mud.Abilities.interfaces.*;
+import com.planet_ink.coffee_mud.Areas.interfaces.*;
+import com.planet_ink.coffee_mud.Behaviors.interfaces.*;
+import com.planet_ink.coffee_mud.CharClasses.interfaces.*;
+import com.planet_ink.coffee_mud.Libraries.interfaces.*;
+import com.planet_ink.coffee_mud.Common.interfaces.*;
+import com.planet_ink.coffee_mud.Exits.interfaces.*;
+import com.planet_ink.coffee_mud.Items.interfaces.*;
+import com.planet_ink.coffee_mud.Locales.interfaces.*;
+import com.planet_ink.coffee_mud.MOBS.interfaces.*;
+import com.planet_ink.coffee_mud.Races.interfaces.*;
+
+import java.net.URLEncoder;
+import java.util.*;
+
+/*
+   Copyright 2003-2014 Bo Zimmerman
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+	   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+public class AreaIDEncoded extends StdWebMacro
+{
+	@Override public String name() { return "AreaIDEncoded"; }
+
+	@Override
+	public String runMacro(HTTPRequest httpReq, String parm)
+	{
+		final String last=httpReq.getUrlParameter("AREA");
+		if(last==null)
+			return " @break@";
+		if(last.length()>0)
+		{
+			final Area A=MUDGrinder.getAreaObject(last);
+			if(A!=null)
+			{
+				try
+				{
+					return clearWebMacros(URLEncoder.encode(last,"UTF-8"));
+				}
+				catch(final java.io.UnsupportedEncodingException e)
+				{
+					Log.errOut(name(),"Wrong Encoding");
+				}
+			}
+		}
+		return "";
+	}
+}
