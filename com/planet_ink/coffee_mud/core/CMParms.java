ccaa25b7f3556f1a3011063dcab5432290fd7e98
bozimmerman <bo@zimmers.net>
2015-02-18 08:34:49 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@12833 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/core/CMParms.java
+++ com/planet_ink/coffee_mud/core/CMParms.java
@@ -939,8 +939,8 @@ public class CMParms
 						text=text.substring(x);
 						x=0;
 						while((x<text.length())
-							&&((!endWithQuote)&&(!Character.isWhitespace(text.charAt(x)))&&(text.charAt(x)!=';')&&(text.charAt(x)!=','))
-								||((endWithQuote)&&(text.charAt(x)!='\"')))
+							&&(((!endWithQuote)&&(!Character.isWhitespace(text.charAt(x)))&&(text.charAt(x)!=';')&&(text.charAt(x)!=','))
+								||((endWithQuote)&&(text.charAt(x)!='\"'))))
 									x++;
 						return text.substring(0,x).trim();
 					}
