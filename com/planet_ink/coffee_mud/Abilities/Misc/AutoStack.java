4069bbe9a9cf0ff3159754fd0fa40e865d04f699
bozimmerman <bo@zimmers.net>
2015-10-11 08:50:41 +0000

    Working on an auto-item stacker.
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@13074 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Abilities/Misc/AutoStack.java
@@ -0,0 +1,286 @@
+package com.planet_ink.coffee_mud.Abilities.Misc;
+
+import com.planet_ink.coffee_mud.Abilities.StdAbility;
+import com.planet_ink.coffee_mud.core.interfaces.*;
+import com.planet_ink.coffee_mud.core.*;
+import com.planet_ink.coffee_mud.core.collections.*;
+import com.planet_ink.coffee_mud.Abilities.interfaces.*;
+import com.planet_ink.coffee_mud.Areas.interfaces.*;
+import com.planet_ink.coffee_mud.Behaviors.interfaces.*;
+import com.planet_ink.coffee_mud.CharClasses.interfaces.*;
+import com.planet_ink.coffee_mud.Commands.interfaces.*;
+import com.planet_ink.coffee_mud.Common.interfaces.*;
+import com.planet_ink.coffee_mud.Exits.interfaces.*;
+import com.planet_ink.coffee_mud.Items.interfaces.*;
+import com.planet_ink.coffee_mud.Libraries.interfaces.*;
+import com.planet_ink.coffee_mud.Locales.interfaces.*;
+import com.planet_ink.coffee_mud.MOBS.interfaces.*;
+import com.planet_ink.coffee_mud.MOBS.interfaces.MOB.Attrib;
+import com.planet_ink.coffee_mud.Races.interfaces.*;
+
+import java.lang.ref.WeakReference;
+import java.util.*;
+
+/*
+ Copyright 2014-2015 Bo Zimmerman
+
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
+
+ http://www.apache.org/licenses/LICENSE-2.0
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
+public class AutoStack extends StdAbility
+{
+	@Override
+	public String ID()
+	{
+		return "AutoStack";
+	}
+
+	private final static String	localizedName	= CMLib.lang().L("AutoStack");
+
+	@Override
+	public String name()
+	{
+		return localizedName;
+	}
+
+	@Override
+	protected int canAffectCode()
+	{
+		return Ability.CAN_ROOMS | Ability.CAN_AREAS;
+	}
+
+	@Override
+	protected int canTargetCode()
+	{
+		return 0;
+	}
+
+	protected final static int	INTERVAL				= (2 * 60) / 4000;
+
+	protected int				numberOfItemsToTrigger	= 1000;
+	protected int				intervalTicks			= INTERVAL;
+	protected long				nextTriggeringTime		= System.currentTimeMillis() + (INTERVAL * 4000);
+	protected int				numberInGroupToTrigger	= 10;
+
+	@Override
+	public void setMiscText(String newMiscText)
+	{
+		super.setMiscText(newMiscText);
+		numberOfItemsToTrigger = CMParms.getParmInt(newMiscText, "TRIGGERCOUNT", 1000);
+		numberInGroupToTrigger = CMParms.getParmInt(newMiscText, "GROUPCOUNT", 10);
+		intervalTicks = CMParms.getParmInt(newMiscText, "CHECKTICKS", INTERVAL);
+		tickDown = intervalTicks;
+		nextTriggeringTime = System.currentTimeMillis() + (CMProps.getTickMillis() * tickDown);
+	}
+
+	protected synchronized void packageAll(Room R)
+	{
+		if (R == null)
+			return;
+
+		if (R.numItems() < numberOfItemsToTrigger)
+			return;
+
+		final Map<String, List<Item>> groupedByName = new TreeMap<String, List<Item>>();
+		final List<PackagedItems> oldPackages = new LinkedList<PackagedItems>();
+		for (Enumeration<Item> i = R.items(); i.hasMoreElements();)
+		{
+			final Item I = i.nextElement();
+			if (I instanceof PackagedItems)
+				oldPackages.add((PackagedItems) I);
+			else if ((I != null) && (!(I instanceof RawMaterial)))
+			{
+				List<Item> itemList = groupedByName.get(I.name());
+				if (itemList == null)
+				{
+					itemList = new LinkedList<Item>();
+					groupedByName.put(I.name(), itemList);
+				}
+				itemList.add(I);
+			}
+		}
+
+		for (final Iterator<List<Item>> i = groupedByName.values().iterator(); i.hasNext();)
+		{
+			final List<Item> itemList = i.next();
+			if (itemList.size() < numberInGroupToTrigger)
+				i.remove();
+			else
+			{
+				final List<Pair<Item, List<Item>>> sameItems = new LinkedList<Pair<Item, List<Item>>>();
+				boolean winner = false;
+				for (final Item I : itemList)
+				{
+					winner = false;
+					for (final Pair<Item, List<Item>> trialList : sameItems)
+					{
+						if (trialList.first.sameAs(I))
+						{
+							winner = true;
+							trialList.second.add(I);
+						}
+					}
+					if (!winner)
+					{
+						final LinkedList<Item> newList = new LinkedList<Item>();
+						newList.add(I);
+						sameItems.add(new Pair<Item, List<Item>>(I, newList));
+					}
+				}
+				for (final Iterator<Pair<Item, List<Item>>> t = sameItems.iterator(); t.hasNext();)
+				{
+					final Pair<Item, List<Item>> set = t.next();
+					if (set.second.size() < numberInGroupToTrigger)
+						t.remove();
+				}
+				if (sameItems.size() > 0)
+				{
+					final List<Pair<Item, PackagedItems>> oldPackChecks = new LinkedList<Pair<Item, PackagedItems>>();
+					try
+					{
+						for (final PackagedItems P : oldPackages)
+							oldPackChecks.add(new Pair<Item, PackagedItems>(P.peekFirstItem(), P));
+						oldPackages.clear();
+						final List<PackagedItems> newPackages = new LinkedList<PackagedItems>();
+						for (final Iterator<Pair<Item, List<Item>>> t = sameItems.iterator(); t.hasNext();)
+						{
+							final Pair<Item, List<Item>> set = t.next();
+							if (set.second.size() < numberInGroupToTrigger)
+								t.remove();
+							else
+							{
+								boolean alreadyPacked = false;
+								for (Pair<Item, PackagedItems> oldPackPair : oldPackChecks)
+								{
+									if (set.first.name().equals(oldPackPair.first.name()))
+									{
+										if (set.first.sameAs(oldPackPair.first))
+										{
+											alreadyPacked = true;
+											oldPackPair.second.setNumberOfItemsInPackage(oldPackPair.second.numberOfItems() + set.second.size());
+										}
+									}
+								}
+								if (!alreadyPacked)
+								{
+									final PackagedItems newPack = (PackagedItems) CMClass.getBasicItem("GenPackagedStack");
+									newPack.packageMe(set.first, set.second.size());
+									long time = set.first.expirationDate();
+									if (time != 0)
+									{
+										for (final Item I : set.second)
+										{
+											if (I.expirationDate() == 0)
+											{
+												time = 0;
+												break;
+											}
+											else if (I.expirationDate() > time)
+												time = I.expirationDate();
+										}
+									}
+									newPack.setExpirationDate(time);
+									newPackages.add(newPack);
+								}
+								for (final Item I : set.second)
+								{
+									I.destroy();
+								}
+							}
+						}
+						for (final PackagedItems newPack : newPackages)
+						{
+							R.addItem(newPack); // yay!
+						}
+					}
+					finally
+					{
+						for (final Pair<Item, PackagedItems> oldPackPair : oldPackChecks)
+							oldPackPair.first.destroy();
+					}
+				}
+			}
+		}
+	}
+
+	@Override
+	public void executeMsg(Environmental myHost, CMMsg msg)
+	{
+		if(affected instanceof Room) // because areas can rely on ticks
+		{
+			if(System.currentTimeMillis() >= nextTriggeringTime)
+			{
+				final Room R=(Room)affected;
+				if((R!=null)&&(!R.amDestroyed()))
+				{
+					if(CMProps.getBoolVar(CMProps.Bool.MUDSTARTED)
+					&&(!CMProps.getBoolVar(CMProps.Bool.MUDSHUTTINGDOWN)))
+					{
+						tickDown = intervalTicks;
+						nextTriggeringTime = System.currentTimeMillis() + (CMProps.getTickMillis() * tickDown);
+						this.packageAll(R);
+						tickDown = intervalTicks;
+						nextTriggeringTime = System.currentTimeMillis() + (CMProps.getTickMillis() * tickDown);
+					}
+				}
+			}
+		}
+		super.executeMsg(myHost, msg);
+	}
+	
+	@Override
+	public boolean tick(Tickable ticking, int tickID)
+	{
+		if(affected instanceof Room)
+		{
+			final Room R=(Room)affected;
+			if((R==null)||(R.amDestroyed()))
+				return false;
+			
+			if (tickID == Tickable.TICKID_MOB)
+			{
+				if((--tickDown)<=0)
+				{
+					tickDown = intervalTicks;
+					nextTriggeringTime = System.currentTimeMillis() + (CMProps.getTickMillis() * tickDown);
+					this.packageAll(R);
+					tickDown = intervalTicks;
+					nextTriggeringTime = System.currentTimeMillis() + (CMProps.getTickMillis() * tickDown);
+				}
+			}
+			return true;
+		}
+		else
+		if(affected instanceof Area)
+		{
+			final Area A=(Area)affected;
+			if((A==null)||(A.amDestroyed()))
+				return false;
+			
+			if (tickID == Tickable.TICKID_MOB)
+			{
+				if((--tickDown)<=0)
+				{
+					tickDown = intervalTicks;
+					nextTriggeringTime = System.currentTimeMillis() + (CMProps.getTickMillis() * tickDown);
+					for(final Enumeration<Room> r=A.getMetroMap();r.hasMoreElements();)
+					{
+						this.packageAll(r.nextElement());
+					}
+					tickDown = intervalTicks;
+					nextTriggeringTime = System.currentTimeMillis() + (CMProps.getTickMillis() * tickDown);
+				}
+			}
+		}
+		return false;
+	}
+}
--- com/planet_ink/coffee_mud/Items/Basic/GenPackagedItems.java
+++ com/planet_ink/coffee_mud/Items/Basic/GenPackagedItems.java
@@ -1,4 +1,5 @@
 package com.planet_ink.coffee_mud.Items.Basic;
+
 import com.planet_ink.coffee_mud.core.interfaces.*;
 import com.planet_ink.coffee_mud.core.*;
 import com.planet_ink.coffee_mud.core.collections.*;
@@ -19,23 +20,28 @@ import java.util.*;
 import com.planet_ink.coffee_mud.Libraries.interfaces.*;
 
 /*
-   Copyright 2005-2015 Bo Zimmerman
+ Copyright 2005-2015 Bo Zimmerman
 
-   Licensed under the Apache License, Version 2.0 (the "License");
-   you may not use this file except in compliance with the License.
-   You may obtain a copy of the License at
+ Licensed under the Apache License, Version 2.0 (the "License");
+ you may not use this file except in compliance with the License.
+ You may obtain a copy of the License at
 
-	   http://www.apache.org/licenses/LICENSE-2.0
+ http://www.apache.org/licenses/LICENSE-2.0
 
-   Unless required by applicable law or agreed to in writing, software
-   distributed under the License is distributed on an "AS IS" BASIS,
-   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-   See the License for the specific language governing permissions and
-   limitations under the License.
-*/
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ */
 public class GenPackagedItems extends GenItem implements PackagedItems
 {
-	@Override public String ID(){ return "GenPackagedItems";}
+	@Override
+	public String ID()
+	{
+		return "GenPackagedItems";
+	}
+
 	public GenPackagedItems()
 	{
 		super();
@@ -43,38 +49,76 @@ public class GenPackagedItems extends GenItem implements PackagedItems
 		basePhyStats.setWeight(150);
 		setDisplayText("");
 		setDescription("");
-		baseGoldValue=5;
+		baseGoldValue = 5;
 		basePhyStats().setLevel(1);
 		setMaterial(RawMaterial.RESOURCE_MEAT);
 		recoverPhyStats();
 	}
-	@Override protected boolean abilityImbuesMagic(){return false;}
-	@Override public String name(){return "a package of "+numberOfItemsInPackage()+" "+Name().trim()+"(s)";}
-	@Override public String displayText(){return "a package of "+numberOfItemsInPackage()+" "+Name().trim()+"(s) sit here.";}
-	@Override public int numberOfItemsInPackage(){return basePhyStats().ability();}
-	@Override public void setNumberOfItemsInPackage(int number){basePhyStats().setAbility(number);phyStats().setAbility(number);}
-	protected byte[]	readableText=null;
-	@Override public String readableText(){return readableText==null?"":CMLib.encoder().decompressString(readableText);}
-	@Override public void setReadableText(String text){readableText=(text.trim().length()==0)?null:CMLib.encoder().compressString(text);}
+
+	@Override
+	protected boolean abilityImbuesMagic()
+	{
+		return false;
+	}
+
+	@Override
+	public String name()
+	{
+		return "a package of " + numberOfItemsInPackage() + " " + Name().trim() + "(s)";
+	}
+
+	@Override
+	public String displayText()
+	{
+		return "a package of " + numberOfItemsInPackage() + " " + Name().trim() + "(s) sit here.";
+	}
+
+	@Override
+	public int numberOfItemsInPackage()
+	{
+		return basePhyStats().ability();
+	}
+
+	@Override
+	public void setNumberOfItemsInPackage(int number)
+	{
+		basePhyStats().setAbility(number);
+		phyStats().setAbility(number);
+	}
+
+	protected byte[]	readableText	= null;
+
+	@Override
+	public String readableText()
+	{
+		return readableText == null ? "" : CMLib.encoder().decompressString(readableText);
+	}
+
+	@Override
+	public void setReadableText(String text)
+	{
+		readableText = (text.trim().length() == 0) ? null : CMLib.encoder().compressString(text);
+	}
+
 	@Override
 	public boolean packageMe(Item I, int number)
 	{
-		if((I==null)
-		||(!CMLib.utensils().disInvokeEffects(I))
-		||(I.amDestroyed()))
+		if ((I == null) 
+		|| (!CMLib.utensils().disInvokeEffects(I)) 
+		|| (I.amDestroyed()))
 			return false;
-		name=CMLib.english().cleanArticles(I.Name());
-		displayText="";
-		setDescription("The contents of the package appears as follows:\n\r"+I.description());
+		name = CMLib.english().cleanArticles(I.Name());
+		displayText = "";
+		setDescription("The contents of the stack appears as follows:\n\r" + I.description());
 		basePhyStats().setLevel(I.basePhyStats().level());
-		basePhyStats().setWeight(I.basePhyStats().weight()*number);
+		basePhyStats().setWeight(I.basePhyStats().weight() * number);
 		basePhyStats().setHeight(I.basePhyStats().height());
 		setMaterial(I.material());
-		setBaseValue(I.baseGoldValue()*number);
-		final StringBuffer itemstr=new StringBuffer("");
+		setBaseValue(I.baseGoldValue() * number);
+		final StringBuffer itemstr = new StringBuffer("");
 		itemstr.append("<PAKITEM>");
-		itemstr.append(CMLib.xml().convertXMLtoTag("PICLASS",CMClass.classID(I)));
-		itemstr.append(CMLib.xml().convertXMLtoTag("PIDATA",CMLib.coffeeMaker().getPropertiesStr(I,true)));
+		itemstr.append(CMLib.xml().convertXMLtoTag("PICLASS", CMClass.classID(I)));
+		itemstr.append(CMLib.xml().convertXMLtoTag("PIDATA", CMLib.coffeeMaker().getPropertiesStr(I, true)));
 		itemstr.append("</PAKITEM>");
 		setPackageText(itemstr.toString());
 		setNumberOfItemsInPackage(number);
@@ -85,16 +129,18 @@ public class GenPackagedItems extends GenItem implements PackagedItems
 	@Override
 	public boolean isPackagable(List<Item> V)
 	{
-		if(V==null)
+		if (V == null)
 			return false;
-		if(V.size()==0)
+		if (V.size() == 0)
 			return false;
-		for(int v1=0;v1<V.size();v1++)
+		for (int v1 = 0; v1 < V.size(); v1++)
 		{
-			final Item I=V.get(v1);
-			for(int v2=v1+1;v2<V.size();v2++)
-				if(!V.get(v2).sameAs(I))
+			final Item I = V.get(v1);
+			for (int v2 = v1 + 1; v2 < V.size(); v2++)
+			{
+				if (!V.get(v2).sameAs(I))
 					return false;
+			}
 		}
 		return true;
 	}
@@ -102,73 +148,75 @@ public class GenPackagedItems extends GenItem implements PackagedItems
 	@Override
 	public Item peekFirstItem()
 	{
-		if(packageText().length()==0)
+		if (packageText().length() == 0)
 			return null;
-		final List<XMLLibrary.XMLpiece> buf=CMLib.xml().parseAllXML(packageText());
-		if(buf==null)
+		final List<XMLLibrary.XMLpiece> buf = CMLib.xml().parseAllXML(packageText());
+		if (buf == null)
 		{
-			Log.errOut("Packaged","Error parsing 'PAKITEM'.");
+			Log.errOut("Packaged", "Error parsing 'PAKITEM'.");
 			return null;
 		}
-		final XMLLibrary.XMLpiece iblk=CMLib.xml().getPieceFromPieces(buf,"PAKITEM");
-		if((iblk==null)||(iblk.contents==null))
+		final XMLLibrary.XMLpiece iblk = CMLib.xml().getPieceFromPieces(buf, "PAKITEM");
+		if ((iblk == null) || (iblk.contents == null))
 		{
-			Log.errOut("Packaged","Error parsing 'PAKITEM'.");
+			Log.errOut("Packaged", "Error parsing 'PAKITEM'.");
 			return null;
 		}
-		final String itemi=CMLib.xml().getValFromPieces(iblk.contents,"PICLASS");
-		final Environmental newOne=CMClass.getItem(itemi);
-		final List<XMLLibrary.XMLpiece> idat=CMLib.xml().getContentsFromPieces(iblk.contents,"PIDATA");
-		if((idat==null)||(newOne==null)||(!(newOne instanceof Item)))
+		final String itemi = CMLib.xml().getValFromPieces(iblk.contents, "PICLASS");
+		final Environmental newOne = CMClass.getItem(itemi);
+		final List<XMLLibrary.XMLpiece> idat = CMLib.xml().getContentsFromPieces(iblk.contents, "PIDATA");
+		if ((idat == null) || (newOne == null) || (!(newOne instanceof Item)))
 		{
-			Log.errOut("Packaged","Error parsing 'PAKITEM' data.");
+			Log.errOut("Packaged", "Error parsing 'PAKITEM' data.");
 			return null;
 		}
-		CMLib.coffeeMaker().setPropertiesStr(newOne,idat,true);
-		return (Item)newOne;
+		CMLib.coffeeMaker().setPropertiesStr(newOne, idat, true);
+		return (Item) newOne;
 	}
 
 	@Override
 	public List<Item> unPackage(int number)
 	{
-		final List<Item> V=new Vector<Item>();
-		if(number>=numberOfItemsInPackage())
-			number=numberOfItemsInPackage();
-		if(number<=0)
+		final List<Item> V = new Vector<Item>();
+		if (number >= numberOfItemsInPackage())
+			number = numberOfItemsInPackage();
+		if (number <= 0)
 			return V;
-		final int itemWeight=basePhyStats().weight()/numberOfItemsInPackage();
-		final int itemValue=baseGoldValue()/numberOfItemsInPackage();
-		final Item I=peekFirstItem();
-		if(I==null)
+		final int itemWeight = basePhyStats().weight() / numberOfItemsInPackage();
+		final int itemValue = baseGoldValue() / numberOfItemsInPackage();
+		final Item I = peekFirstItem();
+		if (I == null)
 			return V;
 		I.recoverPhyStats();
-		for(int i=0;i<number;i++)
-			V.add((Item)I.copyOf());
-		setNumberOfItemsInPackage(numberOfItemsInPackage()-number);
-		if(numberOfItemsInPackage()<=0)
+		for (int i = 0; i < number; i++)
+			V.add((Item) I.copyOf());
+		setNumberOfItemsInPackage(numberOfItemsInPackage() - number);
+		if (numberOfItemsInPackage() <= 0)
 		{
 			destroy();
-		   return V;
+			return V;
 		}
-		basePhyStats().setWeight(itemWeight*number);
-		setBaseValue(itemValue*number);
+		basePhyStats().setWeight(itemWeight * number);
+		setBaseValue(itemValue * number);
 		recoverPhyStats();
 		return V;
 	}
+
 	@Override
 	public String packageText()
 	{
 		return CMLib.xml().restoreAngleBrackets(readableText());
 	}
+
 	@Override
 	public void setPackageText(String text)
 	{
 		setReadableText(CMLib.xml().parseOutAngleBrackets(text));
-		CMLib.flags().setReadable(this,false);
+		CMLib.flags().setReadable(this, false);
 	}
-	
+
 	@Override
-	public int getPackageFlagsBitmap() 
+	public int getPackageFlagsBitmap()
 	{
 		return 0;
 	}
