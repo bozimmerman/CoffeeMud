a4485e91ced1eccc63b46b9d759bb2c06259fa88
bozimmerman <bo@zimmers.net>
2015-12-10 02:00:22 +0000

    Fixed longstanding pet bug.
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@13310 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/MOBS/StdMOB.java
+++ com/planet_ink/coffee_mud/MOBS/StdMOB.java
@@ -1352,7 +1352,7 @@ public class StdMOB implements MOB
 					return false;
 			}
 		}
-		if (CMLib.flags().isUnattackable(mob) && (mob.getVictim() != this))
+		if (CMLib.flags().isUnattackable(mob) && (!mob.isInCombat()))
 			return false;
 		if (targetIsMonster || iAmMonster)
 			return true;
