5b9f1e857dae54be1bb85aadde58fb51c127e66b
bozimmerman <bo@zimmers.net>
2015-12-25 20:30:55 +0000

    More javadoccing CMMiscUtils
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@13412 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Libraries/interfaces/CMMiscUtils.java
+++ com/planet_ink/coffee_mud/Libraries/interfaces/CMMiscUtils.java
@@ -82,13 +82,83 @@ public interface CMMiscUtils extends CMLibrary
 	 * @return the readable comma list.
 	 */
 	public String niceCommaList(List<?> V, boolean andTOrF);
+	
+	/**
+	 * This strange method takes a list of space-delimited expressions of the
+	 * form [CONDITION]number number number number, etc. E.G.: >1 3 2 5 3 2.
+	 * Each list must contain the given num of digits. If the condition falls
+	 * within the given start of range and end of range, then the condition
+	 * range of entries in the returned array is populated with the values on
+	 * that row.
+	 * @param condV the list of fully expressions
+	 * @param numDigits the min number of digits in each expression
+	 * @param startOfRange the starting range to return
+	 * @param endOfRange the ending range to return > startOfRange
+	 * @return the list of number lists
+	 */
 	public long[][] compileConditionalRange(List<String> condV, int numDigits, final int startOfRange, final int endOfRange);
 
+	/**
+	 * Outfits the given mob with the list of given items.  If the mob
+	 * does not have an item with the same name as one on the list, it 
+	 * is given to them magically.  If they can, they will then wear it.
+	 * @param mob the mob to outfit
+	 * @param items the items to outfit the mob with
+	 */
 	public void outfit(MOB mob, List<Item> items);
+	
+	/**
+	 * Returns the language being spoke by the given object (mob, usually).
+	 * A null return usually means Common. 
+	 * @param P the mob to check
+	 * @return the language being spoken.
+	 */
 	public Language getLanguageSpoken(Physical P);
+	
+	/**
+	 * Returns whether the given Item is reachable by the given mob.
+	 * It may not be reachable if the mob is riding something, and the
+	 * item is on the ground, etc.  If the object given is not an
+	 * item, or null, true is always returned.
+	 * @param mob the mob who wants to reach
+	 * @param E the Item to reach for
+	 * @return true, or false if it's unreachable
+	 */
 	public boolean reachableItem(MOB mob, Environmental E);
+	
+	/**
+	 * Recursively extinguishes everything from the given target on down.
+	 * If a room is given, everything in the room, including the room,
+	 * is extinguished.   Ordinary fires and torches also go out.  
+	 * The mundane flag is to prevent extinguishing elementals and magic
+	 * flame spells.
+	 * @param source the mob doing the extinguishing
+	 * @param target the thing to extinguish
+	 * @param mundane true to skip magic and elemental targets
+	 */
 	public void extinguish(MOB source, Physical target, boolean mundane);
+	
+	/**
+	 * Given the allowedArmorLevel code and the mob, this method returns
+	 * whether the given mob is only wearing permitted items on the
+	 * applicable armor slots.  
+	 * @see CharClass#ARMOR_DESCS
+	 * @param mob the mob to check
+	 * @param allowedArmorLevel the allowed armor level
+	 * @return true if the mob is good to go, false otherwise
+	 */
 	public boolean armorCheck(MOB mob, int allowedArmorLevel);
+	
+	/**
+	 * Given the allowedArmorLevel code and the mob, this method returns
+	 * whether the given mob is permitted to wear the given item is
+	 * applicable armor slots given the armor level code.  
+	 * @see CharClass#ARMOR_DESCS
+	 * @param mob the mob to check
+	 * @param I the item to check
+	 * @param allowedArmorLevel the allowed armor level
+	 * @return true if the mob is good to go for that item, false otherwise
+	 */
 	public boolean armorCheck(MOB mob, Item I, int allowedArmorLevel);
 	public void recursiveDropMOB(MOB mob, Room room, Item thisContainer, boolean bodyFlag);
 	public List<Item> deepCopyOf(Item theContainer);
