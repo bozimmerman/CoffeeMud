4a6627cff146760ea5b3228f1c70ae79888d0d66
bozimmerman <bo@zimmers.net>
2016-01-03 19:48:43 +0000

    Welding.
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@13494 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Abilities/Common/Welding.java
@@ -0,0 +1,105 @@
+package com.planet_ink.coffee_mud.Abilities.Common;
+import com.planet_ink.coffee_mud.core.interfaces.*;
+import com.planet_ink.coffee_mud.core.interfaces.ItemPossessor.Expire;
+import com.planet_ink.coffee_mud.core.*;
+import com.planet_ink.coffee_mud.core.collections.*;
+import com.planet_ink.coffee_mud.Abilities.Common.BuildingSkill.Building;
+import com.planet_ink.coffee_mud.Abilities.interfaces.*;
+import com.planet_ink.coffee_mud.Areas.interfaces.*;
+import com.planet_ink.coffee_mud.Behaviors.interfaces.*;
+import com.planet_ink.coffee_mud.CharClasses.interfaces.*;
+import com.planet_ink.coffee_mud.Commands.interfaces.*;
+import com.planet_ink.coffee_mud.Common.interfaces.*;
+import com.planet_ink.coffee_mud.Exits.interfaces.*;
+import com.planet_ink.coffee_mud.Items.interfaces.*;
+import com.planet_ink.coffee_mud.Libraries.interfaces.ListingLibrary;
+import com.planet_ink.coffee_mud.Libraries.interfaces.TrackingLibrary;
+import com.planet_ink.coffee_mud.Locales.interfaces.*;
+import com.planet_ink.coffee_mud.MOBS.interfaces.*;
+import com.planet_ink.coffee_mud.Races.interfaces.*;
+
+import java.util.*;
+
+
+/*
+   Copyright 2015-2015 Bo Zimmerman
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+	   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+
+public class Welding extends BuildingSkill
+{
+	@Override
+	public String ID()
+	{
+		return "Welding";
+	}
+
+	private final static String	localizedName	= CMLib.lang().L("Welding");
+
+	@Override
+	public String name()
+	{
+		return localizedName;
+	}
+
+	private static final String[]	triggerStrings	= I(new String[] { "WELDING" });
+
+	@Override
+	public String[] triggerStrings()
+	{
+		return triggerStrings;
+	}
+
+	@Override
+	public String supportedResourceString()
+	{
+		return "METAL|MITHRIL";
+	}
+
+	@Override
+	public String parametersFile()
+	{
+		return "welding.txt";
+	}
+
+	@Override
+	protected String getMainResourceName()
+	{
+		return "Metal";
+	}
+
+	@Override
+	protected String getSoundName()
+	{
+		return "welding.wav";
+	}
+
+	public Welding()
+	{
+		super();
+	}
+
+	@Override
+	protected int[][] getBasicMaterials(final MOB mob, int woodRequired)
+	{
+		final int[] pm={RawMaterial.MATERIAL_METAL,RawMaterial.MATERIAL_MITHRIL};
+		final int[][] idata=fetchFoundResourceData(mob,
+													woodRequired,"metal",pm,
+													0,null,null,
+													false,
+													0,null);
+		return idata;
+	}
+}
