73c8e7479810985924dbe6b918c18ac3488323af
bozimmerman <bo@zimmers.net>
2016-03-24 03:04:45 +0000

    Fix to room resets.
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@13926 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Locales/StdRoom.java
+++ com/planet_ink/coffee_mud/Locales/StdRoom.java
@@ -1977,8 +1977,6 @@ public class StdRoom implements Room
 	@Override
 	public void addInhabitant(MOB mob)
 	{
-if(roomID().equals("New Area#102"))
-	System.out.println(mob.name()+": "+mob);
 		inhabitants.addElement(mob);
 	}
 
