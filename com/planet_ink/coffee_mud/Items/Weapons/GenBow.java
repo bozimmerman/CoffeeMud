c9ae4708984fa76bd3d794ff56552539cbdec038
bozimmerman <bo@zimmers.net>
2016-04-26 01:15:21 +0000

    Fixed GenBow
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@14008 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Items/Weapons/GenBow.java
+++ com/planet_ink/coffee_mud/Items/Weapons/GenBow.java
@@ -15,7 +15,6 @@ import com.planet_ink.coffee_mud.Locales.interfaces.*;
 import com.planet_ink.coffee_mud.MOBS.interfaces.*;
 import com.planet_ink.coffee_mud.Races.interfaces.*;
 
-
 import java.util.*;
 
 /*
@@ -42,6 +41,7 @@ public class GenBow extends StdBow
 	}
 
 	protected String	readableText="";
+
 	public GenBow()
 	{
 		super();
@@ -85,6 +85,108 @@ public class GenBow extends StdBow
 		miscText="";
 		CMLib.coffeeMaker().setPropertiesStr(this,newText,false);
 		recoverPhyStats();
+	}
+	
+	private final static String[] MYCODES ={"MINRANGE","MAXRANGE","WEAPONTYPE","WEAPONCLASS",
+											"AMMOTYPE","AMMOCAPACITY"};
+
+	@Override
+	public String getStat(String code)
+	{
+		if(CMLib.coffeeMaker().getGenItemCodeNum(code)>=0)
+			return CMLib.coffeeMaker().getGenItemStat(this,code);
+		switch(getCodeNum(code))
+		{
+		case 0:
+			return "" + minRange();
+		case 1:
+			return "" + maxRange();
+		case 2:
+			return "" + weaponDamageType();
+		case 3:
+			return "" + weaponClassification();
+		case 4:
+			return ammunitionType();
+		case 5:
+			return "" + ammunitionCapacity();
+		default:
+			return CMProps.getStatCodeExtensionValue(getStatCodes(), xtraValues, code);
+		}
+	}
+
+	@Override
+	public void setStat(String code, String val)
+	{
+		if(CMLib.coffeeMaker().getGenItemCodeNum(code)>=0)
+			CMLib.coffeeMaker().setGenItemStat(this,code,val);
+		else
+		switch(getCodeNum(code))
+		{
+		case 0:
+			setRanges(CMath.s_parseIntExpression(val), maxRange());
+			break;
+		case 1:
+			setRanges(minRange(), CMath.s_parseIntExpression(val));
+			break;
+		case 2:
+			setWeaponDamageType(CMath.s_parseListIntExpression(Weapon.TYPE_DESCS, val));
+			break;
+		case 3:
+			setWeaponClassification(CMath.s_parseListIntExpression(Weapon.CLASS_DESCS, val));
+			break;
+		case 4:
+			setAmmunitionType(val);
+			break;
+		case 5:
+			setAmmoCapacity(CMath.s_parseIntExpression(val));
+			break;
+		default:
+			CMProps.setStatCodeExtensionValue(getStatCodes(), xtraValues, code, val);
+			break;
+		}
+	}
+
+	@Override
+	protected int getCodeNum(String code)
+	{
+		for(int i=0;i<MYCODES.length;i++)
+		{
+			if(code.equalsIgnoreCase(MYCODES[i]))
+				return i;
+		}
+		return -1;
+	}
+
+	private static String[] codes=null;
+
+	@Override
+	public String[] getStatCodes()
+	{
+		if(codes!=null)
+			return codes;
+		final String[] MYCODES=CMProps.getStatCodesList(GenBow.MYCODES,this);
+		final String[] superCodes=CMParms.toStringArray(GenericBuilder.GenItemCode.values());
+		codes=new String[superCodes.length+MYCODES.length];
+		int i=0;
+		for(;i<superCodes.length;i++)
+			codes[i]=superCodes[i];
+		for(int x=0;x<MYCODES.length;i++,x++)
+			codes[i]=MYCODES[x];
+		return codes;
+	}
+
+	@Override
+	public boolean sameAs(Environmental E)
+	{
+		if(!(E instanceof GenBow))
+			return false;
+		final String[] codes=getStatCodes();
+		for(int i=0;i<codes.length;i++)
+		{
+			if(!E.getStat(codes[i]).equals(getStat(codes[i])))
+				return false;
+		}
+		return true;
 	}
 }
 
