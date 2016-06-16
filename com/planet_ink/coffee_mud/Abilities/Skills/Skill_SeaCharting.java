76b3fdfea01f3ee6f21ed5ab83d6d17dd4ed04b5
bozimmerman <bo@zimmers.net>
2016-06-16 01:10:46 +0000

    Almost done with sea charting.
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@14227 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Abilities/Skills/Skill_SeaCharting.java
+++ com/planet_ink/coffee_mud/Abilities/Skills/Skill_SeaCharting.java
@@ -106,22 +106,139 @@ public class Skill_SeaCharting extends StdSkill
 			return false;
 		}
 
+		if(commands.size()==0)
+		{
+			mob.tell(L("You did not specify whether you wanted to ADD your current location, LIST existing locations, or REMOVE [X} an old chart point.  Try adding ADD, REMOVE X, or LIST."));
+			return false;
+		}
+		String cmd=commands.get(0).toString().toUpperCase().trim();
+		if((!cmd.equals("LIST"))
+		&&(!cmd.equals("REMOVE"))
+		&&(!cmd.equals("ADD")))
+		{
+			mob.tell(L("'@x1' is not a valid argument.  Try ADD, LIST, or REMOVE X, where X is a number.",cmd));
+			return false;
+		}
+		
+		List<String> rooms=CMParms.parseAny(text(),';',true);
+		int removeNum=-1;
+		if(cmd.equals("REMOVE"))
+		{
+			if(rooms.size()==0)
+			{
+				mob.tell(L("There are no chart points to remove.  Try LIST."));
+				return false;
+			}
+			if(commands.size()<2)
+			{
+				mob.tell(L("You must specify which chart point to remove.  Try LIST."));
+				return false;
+			}
+			if(!CMath.isInteger(commands.get(1)))
+			{
+				mob.tell(L("'@x1' is not a valid chart point number to remove.   Try LIST."));
+				return false;
+			}
+			removeNum=CMath.s_int(commands.get(1));
+			if((removeNum<1)||(removeNum>rooms.size()))
+			{
+				mob.tell(L("'@x1' is not a valid chart point number to remove.   Try LIST."));
+				return false;
+			}
+			removeNum--;
+		}
+
+		if(cmd.equalsIgnoreCase("LIST"))
+		{
+			for(int i=0;i<rooms.size();i++)
+			{
+				final Room room=CMLib.map().getRoom(rooms.get(i));
+				if(room == null)
+				{
+					rooms.remove(i);
+					i--;
+				}
+				else
+				{
+					mob.tell((i+1)+") "+CMStrings.padRight(CMStrings.ellipse(room.displayText(mob),40),40)+" ^H("+rooms.get(i)+")^N");
+				}
+			}
+			return true;
+		}
+		
+		final String addStr;
+		if(cmd.equalsIgnoreCase("ADD"))
+		{
+			if(R.getArea() instanceof BoardableShip)
+			{
+				addStr=CMLib.map().getExtendedRoomID(CMLib.map().roomLocation(((BoardableShip)R.getArea()).getShipItem()));
+			}
+			else
+			if((mob.riding() !=null) && (mob.riding().rideBasis() == Rideable.RIDEABLE_WATER))
+			{
+				if(!CMLib.flags().isWaterySurfaceRoom(mob.location()))
+				{
+					mob.tell(L("This place cannot be charted."));
+					return false;
+				}
+				addStr=CMLib.map().getExtendedRoomID(mob.location());
+			}
+			else
+			{
+				mob.tell(L("This place cannot be charted."));
+				return false;
+			}
+			
+			if(addStr.length()==0)
+			{
+				mob.tell(L("This place cannot be charted."));
+				return false;
+			}
+			if(rooms.contains(addStr))
+			{
+				mob.tell(L("You have already charted this room, check #"+(rooms.indexOf(addStr)+1)));
+				return false;
+			}
+		}
+		else
+			addStr="";
+		
 		if(!super.invoke(mob,commands,givenTarget,auto,asLevel))
 			return false;
-
+		
 		final boolean success=proficiencyCheck(mob,0,auto);
 
 		if(success)
 		{
-			final String str=auto?L("<T-NAME> is bucked!"):L("<S-NAME> buck(s) <T-NAME> off <S-NAMESELF>!");
+			final String str;
+			if(cmd.equalsIgnoreCase("ADD"))
+				str=L("<S-NAME> make(s) a mark on <S-HIS-HER> nautical chart for this location.");
+			else
+			if(cmd.equalsIgnoreCase("REMOVE"))
+				str=L("<S-NAME> erase(s) a mark on <S-HIS-HER> nautical chart.!");
+			else
+				str="?!?!?!";
 			final CMMsg msg=CMClass.getMsg(mob,null,this,CMMsg.MSG_DELICATE_HANDS_ACT,str);
 			if(R.okMessage(mob,msg))
 			{
 				R.send(mob,msg);
+				if(cmd.equalsIgnoreCase("ADD"))
+				{
+					if(text().length()==0)
+						setMiscText(addStr);
+					else
+						setMiscText(text()+";"+addStr);
+				}
+				else
+				if(cmd.equalsIgnoreCase("REMOVE"))
+				{
+					rooms.remove(removeNum);
+					setMiscText(CMParms.combine(rooms,';'));
+				}
 			}
 		}
 		else
-			return beneficialVisualFizzle(mob,null,L("<S-NAME> attempt(s) to buck off <S-HIS-HER> riders, but fails."));
+			return beneficialVisualFizzle(mob,null,L("<S-NAME> can't seem to figure out where <S-HE-SHE> is on <S-HIS-HER> nautical charts."));
 
 		return success;
 	}
