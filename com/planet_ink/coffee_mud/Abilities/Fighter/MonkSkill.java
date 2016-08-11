b407bb180fbe5674224115ff1370def6dfb7c9ad
bozimmerman <bo@zimmers.net>
2016-08-11 05:49:55 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@14542 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Abilities/Fighter/MonkSkill.java
+++ com/planet_ink/coffee_mud/Abilities/Fighter/MonkSkill.java
@@ -37,9 +37,20 @@ import java.util.*;
 
 public class MonkSkill extends FighterSkill
 {
-	@Override public String ID() { return "MonkSkill"; }
-	private final static String localizedName = CMLib.lang().L("MonkSkill");
-	@Override public String name() { return localizedName; }
+	@Override
+	public String ID()
+	{
+		return "MonkSkill";
+	}
+
+	private final static String	localizedName	= CMLib.lang().L("MonkSkill");
+
+	@Override
+	public String name()
+	{
+		return localizedName;
+	}
+
 	public boolean anyWeapons(final MOB mob)
 	{
 	   return (mob.fetchWieldedItem()!=null)||(mob.fetchHeldItem()!=null);
