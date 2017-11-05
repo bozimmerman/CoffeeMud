acc4e8a8ca8c2d60b5e0da91952a4748923bf64b
bozimmerman <bo@zimmers.net>
2017-11-05 06:59:11 +0000

    Start on book copying, but this might be tougher than I thought.
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@15697 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Abilities/Common/BookCopying.java
@@ -0,0 +1,212 @@
+package com.planet_ink.coffee_mud.Abilities.Common;
+import com.planet_ink.coffee_mud.core.interfaces.*;
+import com.planet_ink.coffee_mud.core.*;
+import com.planet_ink.coffee_mud.core.collections.*;
+import com.planet_ink.coffee_mud.Abilities.interfaces.*;
+import com.planet_ink.coffee_mud.Areas.interfaces.*;
+import com.planet_ink.coffee_mud.Behaviors.interfaces.*;
+import com.planet_ink.coffee_mud.CharClasses.interfaces.*;
+import com.planet_ink.coffee_mud.Commands.interfaces.*;
+import com.planet_ink.coffee_mud.Common.interfaces.*;
+import com.planet_ink.coffee_mud.Exits.interfaces.*;
+import com.planet_ink.coffee_mud.Items.interfaces.*;
+import com.planet_ink.coffee_mud.Libraries.interfaces.*;
+import com.planet_ink.coffee_mud.Locales.interfaces.*;
+import com.planet_ink.coffee_mud.MOBS.interfaces.*;
+import com.planet_ink.coffee_mud.Races.interfaces.*;
+
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/*
+   Copyright 2017-2017 Bo Zimmerman
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+	   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+public class BookCopying extends CommonSkill
+{
+	@Override
+	public String ID()
+	{
+		return "BookCopying";
+	}
+
+	private final static String	localizedName	= CMLib.lang().L("Book Copying");
+
+	@Override
+	public String name()
+	{
+		return localizedName;
+	}
+
+	private static final String[]	triggerStrings	= I(new String[] { "BOOKCOPYING", "BOOKCOPY", "BCOPY" });
+
+	@Override
+	public String[] triggerStrings()
+	{
+		return triggerStrings;
+	}
+
+	@Override
+	public int classificationCode()
+	{
+		return Ability.ACODE_COMMON_SKILL | Ability.DOMAIN_CALLIGRAPHY;
+	}
+
+	protected Item		foundI	= null;
+	protected Item		targetI	= null;
+	protected String	pageNum	= "";
+	
+	@Override
+	protected boolean canBeDoneSittingDown()
+	{
+		return true;
+	}
+
+	public BookCopying()
+	{
+		super();
+		displayText=L("You are copying a book...");
+		verb=L("copying");
+	}
+
+	@Override
+	public void unInvoke()
+	{
+		if(canBeUninvoked())
+		{
+			if((affected!=null)
+			&&(affected instanceof MOB)
+			&&(!aborted)
+			&&(!helping))
+			{
+				final MOB mob=(MOB)affected;
+				if((foundI==null)||(targetI==null))
+					commonTell(mob,L("You mess up your book copying."));
+				else
+				{
+					final CMMsg msg=CMClass.getMsg(mob,foundI,this,CMMsg.TYP_REWRITE,
+							L("<S-NAME> start(s) copying <T-NAME> into @x1."),
+							pageNum,
+							L("<S-NAME> start(s) copying <T-NAME> into @x1."));
+					if(mob.location().okMessage(mob,msg))
+						mob.location().send(mob,msg);
+				}
+			}
+		}
+		super.unInvoke();
+	}
+
+	public boolean error(final MOB mob)
+	{
+		commonTell(mob,L("You must specify what book to edit, and the optional page/chapter number to edit."));
+		return false;
+	}
+	
+	@Override
+	public boolean invoke(MOB mob, List<String> commands, Physical givenTarget, boolean auto, int asLevel)
+	{
+		if(super.checkStop(mob, commands))
+			return true;
+		if(commands.size()<1)
+			return error(mob);
+		foundI = null;
+		targetI = null;
+		pageNum="";
+		if((commands.size()>1)&&(CMath.isInteger(commands.get(commands.size()-1))))
+			pageNum=commands.remove(commands.size()-1);
+		String itemName = CMParms.combine(commands);
+		Item target=mob.fetchItem(null,Wearable.FILTER_UNWORNONLY,itemName);
+		if((target==null)||(!CMLib.flags().canBeSeenBy(target,mob)))
+		{
+			target=mob.location().findItem(null, itemName);
+			if((target!=null)&&(CMLib.flags().canBeSeenBy(target,mob)))
+			{
+				final Set<MOB> followers=mob.getGroupMembers(new TreeSet<MOB>());
+				boolean ok=false;
+				for(final MOB M : followers)
+				{
+					if(target.secretIdentity().indexOf(getBrand(M))>=0)
+						ok=true;
+				}
+				if(!ok)
+				{
+					commonTell(mob,L("You aren't allowed to copy '@x1'.",itemName));
+					return false;
+				}
+			}
+		}
+		if((target==null)||(!CMLib.flags().canBeSeenBy(target,mob)))
+		{
+			commonTell(mob,L("You don't seem to have a '@x1'.",itemName));
+			return false;
+		}
+		
+		
+		final Ability write=mob.fetchAbility("Skill_Write");
+		if(write==null)
+		{
+			commonTell(mob,L("You must know how to write."));
+			return false;
+		}
+		
+		if((target.material()&RawMaterial.MATERIAL_MASK)!=RawMaterial.MATERIAL_PAPER)
+		{
+			commonTell(mob,L("You can't copy something like that."));
+			return false;
+		}
+		
+		if(!CMLib.flags().isReadable(target))
+		{
+			commonTell(mob,L("That's not even readable!"));
+			return false;
+		}
+		
+		if(!target.isGeneric())
+		{
+			commonTell(mob,L("You aren't able to copy that."));
+			return false;
+		}
+
+		if(!super.invoke(mob,commands,givenTarget,auto,asLevel))
+			return false;
+		foundI=target;
+		verb=L("copying @x1 into @x2",foundI.name(),targetI.name());
+		displayText=L("You are @x1",verb);
+		if((!proficiencyCheck(mob,0,auto))||(!write.proficiencyCheck(mob,0,auto)))
+			foundI = null;
+		final int duration=getDuration(30,mob,1,1);
+		final CMMsg msg=CMClass.getMsg(mob,target,this,getActivityMessageType(),
+				L("<S-NAME> start(s) copying <T-NAME> into @x1.",targetI.name()),
+				pageNum,
+				L("<S-NAME> start(s) copying <T-NAME> into @x1.",targetI.name()));
+		if(mob.location().okMessage(mob,msg))
+		{
+			mob.location().send(mob,msg);
+			if(duration==1)
+			{
+				BookCopying B=(BookCopying)beneficialAffect(mob,mob,asLevel,duration);
+				if(B!=null)
+				{
+					B.tickDown=0;
+					B.tickUp=0;
+					B.unInvoke();
+				}
+			}
+			else
+				beneficialAffect(mob,mob,asLevel,duration);
+		}
+		return true;
+	}
+}
