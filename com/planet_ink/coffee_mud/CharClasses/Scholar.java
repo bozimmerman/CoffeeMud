3c6e521f6a7ce7d8a13cac8c97de7f776e56da89
bozimmerman <bo@zimmers.net>
2017-12-02 07:02:32 +0000

    Encrypted Writing.
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@15779 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/CharClasses/Scholar.java
+++ com/planet_ink/coffee_mud/CharClasses/Scholar.java
@@ -204,6 +204,8 @@ public class Scholar extends StdCharClass
 		CMLib.ableMapper().addCharAbilityMapping(ID(),20,"Publishing",false);
 		CMLib.ableMapper().addCharAbilityMapping(ID(),20,"Skill_HonoraryDegreeMage",false);
 		
+//		CMLib.ableMapper().addCharAbilityMapping(ID(),20,"Skill_PlanarLore",false);
+		CMLib.ableMapper().addCharAbilityMapping(ID(),20,"Skill_EncryptedWriting",false);
 /*
 21	Planar Lore (Q), Encrypting (Q)		
 22	Surveying (G), Honorary Degree: Druidness (Q)		
