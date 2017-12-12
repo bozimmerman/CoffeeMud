1d49f2eb5386bdcf72e42e5f4340a512f594d24f
bozimmerman <bo@zimmers.net>
2017-12-12 08:52:24 +0000

    Start on the outfit container.
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@15856 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Abilities/Properties/Prop_OutfitContainer.java
+++ com/planet_ink/coffee_mud/Abilities/Properties/Prop_OutfitContainer.java
@@ -217,6 +217,10 @@ public class Prop_OutfitContainer extends Property
 						if(!done)
 							thingsToReDo.add(I);
 					}
+					for(Item I : thingsToReDo)
+					{
+						//TODO: I have no idea .. but it will be layer related..
+					}
 					
 					for(Item I : thingsToRemove)
 						CMLib.commands().postRemove(msg.source(), I, true);
