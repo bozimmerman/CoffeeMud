bc63da2ebcb79e6631291ddac4762a4c79a6ae32
bozimmerman <bo@zimmers.net>
2017-12-31 03:13:24 +0000

    Removing drow as a default player race.
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@15894 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Races/Drow.java
+++ com/planet_ink/coffee_mud/Races/Drow.java
@@ -173,7 +173,28 @@ public class Drow extends Elf
 	@Override
 	public int availabilityCode()
 	{
-		return Area.THEME_FANTASY;
+		return Area.THEME_FANTASY|Area.THEME_SKILLONLYMASK;
+	}
+	
+	@Override
+	public boolean tick(Tickable ticking, int tickID)
+	{
+		if(ticking instanceof MOB)
+		{
+			final MOB mob=(MOB)ticking;
+			if((!mob.isPlayer())
+			&&(!CMLib.flags().canBeSeenBy(mob.location(), mob))
+			&&(!CMLib.flags().isInDark(mob.location()))
+			&&(mob.location()!=null)
+			&&(mob.fetchEffect("Spell_DarknessGlobe")==null)
+			&&(CMLib.dice().roll(1, 10, -1)==1))
+			{
+				Ability A=mob.fetchAbility("Spell_DarknessGlobe");
+				if(A!=null)
+					mob.enqueCommands(new XVector<List<String>>(new XVector<String>("CAST","DARKNESS GLOBE")), 0);
+			}
+		}
+		return true;
 	}
 
 	@Override
