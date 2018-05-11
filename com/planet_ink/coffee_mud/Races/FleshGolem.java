e9abf7f85f2120dc0d79c0ff01112841c571a46b
bozimmerman <bo@zimmers.net>
2018-05-11 06:00:45 +0000

    Flesh Golem
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@16257 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Races/FleshGolem.java
@@ -0,0 +1,69 @@
+package com.planet_ink.coffee_mud.Races;
+import com.planet_ink.coffee_mud.core.interfaces.*;
+import com.planet_ink.coffee_mud.core.*;
+import com.planet_ink.coffee_mud.core.collections.*;
+import com.planet_ink.coffee_mud.Abilities.interfaces.*;
+import com.planet_ink.coffee_mud.Areas.interfaces.*;
+import com.planet_ink.coffee_mud.Behaviors.interfaces.*;
+import com.planet_ink.coffee_mud.CharClasses.interfaces.*;
+import com.planet_ink.coffee_mud.Commands.interfaces.*;
+import com.planet_ink.coffee_mud.Common.interfaces.*;
+import com.planet_ink.coffee_mud.Exits.interfaces.*;
+import com.planet_ink.coffee_mud.Items.interfaces.*;
+import com.planet_ink.coffee_mud.Libraries.interfaces.*;
+import com.planet_ink.coffee_mud.Locales.interfaces.*;
+import com.planet_ink.coffee_mud.MOBS.interfaces.*;
+import com.planet_ink.coffee_mud.Races.interfaces.*;
+
+import java.util.*;
+
+/*
+   Copyright 2018-2018 Bo Zimmerman
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+	   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+public class FleshGolem extends StoneGolem
+{
+	@Override
+	public String ID()
+	{
+		return "FleshGolem";
+	}
+
+	private final static String localizedStaticName = CMLib.lang().L("Flesh Golem");
+
+	@Override
+	public String name()
+	{
+		return localizedStaticName;
+	}
+
+	protected static Vector<RawMaterial> resources=new Vector<RawMaterial>();
+
+	@Override
+	public List<RawMaterial> myResources()
+	{
+		synchronized(resources)
+		{
+			if(resources.size()==0)
+			{
+				resources.addElement(makeResource
+					(L("a pound of flesh"),RawMaterial.RESOURCE_MEAT));
+				resources.addElement(makeResource
+					(L("essence of golem"),RawMaterial.RESOURCE_BLOOD));
+			}
+		}
+		return resources;
+	}
+
+}
