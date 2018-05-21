9a59c6f63529810ed3a43ea485db9a33e69fec12
bozimmerman <bo@zimmers.net>
2018-05-21 18:23:52 +0000

    Going back to previous formula.
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@16281 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Abilities/StdAbility.java
+++ com/planet_ink/coffee_mud/Abilities/StdAbility.java
@@ -1924,9 +1924,9 @@ public class StdAbility implements Ability
 			student.tell(L("You are too stupid to learn new skills."));
 			return false;
 		}
-		final int maxInt=CMProps.getIntVar(CMProps.Int.BASEMAXSTAT);
-		final int intLevel=(qLevel > maxInt) ? maxInt : qLevel;
-		if(intLevel>(student.charStats().getStat(CharStats.STAT_INTELLIGENCE)))
+		final int baseInt = CMProps.getIntVar(CMProps.Int.BASEMAXSTAT); // 18
+		final int studentInt=student.charStats().getStat(CharStats.STAT_INTELLIGENCE);
+		if(baseInt + studentInt < qLevel)
 		{
 			if(teacher != null)
 				teacher.tell(L("@x1 is not smart enough to learn level @x2 skills.",student.name(),qLevel+""));
