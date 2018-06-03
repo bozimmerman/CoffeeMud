566f20e48ae8f66de2bffaa646ab43bf2e422723
bozimmerman <bo@zimmers.net>
2018-06-03 01:07:31 +0000

    <S- filter for stuff.
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@16366 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Abilities/Druid/Chant_BestowName.java
+++ com/planet_ink/coffee_mud/Abilities/Druid/Chant_BestowName.java
@@ -115,11 +115,14 @@ public class Chant_BestowName extends Chant
 			mob.tell(L("Your name may not contain a space."));
 			return false;
 		}
+		myName = CMLib.coffeeFilter().secondaryUserInputFilter(myName);
 
 		final MOB target=this.getTarget(mob,commands,givenTarget);
 		if(target==null)
 			return false;
-		if((!CMLib.flags().isAnimalIntelligence(target))||(!target.isMonster())||(!mob.getGroupMembers(new HashSet<MOB>()).contains(target)))
+		if((!CMLib.flags().isAnimalIntelligence(target))
+		||(!target.isMonster())
+		||(!mob.getGroupMembers(new HashSet<MOB>()).contains(target)))
 		{
 			mob.tell(L("This chant only works on non-player animals in your group."));
 			return false;
