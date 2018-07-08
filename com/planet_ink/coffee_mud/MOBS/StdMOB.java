89413015e0a00049ef1964c2fc9733043f5a66fa
bozimmerman <bo@zimmers.net>
2018-07-08 00:51:10 +0000

    level pk check doesn't apply on attackers property
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@16764 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/MOBS/StdMOB.java
+++ com/planet_ink/coffee_mud/MOBS/StdMOB.java
@@ -2874,7 +2874,7 @@ public class StdMOB implements MOB
 				&& (CMath.abs(srcM.phyStats().level() - phyStats().level()) > CMProps.getPKillLevelDiff())
 				&& (!CMSecurity.isAllowed(this, location(), CMSecurity.SecFlag.PKILL))
 				&& (!CMSecurity.isAllowed(srcM, srcM.location(), CMSecurity.SecFlag.PKILL))
-				&& (!CMLib.law().doesHavePriviledgesHere(srcM, location()))
+				&& ((!CMLib.law().doesHavePriviledgesHere(srcM, location()))||(victim==null)||(srcM.isInCombat()))
 				&& ((!(msg.tool() instanceof Ability)) || (((Ability) msg.tool()).classificationCode() & Ability.ALL_ACODES) != Ability.ACODE_DISEASE))
 				{
 					srcM.tell(L("That is not EVEN a fair fight."));
