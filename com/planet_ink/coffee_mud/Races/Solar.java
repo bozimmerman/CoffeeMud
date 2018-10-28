5a0845a01608b8552b3f4437493858406dd2345f
bozimmerman <bo@zimmers.net>
2018-10-28 00:24:28 +0000

    solar has arms.
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@17311 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Races/Solar.java
+++ com/planet_ink/coffee_mud/Races/Solar.java
@@ -99,7 +99,7 @@ public class Solar extends StdRace
 	}
 
 	//  							  an ey ea he ne ar ha to le fo no gi mo wa ta wi
-	private static final int[] parts={0 ,2 ,2 ,1 ,1 ,0 ,2 ,1 ,2 ,2 ,1 ,0 ,1 ,1 ,0 ,2 };
+	private static final int[] parts={0 ,2 ,2 ,1 ,1 ,2 ,2 ,1 ,2 ,2 ,1 ,0 ,1 ,1 ,0 ,2 };
 
 	@Override
 	public int[] bodyMask()
