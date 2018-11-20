982431e5deb1ffa77eef1e8f8025d69c0b2bdf3b
bozimmerman <bo@zimmers.net>
2018-11-20 04:20:12 +0000

    nerfing magic missile
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@17400 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Abilities/Spells/Spell_MagicMissile.java
+++ com/planet_ink/coffee_mud/Abilities/Spells/Spell_MagicMissile.java
@@ -99,7 +99,7 @@ public class Spell_MagicMissile extends Spell
 					mob.location().send(mob,msg);
 					if(msg.value()<=0)
 					{
-						final int damage = CMLib.dice().roll(1,4,(4+super.getXLEVELLevel(mob))/numMissiles);
+						final int damage = CMLib.dice().roll(1,5+super.getXLEVELLevel(mob),(5+super.getXLEVELLevel(mob))/numMissiles);
 						if(target.location()==mob.location())
 							CMLib.combat().postDamage(mob,target,this,damage,CMMsg.MASK_ALWAYS|CMMsg.TYP_CAST_SPELL,Weapon.TYPE_BURSTING,((i==0)?"^SThe missile ":"^SAnother missile ")+"<DAMAGE> <T-NAME>!^?");
 					}
