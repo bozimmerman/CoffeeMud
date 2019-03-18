e3316ba50116ebc2d4c0f78eaf6d7056547eb118
bozimmerman <bo@zimmers.net>
2019-03-18 23:29:46 +0000

    Improved help.
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@17936 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/WebMacros/INIEntry.java
+++ com/planet_ink/coffee_mud/WebMacros/INIEntry.java
@@ -19,7 +19,7 @@ import com.planet_ink.coffee_mud.Races.interfaces.*;
 import java.util.*;
 
 /*
-   Copyright 2004-2019 Bo Zimmerman
+   Copyright 2019-2019 Bo Zimmerman
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
@@ -33,12 +33,12 @@ import java.util.*;
    See the License for the specific language governing permissions and
    limitations under the License.
 */
-public class INIValue extends StdWebMacro
+public class INIEntry extends StdWebMacro
 {
 	@Override
 	public String name()
 	{
-		return "INIValue";
+		return "INIEntry";
 	}
 
 	@Override
@@ -47,73 +47,40 @@ public class INIValue extends StdWebMacro
 		return false;
 	}
 
-	public String getHelpFor(final String tag, final String mask)
+	protected String getFinalValue(final Map<String,String> parms, String key)
 	{
-		final Vector<String> help=new Vector<String>();
-		final List<String> page=CMProps.loadEnumerablePage(CMProps.getVar(CMProps.Str.INIPATH));
-		boolean startOver=false;
-		for(int p=0;p<page.size();p++)
-		{
-			final String s=page.get(p).trim();
-			if(s.trim().length()==0)
-				startOver=true;
-			else
-			if(s.startsWith("#")||s.startsWith("!"))
-			{
-				if(startOver)
-					help.clear();
-				startOver=false;
-				help.addElement(s.substring(1).trim());
-			}
-			else
-			{
-				final int x=s.indexOf('=');
-				if((x>=0)
-				&&(help.size()>0)
-				&&((s.substring(0,x).equals(mask)
-					||(mask.endsWith("*")&&(s.substring(0,x).startsWith(mask.substring(0,mask.length()-1)))))))
-				{
-					final StringBuffer str=new StringBuffer("");
-					for(int i=0;i<help.size();i++)
-						str.append(help.elementAt(i)+"<BR>");
-					return str.toString();
-				}
-				help.clear();
-				startOver=false;
-			}
-		}
-		return "";
-	}
+		key=key.toUpperCase().trim();
+		final String iniVal;
+		if(CMath.s_valueOf(CMProps.Str.class, key) != null)
+			iniVal=CMProps.getVar((CMProps.Str)CMath.s_valueOf(CMProps.Str.class, key));
+		else
+		if(CMath.s_valueOf(CMProps.Int.class, key) != null)
+			iniVal=""+CMProps.getIntVar((CMProps.Int)CMath.s_valueOf(CMProps.Int.class, key));
+		else
+		if(CMath.s_valueOf(CMProps.Bool.class, key) != null)
+			iniVal=""+CMProps.getBoolVar((CMProps.Bool)CMath.s_valueOf(CMProps.Bool.class, key));
+		else
+			iniVal="";
 
-	protected String getFinalValue(final Map<String,String> parms, final CMProps page, final String key, final String mask)
-	{
 		if(parms.containsKey("VALUE"))
-			return clearWebMacros(page.getStr(key));
-		if(parms.containsKey("INIHELP"))
-		{
-			if(parms.containsKey("NOCR"))
-				return clearWebMacros(CMStrings.replaceAll(getHelpFor(key,mask),"<BR>","&nbsp;"));
-			else
-				return clearWebMacros(getHelpFor(key,mask));
-		}
+			return clearWebMacros(iniVal);
 		String retVal="";
 		if(parms.containsKey("ISNULL"))
 		{
-			if((page.getStr(key) == null) || (page.getStr(key).trim().length()==0))
+			if((iniVal == null) || (iniVal.trim().length()==0))
 				retVal="true";
 			else
 				return "false";
 		}
 		if(parms.containsKey("ISNOTNULL"))
 		{
-			if((page.getStr(key) != null) && (page.getStr(key).trim().length()>0))
+			if((iniVal != null) && (iniVal.trim().length()>0))
 				retVal="true";
 			else
 				return "false";
 		}
 		if(parms.containsKey("CONTAINS"))
 		{
-			final String iniVal=page.getStr(key);
 			if((iniVal != null)&&(iniVal.length()>0))
 			{
 				final String val=parms.get("CONTAINS");
@@ -127,7 +94,6 @@ public class INIValue extends StdWebMacro
 		}
 		if(parms.containsKey("NOTCONTAINS"))
 		{
-			final String iniVal=page.getStr(key);
 			if((iniVal != null)&&(iniVal.length()>0))
 			{
 				final String val=parms.get("NOTCONTAINS");
@@ -141,7 +107,6 @@ public class INIValue extends StdWebMacro
 		}
 		if(parms.containsKey("CONTAINSIGNORECASE"))
 		{
-			final String iniVal=page.getStr(key);
 			if((iniVal != null)&&(iniVal.length()>0))
 			{
 				final String val=parms.get("CONTAINSIGNORECASE");
@@ -155,7 +120,6 @@ public class INIValue extends StdWebMacro
 		}
 		if(parms.containsKey("NOTCONTAINSIGNORECASE"))
 		{
-			final String iniVal=page.getStr(key);
 			if((iniVal != null)&&(iniVal.length()>0))
 			{
 				final String val=parms.get("NOTCONTAINSIGNORECASE");
@@ -169,7 +133,6 @@ public class INIValue extends StdWebMacro
 		}
 		if(parms.containsKey("EQUALS"))
 		{
-			final String iniVal=page.getStr(key);
 			if(iniVal != null)
 			{
 				final String val=parms.get("EQUALS");
@@ -183,7 +146,6 @@ public class INIValue extends StdWebMacro
 		}
 		if(parms.containsKey("NOTEQUALS"))
 		{
-			final String iniVal=page.getStr(key);
 			if(iniVal != null)
 			{
 				final String val=parms.get("NOTEQUALS");
@@ -197,7 +159,6 @@ public class INIValue extends StdWebMacro
 		}
 		if(parms.containsKey("EQUALSIGNORECASE"))
 		{
-			final String iniVal=page.getStr(key);
 			if(iniVal != null)
 			{
 				final String val=parms.get("EQUALSIGNORECASE");
@@ -211,7 +172,6 @@ public class INIValue extends StdWebMacro
 		}
 		if(parms.containsKey("NOTEQUALSIGNORECASE"))
 		{
-			final String iniVal=page.getStr(key);
 			if(iniVal != null)
 			{
 				final String val=parms.get("NOTEQUALSIGNORECASE");
@@ -225,7 +185,6 @@ public class INIValue extends StdWebMacro
 		}
 		if(parms.containsKey("GREATERTHAN"))
 		{
-			final String iniVal=page.getStr(key);
 			if((iniVal != null)&&(iniVal.length()>0))
 			{
 				final String val=parms.get("GREATERTHAN");
@@ -239,7 +198,6 @@ public class INIValue extends StdWebMacro
 		}
 		if(parms.containsKey("NOTGREATERTHAN"))
 		{
-			final String iniVal=page.getStr(key);
 			if((iniVal != null)&&(iniVal.length()>0))
 			{
 				final String val=parms.get("NOTGREATERTHAN");
@@ -253,7 +211,6 @@ public class INIValue extends StdWebMacro
 		}
 		if(parms.containsKey("LESSTHAN"))
 		{
-			final String iniVal=page.getStr(key);
 			if((iniVal != null)&&(iniVal.length()>0))
 			{
 				final String val=parms.get("LESSTHAN");
@@ -267,7 +224,6 @@ public class INIValue extends StdWebMacro
 		}
 		if(parms.containsKey("NOTLESSTHAN"))
 		{
-			final String iniVal=page.getStr(key);
 			if((iniVal != null)&&(iniVal.length()>0))
 			{
 				final String val=parms.get("NOTLESSTHAN");
@@ -288,88 +244,20 @@ public class INIValue extends StdWebMacro
 		final java.util.Map<String,String> parms=parseParms(parm);
 		if(parms==null)
 			return "";
-		String last=httpReq.getUrlParameter("INI");
-		final boolean descZapperMask = parms.remove("DESCZAPPERMASK") != null;
-		if(last == null)
-			last = parms.remove("INI");
-		if((parms.size()==0)&&(last!=null)&&(last.length()>0))
-		{
-			final CMProps page=CMProps.loadPropPage(CMProps.getVar(CMProps.Str.INIPATH));
-			if((page==null)||(!page.isLoaded()))
-				return "";
-			if(!descZapperMask)
-				return page.getStr(last);
-			return CMLib.masking().maskDesc(page.getStr(last));
-		}
-		if(parms.containsKey("RESET"))
-		{
-			if(last!=null)
-				httpReq.removeUrlParameter("INI");
-			return "";
-		}
-		if(parms.containsKey("NEXT"))
-		{
-			if(!parms.containsKey("MASK"))
-				return " @break@";
-			final String mask=parms.get("MASK").toUpperCase().trim();
-			String lastID="";
-			final List<String> page=CMProps.loadEnumerablePage(CMProps.getVar(CMProps.Str.INIPATH));
-			for(int p=0;p<page.size();p++)
-			{
-				final String s=page.get(p).trim();
-				if(s.startsWith("#")||s.startsWith("!"))
-					continue;
-				int x=s.indexOf('=');
-				if(x<0)
-					x=s.indexOf(':');
-				if(x<0)
-					continue;
-				final String id=s.substring(0,x).trim().toUpperCase();
-				if((last==null)||((last.length()>0)&&(last.equals(lastID))&&(!id.equals(lastID))))
-				{
-					if(mask.endsWith("*"))
-					{
-						if(!id.startsWith(mask.substring(0,mask.length()-1)))
-							continue;
-					}
-					else
-					if(!mask.equalsIgnoreCase(id))
-						continue;
-					httpReq.addFakeUrlParameter("INI",id);
-					if(parms.containsKey("VALUE"))
-					{
-						final CMProps realPage=CMProps.loadPropPage(CMProps.getVar(CMProps.Str.INIPATH));
-						if(realPage!=null)
-							return realPage.getStr(id);
-					}
-					return "";
-				}
-				lastID=id;
-			}
-			httpReq.addFakeUrlParameter("INI","");
-			if(parms.containsKey("EMPTYOK"))
-				return "<!--EMPTY-->";
-			return " @break@";
-		}
 		if(!parms.containsKey("MASK"))
 			return "'MASK' not found!";
 		final String mask=parms.get("MASK").toUpperCase();
-		final CMProps page=CMProps.loadPropPage(CMProps.getVar(CMProps.Str.INIPATH));
-		if((page==null)||(!page.isLoaded()))
-			return "";
 		if(mask.trim().endsWith("*"))
 		{
-			for(final Enumeration<Object> e=page.keys();e.hasMoreElements();)
+			final List<String> allKeys = new LinkedList<String>();
+
+			for(final Iterator<String> e=allKeys.iterator();e.hasNext();)
 			{
-				final String key=((String)e.nextElement()).toUpperCase();
+				final String key=e.next().toUpperCase();
 				if(key.startsWith(mask.substring(0,mask.length()-1)))
-				{
-					httpReq.addFakeUrlParameter("INI",key);
-					return getFinalValue(parms, page, key, mask);
-				}
+					return getFinalValue(parms, key);
 			}
 		}
-		httpReq.addFakeUrlParameter("INI",mask);
-		return getFinalValue(parms, page, mask, mask);
+		return getFinalValue(parms, mask);
 	}
 }
