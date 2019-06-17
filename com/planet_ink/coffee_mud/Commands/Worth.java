3bbbc1bf3305cd7b3f3cfb25702dd67a345774b8
bozimmerman <bo@zimmers.net>
2019-06-17 20:56:14 +0000

    Worth command.
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@18218 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Commands/Worth.java
@@ -0,0 +1,76 @@
+package com.planet_ink.coffee_mud.Commands;
+import com.planet_ink.coffee_mud.core.interfaces.*;
+import com.planet_ink.coffee_mud.core.*;
+import com.planet_ink.coffee_mud.core.collections.*;
+import com.planet_ink.coffee_mud.Abilities.interfaces.*;
+import com.planet_ink.coffee_mud.Areas.interfaces.*;
+import com.planet_ink.coffee_mud.Behaviors.interfaces.*;
+import com.planet_ink.coffee_mud.CharClasses.interfaces.*;
+import com.planet_ink.coffee_mud.Commands.interfaces.*;
+import com.planet_ink.coffee_mud.Common.interfaces.*;
+import com.planet_ink.coffee_mud.Exits.interfaces.*;
+import com.planet_ink.coffee_mud.Items.interfaces.*;
+import com.planet_ink.coffee_mud.Libraries.interfaces.*;
+import com.planet_ink.coffee_mud.Locales.interfaces.*;
+import com.planet_ink.coffee_mud.MOBS.interfaces.*;
+import com.planet_ink.coffee_mud.Races.interfaces.*;
+
+import java.util.*;
+
+/*
+   Copyright 2019-2019 Bo Zimmerman
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+	   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+public class Worth extends Inventory
+{
+	public Worth()
+	{
+	}
+
+	private final String[]	access	= I(new String[] { "WORTH"});
+
+	@Override
+	public String[] getAccessWords()
+	{
+		return access;
+	}
+
+	@Override
+	public StringBuilder getInventory(final MOB seer, final MOB mob, final String mask, final boolean longInv)
+	{
+		final StringBuilder msg=new StringBuilder("");
+		final InventoryList list = fetchInventory(seer,mob);
+		msg.append(getShowableMoney(list));
+		return msg;
+	}
+
+	@Override
+	public boolean execute(final MOB mob, final List<String> commands, final int metaFlags)
+		throws java.io.IOException
+	{
+		final StringBuilder msg=getInventory(mob,mob,CMParms.combine(commands,1),false);
+		if(msg.length()==0)
+			mob.tell(L("^HYou are ^!Broke!^?\n\r"));
+		else
+		if(!mob.isMonster())
+			mob.session().wraplessPrintln(msg.toString());
+		return false;
+	}
+
+	@Override
+	public boolean canBeOrdered()
+	{
+		return true;
+	}
+}
