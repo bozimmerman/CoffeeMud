a3dc88071a06bbf5dc0eb552bcba9938692523b8
bozimmerman <bo@zimmers.net>
2019-06-26 20:20:26 +0000

    Generalizing the virtues of Property to an abstract class.
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@18237 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Abilities/Properties/Property.java
+++ com/planet_ink/coffee_mud/Abilities/Properties/Property.java
@@ -2,6 +2,7 @@ package com.planet_ink.coffee_mud.Abilities.Properties;
 import com.planet_ink.coffee_mud.core.interfaces.*;
 import com.planet_ink.coffee_mud.core.*;
 import com.planet_ink.coffee_mud.core.collections.*;
+import com.planet_ink.coffee_mud.Abilities.ThinAbility;
 import com.planet_ink.coffee_mud.Abilities.interfaces.*;
 import com.planet_ink.coffee_mud.Areas.interfaces.*;
 import com.planet_ink.coffee_mud.Behaviors.interfaces.*;
@@ -34,8 +35,15 @@ import java.util.List;
    See the License for the specific language governing permissions and
    limitations under the License.
 */
-public class Property implements Ability
+public class Property extends ThinAbility
 {
+	public Property()
+	{
+		super();
+		// CMClass.bumpCounter(this,CMClass.CMObjectType.ABILITY);//removed for
+		// mem & perf
+	}
+
 	@Override
 	public String ID()
 	{
@@ -48,27 +56,6 @@ public class Property implements Ability
 		return "a Property";
 	}
 
-	@Override
-	public String Name()
-	{
-		return name();
-	}
-
-	@Override
-	public String description()
-	{
-		return "";
-	}
-
-	@Override
-	public String displayText()
-	{
-		return "";
-	}
-
-	protected boolean	savable		= true;
-	protected String	miscText	= "";
-	protected Physical	affected	= null;
 
 	/**
 	 * Designates whether, when used as a property/effect, what sort of objects
@@ -107,87 +94,6 @@ public class Property implements Ability
 	}
 
 	@Override
-	public double castingTime(final MOB mob, final List<String> cmds)
-	{
-		return 0.0;
-	}
-
-	@Override
-	public double combatCastingTime(final MOB mob, final List<String> cmds)
-	{
-		return 0.0;
-	}
-
-	@Override
-	public double checkedCastingCost(final MOB mob, final List<String> cmds)
-	{
-		return 0.0;
-	}
-
-	@Override
-	public int abilityCode()
-	{
-		return 0;
-	}
-
-	@Override
-	public void setAbilityCode(final int newCode)
-	{
-	}
-
-	@Override
-	public int adjustedLevel(final MOB mob, final int asLevel)
-	{
-		return -1;
-	}
-
-	@Override
-	public boolean bubbleAffect()
-	{
-		return false;
-	}
-
-	@Override
-	public long flags()
-	{
-		return 0;
-	}
-
-	@Override
-	public int getTickStatus()
-	{
-		return Tickable.STATUS_NOT;
-	}
-
-	@Override
-	public int usageType()
-	{
-		return 0;
-	}
-
-	@Override
-	public void initializeClass()
-	{
-	}
-
-	@Override
-	public ExpertiseLibrary.SkillCost getTrainingCost(final MOB mob)
-	{
-		return CMLib.expertises().createNewSkillCost(ExpertiseLibrary.CostType.TRAIN, Double.valueOf(1.0));
-	}
-
-	@Override
-	public String L(final String str, final String... xs)
-	{
-		return CMLib.lang().fullSessionTranslation(str, xs);
-	}
-
-	@Override
-	public void setName(final String newName)
-	{
-	}
-
-	@Override
 	public void setDescription(final String newDescription)
 	{
 	}
@@ -198,250 +104,10 @@ public class Property implements Ability
 	}
 
 	@Override
-	public String image()
-	{
-		return "";
-	}
-
-	@Override
-	public String rawImage()
-	{
-		return "";
-	}
-
-	@Override
-	public void setImage(final String newImage)
-	{
-	}
-
-	@Override
-	public MOB invoker()
-	{
-		return null;
-	}
-
-	@Override
-	public void setInvoker(final MOB mob)
-	{
-	}
-
-	public static final String[]	empty	= {};
-
-	@Override
-	public String[] triggerStrings()
-	{
-		return empty;
-	}
-
-	@Override
-	public boolean invoke(final MOB mob, final List<String> commands, final Physical target, final boolean auto, final int asLevel)
-	{
-		return false;
-	}
-
-	@Override
-	public boolean invoke(final MOB mob, final Physical target, final boolean auto, final int asLevel)
-	{
-		return false;
-	}
-
-	@Override
-	public boolean preInvoke(final MOB mob, final List<String> commands, final Physical givenTarget, final boolean auto, final int asLevel, final int secondsElapsed, final double actionsRemaining)
-	{
-		return true;
-	}
-
-	@Override
-	public boolean autoInvocation(final MOB mob, final boolean force)
-	{
-		return false;
-	}
-
-	@Override
-	public void unInvoke()
-	{
-	}
-
-	@Override
-	public boolean canBeUninvoked()
-	{
-		return false;
-	}
-
-	@Override
-	public boolean isAutoInvoked()
-	{
-		return true;
-	}
-
-	@Override
-	public boolean isNowAnAutoEffect()
-	{
-		return true;
-	}
-
-	@Override
-	public List<String> externalFiles()
-	{
-		return null;
-	}
-
-	@Override
-	public boolean canBeTaughtBy(final MOB teacher, final MOB student)
-	{
-		return false;
-	}
-
-	@Override
-	public boolean canBePracticedBy(final MOB teacher, final MOB student)
-	{
-		return false;
-	}
-
-	@Override
-	public boolean canBeLearnedBy(final MOB teacher, final MOB student)
-	{
-		return false;
-	}
-
-	@Override
-	public void teach(final MOB teacher, final MOB student)
-	{
-	}
-
-	@Override
-	public void practice(final MOB teacher, final MOB student)
-	{
-	}
-
-	@Override
-	public int maxRange()
-	{
-		return Integer.MAX_VALUE;
-	}
-
-	@Override
-	public int minRange()
-	{
-		return Integer.MIN_VALUE;
-	}
-
-	@Override
-	public long expirationDate()
-	{
-		return 0;
-	}
-
-	@Override
-	public void setExpirationDate(final long time)
-	{
-	}
-
-	@Override
-	public void startTickDown(final MOB invokerMOB, final Physical affected, final int tickTime)
-	{
-		if(affected.fetchEffect(ID())==null)
-			affected.addEffect(this);
-	}
-
-	@Override
-	public int proficiency()
-	{
-		return 0;
-	}
-
-	@Override
-	public void setProficiency(final int newProficiency)
-	{
-	}
-
-	@Override
-	public boolean proficiencyCheck(final MOB mob, final int adjustment, final boolean auto)
-	{
-		return false;
-	}
-
-	@Override
-	public void helpProficiency(final MOB mob, final int adjustment)
-	{
-	}
-
-	@Override
-	public Physical affecting()
-	{
-		return affected;
-	}
-
-	@Override
-	public void setAffectedOne(final Physical P)
-	{
-		affected = P;
-	}
-
-	@Override
-	public boolean putInCommandlist()
-	{
-		return false;
-	}
-
-	@Override
-	public int abstractQuality()
-	{
-		return Ability.QUALITY_INDIFFERENT;
-	}
-
-	@Override
-	public int enchantQuality()
-	{
-		return Ability.QUALITY_INDIFFERENT;
-	}
-
-	@Override
-	public int castingQuality(final MOB invoker, final Physical target)
-	{
-		return Ability.QUALITY_INDIFFERENT;
-	}
-
-	@Override
 	public int classificationCode()
 	{
 		return Ability.ACODE_PROPERTY;
 	}
-
-	@Override
-	public boolean isSavable()
-	{
-		return savable;
-	}
-
-	@Override
-	public void setSavable(final boolean truefalse)
-	{
-		savable = truefalse;
-	}
-
-	protected boolean	amDestroyed	= false;
-
-	@Override
-	public void destroy()
-	{
-		amDestroyed = true;
-		affected = null;
-		miscText = null;
-	}
-
-	@Override
-	public boolean amDestroyed()
-	{
-		return amDestroyed;
-	}
-
-	/*
-	protected void finalize()
-	{
-		CMClass.unbumpCounter(this, CMClass.CMObjectType.ABILITY);
-	}// removed for mem & perf
-	*/
 
 	@Override
 	public CMObject newInstance()
@@ -455,82 +121,6 @@ public class Property implements Ability
 			Log.errOut(ID(), e);
 		}
 		return new Property();
-	}
-
-	public Property()
-	{
-		super();
-		// CMClass.bumpCounter(this,CMClass.CMObjectType.ABILITY);//removed for
-		// mem & perf
-	}
-
-	@Override
-	public int getSaveStatIndex()
-	{
-		return getStatCodes().length;
-	}
-
-	private static final String[]	CODES	= { "CLASS", "TEXT" };
-
-	@Override
-	public String[] getStatCodes()
-	{
-		return CODES;
-	}
-
-	@Override
-	public boolean isStat(final String code)
-	{
-		return CMParms.indexOf(getStatCodes(), code.toUpperCase().trim()) >= 0;
-	}
-
-	protected int getCodeNum(final String code)
-	{
-		for(int i=0;i<CODES.length;i++)
-		{
-			if(code.equalsIgnoreCase(CODES[i]))
-				return i;
-		}
-		return -1;
-	}
-
-	@Override
-	public String getStat(final String code)
-	{
-		switch(getCodeNum(code))
-		{
-		case 0:
-			return ID();
-		case 1:
-			return text();
-		}
-		return "";
-	}
-
-	@Override
-	public void setStat(final String code, final String val)
-	{
-		switch(getCodeNum(code))
-		{
-		case 0:
-			return;
-		case 1:
-			setMiscText(val);
-			break;
-		}
-	}
-
-	@Override
-	public boolean sameAs(final Environmental E)
-	{
-		if(!(E instanceof Property))
-			return false;
-		for(int i=0;i<CODES.length;i++)
-		{
-			if(!E.getStat(CODES[i]).equals(getStat(CODES[i])))
-				return false;
-		}
-		return true;
 	}
 
 	private void cloneFix(final Ability E)
@@ -555,48 +145,6 @@ public class Property implements Ability
 	}
 
 	@Override
-	public int compareTo(final CMObject o)
-	{
-		return CMClass.classID(this).compareToIgnoreCase(CMClass.classID(o));
-	}
-
-	@Override
-	public void setMiscText(final String newMiscText)
-	{
-		miscText = newMiscText;
-	}
-
-	@Override
-	public String text()
-	{
-		return miscText;
-	}
-
-	@Override
-	public String miscTextFormat()
-	{
-		return CMParms.FORMAT_UNDEFINED;
-	}
-
-	@Override
-	public boolean appropriateToMyFactions(final MOB mob)
-	{
-		return true;
-	}
-
-	@Override
-	public String accountForYourself()
-	{
-		return "";
-	}
-
-	@Override
-	public String requirements(final MOB mob)
-	{
-		return "";
-	}
-
-	@Override
 	public boolean canAffect(final Physical P)
 	{
 		if((P==null)&&(canAffectCode()==0))
@@ -617,65 +165,10 @@ public class Property implements Ability
 	}
 
 	@Override
-	public boolean canTarget(final Physical P)
+	public boolean sameAs(final Environmental E)
 	{
-		return false;
-	}
-
-	@Override
-	public void affectPhyStats(final Physical affected, final PhyStats affectableStats)
-	{
-	}
-
-	@Override
-	public void affectCharStats(final MOB affectedMob, final CharStats affectableStats)
-	{
-	}
-
-	@Override
-	public void affectCharState(final MOB affectedMob, final CharState affectableMaxState)
-	{
-	}
-
-	@Override
-	public void executeMsg(final Environmental myHost, final CMMsg msg)
-	{
-		return;
-	}
-
-	@Override
-	public boolean okMessage(final Environmental myHost, final CMMsg msg)
-	{
-		return true;
-	}
-
-	@Override
-	public boolean tick(final Tickable ticking, final int tickID)
-	{
-		return true;
-	}
-
-	@Override
-	public void makeLongLasting()
-	{
-	}
-
-	@Override
-	public void makeNonUninvokable()
-	{
-	}
-
-	private static final int[]	cost	= new int[Ability.CACHEINDEX_TOTAL];
-
-	@Override
-	public int[] usageCost(final MOB mob, final boolean ignoreCostOverride)
-	{
-		return cost;
-	}
-
-	@Override
-	public boolean isGeneric()
-	{
-		return false;
+		if(!(E instanceof ThinAbility))
+			return false;
+		return super.sameAs(E);
 	}
 }
