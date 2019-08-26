2e404da09a8d180f3bed3b1dea86e3c508f595c4
bozimmerman <bo@zimmers.net>
2019-08-26 04:35:38 +0000

    Ability to use get_prog on containers to check for removing items from it.
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@18510 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Common/DefaultScriptingEngine.java
+++ com/planet_ink/coffee_mud/Common/DefaultScriptingEngine.java
@@ -12634,8 +12634,7 @@ public class DefaultScriptingEngine implements ScriptingEngine
 							if(lastMsg==msg)
 								break;
 							lastMsg=msg;
-							final Item pTool = ((msg.tool() instanceof Item)&&(msg.target()==affecting))?((Item)msg.tool()):defaultItem;
-							enqueResponse(affecting,msg.source(),msg.target(),monster,checkInE,pTool,script,1,check, t);
+							enqueResponse(affecting,msg.source(),msg.target(),monster,checkInE,defaultItem,script,1,check, t);
 							return;
 						}
 					}
