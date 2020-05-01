a93b4771da388bf6807146eb0632d926d08f51f3
bozimmerman <bo@zimmers.net>
2020-05-01 10:10:16 +0000

    nothing to see here.
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@19436 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/core/database/RoomLoader.java
+++ com/planet_ink/coffee_mud/core/database/RoomLoader.java
@@ -831,12 +831,7 @@ public class RoomLoader
 				final String itemID=DBConnections.getRes(R,"CMITID");
 				final Item newItem=CMClass.getItem(itemID);
 				if(newItem==null)
-				{
 					Log.errOut("Room","Couldn't find item '"+itemID+"' for room "+roomID);
-					for(final Enumeration<Item> i = CMClass.basicItems();i.hasMoreElements();)
-						Log.errOut(i.nextElement().ID());
-					System.exit(-1);
-				}
 				else
 				{
 					newItem.setDatabaseID(itemNum);
@@ -1098,12 +1093,7 @@ public class RoomLoader
 				final String itemID=DBConnections.getRes(R,"CMITID");
 				final Item newItem=CMClass.getItem(itemID);
 				if(newItem==null)
-				{
 					Log.errOut("Room","Couldn't find item '"+itemID+"' for room "+roomID);
-					for(final Enumeration<Item> i = CMClass.basicItems();i.hasMoreElements();)
-						Log.errOut(i.nextElement().ID());
-					System.exit(-1);
-				}
 				else
 				{
 					newItem.setDatabaseID(itemNum);
