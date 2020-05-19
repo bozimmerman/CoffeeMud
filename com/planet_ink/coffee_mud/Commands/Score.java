6ca4bf08a659a2e4fb8c88ecb566442ed7e68cda
bozimmerman <bo@zimmers.net>
2020-05-19 22:24:30 +0000

    showing deferred exp at the proper moment.
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@19490 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Commands/Score.java
+++ com/planet_ink/coffee_mud/Commands/Score.java
@@ -251,24 +251,26 @@ public class Score extends Affect
 					msg.append(L("You have scored ^!@x1^? ^<HELP^>experience points^</HELP^>, ^!@x2^? over your last level.\n\r",""+mob.getExperience(),""+mob.getExpNeededDelevel()));
 				}
 				else
-					msg.append(L("You have scored ^!@x1^? ^<HELP^>experience points^</HELP^>, and need ^!@x2^? to advance.\n\r",""+mob.getExperience(),""+mob.getExpNeededLevel()));
-			}
-			else
-				msg.append(L("You have scored ^!@x1^? ^<HELP^>experience points^</HELP^>.\n\r",""+mob.getExperience()));
-			final PlayerStats pStats = mob.playerStats();
-			if((CMProps.getIntVar(CMProps.Int.EXPDEFER_PCT)>0)
-			&&(!skipDeferMessage)
-			&&(pStats != null))
-			{
-				if(pStats.getDeferredXP() + pStats.getRolePlayXP() < pStats.getMaxDeferredXP())
 				{
-					if(((mob.getExperience()+pStats.getDeferredXP() + pStats.getRolePlayXP())>=mob.getExpNextLevel())
-					&&(mob.getExpNeededLevel()<Integer.MAX_VALUE))
-						msg.append(L("^!You've earned enough experience to gain a level.^?\n\r"));
+					msg.append(L("You have scored ^!@x1^? ^<HELP^>experience points^</HELP^>, and need ^!@x2^? to advance.\n\r",""+mob.getExperience(),""+mob.getExpNeededLevel()));
+					final PlayerStats pStats = mob.playerStats();
+					if((CMProps.getIntVar(CMProps.Int.EXPDEFER_PCT)>0)
+					&&(!skipDeferMessage)
+					&&(pStats != null))
+					{
+						if(pStats.getDeferredXP() + pStats.getRolePlayXP() < pStats.getMaxDeferredXP())
+						{
+							if(((mob.getExperience()+pStats.getDeferredXP() + pStats.getRolePlayXP())>=mob.getExpNextLevel())
+							&&(mob.getExpNeededLevel()<Integer.MAX_VALUE))
+								msg.append(L("^!You've earned enough experience to gain a level.^?\n\r"));
+						}
+					}
+					else
+						msg.append(L("^!You cannot defer any more experience for later.^?\n\r"));
 				}
 			}
 			else
-				msg.append(L("^!You cannot defer any more experience for later.^?\n\r"));
+				msg.append(L("You have scored ^!@x1^? ^<HELP^>experience points^</HELP^>.\n\r",""+mob.getExperience()));
 		}
 		msg.append(L("You have been online for ^!@x1^? hours.\n\r",""+Math.round(CMath.div(mob.getAgeMinutes(),60.0))));
 		final FactionManager fac=CMLib.factions();
