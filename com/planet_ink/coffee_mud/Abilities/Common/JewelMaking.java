3ffcaca69596f222cbe363eaabde03382fd805c8
bozimmerman <bo@zimmers.net>
2020-05-27 21:47:05 +0000

    fix to info sculpted charm
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@19503 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Abilities/Common/JewelMaking.java
+++ com/planet_ink/coffee_mud/Abilities/Common/JewelMaking.java
@@ -627,40 +627,43 @@ public class JewelMaking extends EnhancedCraftingSkill implements ItemCraftor, M
 			woodRequired=data[0][FOUND_AMT];
 
 			final Session session=mob.session();
-			if((misctype.equalsIgnoreCase("statue"))
-			&&(session!=null)
+			if((misctype.indexOf("STATUE")>=0)
 			&&((statue==null)||(statue.trim().length()==0)))
 			{
 				final Ability me=this;
 				final Physical target=givenTarget;
-				if(autoGenerate>0)
+				if((autoGenerate>0)
+				||(session==null))
 					statue=mob.Name();
 				else
-				session.prompt(new InputCallback(InputCallback.Type.PROMPT,"",0)
 				{
-					@Override
-					public void showPrompt()
+					session.prompt(new InputCallback(InputCallback.Type.PROMPT,"",0)
 					{
-						session.promptPrint(L("What is this item a representation of?\n\r: "));
-					}
+						@Override
+						public void showPrompt()
+						{
+							session.promptPrint(L("What is this item a representation of?\n\r: "));
+						}
 
-					@Override
-					public void timedOut()
-					{
-					}
+						@Override
 
-					@Override
-					public void callBack()
-					{
-						final String of=this.input;
-						if((of.trim().length()==0)||(of.indexOf('<')>=0))
-							return;
-						final Vector<String> newCommands=new XVector<String>(originalCommands);
-						newCommands.add("STATUE="+of);
-						me.invoke(mob, newCommands, target, auto, asLevel);
-					}
-				});
-				return false;
+						public void timedOut()
+						{
+						}
+
+						@Override
+						public void callBack()
+						{
+							final String of=this.input;
+							if((of.trim().length()==0)||(of.indexOf('<')>=0))
+								return;
+							final Vector<String> newCommands=new XVector<String>(originalCommands);
+							newCommands.add("STATUE="+of);
+							me.invoke(mob, newCommands, target, auto, asLevel);
+						}
+					});
+					return false;
+				}
 			}
 
 			if(!super.invoke(mob,commands,givenTarget,auto,asLevel))
