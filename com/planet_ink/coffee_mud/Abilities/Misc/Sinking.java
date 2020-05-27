18212bb8037e5e9b98ea4aa2ce9a60fe6e3b9d44
bozimmerman <bo@zimmers.net>
2020-05-27 23:41:11 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@19505 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Abilities/Misc/Sinking.java
+++ com/planet_ink/coffee_mud/Abilities/Misc/Sinking.java
@@ -62,12 +62,6 @@ public class Sinking extends StdAbility
 	}
 
 	@Override
-	public int classificationCode()
-	{
-		return Ability.ACODE_PROPERTY;
-	}
-
-	@Override
 	protected int canTargetCode()
 	{
 		return 0;
