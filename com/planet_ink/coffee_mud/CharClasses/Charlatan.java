643180bc4928034000b0a03c73d20fa91fdc379f
bozimmerman <bo@zimmers.net>
2020-06-08 17:19:32 +0000

    better charlatan divination fizzle
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@19542 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/CharClasses/Charlatan.java
+++ com/planet_ink/coffee_mud/CharClasses/Charlatan.java
@@ -334,7 +334,7 @@ public class Charlatan extends StdCharClass
 				&&((((Ability)msg.tool()).classificationCode()&Ability.ALL_DOMAINS)==Ability.DOMAIN_DIVINATION)
 				&&(CMLib.dice().roll(1,100,0)<(myChar.charStats().getClassLevel(this)*4)))
 				{
-					if(msg.source().name().equalsIgnoreCase("someone"))
+					if(msg.source().name().equalsIgnoreCase("someone") && msg.source().isMonster() && (!msg.source().isPlayer()))
 						myChar.location().show(msg.source(),myChar,CMMsg.MSG_OK_ACTION,L("<T-NAME> fool(s) <T-NAMESELF>, causing @x1 to fizzle.",msg.tool().name()));
 					else
 						myChar.location().show(msg.source(),myChar,CMMsg.MSG_OK_ACTION,L("<T-NAME> fool(s) <S-NAMESELF>, causing <S-HIM-HER> to fizzle @x1.",msg.tool().name()));
