2525265c14eade19203b72ec5056b38f129b54a4
john <aringol@gmail.com>
2020-06-10 22:39:08 -0400

    Adds a new spell, Spell_WalkWithDeath. A modified short duration invisibility spell for PVP. Lasts 30 seconds, until the player moves 10 spaces, or the player takes a malicious action.

---
+++ com/planet_ink/coffee_mud/Abilities/Spells/Spell_WalkWithDeath.java
@@ -0,0 +1,172 @@
+package com.planet_ink.coffee_mud.Abilities.Spells;
+
+import com.planet_ink.coffee_mud.core.interfaces.*;
+import com.planet_ink.coffee_mud.core.*;
+import com.planet_ink.coffee_mud.core.collections.*;
+import com.planet_ink.coffee_mud.Abilities.interfaces.*;
+import com.planet_ink.coffee_mud.Areas.interfaces.*;
+import com.planet_ink.coffee_mud.Behaviors.interfaces.*;
+import com.planet_ink.coffee_mud.CharClasses.interfaces.*;
+import com.planet_ink.coffee_mud.Commands.interfaces.*;
+import com.planet_ink.coffee_mud.Common.interfaces.*;
+import com.planet_ink.coffee_mud.Exits.interfaces.*;
+import com.planet_ink.coffee_mud.Items.interfaces.*;
+import com.planet_ink.coffee_mud.Libraries.interfaces.*;
+import com.planet_ink.coffee_mud.Locales.interfaces.*;
+import com.planet_ink.coffee_mud.MOBS.interfaces.*;
+import com.planet_ink.coffee_mud.Races.interfaces.*;
+
+import java.util.*;
+
+/*
+   Copyright 2001-2020 Bo Zimmerman
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+	   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+
+public class Spell_WalkWithDeath extends Spell {
+	
+	@Override public String ID() { 
+		return "Spell_WalkWithDeath";
+	}
+	private final static String localizedName = CMLib.lang().L("Walk With Death");
+	
+	@Override public String name() { 
+		return localizedName; 
+	}
+	
+	private final static String localizedStaticDisplay = CMLib.lang().L("(Limited Invisibility spell)");
+	
+	@Override public String displayText() { 
+		return localizedStaticDisplay; 
+	}
+	
+	@Override public int abstractQuality(){ 
+		return Ability.QUALITY_BENEFICIAL_SELF;
+	}
+	
+	@Override protected int canAffectCode(){
+		return CAN_MOBS;
+	}
+	
+	@Override public int classificationCode(){
+		return Ability.ACODE_SPELL|Ability.DOMAIN_ILLUSION;
+	}
+
+	int steps = 10;
+
+	@Override
+	public void executeMsg(final Environmental myHost, final CMMsg msg){
+		super.executeMsg(myHost,msg);
+
+		if(!(affected instanceof MOB)){
+			return;
+		}
+
+		final MOB mob=(MOB)affected;
+
+		//Reduce steps by 1 with every room movement
+		if((msg.amISource(mob))&&(msg.sourceMinor() == CMMsg.TYP_ENTER){
+			steps--;
+		}
+
+		//Out of steps, remove the invisibility
+		if(steps == 0){
+			unInvoke();
+		}
+
+		//Mob performed malicious act, remove the invisibility
+		if((msg.amISource(mob))&&(CMath.bset(msg.sourceMajor(),CMMsg.MASK_MALICIOUS))){
+			unInvoke();
+		}
+		return;
+	}
+
+	//Set the mob to be invisible
+	@Override
+	public void affectPhyStats(Physical affected, PhyStats affectableStats)
+	{
+		super.affectPhyStats(affected,affectableStats);
+		affectableStats.setDisposition(affectableStats.disposition()|PhyStats.IS_INVISIBLE);
+	}
+
+
+	@Override
+	public int castingQuality(MOB mob, Physical target)
+	{
+		if(mob!=null)
+		{
+			if(target instanceof MOB)
+			{
+				if(((MOB)target).isInCombat())
+					return Ability.QUALITY_INDIFFERENT;
+			}
+		}
+		return super.castingQuality(mob,target);
+	}
+
+	//Remove the affect
+	@Override
+	public void unInvoke()
+	{
+
+		if(!(affected instanceof MOB)){
+			return;
+		}
+
+		final MOB mob=(MOB)affected;
+
+		super.unInvoke();
+
+		if(canBeUninvoked()){
+			if((mob.location()!=null)&&(!mob.amDead())){
+				mob.location().show(mob,null,CMMsg.MSG_OK_VISUAL,L("<S-NAME> phase(s) back into view."));
+			}
+		}
+	}
+
+
+	//Apply the affect to the invoker
+	@Override
+	public boolean invoke(MOB mob, List<String> commands, Physical givenTarget, boolean auto, int asLevel)
+	{
+		
+		final MOB target=this.getTarget(mob,commands,givenTarget);
+		
+		if(target==null){
+			return false;
+		}
+
+		if(!super.invoke(mob,commands,givenTarget,auto,asLevel)){
+			return false;
+		}
+
+		final boolean success=proficiencyCheck(mob,0,auto);
+
+		if(success)
+		{
+			final CMMsg msg=CMClass.getMsg(mob,target,this,verbalCastCode(mob,target,auto),auto?"":L("^S<S-NAME> cast(s) a spell on <T-NAMESELF>.^?"));
+			
+			if(mob.location().okMessage(mob,msg)){
+				mob.location().send(mob,msg);
+				mob.location().show(target,null,CMMsg.MSG_OK_VISUAL,L("<S-NAME> tear(s) a hole into the realm of the dead and dissapear(s) from view!"));
+				beneficialAffect(mob,target,asLevel,0);
+			}
+		}
+		else{
+			return beneficialWordsFizzle(mob,target,L("<S-NAME> speak(s) softly to <T-NAMESELF>, but nothing more happens."));
+		}
+
+		return success;
+	}
+}
