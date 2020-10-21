c97ffe864a75375440e1b09e275102959f6e3b59
bozimmerman <bo@zimmers.net>
2020-10-21 22:18:13 +0000

    adjuster docs, and minor version bump.
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@19810 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/application/MUD.java
+++ com/planet_ink/coffee_mud/application/MUD.java
@@ -67,7 +67,7 @@ import java.sql.*;
 public class MUD extends Thread implements MudHost
 {
 	private static final float	  HOST_VERSION_MAJOR	= (float)5.9;
-	private static final float	  HOST_VERSION_MINOR	= (float)10.1;
+	private static final float	  HOST_VERSION_MINOR	= (float)10.2;
 
 	private static enum MudState
 	{
