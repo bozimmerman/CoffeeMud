5d24a782e5b113a3204e0fe326f3fe3ce6f62d0f
bozimmerman <bo@zimmers.net>
2021-02-13 08:37:48 +0000

    enhanced prefict phase.
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@20457 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Abilities/Druid/Chant_PredictPhase.java
+++ com/planet_ink/coffee_mud/Abilities/Druid/Chant_PredictPhase.java
@@ -87,6 +87,56 @@ public class Chant_PredictPhase extends Chant
 			{
 				mob.location().send(mob,msg);
 				mob.tell(mob.location().getArea().getTimeObj().getMoonPhase(mob.location()).getDesc());
+				final int exper = super.getXLEVELLevel(mob);
+				if(exper > 0)
+				{
+					final CharState cs = (CharState)CMClass.getCommon("DefaultCharState");
+					cs.setAllValues(0);
+					final CharStats ce = (CharStats)CMClass.getCommon("DefaultCharStats");
+					ce.setAllValues(0);
+					final PhyStats ps = (PhyStats)CMClass.getCommon("DefaultPhyStats");
+					ps.setAllValues(0);
+					mob.baseCharStats().getCurrentClass().affectCharState(mob, cs);
+					mob.baseCharStats().getCurrentClass().affectCharStats(mob, ce);
+					mob.baseCharStats().getCurrentClass().affectPhyStats(mob, ps);
+					final List<String> report = new ArrayList<String>();
+					if(cs.getMovement()>0)
+						report.add(L("movement +@x1",""+cs.getMovement()));
+					if((exper>=2)&&(cs.getHitPoints()>0))
+						report.add(L("hit points +@x1",""+cs.getHitPoints()));
+					if((exper>=3)&&(cs.getMana()>0))
+						report.add(L("mana +@x1",""+cs.getMana()));
+					if((exper>=4)&&(ps.attackAdjustment()>0))
+						report.add(L("attack +@x1",""+ps.attackAdjustment()));
+					if((exper>=5)&&(ps.armor()<0))
+						report.add(L("armor +@x1",""+(-1*ps.armor())));
+					if((exper>=6)&&(cs.getMovement()<0))
+						report.add(L("movement @x1",""+cs.getMovement()));
+					if((exper>=7)&&(cs.getHitPoints()<0))
+						report.add(L("hit points @x1",""+cs.getHitPoints()));
+					if((exper>=8)&&(cs.getMana()<0))
+						report.add(L("mana @x1",""+cs.getMana()));
+					if((exper>=9)&&(ps.attackAdjustment()<0))
+						report.add(L("attack @x1",""+ps.attackAdjustment()));
+					if((exper>=10)&&(ps.armor()>0))
+						report.add(L("armor @x1",""+(-1*ps.armor())));
+					if(exper>=5)
+					{
+						final List<String> subReport = new ArrayList<String>();
+						for(int stat : CharStats.CODES.ALLCODES())
+						{
+							if(ce.getStat(stat)>0)
+								subReport.add(CharStats.CODES.NAME(stat).toLowerCase()+" +"+ce.getStat(stat));
+							else
+								subReport.add(CharStats.CODES.NAME(stat).toLowerCase()+" "+ce.getStat(stat));
+						}
+						while(subReport.size()>(exper-4))
+							subReport.remove(CMLib.dice().roll(1, subReport.size(), -1));
+						report.addAll(subReport);
+					}
+					if(report.size()>0)
+						mob.tell(L("The moon is having the following sway over you: @x1",CMLib.english().toEnglishStringList(report)));
+				}
 			}
 		}
 		else
