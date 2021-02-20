221bb4af565a5e5d7e6ce69074802104ff4c07d5
bozimmerman <bo@zimmers.net>
2021-02-20 21:53:19 +0000

    5.9.11
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@20497 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/application/MUD.java
+++ com/planet_ink/coffee_mud/application/MUD.java
@@ -67,7 +67,7 @@ import java.sql.*;
 public class MUD extends Thread implements MudHost
 {
 	private static final float	  HOST_VERSION_MAJOR	= (float)5.9;
-	private static final float	  HOST_VERSION_MINOR	= (float)10.5;
+	private static final int	  HOST_VERSION_MINOR	= 11;
 
 	private static enum MudState
 	{
