f9f4308c318dee7385db12191c895497401d288c
bozimmerman <bo@zimmers.net>
2021-03-08 17:22:39 +0000

    fix to plant self qualification
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@20577 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/CharClasses/Gaian.java
+++ com/planet_ink/coffee_mud/CharClasses/Gaian.java
@@ -174,7 +174,7 @@ public class Gaian extends StdCharClass
 		CMLib.ableMapper().addCharAbilityMapping(ID(),7,"Ranger_Hide",false);
 		CMLib.ableMapper().addCharAbilityMapping(ID(),7,"Druid_KnowPlants",true);
 		CMLib.ableMapper().addCharAbilityMapping(ID(),7,"Chant_Goodberry",false);
-		if(CMLib.factions().isAlignmentLoaded(Faction.Align.LAWFUL)||CMLib.factions().isAlignmentLoaded(Faction.Align.CHAOTIC))
+		if(CMLib.factions().isAlignmentLoaded(Faction.Align.INDIFF)||CMLib.factions().isAlignmentLoaded(Faction.Align.NEUTRAL))
 			CMLib.ableMapper().addCharAbilityMapping(ID(),7,"Chant_PlantSelf",false);
 
 		CMLib.ableMapper().addCharAbilityMapping(ID(),8,"Chant_GrowClub",false);
