bdd9f1e7a22f663a7159715934be386fb81a71a1
bozimmerman <bo@zimmers.net>
2021-04-30 18:29:07 +0000

    Javadoccing journal entry
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@20801 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Common/interfaces/JournalEntry.java
+++ com/planet_ink/coffee_mud/Common/interfaces/JournalEntry.java
@@ -40,45 +40,246 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
+/**
+ * An entry in a journal or book
+ *
+ * @author Bo Zimmerman
+ *
+ */
 public interface JournalEntry extends CMCommon, Cloneable
 {
+	/**
+	 * The fully unique key for this entry, unique across all entries in all books and journals
+	 * @return the key
+	 */
 	public String key();
+
+	/**
+	 * Sets the fully unique key for this entry, unique across all entries in all books and journals
+	 * @param key the key
+	 */
 	public JournalEntry key(String key);
+
+	/**
+	 * The author of the entry
+	 * @return author of the entry
+	 */
 	public String from();
+
+	/**
+	 * Sets the author of the entry
+	 * @param from author of the entry
+	 * @return the journal
+	 */
 	public JournalEntry from(String from);
+
+	/**
+	 * Who the entry is for, usually ALL
+	 * @return who the entry is for, usually ALL
+	 */
 	public String to();
+
+	/**
+	 * Sets who the entry is for, usually ALL
+	 * @param to who the entry is for, usually ALL
+	 * @return the journal
+	 */
 	public JournalEntry to(String to);
+
+	/**
+	 * The subject of the entry
+	 * @return the subject of the entry
+	 */
 	public String subj();
+
+	/**
+	 * Sets the subject of the entry
+	 * @param subj the subject of the entry
+	 * @return the journal
+	 */
 	public JournalEntry subj(String subj);
+
+	/**
+	 * The content of the entry.
+	 * @return the content of the entry.
+	 */
 	public String msg();
+
+	/**
+	 * Sets the content of the entry.
+	 * @param msg the content of the entry.
+	 * @return the journal
+	 */
 	public JournalEntry msg(String msg);
+
+	/**
+	 * The date/time, in ms, that the message was posted
+	 * @return the date/time, in ms, that the message was posted
+	 */
 	public long date();
+
+	/**
+	 * Sets the date/time, in ms, that the message was posted
+	 * @param date the date/time, in ms, that the message was posted
+	 * @return the journal
+	 */
 	public JournalEntry date(long date);
+
+	/**
+	 * The date/time, in ms, that the message was updated
+	 * @return the date/time, in ms, that the message was updated
+	 */
 	public long update();
+
+	/**
+	 * Sets the date/time, in ms, that the message was updated
+	 * @param update the date/time, in ms, that the message was updated
+	 * @return the journal
+	 */
 	public JournalEntry update(long update);
+
+	/**
+	 * The entry key of the entry that this entry is a reply to
+	 * @return the entry key of the entry that this entry is a reply to
+	 */
 	public String parent();
+
+	/**
+	 * Sets the entry key of the entry that this entry is a reply to
+	 * @param parent the entry key of the entry that this entry is a reply to
+	 * @return the journal
+	 */
 	public JournalEntry parent(String parent);
+
+	/**
+	 * The attribute flags for this entry
+	 * @return the attribute flags for this entry
+	 */
 	public long attributes();
+
+	/**
+	 * Sets the attribute flags for this entry
+	 * @param attributes the attribute flags for this entry
+	 * @return the journal
+	 */
 	public JournalEntry attributes(long attributes);
+
+	/**
+	 * The "image path" or, for mailboxes, the Journal Name this mail served as a source for.
+	 * @return the "image path" or, for mailboxes, the Journal Name this mail served as a source for.
+	 */
 	public String data();
+
+	/**
+	 * Sets the "image path" or, for mailboxes, the Journal Name this mail served as a source for.
+	 * @param data the "image path" or, for mailboxes, the Journal Name this mail served as a source for.
+	 * @return the journal
+	 */
 	public JournalEntry data(String data);
+
+	/**
+	 * The cardinal entry number for the journal entry.
+	 * @return the cardinal entry number for the journal entry.
+	 */
 	public int cardinal();
+
+	/**
+	 * Sets the cardinal entry number for the journal entry.
+	 * @param cardinal the cardinal entry number for the journal entry.
+	 * @return the journal
+	 */
 	public JournalEntry cardinal(int cardinal);
+
+	/**
+	 * The image icon for forum entries
+	 * @return the image icon for forum entries
+	 */
 	public String msgIcon();
+
+	/**
+	 * Sets the image icon for forum entries
+	 * @param msgIcon the image icon for forum entries
+	 * @return the journal
+	 */
 	public JournalEntry msgIcon(String msgIcon);
+
+	/**
+	 * The number of replies to this entry.
+	 * @return the number of replies to this entry.
+	 */
 	public int replies();
+
+	/**
+	 * Sets he number of replies to this entry.
+	 * @param replies the number of replies to this entry.
+	 * @return the journal
+	 */
 	public JournalEntry replies(int replies);
+
+	/***
+	 * The number of views of this entry
+	 * @return the number of views of this entry
+	 */
 	public int views();
+
+	/**
+	 * Sets the number of views of this entry
+	 * @param views the number of views of this entry
+	 * @return the journal
+	 */
 	public JournalEntry views(int views);
+
+	/**
+	 * Returns whether this is the last entry in the db.  This
+	 * is not actually stored in the DB, but is derived
+	 * from the reading process.
+	 * @return whether this is the last entry in the db
+	 */
 	public boolean isLastEntry();
+
+	/**
+	 * Sets whether this is the last entry in the db
+	 * @param lastEntry whether this is the last entry in the db
+	 * @return the journal
+	 */
 	public JournalEntry lastEntry(boolean lastEntry);
+
+	/**
+	 * The friendly viewable form of this message in the command line.
+	 * Is not included in any xml builds or in the DB.
+	 * @return the friendly message
+	 */
 	public StringBuffer derivedBuildMessage();
+
+	/**
+	 * The friendly viewable form of this message in the command line.
+	 * Is not included in any xml builds or in the DB.
+	 * @param msg the friendly message
+	 * @return the journal
+	 */
 	public JournalEntry derivedBuildMessage(StringBuffer msg);
+
+	/**
+	 * Compares two journal entry objects to each other
+	 * @param o another journal entry obj
+	 * @return true or false
+	 */
 	public int compareTo(JournalEntry o);
 
 	@Override
+	/**
+	 * Returns a copy of the journal entry
+	 * @return the new entry
+	 */
 	public JournalEntry copyOf();
 
+	/**
+	 * Flagging this entry as stuck to the top
+	 */
 	public final static long ATTRIBUTE_STUCKY=2;
+
+	/**
+	 * Flatting this entry as protected from auto-purging
+	 */
 	public final static long ATTRIBUTE_PROTECTED=1;
 }
