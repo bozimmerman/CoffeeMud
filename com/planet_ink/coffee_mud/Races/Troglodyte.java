119ead66d8833969981f8fc7f5a61f3835cb23c8
bozimmerman <bo@zimmers.net>
2022-07-14 21:20:47 +0000

    added trog
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@21564 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Races/Troglodyte.java
@@ -0,0 +1,222 @@
+package com.planet_ink.coffee_mud.Races;
+import com.planet_ink.coffee_mud.core.interfaces.*;
+import com.planet_ink.coffee_mud.core.*;
+import com.planet_ink.coffee_mud.core.collections.*;
+import com.planet_ink.coffee_mud.Abilities.interfaces.*;
+import com.planet_ink.coffee_mud.Areas.interfaces.*;
+import com.planet_ink.coffee_mud.Behaviors.interfaces.*;
+import com.planet_ink.coffee_mud.CharClasses.interfaces.*;
+import com.planet_ink.coffee_mud.Commands.interfaces.*;
+import com.planet_ink.coffee_mud.Common.interfaces.*;
+import com.planet_ink.coffee_mud.Exits.interfaces.*;
+import com.planet_ink.coffee_mud.Items.interfaces.*;
+import com.planet_ink.coffee_mud.Libraries.interfaces.*;
+import com.planet_ink.coffee_mud.Locales.interfaces.*;
+import com.planet_ink.coffee_mud.MOBS.interfaces.*;
+import com.planet_ink.coffee_mud.Races.interfaces.*;
+
+import java.util.*;
+
+/*
+   Copyright 2003-2022 Bo Zimmerman
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+	   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+public class Troglodyte extends StdRace
+{
+	@Override
+	public String ID()
+	{
+		return "Troglodyte";
+	}
+
+	public Troglodyte()
+	{
+		super();
+	}
+
+	private final static String localizedStaticName = CMLib.lang().L("Troglodyte");
+
+	@Override
+	public String name()
+	{
+		return localizedStaticName;
+	}
+
+	@Override
+	public int availabilityCode()
+	{
+		return Area.THEME_FANTASY | Area.THEME_SKILLONLYMASK;
+	}
+
+	@Override
+	public int shortestMale()
+	{
+		return 60;
+	}
+
+	@Override
+	public int shortestFemale()
+	{
+		return 50;
+	}
+
+	@Override
+	public int heightVariance()
+	{
+		return 5;
+	}
+
+	@Override
+	public int lightestWeight()
+	{
+		return 190;
+	}
+
+	@Override
+	public int weightVariance()
+	{
+		return 25;
+	}
+
+	@Override
+	public long forbiddenWornBits()
+	{
+		return 0;
+	}
+
+	private final static String localizedStaticRacialCat = CMLib.lang().L("Humanoid");
+
+	@Override
+	public String racialCategory()
+	{
+		return localizedStaticRacialCat;
+	}
+
+	//  							  an ey ea he ne ar ha to le fo no gi mo wa ta wi
+	private static final int[] parts={0 ,2 ,2 ,1 ,1 ,2 ,2 ,1 ,2 ,2 ,1 ,0 ,1 ,1 ,1 ,0 };
+
+	@Override
+	public int[] bodyMask()
+	{
+		return parts;
+	}
+
+	private final int[]	agingChart	= { 0, 1, 3, 12, 20, 53, 80, 90, 100 };
+
+	@Override
+	public int[] getAgingChart()
+	{
+		return agingChart;
+	}
+
+	protected static Vector<RawMaterial> resources=new Vector<RawMaterial>();
+
+	@Override
+	public void affectCharStats(final MOB affectedMOB, final CharStats affectableStats)
+	{
+		super.affectCharStats(affectedMOB, affectableStats);
+		affectableStats.adjStat(CharStats.STAT_INTELLIGENCE,-1);
+		affectableStats.adjStat(CharStats.STAT_CONSTITUTION,-1);
+		affectableStats.adjStat(CharStats.STAT_STRENGTH,3);
+	}
+
+	@Override
+	public void unaffectCharStats(final MOB affectedMOB, final CharStats affectableStats)
+	{
+		super.unaffectCharStats(affectedMOB, affectableStats);
+		affectableStats.adjStat(CharStats.STAT_INTELLIGENCE,+1);
+		affectableStats.adjStat(CharStats.STAT_CONSTITUTION,+1);
+		affectableStats.adjStat(CharStats.STAT_STRENGTH,-3);
+	}
+
+	@Override
+	public List<Item> outfit(final MOB myChar)
+	{
+		if(outfitChoices==null)
+		{
+			outfitChoices = new Vector<Item>();
+		}
+		return outfitChoices;
+	}
+
+	@Override
+	public Weapon myNaturalWeapon()
+	{
+		if(naturalWeapon==null)
+		{
+			naturalWeapon=CMClass.getWeapon("StdWeapon");
+			naturalWeapon.setName(L("sharp claws"));
+			naturalWeapon.setMaterial(RawMaterial.RESOURCE_BONE);
+			naturalWeapon.setUsesRemaining(1000);
+			naturalWeapon.setWeaponDamageType(Weapon.TYPE_SLASHING);
+		}
+		return naturalWeapon;
+	}
+
+	@Override
+	public String healthText(final MOB viewer, final MOB mob)
+	{
+		final double pct=(CMath.div(mob.curState().getHitPoints(),mob.maxState().getHitPoints()));
+
+		if(pct<.10)
+			return L("^r@x1^r is facing a cold death!^N",mob.name(viewer));
+		else
+		if(pct<.20)
+			return L("^r@x1^r is covered in blood.^N",mob.name(viewer));
+		else
+		if(pct<.30)
+			return L("^r@x1^r is bleeding badly from lots of wounds.^N",mob.name(viewer));
+		else
+		if(pct<.40)
+			return L("^y@x1^y has numerous bloody wounds and gashes.^N",mob.name(viewer));
+		else
+		if(pct<.50)
+			return L("^y@x1^y has some bloody wounds and gashes.^N",mob.name(viewer));
+		else
+		if(pct<.60)
+			return L("^p@x1^p has a few bloody wounds.^N",mob.name(viewer));
+		else
+		if(pct<.70)
+			return L("^p@x1^p is cut and bruised heavily.^N",mob.name(viewer));
+		else
+		if(pct<.80)
+			return L("^g@x1^g has some minor cuts and bruises.^N",mob.name(viewer));
+		else
+		if(pct<.90)
+			return L("^g@x1^g has a few bruises and scratched scales.^N",mob.name(viewer));
+		else
+		if(pct<.99)
+			return L("^g@x1^g has a few small bruises.^N",mob.name(viewer));
+		else
+			return L("^c@x1^c is in perfect health.^N",mob.name(viewer));
+	}
+
+	@Override
+	public List<RawMaterial> myResources()
+	{
+		synchronized(resources)
+		{
+			if(resources.size()==0)
+			{
+				resources.addElement(makeResource
+				(L("a @x1 arm",name().toLowerCase()),RawMaterial.RESOURCE_MEAT));
+				resources.addElement(makeResource
+				(L("an aqua blue scale",name().toLowerCase()),RawMaterial.RESOURCE_SCALES));
+				resources.addElement(makeResource
+				(L("some @x1 blood",name().toLowerCase()),RawMaterial.RESOURCE_BLOOD));
+			}
+		}
+		return resources;
+	}
+}
