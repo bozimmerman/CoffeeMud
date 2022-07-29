a70c10ae2794ecff52c655a7e9f9780e8644050d
bozimmerman <bo@zimmers.net>
2022-07-29 03:14:28 +0000

    Skill_Math
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@21629 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/CharClasses/Archon.java
+++ com/planet_ink/coffee_mud/CharClasses/Archon.java
@@ -83,6 +83,7 @@ public class Archon extends StdCharClass implements ArchonOnly
 		CMLib.ableMapper().addCharAbilityMapping(ID(),1,"AstroEngineering",100,"",true,SecretFlag.SECRET);
 		CMLib.ableMapper().addCharAbilityMapping(ID(),1,"Welding",100,"",true,SecretFlag.SECRET);
 		CMLib.ableMapper().addCharAbilityMapping(ID(),1,"Irrigation",100,"",true,SecretFlag.SECRET);
+		CMLib.ableMapper().addCharAbilityMapping(ID(),1,"Skill_Math",100,"",true,SecretFlag.SECRET);
 
 		CMLib.ableMapper().addCharAbilityMapping(ID(),1,"PigLatin",100,true);
 		CMLib.ableMapper().addCharAbilityMapping(ID(),1,"Common",100,"",true,SecretFlag.SECRET);
