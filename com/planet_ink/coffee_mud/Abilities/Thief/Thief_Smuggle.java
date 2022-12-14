0ff20a47738e1edf6dc103c0659fd474f34250fe
bozimmerman <bo@zimmers.net>
2022-12-14 23:07:53 +0000

    Smuggle and other Gaoler stuff.
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@21942 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Abilities/Thief/Thief_Smuggle.java
@@ -0,0 +1,168 @@
+package com.planet_ink.coffee_mud.Abilities.Thief;
+import com.planet_ink.coffee_mud.core.interfaces.*;
+import com.planet_ink.coffee_mud.core.*;
+import com.planet_ink.coffee_mud.core.collections.*;
+import com.planet_ink.coffee_mud.Abilities.interfaces.*;
+import com.planet_ink.coffee_mud.Areas.interfaces.*;
+import com.planet_ink.coffee_mud.Behaviors.interfaces.*;
+import com.planet_ink.coffee_mud.CharClasses.interfaces.*;
+import com.planet_ink.coffee_mud.Commands.interfaces.*;
+import com.planet_ink.coffee_mud.Common.interfaces.*;
+import com.planet_ink.coffee_mud.Exits.interfaces.*;
+import com.planet_ink.coffee_mud.Items.interfaces.*;
+import com.planet_ink.coffee_mud.Libraries.interfaces.*;
+import com.planet_ink.coffee_mud.Locales.interfaces.*;
+import com.planet_ink.coffee_mud.MOBS.interfaces.*;
+import com.planet_ink.coffee_mud.Races.interfaces.*;
+
+import java.util.*;
+
+/*
+   Copyright 2022-2022 Bo Zimmerman
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+	   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+public class Thief_Smuggle extends ThiefSkill
+{
+	@Override
+	public String ID()
+	{
+		return "Thief_Smuggle";
+	}
+
+	private final static String localizedName = CMLib.lang().L("Smuggle");
+
+	@Override
+	public String name()
+	{
+		return localizedName;
+	}
+
+	@Override
+	public String displayText()
+	{
+		return "";
+	}
+
+	@Override
+	protected int canAffectCode()
+	{
+		return CAN_ITEMS;
+	}
+
+	@Override
+	protected int canTargetCode()
+	{
+		return CAN_ITEMS;
+	}
+
+	@Override
+	public int abstractQuality()
+	{
+		return Ability.QUALITY_OK_SELF;
+	}
+
+	@Override
+	public int classificationCode()
+	{
+		return Ability.ACODE_THIEF_SKILL|Ability.DOMAIN_CRIMINAL;
+	}
+
+	private static final String[] triggerStrings =I(new String[] {"SMUGGLE"});
+	@Override
+	public String[] triggerStrings()
+	{
+		return triggerStrings;
+	}
+
+	@Override
+	public int usageType()
+	{
+		return USAGE_MOVEMENT|USAGE_MANA;
+	}
+
+	@Override
+	protected boolean ignoreCompounding()
+	{
+		return true;
+	}
+
+	@Override
+	public void executeMsg(final Environmental myHost, final CMMsg msg)
+	{
+		if(msg.target() == affected)
+		{
+			switch(msg.targetMinor())
+			{
+			case CMMsg.TYP_WEAR:
+			case CMMsg.TYP_WIELD:
+			case CMMsg.TYP_HOLD:
+			case CMMsg.TYP_DROP:
+				unInvoke();
+				break;
+			default:
+				break;
+			}
+		}
+		return;
+	}
+
+	@Override
+	public void affectPhyStats(final Physical affected, final PhyStats affectableStats)
+	{
+		super.affectPhyStats(affected,affectableStats);
+		affectableStats.setSensesMask(affectableStats.sensesMask()|PhyStats.SENSE_UNLOCATABLE);
+		affectableStats.addAmbiance(L("(smuggling)"));
+	}
+
+	@Override
+	public boolean invoke(final MOB mob, final List<String> commands, final Physical givenTarget, final boolean auto, final int asLevel)
+	{
+		final Room R = mob.location();
+		final Physical target = super.getTarget(mob, R, givenTarget, commands, Wearable.FILTER_UNWORNONLY);
+		if((target == null)||(R==null))
+			return false;
+		if(mob.isInCombat())
+		{
+			mob.tell(L("Not while in combat!"));
+			return false;
+		}
+		if(!(target instanceof Item))
+		{
+			mob.tell(L("You can't smuggle @x1.",target.name()));
+			return false;
+		}
+
+		if(!super.invoke(mob,commands,givenTarget,auto,asLevel))
+			return false;
+
+		boolean success=proficiencyCheck(mob,0,auto);
+
+		if(success)
+		{
+			final CMMsg msg=CMClass.getMsg(mob,null,this,auto?CMMsg.MSG_OK_ACTION:(CMMsg.MSG_DELICATE_HANDS_ACT|CMMsg.MASK_MOVE),
+					L("<S-NAME> prepare(s) <T-NAME> for smuggling."),CMMsg.NO_EFFECT,null,CMMsg.NO_EFFECT,null);
+			if(R.okMessage(mob,msg))
+			{
+				R.send(mob,msg);
+				super.beneficialAffect(mob,target,asLevel,0);
+				target.recoverPhyStats();
+			}
+			else
+				success=false;
+		}
+		else
+			beneficialVisualFizzle(mob,target,L("<S-NAME> attempt(s) to hide and fail(s)."));
+		return success;
+	}
+}
