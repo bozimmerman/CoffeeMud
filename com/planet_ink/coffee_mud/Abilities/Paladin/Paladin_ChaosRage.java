bcd694f4a9fede687288da325c1a8b1c957a15aa
bozimmerman <bo@zimmers.net>
2023-05-23 00:29:27 +0000

    More anti-paladin progress.
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@22283 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Abilities/Paladin/Paladin_ChaosRage.java
+++ com/planet_ink/coffee_mud/Abilities/Paladin/Paladin_ChaosRage.java
@@ -33,7 +33,7 @@ import java.util.*;
    See the License for the specific language governing permissions and
    limitations under the License.
 */
-public class Paladin_ChaosRage extends PaladinSkill
+public class Paladin_ChaosRage extends StdAbility
 {
 	@Override
 	public String ID()
