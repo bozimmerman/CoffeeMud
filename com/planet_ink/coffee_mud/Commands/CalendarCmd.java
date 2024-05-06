60574da6786e96c7e694a8c3cebb2542f4d593ac
bozimmerman <bo@zimmers.net>
2024-05-06 18:43:45 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@23005 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Commands/CalendarCmd.java
+++ com/planet_ink/coffee_mud/Commands/CalendarCmd.java
@@ -114,7 +114,7 @@ public class CalendarCmd extends StdCommand
 		final List<JournalEntry> calendar = new Vector<JournalEntry>();
 		for(final JournalEntry holiday : CMLib.quests().getHolidayEntries(true))
 		{
-			if((fromTm>=holiday.date())&&(holiday.expiration()<=toTm))
+			if((holiday.date()<=toTm) && (holiday.expiration()>=fromTm))
 				calendar.add(holiday);
 		}
 		calendar.addAll(CMLib.database().DBReadJournalMsgsByTimeStamps("SYSTEM_CALENDAR", "SYSTEM", fromTm, toTm));
