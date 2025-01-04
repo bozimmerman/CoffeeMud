d1f768ebce9322510f121232c4ba06fc4340404e
bozimmerman <bo@zimmers.net>
2025-01-04 22:32:32 +0000

    npe fix
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@23544 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Items/Basic/StdItem.java
+++ com/planet_ink/coffee_mud/Items/Basic/StdItem.java
@@ -997,8 +997,7 @@ public class StdItem implements Item
 						else
 							return true;
 					}
-					final String wearName = (alreadyWearing != null) ? alreadyWearing.name(mob)
-											: L("something");
+					final String wearName = (alreadyWearing != null) ? alreadyWearing.name(mob) : L("something");
 					if(cantWearAt==Wearable.WORN_HELD)
 						mob.tell(L("You are already holding @x1.",wearName));
 					else
