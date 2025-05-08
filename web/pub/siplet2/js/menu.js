1bbcf0418b2afbeefaf617bb3d15a974f82e8d60
bozimmerman <bo@zimmers.net>
2025-05-08 18:33:24 +0000

    siplet2 progress.
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@23646 0d6f1817-ed0e-0410-87c9-987e46238f29

--- web/pub/siplet2/js/menu.js
+++ web/pub/siplet2/js/menu.js
@@ -1,5 +1,6 @@
 var menuAreaHeight = 20;
 var menuArea = null;
+var menuWindow = null;
 function configureMenu(obj)
 {
 	menuArea=obj;
@@ -10,18 +11,17 @@ function configureMenu(obj)
 	menuArea.style.background='#404040';
 	
 	var menuData = [
-		{"Muds": [
-			{"n":"Open"},
+		{"Window": [
+			{"n":"New"},
 			{"n":"Disconnect"},
-			{"n":"Reconnect"}]},
+			{"n":"Reconnect"}
+		]},
 		{"Options": [
-			{"n":"blah"},
-			{"n":"blah"},
-			{"n":"blah"}]},
+			{"n":"Global"}
+		]},
 		{"Help": [
-			{"n":"blah"},
-			{"n":"blah"},
-			{"n":"About","a":"javascript:menuAbout()"}]}
+			{"n":"About","a":"javascript:menuAbout()"}
+		]}
 	];
 	var html = '';
 	html += '<TABLE style="border: 1px solid white; border-collapse: collapse; height: 20px; table-layout: fixed; width: 100%;">';
@@ -49,7 +49,66 @@ function configureMenu(obj)
 	menuArea.innerHTML = html;
 }
 
+function hideOptionWindow()
+{
+	if(window.menuWindow != null)
+	{
+		window.menuWindow.style.visibility = 'hidden';
+		window.menuWindow.onclick = function() {};
+		var contentWindow = window.menuWindow.getElementsByTagName('div')[1];
+		contentWindow.innerHTML = '';
+	}
+}
+
+function getOptionWindow(heading, w, h)
+{
+	if(window.menuWindow == null)
+	{
+		window.menuWindow = document.createElement('div');
+		window.menuWindow.style.cssText = "position:absolute;top:20%;left:10%;height:60%;width:80%;z-index:99;";
+		window.menuWindow.style.cssText += "border-style:solid;border-width:5px;border-color:white;";
+		window.menuWindow.style.backgroundColor = 'darkgray';
+		window.menuWindow.style.visibility = 'visible';
+		window.menuWindow.style.foregroundColor = 'black';
+		document.body.appendChild(window.menuWindow);
+		var titleBar = document.createElement('div');
+		titleBar.style.cssText = "position:absolute;top:0%;left:0%;height:20px;width:100%;";
+		titleBar.style.backgroundColor = 'white';
+		titleBar.style.foregroundColor = 'black';
+		var contentWindow = document.createElement('div');
+		contentWindow.style.cssText = "position:absolute;top:20px;left:0%;height:calc(100% - 20px);width:100%;";
+		contentWindow.style.backgroundColor = 'lightgray';
+		contentWindow.style.foregroundColor = 'black';
+		window.menuWindow.appendChild(titleBar);
+		window.menuWindow.appendChild(contentWindow);
+	}
+	window.menuWindow.onclick = function() {};
+	var titleBar = window.menuWindow.getElementsByTagName('div')[0];
+	var contentWindow = window.menuWindow.getElementsByTagName('div')[1];
+	contentWindow.innerHTML = '';
+	window.menuWindow.style.top = 'calc(' + (((100-h)/2)+'%') + ' - '+inputAreaHeight+'px);';
+	window.menuWindow.style.left = ((100-w)/2)+'%';
+	window.menuWindow.style.height = h+'%';
+	window.menuWindow.style.width = w+'%';
+	window.menuWindow.style.visibility = 'visible';
+	titleBar.innerHTML = '<FONT COLOR=BLACK>'+heading+'</FONT>'+
+		'<IMG style="float: right; width: 16px; height: 16px;" '
+		+'ONCLICK="hideOptionWindow();" '
+		+'SRC="images/close.gif">';
+	return contentWindow;
+}
+
 function menuAbout()
 {
-	window.alert('siplet!');
+	var content = getOptionWindow("About",60,40);
+	var aboutHtml = '<div style="position:absolute;top:50%;left:50%;white-space:nowrap;';
+	aboutHtml += 'transform:translate(-50%,-50%);text-align:center;line-height: 1.5;">';
+	aboutHtml += '<div style="display:block;margin:0 auto;"><h1>Siplet v'+Siplet.VERSION_MAJOR;
+	if(Siplet.VERSION_MINOR != 0)
+		aboutHtml += '.'+Siplet.VERSION_MINOR;
+	aboutHtml += '</h1></div><BR><BR><div style="display:block;margin:0 auto;">';
+	aboutHtml += '(C)2025-2025 Bo Zimmerman</div>';
+	aboutHtml += '</div>';
+	content.innerHTML=aboutHtml;
+	this.menuWindow.onclick = hideOptionWindow;
 }
