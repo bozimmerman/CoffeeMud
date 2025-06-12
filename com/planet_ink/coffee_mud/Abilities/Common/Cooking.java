f3a75c22b6edd73b4e1f7573b6c4e807846eee4a
bozimmerman <bo@zimmers.net>
2025-06-12 17:18:38 +0000

    Undoing chocolate fix
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@23909 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Abilities/Common/Cooking.java
+++ com/planet_ink/coffee_mud/Abilities/Common/Cooking.java
@@ -417,9 +417,6 @@ public class Cooking extends EnhancedCraftingSkill implements ItemCraftor
 				if(I instanceof RawMaterial)
 					INT=Integer.valueOf(INT.intValue()+((RawMaterial)I).phyStats().weight());
 				else
-				if(I instanceof Food)
-					INT=Integer.valueOf(INT.intValue()+((Food)I).phyStats().weight());
-				else
 					INT=Integer.valueOf(INT.intValue()+1);
 				h.put(ing+"/"+I.rawSecretIdentity().toUpperCase()+"/"+I.Name().toUpperCase()+"/",INT);
 			}
@@ -486,8 +483,6 @@ public class Cooking extends EnhancedCraftingSkill implements ItemCraftor
 								NotEnoughForThisRun=true;
 							if(amounts[i]==0)
 								RanOutOfSomething=true;
-							else
-								break;
 						}
 					}
 				}
