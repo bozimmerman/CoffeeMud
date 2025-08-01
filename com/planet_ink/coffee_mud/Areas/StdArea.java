9db4ca11c74e75b1a0a79c931c428cdc5bf5c57d
bozimmerman <bo@zimmers.net>
2025-08-01 04:49:28 +0000

    random room fix
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@24136 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Areas/StdArea.java
+++ com/planet_ink/coffee_mud/Areas/StdArea.java
@@ -2125,6 +2125,13 @@ public class StdArea implements Area
 		if ((!roomID.startsWith(Name()))
 		&& (roomID.toUpperCase().startsWith(Name().toUpperCase() + "#")))
 			roomID = Name() + roomID.substring(Name().length()); // for case sensitive situations
+		final int grid = roomID.lastIndexOf("#(");
+		if(grid > 0)
+		{
+			final Room R = prooms.get(roomID.substring(0,grid));
+			if(R instanceof GridLocale)
+				return ((GridLocale)R).getGridChild(roomID);
+		}
 		return prooms.get(roomID);
 	}
 
