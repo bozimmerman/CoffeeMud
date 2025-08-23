02c3153d9521c299db79fdde3e16235019e0538c
bozimmerman <bo@zimmers.net>
2025-08-23 01:07:39 +0000

    supporting built-in icons
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@24229 0d6f1817-ed0e-0410-87c9-987e46238f29

--- web/pub/siplet/js/filesys.js
+++ web/pub/siplet/js/filesys.js
@@ -6,6 +6,10 @@ function SipletFileSystem(dbName)
 	this.db = null;
 	this.enabled = ('indexedDB' in window);
 	this.blobCache = {};
+	this.iconsPath = '/icons';
+	this.iconFiles = [];
+	for (i=1; i<=2192; i++) 
+		this.iconFiles.push('fc'+i+'.png');
 
 	this.init = function(callBack) 
 	{
@@ -44,6 +48,41 @@ function SipletFileSystem(dbName)
 	this._load = function(path, callBack, noCache) 
 	{
 		path = this.normalizePath(path);
+		if (path.startsWith(this.iconsPath + '/')) 
+		{
+			var filename = path.substring(this.iconsPath.length + 1);
+			if (this.iconFiles.indexOf(filename)>=0)
+			{
+				if ((path in this.blobCache) && (true !== noCache)) 
+				{
+					callBack(null, this.blobCache[path].url);
+					return;
+				}
+				fetch(path.substring(1)).then(function(response) 
+				{
+					if (!response.ok)
+						throw new Error('File not found');
+					return response.blob();
+				}).then(function(blob) 
+				{
+					var url = URL.createObjectURL(blob);
+					if (true !== noCache)
+						self.blobCache[path] = { url: url, blob: blob };
+					callBack(null, url);
+				}).catch(function(err) 
+				{
+					callBack(err);
+				});
+				return;
+			} 
+			else 
+			{
+				callBack(new Error('no data'));
+				return;
+			}
+		}
+
+		
 		if((path in this.blobCache) && (true !== noCache))
 			callBack(null, this.blobCache[path].url);
 		var tx = this.db.transaction(this.storeName, 'readonly');
@@ -82,7 +121,7 @@ function SipletFileSystem(dbName)
 		path = this.normalizePath(path);
 		if(path in this.blobCache)
 		{
-			URL.revokeObjectURL(blobCache[path].url);
+			URL.revokeObjectURL(this.blobCache[path].url);
 			delete this.blobCache[path];
 		}
 	}
@@ -95,8 +134,10 @@ function SipletFileSystem(dbName)
 			this._save(path, data, callBack);
 		else 
 		{
-			this.init(function(err) {
-				if (err) return callBack(err);
+			this.init(function(err) 
+			{
+				if (err) 
+					return callBack(err);
 				self._save(path, data, callBack);
 			});
 		}
@@ -130,8 +171,14 @@ function SipletFileSystem(dbName)
 	this._save = function(path, data, callBack) 
 	{
 		path = this.normalizePath(path);
+		if(path.startsWith(this.iconsPath)) 
+		{
+			callBack(new Error('Cannot modify protected directory'));
+			return;
+		}
 		var parent = this.getParentPath(path);
-		this.normalizeData(path, data, function(err, finalData){
+		this.normalizeData(path, data, function(err, finalData)
+		{
 			if(err)
 			{
 				callBack(err);
@@ -140,54 +187,82 @@ function SipletFileSystem(dbName)
 			var tx = self.db.transaction(self.storeName, 'readwrite');
 			var store = tx.objectStore(self.storeName);
 			var request = store.put({ path: path, data: finalData, parent: parent });
-			request.onsuccess = function() {
+			request.onsuccess = function() 
+			{
 				callBack(null);
 			};
-			request.onerror = function() {
+			request.onerror = function() 
+			{
 				callBack(request.error);
 			};
 		});
 	};
 
-	this.exists = function(path, callBack) {
-		if (!this.enabled) {
+	this.exists = function(path, callBack) 
+	{
+		if (!this.enabled) 
+		{
 			callBack('not supported');
 			return;
 		}
-		if (this.db) {
+		if (this.db)
 			this._exists(path, callBack);
-		} else {
-			const self = this;
-			this.init(function(err) {
-				if (err) return callBack(err);
+		else 
+		{
+			var self = this;
+			this.init(function(err) 
+			{
+				if (err) 
+					return callBack(err);
 				self._exists(path, callBack);
 			});
 		}
 	};
 	
-	this._exists = function(path, callBack) {
+	this._exists = function(path, callBack) 
+	{
 		path = this.normalizePath(path);
-		const tx = this.db.transaction(this.storeName, 'readonly');
-		const store = tx.objectStore(this.storeName);
-		const request = store.get(path);
-		request.onsuccess = function() {
+		if(path === this.iconsPath) 
+		{
+			callBack(null, true);
+			return;
+		}
+		if(path.startsWith(this.iconsPath+'/')) 
+		{
+			var filename = path.substring(this.iconsPath.length+1);
+			if((!filename.includes('/'))&&(this.iconFiles.indexOf(filename)>=0))
+				callBack(null, true);
+			else
+				callBack(null, false);
+			return;
+		}
+		var tx = this.db.transaction(this.storeName, 'readonly');
+		var store = tx.objectStore(this.storeName);
+		var request = store.get(path);
+		request.onsuccess = function() 
+		{
 			callBack(null, !!request.result);
 		};
-		request.onerror = function() {
+		request.onerror = function() 
+		{
 			callBack(request.error);
 		};
 	};
 
-	this.getMediaTree = function(callBack) {
-		if (!this.enabled) {
+	this.getMediaTree = function(callBack) 
+	{
+		if (!this.enabled) 
+		{
 			callBack('not supported');
 			return;
 		}
-		if (this.db) {
+		if (this.db)
 			this._getMediaTree(callBack);
-		} else {
-			const self = this;
-			this.init(function(err) {
+		else 
+		{
+			var self = this;
+			this.init(function(err) 
+			{
 				if (err) return callBack(err);
 				self._getMediaTree(callBack);
 			});
@@ -196,30 +271,38 @@ function SipletFileSystem(dbName)
 
 	this._getMediaTree = function(callBack) 
 	{
-		const tx = this.db.transaction(this.storeName, 'readonly');
-		const store = tx.objectStore(this.storeName);
-		const request = store.getAll();
-		request.onsuccess = function() {
-			const entries = request.result;
-			const tree = buildTree(entries);
+		var tx = this.db.transaction(this.storeName, 'readonly');
+		var store = tx.objectStore(this.storeName);
+		var request = store.getAll();
+		var me = this;
+		request.onsuccess = function() 
+		{
+			var entries = request.result;
+			me.iconFiles.forEach(function(filename) 
+			{
+				var iconPath = me.iconsPath+'/'+filename;
+				entries.push({ path: iconPath, parent: this.iconsPath });
+			});
+			var tree = buildTree(entries);
 			callBack(null, tree);
 		};
-		request.onerror = function() {
+		request.onerror = function() 
+		{
 			callBack(request.error);
 		};
 		function buildTree(entries) 
 		{
-			const root = { name: '', children: [], entries: [], isFolder: true, path: '', parent: null };
+			var root = { name: '', children: [], entries: [], isFolder: true, path: '', parent: null };
 			entries.forEach(function(entry) 
 			{
-				const parts = entry.path.split('/').filter(part => part);
-				let current = root;
-				let currentPath = '';
-				for (let i = 0; i < parts.length - 1; i++) 
+				var parts = entry.path.split('/').filter(part => part);
+				var current = root;
+				var currentPath = '';
+				for (var i = 0; i < parts.length - 1; i++) 
 				{
-					const part = parts[i];
+					var part = parts[i];
 					currentPath = currentPath ? `${currentPath}/${part}` : part;
-					let folder = current.children.find(node => node.name === part && node.isFolder);
+					var folder = current.children.find(node => node.name === part && node.isFolder);
 					if (!folder) 
 					{
 						folder = { name: part, path: currentPath, isFolder: true, children: [], entries: [], parent: current};
@@ -227,7 +310,7 @@ function SipletFileSystem(dbName)
 					}
 					current = folder;
 				}
-				const fileName = parts[parts.length - 1];
+				var fileName = parts[parts.length - 1];
 				current.entries.push({
 					name: fileName,
 					id: entry.id,
@@ -267,30 +350,42 @@ function SipletFileSystem(dbName)
 	};
 
 	this.delete = function(path, callBack) {
-		if (!this.enabled) {
+		if (!this.enabled) 
+		{
 			callBack('not supported');
 			return;
 		}
-		if (this.db) {
+		if (this.db) 
 			this._delete(path, callBack);
-		} else {
-			const self = this;
-			this.init(function(err) {
-				if (err) return callBack(err);
+		else 
+		{
+			var self = this;
+			this.init(function(err) 
+			{
+				if (err) 
+					return callBack(err);
 				self._delete(path, callBack);
 			});
 		}
 	};
 	
-	this._delete = function(path, callBack) {
+	this._delete = function(path, callBack) 
+	{
 		path = this.normalizePath(path);
-		const tx = this.db.transaction(this.storeName, 'readwrite');
-		const store = tx.objectStore(this.storeName);
-		const request = store.delete(path);
-		request.onsuccess = function() {
+		if (path.startsWith(this.iconsPath)) 
+		{
+			callBack(new Error('Cannot delete from protected directory'));
+			return;
+		}
+		var tx = this.db.transaction(this.storeName, 'readwrite');
+		var store = tx.objectStore(this.storeName);
+		var request = store.delete(path);
+		request.onsuccess = function() 
+		{
 			callBack(null);
 		};
-		request.onerror = function() {
+		request.onerror = function() 
+		{
 			callBack(request.error);
 		};
 	};
