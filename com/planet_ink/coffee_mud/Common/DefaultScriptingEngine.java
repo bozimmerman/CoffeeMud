99fc576894f70b4606173eada00883bbacaa675b
bozimmerman <bo@zimmers.net>
2025-10-09 04:39:10 +0000

    this bug should not even happen.
    git-svn-id: svn://192.168.1.10/public/CoffeeMud@24583 0d6f1817-ed0e-0410-87c9-987e46238f29

--- com/planet_ink/coffee_mud/Common/DefaultScriptingEngine.java
+++ com/planet_ink/coffee_mud/Common/DefaultScriptingEngine.java
@@ -9851,7 +9851,7 @@ public class DefaultScriptingEngine implements ScriptingEngine
 						final long tm=System.currentTimeMillis()+(10 * 1000);
 						for(int forLoop=fromInt;forLoop!=toInt;forLoop+=increment)
 						{
-							if ((inList != null) && (forLoop < inList.size()))
+							if ((inList != null) && (inList.size()>0) && (forLoop < inList.size()))
 								ctx.tmp[whichVar] = inList.get(forLoop);
 							else
 								ctx.tmp[whichVar]=""+forLoop;
@@ -9865,7 +9865,7 @@ public class DefaultScriptingEngine implements ScriptingEngine
 								break;
 							}
 						}
-						if ((inList != null) && (toInt < inList.size()))
+						if ((inList != null) && (inList.size()>0) && (toInt < inList.size()))
 							ctx.tmp[whichVar] = inList.get(toInt);
 						else
 							ctx.tmp[whichVar]=""+toInt;
