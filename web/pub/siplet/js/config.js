7a284668cea76e0215bd7137cf45ca8a2f308013
bozimmerman <bo@zimmers.net>
2025-10-11 06:10:53 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@24593 0d6f1817-ed0e-0410-87c9-987e46238f29

--- web/pub/siplet/js/config.js
+++ web/pub/siplet/js/config.js
@@ -2,7 +2,7 @@ window.isElectron = typeof window.process === 'object' && window.process.version
 var Siplet =
 {
 	VERSION_MAJOR: '3.2',
-	VERSION_MINOR: '5',
+	VERSION_MINOR: '6',
 	COFFEE_MUD: false,
 	NAME: window.isElectron?'Sip MUD Client':'Siplet',
 	R: /^win\.[\w]+(\.[\w]+)*$/
