6a5779472cadae49d149000d44dcb259d658c531
bozimmerman <bo@zimmers.net>
2025-12-07 02:46:41 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@24972 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/core/interfaces/AutoBundler.java
@@ -0,0 +1,33 @@
+package com.planet_ink.coffee_mud.core.interfaces;
+
+/*
+Copyright 2025-2025 Bo Zimmerman
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+	   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+/**
+* Interface for things which automatically bundle with other like
+* things, to save references.
+*
+* @author Bo Zimmerman
+*
+*/
+public interface AutoBundler extends Contingent
+{
+	/**
+	 * Cause this item to automatically join like items.
+	 * This might cause it to be destroyed.
+	 * @return
+	 */
+	public boolean autoBundle();
+}
