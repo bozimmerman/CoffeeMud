46b88292c6bc28b047b0ebc4a11e91a00ab6b470
bozimmerman <bo@zimmers.net>
2026-04-07 00:13:54 +0000

    git-svn-id: svn://192.168.1.10/public/CoffeeMud@25330 0d6f1817-ed0e-0410-87c9-987e46238f29

---
+++ com/planet_ink/coffee_mud/Items/Armor/GenHat.java
@@ -0,0 +1,58 @@
+package com.planet_ink.coffee_mud.Items.Armor;
+import com.planet_ink.coffee_mud.core.interfaces.*;
+import com.planet_ink.coffee_mud.core.*;
+import com.planet_ink.coffee_mud.core.collections.*;
+import com.planet_ink.coffee_mud.Abilities.interfaces.*;
+import com.planet_ink.coffee_mud.Areas.interfaces.*;
+import com.planet_ink.coffee_mud.Behaviors.interfaces.*;
+import com.planet_ink.coffee_mud.CharClasses.interfaces.*;
+import com.planet_ink.coffee_mud.Commands.interfaces.*;
+import com.planet_ink.coffee_mud.Common.interfaces.*;
+import com.planet_ink.coffee_mud.Exits.interfaces.*;
+import com.planet_ink.coffee_mud.Items.interfaces.*;
+import com.planet_ink.coffee_mud.Libraries.interfaces.*;
+import com.planet_ink.coffee_mud.Locales.interfaces.*;
+import com.planet_ink.coffee_mud.MOBS.interfaces.*;
+import com.planet_ink.coffee_mud.Races.interfaces.*;
+
+/*
+   Copyright 2026-2026 Bo Zimmerman
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+	   http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
+public class GenHat extends GenArmor
+{
+	@Override
+	public String ID()
+	{
+		return "GenHat";
+	}
+
+	public GenHat()
+	{
+		super();
+
+		setName("a hat");
+		setDisplayText("a simple hat lies here.");
+		setDescription("Pretty nondescript; it's just a hat.");
+		properWornBitmap=Wearable.WORN_HEAD;
+		wornLogicalAnd=false;
+		basePhyStats().setArmor(0);
+		basePhyStats().setWeight(1);
+		setCapacity(20);
+		basePhyStats().setAbility(0);
+		baseGoldValue=1;
+		recoverPhyStats();
+		material=RawMaterial.RESOURCE_COTTON;
+	}
+}
